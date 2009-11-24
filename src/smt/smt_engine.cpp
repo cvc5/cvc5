86ce2119c466cdf0240acb97d4fc65cddede19fe
Morgan Deters <mdeters@gmail.com>
2009-11-24 23:04:45 +0000

    oops, missed a file

---
+++ src/smt/smt_engine.cpp
@@ -0,0 +1,17 @@
+/*********************                                           -*- C++ -*-  */
+/** smt_engine.cpp
+ ** This file is part of the CVC4 prototype.
+ ** Copyright (c) 2009 The Analysis of Computer Systems Group (ACSys)
+ ** Courant Institute of Mathematical Sciences
+ ** New York University
+ ** See the file COPYING in the top-level source directory for licensing
+ ** information.
+ **
+ **/
+
+#include "smt/smt_engine.h"
+
+namespace CVC4 {
+
+
+}/* CVC4 namespace */
