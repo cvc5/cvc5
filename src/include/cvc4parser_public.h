f208251e33814430decb89c2c9001b5a675402f9
Morgan Deters <mdeters@gmail.com>
2010-04-01 06:00:17 +0000

    minor forgotten things in last commit

---
+++ src/include/cvc4parser_public.h
@@ -0,0 +1,22 @@
+/*********************                                                        */
+/** cvc4parser_public.h
+ ** Original author: mdeters
+ ** Major contributors: none
+ ** Minor contributors (to current version): none
+ ** This file is part of the CVC4 prototype.
+ ** Copyright (c) 2009, 2010  The Analysis of Computer Systems Group (ACSys)
+ ** Courant Institute of Mathematical Sciences
+ ** New York University
+ ** See the file COPYING in the top-level source directory for licensing
+ ** information.
+ **
+ ** Macros that should be defined everywhere during the building of
+ ** the libraries and driver binary, and also exported to the user.
+ **/
+
+#ifndef __CVC4PARSER_PUBLIC_H
+#define __CVC4PARSER_PUBLIC_H
+
+#include "cvc4_public.h"
+
+#endif /* __CVC4PARSER_PUBLIC_H */
