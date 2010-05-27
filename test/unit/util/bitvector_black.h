671a3d6d5ae8a89a1bb846a78f5ec9c064edc655
Morgan Deters <mdeters@gmail.com>
2010-05-27 21:18:56 +0000

    fix compiler comparison-signedness warnings

--- test/unit/util/bitvector_black.h
+++ test/unit/util/bitvector_black.h
@@ -28,25 +28,25 @@ public:
 
   void testStringConstructor() {
     BitVector b1("0101",2);
-    TS_ASSERT_EQUALS( 4, b1.getSize() );
+    TS_ASSERT_EQUALS( 4u, b1.getSize() );
     TS_ASSERT_EQUALS( "0101", b1.toString() );
     TS_ASSERT_EQUALS( "5", b1.toString(10) );
     TS_ASSERT_EQUALS( "5", b1.toString(16) );
 
     BitVector b2("000001", 2);
-    TS_ASSERT_EQUALS( 6, b2.getSize() );
+    TS_ASSERT_EQUALS( 6u, b2.getSize() );
     TS_ASSERT_EQUALS( "000001", b2.toString() );
     TS_ASSERT_EQUALS( "1", b2.toString(10) );
     TS_ASSERT_EQUALS( "1", b2.toString(16) );
 
     BitVector b3("7f", 16);
-    TS_ASSERT_EQUALS( 8, b3.getSize() );
+    TS_ASSERT_EQUALS( 8u, b3.getSize() );
     TS_ASSERT_EQUALS( "01111111", b3.toString() );
     TS_ASSERT_EQUALS( "127", b3.toString(10) );
     TS_ASSERT_EQUALS( "7f", b3.toString(16) );
 
     BitVector b4("01a", 16);
-    TS_ASSERT_EQUALS( 12, b4.getSize() );
+    TS_ASSERT_EQUALS( 12u, b4.getSize() );
     TS_ASSERT_EQUALS( "000000011010", b4.toString() );
     TS_ASSERT_EQUALS( "26", b4.toString(10) );
     TS_ASSERT_EQUALS( "1a", b4.toString(16) );
