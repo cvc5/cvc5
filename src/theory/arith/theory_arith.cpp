6624b97e0460dcf39eb24699e10f96e781fb11a9
Kshitij Bansal <kshitij@cs.nyu.edu>
2012-04-18 08:41:13 +0000

    cout -> warning. Happening in portfolio rest of the search go through, but still should be investigated

--- src/theory/arith/theory_arith.cpp
+++ src/theory/arith/theory_arith.cpp
@@ -997,8 +997,8 @@ Node TheoryArith::assertionCases(TNode assertion){
       if(Debug.isOn("whytheoryenginewhy")){
         debugPrintFacts();
       }
-      cout << "Theory engine is sending me both a literal and its negation?"
-           << "BOOOOOOOOOOOOOOOOOOOOOO!!!!"<< endl;
+      Warning() << "arith: Theory engine is sending me both a literal and its negation?"
+                << "BOOOOOOOOOOOOOOOOOOOOOO!!!!"<< endl;
     }
     Debug("arith::eq") << constraint << endl;
     Debug("arith::eq") << negation << endl;
