8166b6cef258b198d0ffc97d125da3c85acf9708
Tim King <taking@cs.nyu.edu>
2012-07-06 17:47:49 +0000

    Adding std namespace to a couple of make_pair instances.

--- src/theory/inst_match_impl.h
+++ src/theory/inst_match_impl.h
@@ -43,7 +43,7 @@ void InstMatchTrie2<modEq>::addSubTree( Tree * root, mapIter current, mapIter en
 
   Assert(root->e.find(current->second) == root->e.end());
   Tree * root2 = new Tree(currLevel);
-  root->e.insert(make_pair(current->second, root2));
+  root->e.insert(std::make_pair(current->second, root2));
   addSubTree(root2, ++current, end, currLevel );
 }
 
@@ -111,7 +111,7 @@ bool InstMatchTrie2<modEq>::addInstMatch( InstMatch& m ) {
    addSubTree( e, diverge, end, currLevel );
    if(e->level != currLevel)
      //If same level that e, will be removed at the same time than e
-     d_mods.push_back(make_pair(e,diverge->second));
+     d_mods.push_back(std::make_pair(e,diverge->second));
    return true;
  }else{
    return false;
