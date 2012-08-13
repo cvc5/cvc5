9684cf346efe1ae33fd0b560b1cae5d11c5fc4b4
Morgan Deters <mdeters@gmail.com>
2012-08-13 20:56:15 +0000

    fix integer parsing error.. thanks dejan for the report.  this indicates that we have a problem with our Integer class though; it appears to behave differently for GMP and CLN

--- src/options/options_template.cpp
+++ src/options/options_template.cpp
@@ -75,7 +75,7 @@ template <class T>
 struct OptionHandler<T, true, true> {
   static T handle(std::string option, std::string optarg) {
     try {
-      Integer i(optarg, 0);
+      Integer i(optarg, 10);
 
       if(! std::numeric_limits<T>::is_signed && i < 0) {
         // unsigned type but user gave negative argument
