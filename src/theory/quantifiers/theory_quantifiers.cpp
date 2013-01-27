5f58ecb6638f0e0fe63b67f1790b997684655bdd
Morgan Deters <mdeters@cs.nyu.edu>
2013-01-26 21:09:55 -0500

    another fix for quantifier models (related to bug 486)

--- src/theory/quantifiers/theory_quantifiers.cpp
+++ src/theory/quantifiers/theory_quantifiers.cpp
@@ -94,7 +94,11 @@ Node TheoryQuantifiers::getValue(TNode n) {
 
 void TheoryQuantifiers::collectModelInfo( TheoryModel* m, bool fullModel ){
   for(assertions_iterator i = facts_begin(); i != facts_end(); ++i) {
-    m->assertPredicate(*i, true);
+    if((*i).assertion.getKind() == kind::NOT) {
+      m->assertPredicate((*i).assertion[0], false);
+    } else {
+      m->assertPredicate(*i, true);
+    }
   }
 }
 
