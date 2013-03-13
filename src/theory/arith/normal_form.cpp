2a2c5102e10a8b3f1091bc50916fda5e766b5d4a
Morgan Deters <mdeters@cs.nyu.edu>
2013-03-13 18:27:11 -0400

    Better error in case of nonlinear assertions while in linear logic

--- src/theory/arith/normal_form.cpp
+++ src/theory/arith/normal_form.cpp
@@ -28,8 +28,8 @@ namespace arith {
 bool Variable::isDivMember(Node n){
   switch(n.getKind()){
   case kind::DIVISION:
-    //case kind::INTS_DIVISION:
-    //case kind::INTS_MODULUS:
+  case kind::INTS_DIVISION:
+  case kind::INTS_MODULUS:
   case kind::DIVISION_TOTAL:
   case kind::INTS_DIVISION_TOTAL:
   case kind::INTS_MODULUS_TOTAL:
