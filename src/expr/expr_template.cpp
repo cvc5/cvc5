7587463f338164d9adbebd117ecf63246e2d32e8
Morgan Deters <mdeters@cs.nyu.edu>
2013-11-07 18:03:42 -0500

    Minor fixups to last commit

--- src/expr/expr_template.cpp
+++ src/expr/expr_template.cpp
@@ -31,7 +31,7 @@ ${includes}
 // compiler directs the user to the template file instead of the
 // generated one.  We don't want the user to modify the generated one,
 // since it'll get overwritten on a later build.
-#line 34 "${template}"
+#line 35 "${template}"
 
 using namespace CVC4::kind;
 using namespace std;
