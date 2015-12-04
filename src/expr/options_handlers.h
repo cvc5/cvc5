079eb73aa49c1d5a4b9e7e6f3b500e4940408309
Tim King <taking@google.com>
2015-12-03 23:46:48 -0500

    Reverting a previous change to the options_handlers.h. Using inline defintions again to better handle a circular dependency.

--- src/expr/options_handlers.h
+++ src/expr/options_handlers.h
@@ -19,19 +19,49 @@
 #ifndef __CVC4__EXPR__OPTIONS_HANDLERS_H
 #define __CVC4__EXPR__OPTIONS_HANDLERS_H
 
-#include <string>
+#include "util/output.h"
+#include "util/dump.h"
 
 namespace CVC4 {
-class SmtEngine;
-
 namespace expr {
 
+inline void setDefaultExprDepth(std::string option, int depth, SmtEngine* smt) {
+  if(depth < -1) {
+    throw OptionException("--default-expr-depth requires a positive argument, or -1.");
+  }
 
-void setDefaultExprDepth(std::string option, int depth, SmtEngine* smt);
+  Debug.getStream() << Expr::setdepth(depth);
+  Trace.getStream() << Expr::setdepth(depth);
+  Notice.getStream() << Expr::setdepth(depth);
+  Chat.getStream() << Expr::setdepth(depth);
+  Message.getStream() << Expr::setdepth(depth);
+  Warning.getStream() << Expr::setdepth(depth);
+  // intentionally exclude Dump stream from this list
+}
 
-void setDefaultDagThresh(std::string option, int dag, SmtEngine* smt);
+inline void setDefaultDagThresh(std::string option, int dag, SmtEngine* smt) {
+  if(dag < 0) {
+    throw OptionException("--default-dag-thresh requires a nonnegative argument.");
+  }
 
-void setPrintExprTypes(std::string option, SmtEngine* smt);
+  Debug.getStream() << Expr::dag(dag);
+  Trace.getStream() << Expr::dag(dag);
+  Notice.getStream() << Expr::dag(dag);
+  Chat.getStream() << Expr::dag(dag);
+  Message.getStream() << Expr::dag(dag);
+  Warning.getStream() << Expr::dag(dag);
+  Dump.getStream() << Expr::dag(dag);
+}
+
+inline void setPrintExprTypes(std::string option, SmtEngine* smt) {
+  Debug.getStream() << Expr::printtypes(true);
+  Trace.getStream() << Expr::printtypes(true);
+  Notice.getStream() << Expr::printtypes(true);
+  Chat.getStream() << Expr::printtypes(true);
+  Message.getStream() << Expr::printtypes(true);
+  Warning.getStream() << Expr::printtypes(true);
+  // intentionally exclude Dump stream from this list
+}
 
 }/* CVC4::expr namespace */
 }/* CVC4 namespace */
