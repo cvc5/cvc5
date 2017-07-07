55d4f2a0bb62fb12bc5b2590a5d157157a2715b6
Mathias Preiner <mathias.preiner@gmail.com>
2017-07-07 16:38:42 -0700

    Update copyright year and refer to authors URL.

--- src/base/configuration_private.h
+++ src/base/configuration_private.h
@@ -149,8 +149,9 @@ This is CVC4 version " CVC4_RELEASE_STRING ) + \
     )) + "\n\
 compiled with " + ::CVC4::Configuration::getCompiler() + "\n\
 on " + ::CVC4::Configuration::getCompiledDateTime() + "\n\n\
-Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014\n\
-  New York University and The University of Iowa\n\n" + \
+Copyright (c) 2009-2017\n\
+by the authors and their institutional affiliations listed at \n\
+http:\/\/cvc4.cs.stanford.edu/authors\n\n" + \
     ( IS_GPL_BUILD ? "\
 This build of CVC4 uses GPLed libraries, and is thus covered by the GNU\n\
 General Public License (GPL) version 3.  Versions of CVC4 are available\n\
