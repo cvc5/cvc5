11c69354a88a28ffef94365f32f516a83edf54ad
Aina Niemetz <aina.niemetz@gmail.com>
2017-08-11 21:28:26 -0700

    Configure LFSC Checker to use cmake.

---
+++ proofs/lfsc_checker/cmake/FindGMP.cmake
@@ -0,0 +1,12 @@
+# Try to find the GMP librairies
+# GMP_FOUND - system has GMP lib
+# GMP_INCLUDE_DIR - the GMP include directory
+# GMP_LIBRARIES - Libraries needed to use GMP
+
+find_path(GMP_INCLUDE_DIR NAMES gmp.h)
+find_library(GMP_LIBRARIES NAMES gmp libgmp)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES)
+
+mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES)
