9266e048be8f030e77395903ba707fa879cf1b84
Aina Niemetz <aina.niemetz@gmail.com>
2017-09-07 14:00:20 -0700

    Added testcase.

--- test/unit/theory/theory_bv_bvgauss_white.h
+++ test/unit/theory/theory_bv_bvgauss_white.h
@@ -98,6 +98,21 @@ public:
     std::vector< Integer > rhs;
     std::vector< std::vector< Integer >> lhs;
 
+    //   lhs     rhs        lhs    rhs  modulo 17
+    //  --^---    ^        --^--    ^
+    //  1 0 0    4   -->   1 0 0    4
+    //  0 1 0   15         0 1 0   15
+    //  0 0 1    3         0 0 1    3
+    rhs = { Integer(4), Integer(15), Integer(3) };
+    lhs =
+        {
+          { Integer(1), Integer(0), Integer(0) },
+          { Integer(0), Integer(1), Integer(0) },
+          { Integer(0), Integer(0), Integer(1) }
+        };
+    std::cout << "matrix 0, modulo 17" << std::endl;
+    testGaussElimX (Integer(17), rhs, lhs, true);
+    
     //   lhs     rhs        lhs    rhs  modulo { 9,17,59 }
     //  --^---    ^        --^--    ^
     //  2 4  6   18   -->  1 0 0    4
@@ -110,11 +125,11 @@ public:
           { Integer(4), Integer(5), Integer(6) },
           { Integer(3), Integer(1), Integer(-2) }
         };
-    std::cout << "matrix 0, modulo 9" << std::endl;
+    std::cout << "matrix 1, modulo 9" << std::endl;
     testGaussElimX (Integer(9), rhs, lhs, false);
-    std::cout << "matrix 0, modulo 17" << std::endl;
+    std::cout << "matrix 1, modulo 17" << std::endl;
     testGaussElimX (Integer(17), rhs, lhs, true);
-    std::cout << "matrix 0, modulo 59" << std::endl;
+    std::cout << "matrix 1, modulo 59" << std::endl;
     testGaussElimX (Integer(59), rhs, lhs, true);
 
     //   lhs   rhs        lhs   rhs  modulo { 1,2,3,4,5,6,7,8,9,10,11 }
@@ -129,27 +144,27 @@ public:
           { Integer(2), Integer(3), Integer(5) },
           { Integer(4), Integer(0), Integer(5) }
         };
-    std::cout << "matrix 1, modulo 1" << std::endl;
+    std::cout << "matrix 2, modulo 1" << std::endl;
     testGaussElimX (Integer(1), rhs, lhs, true);
-    std::cout << "matrix 1, modulo 2" << std::endl;
+    std::cout << "matrix 2, modulo 2" << std::endl;
     testGaussElimX (Integer(2), rhs, lhs, true);
-    std::cout << "matrix 1, modulo 3" << std::endl;
+    std::cout << "matrix 2, modulo 3" << std::endl;
     testGaussElimX (Integer(3), rhs, lhs, true);
-    std::cout << "matrix 1, modulo 4" << std::endl;
+    std::cout << "matrix 2, modulo 4" << std::endl;
     testGaussElimX (Integer(4), rhs, lhs, false);
-    std::cout << "matrix 1, modulo 5" << std::endl;
+    std::cout << "matrix 2, modulo 5" << std::endl;
     testGaussElimX (Integer(5), rhs, lhs, true);
-    std::cout << "matrix 1, modulo 6" << std::endl;
+    std::cout << "matrix 2, modulo 6" << std::endl;
     testGaussElimX (Integer(6), rhs, lhs, false);
-    std::cout << "matrix 1, modulo 7" << std::endl;
+    std::cout << "matrix 2, modulo 7" << std::endl;
     testGaussElimX (Integer(7), rhs, lhs, true);
-    std::cout << "matrix 1, modulo 8" << std::endl;
+    std::cout << "matrix 2, modulo 8" << std::endl;
     testGaussElimX (Integer(8), rhs, lhs, false);
-    std::cout << "matrix 1, modulo 9" << std::endl;
+    std::cout << "matrix 2, modulo 9" << std::endl;
     testGaussElimX (Integer(9), rhs, lhs, true);
-    std::cout << "matrix 1, modulo 10" << std::endl;
+    std::cout << "matrix 2, modulo 10" << std::endl;
     testGaussElimX (Integer(10), rhs, lhs, false);
-    std::cout << "matrix 1, modulo 11" << std::endl;
+    std::cout << "matrix 2, modulo 11" << std::endl;
     testGaussElimX (Integer(11), rhs, lhs, true);
 
     //   lhs   rhs        lhs   rhs   modulo 11
@@ -164,7 +179,7 @@ public:
           { Integer(1), Integer(1), Integer(1) },
           { Integer(3), Integer(2), Integer(5) }
         };
-    std::cout << "matrix 2, modulo 11" << std::endl;
+    std::cout << "matrix 3, modulo 11" << std::endl;
     testGaussElimX (Integer(11), rhs, lhs, true);
 
     //   lhs   rhs        lhs   rhs   modulo 11
@@ -179,7 +194,7 @@ public:
           { Integer(0), Integer(4), Integer(5) },
           { Integer(3), Integer(2), Integer(5) }
         };
-    std::cout << "matrix 3, modulo 11" << std::endl;
+    std::cout << "matrix 4, modulo 11" << std::endl;
     testGaussElimX (Integer(11), rhs, lhs, true);
 
     //   lhs   rhs        lhs   rhs   modulo 11
@@ -194,7 +209,7 @@ public:
           { Integer(3), Integer(2), Integer(5) },
           { Integer(0), Integer(4), Integer(5) }
         };
-    std::cout << "matrix 4, modulo 11" << std::endl;
+    std::cout << "matrix 5, modulo 11" << std::endl;
     testGaussElimX (Integer(11), rhs, lhs, true);
 
     //   lhs   rhs        lhs   rhs   modulo 11
@@ -209,7 +224,7 @@ public:
           { Integer(1), Integer(1), Integer(1) },
           { Integer(3), Integer(2), Integer(5) }
         };
-    std::cout << "matrix 5, modulo 11" << std::endl;
+    std::cout << "matrix 6, modulo 11" << std::endl;
     testGaussElimX (Integer(11), rhs, lhs, true);
 
     //   lhs   rhs        lhs   rhs   modulo 11
@@ -224,7 +239,7 @@ public:
           { Integer(0), Integer(0), Integer(5) },
           { Integer(3), Integer(2), Integer(5) }
         };
-    std::cout << "matrix 6, modulo 11" << std::endl;
+    std::cout << "matrix 7, modulo 11" << std::endl;
     testGaussElimX (Integer(11), rhs, lhs, true);
 
     //   lhs   rhs        lhs   rhs   modulo 11
@@ -239,7 +254,7 @@ public:
           { Integer(3), Integer(2), Integer(5) },
           { Integer(0), Integer(0), Integer(5) }
         };
-    std::cout << "matrix 7, modulo 11" << std::endl;
+    std::cout << "matrix 8, modulo 11" << std::endl;
     testGaussElimX (Integer(11), rhs, lhs, true);
 
     //      lhs       rhs        lhs     rhs   modulo 11
@@ -256,7 +271,7 @@ public:
           { Integer(1), Integer(-1), Integer(-1), Integer(-2) },
           { Integer(2), Integer(-1), Integer(2),  Integer(-1) }
         };
-    std::cout << "matrix 8, modulo 11" << std::endl;
+    std::cout << "matrix 9, modulo 11" << std::endl;
     testGaussElimX (Integer(11), rhs, lhs, true);
   }
 
@@ -280,7 +295,7 @@ public:
           { Integer(3), Integer(2), Integer(5) },
           { Integer(0), Integer(4), Integer(5) }
         };
-    std::cout << "matrix 4, modulo 0" << std::endl;
+    std::cout << "matrix 5, modulo 0" << std::endl;
     testGaussElimT<AssertionException> (Integer(0), rhs, lhs);
   }
 };
