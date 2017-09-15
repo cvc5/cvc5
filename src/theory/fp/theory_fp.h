9dd1ce13031bd63d16bb55175190805e691dc856
martin <martin@raphael>
2017-09-15 18:10:07 +0100

    Implement the theory solver using a dummy version of the converter.
    This includes equality reasoning and so should be able to solver some basic problems.

--- src/theory/fp/theory_fp.h
+++ src/theory/fp/theory_fp.h
@@ -21,12 +21,85 @@
 #define __CVC4__THEORY__FP__THEORY_FP_H
 
 #include "theory/theory.h"
+#include "theory/uf/equality_engine.h"
+
+#include "theory/fp/fp_converter.h"
+
+#include "context/cdo.h"
 
 namespace CVC4 {
 namespace theory {
 namespace fp {
 
 class TheoryFp : public Theory {
+protected :
+  /** Equality engine */
+  class NotifyClass : public eq::EqualityEngineNotify {
+    protected :
+      TheoryFp& theorySolver;
+
+    public:
+    NotifyClass(TheoryFp& solver): theorySolver(solver) {}
+    bool eqNotifyTriggerEquality(TNode equality, bool value);
+    bool eqNotifyTriggerPredicate(TNode predicate, bool value);
+    bool eqNotifyTriggerTermEquality(TheoryId tag, TNode t1, TNode t2, bool value);
+    void eqNotifyConstantTermMerge(TNode t1, TNode t2);
+    void eqNotifyNewClass(TNode t) { }
+    void eqNotifyPreMerge(TNode t1, TNode t2) { }
+    void eqNotifyPostMerge(TNode t1, TNode t2) { }
+    void eqNotifyDisequal(TNode t1, TNode t2, TNode reason) { }
+  };
+  friend NotifyClass;
+
+  NotifyClass notification;
+  eq::EqualityEngine equalityEngine;
+
+
+  /** General utility **/
+  void registerTerm(TNode node);
+  bool isRegistered(TNode node);
+
+  context::CDHashSet<Node, NodeHashFunction> registeredTerms;
+
+
+  /** Bit-blasting conversion */
+  fpConverter conv;
+  bool expansionRequested;
+
+  void convertAndEquateTerm(TNode node);
+
+
+  /** Interaction with the rest of the solver **/
+  void handleLemma(Node node);
+  bool handlePropagation(TNode node);
+  void handleConflict(TNode node);
+
+  context::CDO<bool> conflict;
+  context::CDO<Node> conflictNode;
+
+  /** Uninterpretted functions for partially defined functions. **/
+  typedef context::CDHashMap<TypeNode, Node, TypeNodeHashFunction> comparisonUFMap;
+
+  comparisonUFMap minMap;
+  comparisonUFMap maxMap;
+
+  Node minUF(Node);
+  Node maxUF(Node);
+
+  typedef context::CDHashMap<std::pair<TypeNode, TypeNode>, Node, PairTypeNodeHashFunction> conversionUFMap;
+
+  conversionUFMap toUBVMap;
+  conversionUFMap toSBVMap;
+
+  Node toUBVUF(Node);
+  Node toSBVUF(Node);
+
+
+  comparisonUFMap toRealMap;
+
+  Node toRealUF(Node);
+
+
 public:
 
   /** Constructs a new instance of TheoryFp w.r.t. the provided contexts. */
@@ -36,13 +109,23 @@ public:
            Valuation valuation,
            const LogicInfo& logicInfo);
 
-  Node expandDefinition(LogicRequest &, Node node);
+  Node expandDefinition(LogicRequest &lr, Node node);
+
+  void preRegisterTerm(TNode node);
+  void addSharedTerm(TNode node);
 
   void check(Effort);
+
+  Node getModelValue(TNode var);
+  void collectModelInfo(TheoryModel *m);
 
   std::string identify() const {
     return "THEORY_FP";
   }
+
+  void setMasterEqualityEngine(eq::EqualityEngine* eq);
+
+  Node explain(TNode n);
 
 };/* class TheoryFp */
 
