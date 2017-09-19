14db2204580eac45d13d5ce55b034f55b0338848
Aina Niemetz <aina.niemetz@gmail.com>
2017-09-19 15:20:33 -0700

    Added gauss elim for urem. TODO more unit tests.

--- src/theory/bv/bvgauss.h
+++ src/theory/bv/bvgauss.h
@@ -6,20 +6,30 @@
 #include "expr/node.h"
 #include "util/bitvector.h"
 #include <vector>
+#include <unordered_map>
 
 namespace CVC4 {
 namespace theory {
 namespace bv {
 
-class BVGaussElim {
-  //static void gaussElimRewrite (std::vector<Node> & assertionsToPreprocess);
+class BVGaussElim
+{
+  static void gaussElimRewrite (std::vector<Node> & assertionsToPreprocess);
+
   private:
+
   enum class Result { UNIQUE, PARTIAL, NONE };
-  static Result gaussElim (Integer prime,
-                           std::vector< Integer > & rhs,
-                           std::vector< std::vector< Integer >> & lhs,
-                           std::vector< Integer > & resrhs,
-                           std::vector< std::vector< Integer >> & reslhs);
+
+  static Result gaussElimRewriteForUrem (
+      std::vector< TNode > & equations,
+      std::unordered_map< TNode, TNode, TNodeHashFunction > & res);
+
+  static Result gaussElim (
+      Integer prime,
+      std::vector< Integer > & rhs,
+      std::vector< std::vector< Integer >> & lhs,
+      std::vector< Integer > & resrhs,
+      std::vector< std::vector< Integer >> & reslhs);
 };
 
 }/* CVC4::theory::bv namespace */
