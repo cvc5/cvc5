07b4ff41592964d1de19041126602b86e316ca0e
Mathias Preiner <mathias.preiner@gmail.com>
2017-09-29 13:59:38 -0700

    Move BvInverter class to separate file.

--- src/theory/quantifiers/ceg_t_instantiator.h
+++ src/theory/quantifiers/ceg_t_instantiator.h
@@ -18,6 +18,7 @@
 #ifndef __CVC4__CEG_T_INSTANTIATOR_H
 #define __CVC4__CEG_T_INSTANTIATOR_H
 
+#include "theory/quantifiers/bv_inverter.h"
 #include "theory/quantifiers/ceg_instantiator.h"
 
 #include <unordered_set>
@@ -79,103 +80,6 @@ public:
   std::string identify() const { return "Epr"; }
 };
 
-
-// virtual class for model queries
-class BvInverterModelQuery {
-public:
-  BvInverterModelQuery(){}
-  ~BvInverterModelQuery(){}
-  virtual Node getModelValue( Node n ) = 0;
-};
-
-// class for storing information about the solved status
-class BvInverterStatus {
-public:
-  BvInverterStatus() : d_status(0) {}
-  ~BvInverterStatus(){}
-  int d_status;
-  // TODO : may not need this (conditions are now appear explicitly in solved forms)
-  // side conditions 
-  std::vector< Node > d_conds;
-};
-
-// inverter class
-// TODO : move to theory/bv/ if generally useful?
-class BvInverter {
-private:
-  /** dummy variables for each type */
-  std::map< TypeNode, Node > d_solve_var;
-  /** stores the inversion skolems, for each condition */
-  std::unordered_map< Node, Node, NodeHashFunction > d_inversion_skolem_cache;
-  /** helper function for getPathToPv */
-  Node getPathToPv( Node lit, Node pv, Node sv, std::vector< unsigned >& path, std::unordered_set< TNode, TNodeHashFunction >& visited );
-  /** helper function for eliminateSkolemFunctions */
-  Node eliminateSkolemFunctions( TNode n, std::vector< Node >& side_conditions, std::unordered_map< TNode, Node, TNodeHashFunction >& visited ); 
-  // is operator k invertible?
-  bool isInvertible( Kind k );
-  /** get inversion skolem for condition
-  * precondition : exists x. cond( x ) is a tautology in BV,
-  *               where x is getSolveVariable( tn ).
-  * returns fresh skolem k of type tn, where we may assume cond( k ).
-  */
-  Node getInversionSkolemFor( Node cond, TypeNode tn );
-  /** get inversion skolem function for type tn.
-  *   This is a function of type ( Bool -> tn ) that is used for explicitly storing side conditions
-  *   inside terms. For all ( cond, tn ), if :
-  *
-  *   f = getInversionSkolemFunctionFor( tn )
-  *   k = getInversionSkolemFor( cond, tn )
-  *   then:
-  *   f( cond ) is a placeholder for k.
-  *
-  * In the call eliminateSkolemFunctions, we replace all f( cond ) with k and add cond{ x -> k } to side_conditions.
-  * The advantage is that f( cond ) explicitly contains the side condition so it automatically
-  * updates with substitutions.
-  */
-  Node getInversionSkolemFunctionFor( TypeNode tn );
-public:
-  BvInverter(){}
-  ~BvInverter(){}
-  /** get dummy fresh variable of type tn, used as argument for sv */
-  Node getSolveVariable( TypeNode tn );
-  /** get inversion node, if :
-  *
-  *   f = getInversionSkolemFunctionFor( tn )
-  *
-  * This returns f( cond ).
-  */
-  Node getInversionNode( Node cond, TypeNode tn );
-  /** Get path to pv in lit, replace that occurrence by sv and all others by pvs.
-  * If return value R is non-null, then :
-  *   lit.path = pv
-  *   R.path = sv
-  *   R.path' = pvs for all lit.path' = pv, where path' != path
-  */
-  Node getPathToPv( Node lit, Node pv, Node sv, Node pvs, std::vector< unsigned >& path );
-  /** eliminate skolem functions in node n
-  *
-  * This eliminates all Skolem functions from Node n and replaces them with finalized 
-  * Skolem variables. 
-  * 
-  * For each skolem variable we introduce, we ensure its associated side condition is
-  * added to side_conditions.
-  *
-  * Apart from Skolem functions, all subterms of n should be eligible for instantiation.
-  */
-  Node eliminateSkolemFunctions( TNode n, std::vector< Node >& side_conditions );
-  /** solve_bv_constraint
-  * solve for sv in constraint ( (pol ? _ : not) sv_t <rk> t ), where sv_t.path = sv
-  * status accumulates side conditions
-  */
-  Node solve_bv_constraint( Node sv, Node sv_t, Node t, Kind rk, bool pol, std::vector< unsigned >& path,
-                            BvInverterModelQuery * m, BvInverterStatus& status );
-  /** solve_bv_lit
-  * solve for sv in lit, where lit.path = sv
-  * status accumulates side conditions
-  */
-  Node solve_bv_lit( Node sv, Node lit, bool pol, std::vector< unsigned >& path,
-                     BvInverterModelQuery * m, BvInverterStatus& status );
-};
 
 class BvInstantiator : public Instantiator {
 private:
