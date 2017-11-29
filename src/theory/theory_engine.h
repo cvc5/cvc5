6d5ee78e5ad17adb3712367715193dfab771ada8
ajreynol <andrew.j.reynolds@gmail.com>
2017-11-29 08:22:38 -0600

    Fix indent.

--- src/theory/theory_engine.h
+++ src/theory/theory_engine.h
@@ -855,30 +855,30 @@ private:
   std::set< std::string > d_theoryAlternatives;
 
   std::map< std::string, std::vector< theory::Theory* > > d_attr_handle;
-public:
+ public:
+
+    /**
+    * Set user attribute.
+    * This function is called when an attribute is set by a user.  In SMT-LIBv2 this is done
+    * via the syntax (! n :attr)
+    */
+  void setUserAttribute(const std::string& attr,
+                        Node n,
+                        const std::vector<Node>& node_values,
+                        const std::string& str_value);
 
   /**
-   * Set user attribute.
-   * This function is called when an attribute is set by a user.  In SMT-LIBv2 this is done
-   * via the syntax (! n :attr)
-   */
- void setUserAttribute(const std::string& attr,
-                       Node n,
-                       const std::vector<Node>& node_values,
-                       const std::string& str_value);
+    * Handle user attribute.
+    * Associates theory t with the attribute attr.  Theory t will be
+    * notified whenever an attribute of name attr is set.
+    */
+  void handleUserAttribute(const char* attr, theory::Theory* t);
 
- /**
-  * Handle user attribute.
-  * Associates theory t with the attribute attr.  Theory t will be
-  * notified whenever an attribute of name attr is set.
-  */
- void handleUserAttribute(const char* attr, theory::Theory* t);
-
- /**
-  * Check that the theory assertions are satisfied in the model.
-  * This function is called from the smt engine's checkModel routine.
-  */
- void checkTheoryAssertionsWithModel(bool hardFailure);
+  /**
+    * Check that the theory assertions are satisfied in the model.
+    * This function is called from the smt engine's checkModel routine.
+    */
+  void checkTheoryAssertionsWithModel(bool hardFailure);
 
 private:
   IntStat d_arithSubstitutionsAdded;
