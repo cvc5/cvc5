f99de895a6385ec991ab94c71d4203fc658f7d97
ajreynol <andrew.j.reynolds@gmail.com>
2017-12-01 17:15:57 -0600

    Minor

--- src/theory/theory_engine.cpp
+++ src/theory/theory_engine.cpp
@@ -605,8 +605,6 @@ void TheoryEngine::check(Theory::Effort effort) {
             if( theory->needsCheckLastEffort() ){
               if( !d_curr_model->isBuilt() ){
                 if( !d_curr_model_builder->buildModel(d_curr_model) ){
-                  //model building should fail only if the model builder adds lemmas		
-                  Assert( needCheck() );
                   break;
                 }
               }
@@ -643,6 +641,8 @@ void TheoryEngine::check(Theory::Effort effort) {
       {
         if (!d_curr_model->isBuiltSuccess())
         {
+          //model building should fail only if the model builder adds lemmas		
+          Assert( needCheck() );
           // incomplete if model building failed
           d_incomplete = true;
         }
