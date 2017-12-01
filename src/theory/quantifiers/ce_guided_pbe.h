ffc78cdf25327d18f7ff5b265f78480248907cab
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2017-12-01 11:43:02 -0600

    Minor additions for sygus (#1419)

--- src/theory/quantifiers/ce_guided_pbe.h
+++ src/theory/quantifiers/ce_guided_pbe.h
@@ -742,14 +742,6 @@ class CegConjecturePbe {
                                     std::map< Node, std::vector< unsigned > > incr,
                                     UnifContext& x );
   //------------------------------ end constructing solutions
-
-  /** get guard status
-   *
-   * Returns 1 if g is asserted true in the SAT solver.
-   * Returns -1 if g is asserted false in the SAT solver.
-   * Returns 0 otherwise.
-   */
-  int getGuardStatus(Node g);
 };
 
 }/* namespace CVC4::theory::quantifiers */
