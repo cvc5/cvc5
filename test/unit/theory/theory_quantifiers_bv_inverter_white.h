c029d9dd501699c99c1851b0b6371b8f8ba454d4
Aina Niemetz <aina.niemetz@gmail.com>
2017-12-06 13:14:43 -0800

    Added unit tests for getBvScMult, getBvScUrem, getBvScUdiv.

--- test/unit/theory/theory_quantifiers_bv_inverter_white.h
+++ test/unit/theory/theory_quantifiers_bv_inverter_white.h
@@ -63,9 +63,40 @@ class TheoryQuantifiersBvInverter : public CxxTest::TestSuite
         : (k == kind::BITVECTOR_SLT ? kind::BITVECTOR_SGE : kind::DISTINCT);
     }
     Node body = idx == 0
-      ? d_nm->mkNode(k, d_x, d_t) : d_nm->mkNode(k, d_t, d_x);
+      ? d_nm->mkNode(k, d_x, d_t)
+      : d_nm->mkNode(k, d_t, d_x);
     Node scr = d_nm->mkNode(kind::EXISTS, d_bvarlist, body);
-    Expr a = d_nm->mkNode(kind::EQUAL, scl, scr).notNode().toExpr();
+    Expr a = d_nm->mkNode(kind::DISTINCT, scl, scr).toExpr();
+    Result res = d_smt->checkSat(a);
+    TS_ASSERT(res.d_sat == Result::UNSAT);
+  }
+
+  void runTest(Kind k,
+               unsigned idx,
+               Node (*getsc)(Kind, unsigned, Node, Node, Node))
+  {
+    Assert(k == kind::BITVECTOR_MULT
+           || k == kind::BITVECTOR_UREM_TOTAL
+           || k == kind::BITVECTOR_UDIV_TOTAL
+           || k == kind::BITVECTOR_AND
+           || k == kind::BITVECTOR_OR
+           || k == kind::BITVECTOR_LSHR
+           || k == kind::BITVECTOR_ASHR
+           || k == kind::BITVECTOR_SHL);
+    Assert(k != kind::BITVECTOR_UREM_TOTAL || idx == 1);
+    Assert((k != kind::BITVECTOR_LSHR
+            && k != kind::BITVECTOR_ASHR
+            && k != kind::BITVECTOR_SHL)
+           || idx == 0);
+
+    Node sc = getsc(k, idx, d_sk, d_s, d_t);
+    Kind ksc = sc.getKind();
+    TS_ASSERT(ksc == kind::IMPLIES);
+    Node body = idx == 0
+      ? d_nm->mkNode(k, d_x, d_s).eqNode(d_t)
+      : d_nm->mkNode(k, d_s, d_x).eqNode(d_t);
+    Node scr = d_nm->mkNode(kind::EXISTS, d_bvarlist, body);
+    Expr a = d_nm->mkNode(kind::DISTINCT, sc[0], scr).toExpr();
     Result res = d_smt->checkSat(a);
     TS_ASSERT(res.d_sat == Result::UNSAT);
   }
@@ -82,8 +113,8 @@ class TheoryQuantifiersBvInverter : public CxxTest::TestSuite
     d_smt->setOption("trace", "theory::assertions");
     d_scope = new SmtScope(d_smt);
 
-    d_s = d_nm->mkVar("s", d_nm->mkBitVectorType(8));
-    d_t = d_nm->mkVar("t", d_nm->mkBitVectorType(8));
+    d_s = d_nm->mkVar("s", d_nm->mkBitVectorType(4));
+    d_t = d_nm->mkVar("t", d_nm->mkBitVectorType(4));
     d_sk = d_nm->mkSkolem("sk", d_t.getType());
     d_x = d_nm->mkBoundVar(d_t.getType());
     d_bvarlist = d_nm->mkNode(kind::BOUND_VAR_LIST, { d_x });
@@ -144,24 +175,47 @@ class TheoryQuantifiersBvInverter : public CxxTest::TestSuite
   void testGetScBvEq0()
   {
     runTestPred(false, EQUAL, 0, getScBvEq);
+    TS_ASSERT_THROWS(runTestPred(true, EQUAL, 0, getScBvEq),
+                     AssertionException);
   }
 
   void testGetScBvEq1()
   {
     runTestPred(false, EQUAL, 1, getScBvEq);
+    TS_ASSERT_THROWS(runTestPred(true, EQUAL, 1, getScBvEq),
+                     AssertionException);
   }
 
-  //void testGetScBvMult()
-  //{
-  //}
+  void testGetScBvMult0()
+  {
+    runTest(BITVECTOR_MULT, 0, getScBvMult);
+  }
 
-  //void testGetScBvUrem()
-  //{
-  //}
+  void testGetScBvMult1()
+  {
+    runTest(BITVECTOR_MULT, 1, getScBvMult);
+  }
 
-  //void testGetScBvUdiv()
-  //{
-  //}
+  void testGetScBvUrem0()
+  {
+    TS_ASSERT_THROWS(runTest(BITVECTOR_UREM_TOTAL, 0, getScBvUrem),
+                     AssertionException);
+  }
+
+  void testGetScBvUrem1()
+  {
+    runTest(BITVECTOR_UREM_TOTAL, 1, getScBvUrem);
+  }
+
+  void testGetScBvUdiv0()
+  {
+    runTest(BITVECTOR_UDIV_TOTAL, 0, getScBvUdiv);
+  }
+
+  void testGetScBvUdiv1()
+  {
+    runTest(BITVECTOR_UDIV_TOTAL, 1, getScBvUdiv);
+  }
 
   //void testGetScBvAndOr()
   //{
