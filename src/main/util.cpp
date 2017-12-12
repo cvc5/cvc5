cae651f9b188f00771937c5562491753c319a8ce
Mathias Preiner <mathias.preiner@gmail.com>
2017-12-11 17:52:58 -0800

    Add SIGTERM handler.
    Print statistics if CVC4 gets a SIGTERM signal.

--- src/main/util.cpp
+++ src/main/util.cpp
@@ -77,6 +77,14 @@ void timeout_handler(int sig, siginfo_t* info, void*) {
   abort();
 }
 
+/** Handler for SIGTERM. */
+void sigterm_handler(int sig, siginfo_t* info, void*)
+{
+  safe_print(STDERR_FILENO, "CVC4 interrupted by SIGTERM.\n");
+  print_statistics();
+  abort();
+}
+
 /** Handler for SIGINT, i.e., when the user hits control C. */
 void sigint_handler(int sig, siginfo_t* info, void*) {
   safe_print(STDERR_FILENO, "CVC4 interrupted by user.\n");
@@ -322,6 +330,14 @@ void cvc4_init() throw(Exception) {
 #endif /* HAVE_SIGALTSTACK */
 
 #endif /* __WIN32__ */
+
+  struct sigaction act5;
+  act5.sa_sigaction = sigterm_handler;
+  act5.sa_flags = SA_SIGINFO;
+  sigemptyset(&act5.sa_mask);
+  if(sigaction(SIGTERM, &act5, NULL)) {
+    throw Exception(string("sigaction(SIGTERM) failure: ") + strerror(errno));
+  }
 
   set_unexpected(cvc4unexpected);
   default_terminator = set_terminate(cvc4terminate);
