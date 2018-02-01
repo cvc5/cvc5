6f1a85d600a78ce5388336f3e4d747df6c158239
Haniel Barbosa <hanielbbarbosa@gmail.com>
2018-02-01 16:12:17 -0600

    more

--- src/smt/smt_engine.cpp
+++ src/smt/smt_engine.cpp
@@ -1353,13 +1353,13 @@ void SmtEngine::setDefaults() {
     */
   }
 
-  if(options::checkModels() || options::checkSynthSol()) {
-    if(! options::produceAssertions()) {
+  if ((options::checkModels() || options::checkSynthSol())
+      && !options::produceAssertions())
+  {
       Notice() << "SmtEngine: turning on produce-assertions to support "
                << "check-models or check-synth-sol." << endl;
       setOption("produce-assertions", SExpr("true"));
     }
-  }
 
   if(options::unsatCores()) {
     if(options::simplificationMode() != SIMPLIFICATION_MODE_NONE) {
@@ -5535,20 +5535,33 @@ void SmtEngine::checkSynthSol()
     Notice() << "SmtEngine::checkSynthSol(): -- expands to " << conj << endl;
     Trace("check-synth-sol") << "Expanded assertion " << conj << "\n";
 
-    // Apply solution map
-    conj = conj.substitute(function_vars.begin(),
+    // Apply solution map to conjecture body
+    conjBody = conj[1].substitute(function_vars.begin(),
                            function_vars.end(),
                            function_sols.begin(),
                            function_sols.end());
-    Notice() << "SmtEngine::checkSynthSol(): -- substitutes to " << conj
+
+    Notice() << "SmtEngine::checkSynthSol(): -- body substitutes to " << conjBody
              << endl;
-    Trace("check-synth-sol") << "Substituted assertion " << conj << "\n";
-    Node negSolvedConj = nm->mkNode(kind::NOT, conj);
+    Trace("check-synth-sol") << "Substituted body of assertion to " << conjBody << "\n";
+    /* Skolemize conjecture */
+    std::vector<Node> vars, skos;
+    for (unsigned j = 0, size = conj[0].size(); j < size; ++j)
+    {
+      vars.push_back(conj[0][j]);
+      std::stringstream ss;
+      ss << "sk_" << j;
+      skos.push_back(nm->mkSkolem(ss.str(), TypeNode::fromType(conj[0][j].getType())));
+      Trace("check-synth-sol") << "\tSkolemizing " << conj[0][j] << " to " << skos.back() << "\n";
+    }
+    conjBody =
+        conjBody.substitute(vars.begin(), vars.end(), skos.begin(), skos.end());
+
     Notice()
-        << "SmtEngine::checkSynthSol(): asserting negated solved conjecture"
-        << negSolvedConj << endl;
-    Trace("check-synth-sol") << "Negated assertion " << negSolvedConj << "\n";
-    solChecker.assertFormula(negSolvedConj.toExpr());
+        << "SmtEngine::checkSynthSol(): asserting skomezied solved conjecture"
+        << conjBody << endl;
+    Trace("check-synth-sol") << "Skolemized negated conjecture " << conjBody << "\n";
+    solChecker.assertFormula(conjBody.toExpr());
     Result r = solChecker.checkSat();
     Notice() << "SmtEngine::checkSynthSol(): result is " << r << endl;
     Trace("check-synth-sol") << "Satsifiability check: " << r << "\n";
