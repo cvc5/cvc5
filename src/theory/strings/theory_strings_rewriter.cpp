9c55576739931a1364460b23529abb477eb7fad4
ajreynol <andrew.j.reynolds@gmail.com>
2018-03-01 09:05:44 -0600

    Clean

--- src/theory/strings/theory_strings_rewriter.cpp
+++ src/theory/strings/theory_strings_rewriter.cpp
@@ -1953,22 +1953,6 @@ Node TheoryStringsRewriter::rewriteReplace( Node node ) {
     return returnRewrite(node, node[0], "rpl-rpl-empty");
   }
 
-  /*
-  if (node[0].isConst())
-  {
-    CVC4::String s = node[0].getConst<String>();
-    if (s.isEmptyString())
-    {
-      return returnRewrite(node, node[0], "rpl-empty");
-    }
-    if (node[0] == node[2] && s.size() == 1)
-    {
-      // str.replace( "A", x, "A" ) -> "A"
-      return returnRewrite(node, node[0], "rpl-char-id");
-    }
-  }
-  */
-
   std::vector<Node> children0;
   getConcat(node[0], children0);
 
