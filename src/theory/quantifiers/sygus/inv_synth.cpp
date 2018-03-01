15a919378dd7c6f9ae29a570f4ec53b9839765f1
Haniel Barbosa <hanielbbarbosa@gmail.com>
2018-03-01 15:15:33 -0600

    start

---
+++ src/theory/quantifiers/sygus/inv_synth.cpp
@@ -0,0 +1,24 @@
+/*********************                                                        */
+/*! \file inv_synth.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Haniel Barbosa
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief implementation of class for specialized approaches for invariant
+ ** synthesis
+ **/
+
+#include "theory/quantifiers/sygus/inv_synth.h"
+
+namespace CVC4 {
+namespace theory {
+namespace quantifiers {
+
+}  // namespace quantifiers
+}  // namespace theory
+}  // namespace CVC4
