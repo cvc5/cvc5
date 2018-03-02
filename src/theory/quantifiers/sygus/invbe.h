1572143e5a1acb3124bb3881ff0730d23badde08
Haniel Barbosa <hanielbbarbosa@gmail.com>
2018-03-02 15:36:59 -0600

    doc

--- src/theory/quantifiers/sygus/invbe.h
+++ src/theory/quantifiers/sygus/invbe.h
@@ -34,23 +34,23 @@ namespace quantifiers {
 /** Synthesizes invariants
  *
  * The primary approach consists of successively strengthening and weakining
- * candidate invariants based on counterexamples produced during verification
- * that the invariant satisfies all conditions
+ * candidate invariants based on counterexamples produced while verifying
+ * that a candidate invariant satisfies the constraints
  *
  * Synthesis is used for deriving "features", atomic expressions that are used
  * to separate "good", "bad", and "conditional" valuations to the state
  * variables in the transition system. Once points can be separated without
- * conflicts, an arbitrary CNF is built with the features, thus yielding the
- * invariant.
+ * conflicts, a CNF is built (not using synthesis) with the features, thus
+ * yielding the invariant.
  *
  * Strengthening occurs when testing the candidate yields a "bad" or a
- * "conditional" valuation, while weakining when testing yields a "good"
+ * "conditional" valuation, while weakining occurs when testing yields a "good"
  * point. "Good" points are those in which the invariant must always hold, "bad"
  * in which it must never hold, and "conditional" those in which the invariant
- * cannot hold then fail to hold (representing the invariant not being respected
- * by the transition relation). Good and bad points are independent of the
- * candidate being derived, while conditional points are good points which yield
- * bad points for an specific candidate.
+ * cannot hold then fail to hold (representing the invariant not being
+ * inductive, as applying a transition invalidates it). Good and bad points are
+ * independent of the candidate being derived, while conditional points are
+ * those in which an specific candidate went from good points to bad points .
  *
  * This appoarch is inspired by Padhi et al. PLDI 2016
  */
