2844953cfaded11bd46f1ebc304ec5a815602b91
Aina Niemetz <aina.niemetz@gmail.com>
2018-03-03 19:05:31 -0800

    Add uniform way to serialize containers of Expr to stream.

--- src/expr/expr_template.cpp
+++ src/expr/expr_template.cpp
@@ -54,6 +54,26 @@ std::ostream& operator<<(std::ostream& out, const Expr& e) {
   }
 }
 
+std::ostream& operator<<(std::ostream& out, const std::vector<Expr>& container)
+{
+  container_to_stream(out, container);
+  return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const std::set<Expr>& container)
+{
+  container_to_stream(out, container);
+  return out;
+}
+
+std::ostream& operator<<(
+    std::ostream& out,
+    const std::unordered_set<Expr, ExprHashFunction>& container)
+{
+  container_to_stream(out, container);
+  return out;
+}
+
 TypeCheckingException::TypeCheckingException(const TypeCheckingException& t)
     : Exception(t.d_msg), d_expr(new Expr(t.getExpression()))
 {
