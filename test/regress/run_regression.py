c9e922dc084dca1f0c926067fcb79104789a0cfe
Andres Noetzli <andres.noetzli@gmail.com>
2018-03-19 16:32:15 -0700

    Addressing pylint warnings and minor fixes

--- test/regress/run_regression.py
+++ test/regress/run_regression.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+"""
+Usage:
+
+    run_regression.py [ --proof | --dump ] [ wrapper ] cvc4-binary
+        [ benchmark.cvc | benchmark.smt | benchmark.smt2 | benchmark.p ]
+
+Runs benchmark and checks for correct exit status and output.
+"""
+
 import argparse
 import difflib
 import os
@@ -16,6 +26,13 @@ COMMAND_LINE = 'COMMAND-LINE: '
 
 def run_benchmark(dump, wrapper, scrubber, error_scrubber, cvc4_binary,
                   command_line, benchmark_dir, benchmark_filename):
+    """Runs CVC4 on a benchmark file `benchmark_file` in the directory
+    `benchmark_dir` using the binary `cvc4_binary` with the command line
+    options `command_line`. The output is scrubbed using `scrubber` and
+    `error_scrubber` for stdout and stderr, respectively. If dump is true, the
+    function first uses CVC4 to read in and dump the benchmark file and then
+    uses that as input."""
+
     bin_args = wrapper[:]
     bin_args.append(cvc4_binary)
 
@@ -24,7 +41,7 @@ def run_benchmark(dump, wrapper, scrubber, error_scrubber, cvc4_binary,
     exit_status = None
     if dump:
         dump_args = [
-            '--preprocess-only', '--dump=clauses', '--output-lang=smt2', '-q'
+            '--preprocess-only', '--dump=clauses', '--output-lang=smt2', '-qq'
         ]
         dump_process = subprocess.Popen(
             bin_args + command_line + dump_args + [benchmark_filename],
@@ -64,10 +81,22 @@ def run_benchmark(dump, wrapper, scrubber, error_scrubber, cvc4_binary,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
         error, _ = error_scrubber_process.communicate(input=error)
-    return (output.strip(), error.strip(), process.returncode)
+
+    # Popen in Python 3 returns a bytes object instead of a string for
+    # stdout/stderr.
+    if isinstance(output, bytes):
+        output = output.decode()
+    if isinstance(error, bytes):
+        error = error.decode()
+    return (output.strip(), error.strip(), exit_status)
 
 
-def main(proof, dump, wrapper, cvc4_binary, benchmark_path):
+def run_regression(proof, dump, wrapper, cvc4_binary, benchmark_path):
+    """Determines the expected output for a benchmark, runs CVC4 on it and then
+    checks whether the output corresponds to the expected output. Optionally
+    uses a wrapper `wrapper`, tests proof generation (if proof is true), or
+    dumps a benchmark and uses that as the input (if dump is true)."""
+
     if not os.access(cvc4_binary, os.X_OK):
         sys.exit(
             '"{}" does not exist or is not executable'.format(cvc4_binary))
@@ -110,8 +139,9 @@ def main(proof, dump, wrapper, cvc4_binary, benchmark_path):
     else:
         metadata_filename = benchmark_path
 
-    with open(metadata_filename, 'r') as f:
-        metadata_lines = f.readlines()
+    metadata_lines = None
+    with open(metadata_filename, 'r') as metadata_file:
+        metadata_lines = metadata_file.readlines()
     metadata_content = ''.join(metadata_lines)
 
     # Extract the metadata for the benchmark.
@@ -145,12 +175,12 @@ def main(proof, dump, wrapper, cvc4_binary, benchmark_path):
     # Expected output/expected error has not been defined in the metadata for
     # the benchmark. Try to extract the information from the benchmark itself.
     if expected_output == '' and expected_error == '':
-        m = None
+        match = None
         if status_regex is not None:
-            m = re.search(status_regex, metadata_content)
+            match = re.search(status_regex, metadata_content)
 
-        if m is not None:
-            expected_output = status_to_output(m.group(1))
+        if match is not None:
+            expected_output = status_to_output(match.group(1))
         elif expected_exit_status is None:
             # If there is no expected output/error and the exit status has not
             # been set explicitly, the benchmark is invalid.
@@ -196,14 +226,15 @@ def main(proof, dump, wrapper, cvc4_binary, benchmark_path):
                 '--check-proofs', '--no-bv-eq', '--no-bv-ineq',
                 '--no-bv-algebraic'
             ]
-    if len(extra_command_line_args) > 0:
-        command_line_args_configs.append(basic_command_line_args +
-                                         extra_command_line_args)
+    if extra_command_line_args:
+        command_line_args_configs.append(
+            basic_command_line_args + extra_command_line_args)
 
     # Run CVC4 on the benchmark with the different option sets and check
     # whether the exit status, stdout output, stderr output are as expected.
     print('1..{}'.format(len(command_line_args_configs)))
-    for i, command_line_args in enumerate(command_line_args_configs):
+    print('# Starting')
+    for command_line_args in command_line_args_configs:
         output, error, exit_status = run_benchmark(
             dump, wrapper, scrubber, error_scrubber, cvc4_binary,
             command_line_args, benchmark_dir, benchmark_basename)
@@ -229,20 +260,27 @@ def main(proof, dump, wrapper, cvc4_binary, benchmark_path):
             print('ok - Flags: {}'.format(command_line_args))
 
 
-if __name__ == "__main__":
+def main():
+    """Parses the command line arguments and then calls the core of the
+    script."""
+
     parser = argparse.ArgumentParser(
         description=
         'Runs benchmark and checks for correct exit status and output.')
     parser.add_argument('--proof', action='store_true')
     parser.add_argument('--dump', action='store_true')
-    parser.add_argument('wrapper', nargs='*', default=None)
+    parser.add_argument('wrapper', nargs='*')
     parser.add_argument('cvc4_binary')
     parser.add_argument('benchmark')
     args = parser.parse_args()
     cvc4_binary = os.path.abspath(args.cvc4_binary)
 
     wrapper = args.wrapper
-    if os.environ.get('VALGRIND') == '1' and len(wrapper) == 0:
+    if os.environ.get('VALGRIND') == '1' and not wrapper:
         wrapper = ['libtool', '--mode=execute', 'valgrind']
 
-    main(args.proof, args.dump, wrapper, cvc4_binary, args.benchmark)
+    run_regression(args.proof, args.dump, wrapper, cvc4_binary, args.benchmark)
+
+
+if __name__ == "__main__":
+    main()
