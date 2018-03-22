7b7799c8cb92e42030eb130d88679f1fa6e4fe69
ajreynol <andrew.j.reynolds@gmail.com>
2018-03-22 15:39:52 -0500

    Initial

---
+++ src/theory/quantifiers/sygus_infer.cpp
@@ -0,0 +1,35 @@
+/*********************                                                        */
+/*! \file sygus_infer.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implementation of sygus_infer
+ **/
+
+#include "theory/quantifiers/sygus_infer.h"
+
+using namespace std;
+using namespace CVC4::kind;
+
+namespace CVC4 {
+namespace theory {
+namespace quantifiers {
+  
+SygusInfer::SygusInfer() {
+
+}
+
+bool SygusInfer::simplify(std::vector<Node>& assertions)
+{
+  
+}
+
+} /* CVC4::theory::quantifiers namespace */
+} /* CVC4::theory namespace */
+} /* CVC4 namespace */
---
+++ src/theory/quantifiers/sygus_infer.h
@@ -0,0 +1,49 @@
+/*********************                                                        */
+/*! \file sygus_infer.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief sygus_infer
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef __CVC4__THEORY__QUANTIFIERS__SYGUS_INFER_H
+#define __CVC4__THEORY__QUANTIFIERS__SYGUS_INFER_H
+
+#include <map>
+
+namespace CVC4 {
+namespace theory {
+namespace quantifiers {
+
+/** SygusInfer
+ * 
+ * A preprocessing utility to turn a set of (quantified) assertions into a 
+ * single SyGuS conjecture.
+ */
+class SygusInfer 
+{
+public:
+  SygusInfer();
+  ~SygusInfer(){}
+  /** simplify assertions
+   *
+   * Either replaces assertions with an equivalent SyGus conjecture and returns
+   * true (if possible), or otherwise returns false.
+   */
+  bool simplify(std::vector<Node>& assertions);
+};
+
+
+} /* CVC4::theory::quantifiers namespace */
+} /* CVC4::theory namespace */
+} /* CVC4 namespace */
+
+#endif /* __CVC4__THEORY__QUANTIFIERS__SYGUS_INFER_H */
