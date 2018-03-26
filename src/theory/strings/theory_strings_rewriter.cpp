dd801d5aa44c03cb8e1945c5d32258d3c0c11e34
Andres Noetzli <andres.noetzli@gmail.com>
2018-03-25 21:21:08 -0700

    Add reasoning for inequalities in str rewriter
    This commit adds support for reasoning about entailments with inequality
    assumptions, e.g. x + 5 < 5 |= x < 0. This is used when rewriting
    substrings operations:
    
    ```
    (str.substr s x y) --> "" if x < len(s) |= 0 >= y
    ```

--- src/theory/strings/theory_strings_rewriter.cpp
+++ src/theory/strings/theory_strings_rewriter.cpp
@@ -1520,15 +1520,13 @@ Node TheoryStringsRewriter::rewriteSubstr(Node node)
         }
       }
 
-      if (tot_len == nm->mkConst(Rational(1)))
+      // (str.substr s x y) --> "" if x < len(s) |= 0 >= y
+      Node n1_lt_tot_len =
+          Rewriter::rewrite(nm->mkNode(kind::LT, node[1], tot_len));
+      if (checkEntailArithWithAssumption(n1_lt_tot_len, zero, node[2], false))
       {
-        Node n1_eq_zero =
-            Rewriter::rewrite(nm->mkNode(kind::EQUAL, node[1], zero));
-        if (checkEntailArithWithAssumption(n1_eq_zero, zero, node[2], false))
-        {
-          Node ret = nm->mkConst(::CVC4::String(""));
-          return returnRewrite(node, ret, "ss-len-one-unsat");
-        }
+        Node ret = nm->mkConst(::CVC4::String(""));
+        return returnRewrite(node, ret, "ss-start-entails-zero-len");
       }
     }
     if (!curr.isNull())
@@ -3062,7 +3060,7 @@ bool TheoryStringsRewriter::checkEntailArithWithEqAssumption(Node assumption,
     return false;
   }
 
-  a = a.substitute(TNode(v), TNode(solution));
+  a = Rewriter::rewrite(a.substitute(TNode(v), TNode(solution)));
   return checkEntailArith(a, strict);
 }
 
@@ -3071,11 +3069,35 @@ bool TheoryStringsRewriter::checkEntailArithWithAssumption(Node assumption,
                                                            Node b,
                                                            bool strict)
 {
-  // TODO: Add support for inequality assumptions.
-  Assert(assumption.getKind() == kind::EQUAL);
   Assert(Rewriter::rewrite(assumption) == assumption);
 
   NodeManager* nm = NodeManager::currentNM();
+
+  if (assumption.getKind() != kind::EQUAL)
+  {
+    // We rewrite inequality assumptions from x <= y to x + (str.len s) = y
+    // where s is some fresh string variable. We use (str.len s) because
+    // (str.len s) must be non-negative for the equation to hold.
+    Node x, y;
+    if (assumption.getKind() == kind::GEQ)
+    {
+      x = assumption[0];
+      y = assumption[1];
+    }
+    else
+    {
+      // (not (>= s t)) --> (>= (t - 1) s)
+      Assert(assumption.getKind() == kind::NOT
+             && assumption[0].getKind() == kind::GEQ);
+      x = nm->mkNode(kind::MINUS, assumption[0][1], nm->mkConst(Rational(1)));
+      y = assumption[0][0];
+    }
+
+    Node s = nm->mkSkolem("s", nm->stringType());
+    Node slen = nm->mkNode(kind::STRING_LENGTH, s);
+    assumption = Rewriter::rewrite(
+        nm->mkNode(kind::EQUAL, x, nm->mkNode(kind::PLUS, y, slen)));
+  }
 
   Node diff = nm->mkNode(kind::MINUS, a, b);
   return checkEntailArithWithEqAssumption(assumption, diff, strict);
@@ -3090,7 +3112,6 @@ bool TheoryStringsRewriter::checkEntailArithWithAssumptions(
   bool res = false;
   for (const auto& assumption : assumptions)
   {
-    Assert(assumption.getKind() == kind::EQUAL);
     Assert(Rewriter::rewrite(assumption) == assumption);
 
     if (checkEntailArithWithAssumption(assumption, a, b, strict))
