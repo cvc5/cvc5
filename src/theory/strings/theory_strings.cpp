bd97d43e8eac8ad4fe7b4bce8591d347c0cc1c31
ajreynol <andrew.j.reynolds@gmail.com>
2018-04-15 18:41:16 -0500

    Improve

--- src/theory/strings/theory_strings.cpp
+++ src/theory/strings/theory_strings.cpp
@@ -648,9 +648,11 @@ void TheoryStrings::preRegisterTerm(TNode n) {
         TypeNode tn = n.getType();
         if( tn.isString() ) {
           registerTerm( n, 0 );
-          // FMF
-          if (options::stringFMF()
-              && d_all_skolems.find(n) == d_all_skolems.end())
+          // if finite model finding is enabled,
+          // then we minimize the length of this term if it is a variable
+          // but not an internally generated Skolem, or a term that does
+          // not belong to this theory.
+          if ( options::stringFMF() && ( ( n.isVar() && d_all_skolems.find(n)==d_all_skolems.end() ) || theoryOf(n.getKind()!=THEORY_STRINGS ) )
           {
             d_input_vars.insert(n);
           }
