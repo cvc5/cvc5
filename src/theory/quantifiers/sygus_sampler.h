cd418e07a45f96381ebcbd8eb88131d9c88e4f92
ajreynol <andrew.j.reynolds@gmail.com>
2018-04-20 16:58:59 -0500

    Improve sygus sampler for strings.

--- src/theory/quantifiers/sygus_sampler.h
+++ src/theory/quantifiers/sygus_sampler.h
@@ -335,8 +335,12 @@ class SygusSampler : public LazyTrieEvaluator
   std::map<TypeNode, std::vector<unsigned> > d_rvalue_cindices;
   /** map from sygus types to non-variable nullary constructors */
   std::map<TypeNode, std::vector<unsigned> > d_rvalue_null_cindices;
+  /** the random string alphabet */
+  std::vector< unsigned > d_rstring_alphabet;
   /** map from variables to sygus types that include them */
   std::map<Node, std::vector<TypeNode> > d_var_sygus_types;
+  /** map from constants to sygus types that include them */
+  std::map<Node, std::vector<TypeNode> > d_const_sygus_types;
   /** register sygus type, intializes the above two data structures */
   void registerSygusType(TypeNode tn);
 };
