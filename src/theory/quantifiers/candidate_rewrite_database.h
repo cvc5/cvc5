2c7348657b3beae151822a80f67d0cd38dfcabb0
ajreynol <andrew.j.reynolds@gmail.com>
2018-04-25 09:33:46 -0500

    Address review.

--- src/theory/quantifiers/candidate_rewrite_database.h
+++ src/theory/quantifiers/candidate_rewrite_database.h
@@ -4,7 +4,7 @@
  ** Top contributors (to current version):
  **   Andrew Reynolds
  ** This file is part of the CVC4 project.
- ** Copyright (c) 2009-2017 by the authors listed in the file AUTHORS
+ ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
@@ -69,7 +69,7 @@ class CandidateRewriteDatabase
  private:
   /** reference to quantifier engine */
   QuantifiersEngine* d_qe;
-  /** the function to the synthesize we are testing */
+  /** the function-to-synthesize we are testing */
   Node d_candidate;
   /** sygus sampler objects for each program variable
    *
