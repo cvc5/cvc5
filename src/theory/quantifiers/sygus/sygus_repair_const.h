2b2d1d1456579df3dfdc80486425a77c0d4f893f
ajreynol <andrew.j.reynolds@gmail.com>
2018-05-22 12:24:28 -0500

    Doc

--- src/theory/quantifiers/sygus/sygus_repair_const.h
+++ src/theory/quantifiers/sygus/sygus_repair_const.h
@@ -33,9 +33,9 @@ class CegConjecture;
  * This module is used to repair portions of candidate solutions. In particular,
  * given a synthesis conjecture:
  *   exists f. forall x. P( f, x )
- * and a candidate solution f = \x. t[x,c] where c are constants, this function
- * checks whether there exists a term of the form \x. t[x,c'] for some constants
- * c' such that:
+ * and a candidate solution f = \x. t[x,r] where c are repairable terms, this
+ * function checks whether there exists a term of the form \x. t[x,c'] for some
+ * constants c' such that:
  *   forall x. P( (\x. t[x,c']), x )  [***]
  * is satisfiable, where notice that the above formula after beta-reduction may
  * be one in pure first-order logic in a decidable theory (say linear
@@ -68,11 +68,15 @@ class SygusRepairConst
    * is a solution for the synthesis conjecture associated with this class.
    * Moreover, it is the case that
    *    repair_cv[j] != candidate_values[j], for at least one j.
+   * We always consider applications of the "any constant" constructors in 
+   * candidate_values to be repairable. In addition, if the flag 
+   * useConstantsAsHoles is true, we consider all constants whose (sygus) type
+   * admit alls constants to be repairable.
    */
   bool repairSolution(const std::vector<Node>& candidates,
                       const std::vector<Node>& candidate_values,
                       std::vector<Node>& repair_cv,
-                      bool useConstants=true
+                      bool useConstantsAsHoles=true
                      );
   /** must repair? 
    * 
@@ -118,24 +122,25 @@ class SygusRepairConst
    * 
    * This returns true if n can be repaired by this class. In particular, we
    * return true if n is an "any constant" constructor, or it is a constant
-   * in a type that allows all constants and useConstants is true.
+   * in a type that allows all constants and useConstantsAsHoles is true.
    */
-  inline bool isRepairable(Node n, bool useConstants=true) const;
+  inline bool isRepairable(Node n, bool useConstantsAsHoles=true) const;
   /** get skeleton
    *
-   * Returns a skeleton for n, where the subterms of n that are repairable
-   * constants are replaced by free variables. Since we are interested in
+   * Returns a skeleton for sygus datatype value n, where the subterms of n that
+   * are repairable are replaced by free variables. Since we are interested in
    * returning canonical skeletons, the free variables we use in this
    * replacement are taken from TermDbSygus, where we track indices
    * in free_var_count. Variables we introduce in this way are added to sk_vars.
    * The mapping sk_vars_to_subs contains entries v -> c, where v is a
-   * variable in sk_vars, and c is the term in n that it replaced.
+   * variable in sk_vars, and c is the term in n that it replaced. The flag
+   * useConstantsAsHoles affects which terms we consider to be repairable.
    */
   Node getSkeleton(Node n,
                    std::map<TypeNode, int>& free_var_count,
                    std::vector<Node>& sk_vars,
                    std::map<Node, Node>& sk_vars_to_subs,
-                      bool useConstants);
+                   bool useConstantsAsHoles);
   /** get first-order query
    *
    * This function returns a formula that is equivalent to the negation of the
