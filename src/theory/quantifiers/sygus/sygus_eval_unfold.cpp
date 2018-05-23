7d8a8db7551fe5e6b939896e660b3ba2addcaf6c
ajreynol <andrew.j.reynolds@gmail.com>
2018-05-23 17:08:28 -0500

    More

--- src/theory/quantifiers/sygus/sygus_eval_unfold.cpp
+++ src/theory/quantifiers/sygus/sygus_eval_unfold.cpp
@@ -31,6 +31,11 @@ SygusEvalUnfold::SygusEvalUnfold(TermDbSygus* tds) : d_tds(tds) {}
 void SygusEvalUnfold::registerEvalTerm(Node n)
 {
   Assert(options::sygusEvalUnfold());
+  // is this a sygus evaluation function application?
+  if( !datatypes::DatatypesRewriter::isSygusEvalApp(n) )
+  {
+    return;
+  }
   if (d_eval_processed.find(n) != d_eval_processed.end())
   {
     return;
@@ -38,12 +43,6 @@ void SygusEvalUnfold::registerEvalTerm(Node n)
   Trace("sygus-eval-unfold")
       << "SygusEvalUnfold: register eval term : " << n << std::endl;
   d_eval_processed.insert(n);
-  
-  // is this a sygus evaluation function application?
-  if( !datatypes::DatatypesRewriter::isSygusEvalApp(n) )
-  {
-    return;
-  }
   TypeNode tn = n[0].getType();
   Assert(tn.isDatatype());
   const Datatype& dt = static_cast<DatatypeType>(tn.toType()).getDatatype();
@@ -122,7 +121,6 @@ void SygusEvalUnfold::registerModelValue(Node a,
       }
       // evaluation children
       std::vector<Node> eval_children;
-      eval_children.push_back(Node::fromExpr(dt.getSygusEvaluationFunc()));
       eval_children.push_back(n);
       // for each evaluation
       for (unsigned i = start; i < curr_size; i++)
@@ -147,8 +145,8 @@ void SygusEvalUnfold::registerModelValue(Node a,
           vtm[n] = vn;
           eval_children.insert(
               eval_children.end(), it->second[i].begin(), it->second[i].end());
-          Node eval_fun = nm->mkNode(APPLY_UF, eval_children);
-          eval_children.resize(2);
+          Node eval_fun = datatypes::DatatypesRewriter::mkSygusEvalApp(dt,eval_children);
+          eval_children.resize(1);
           res = d_tds->unfold(eval_fun, vtm, exp);
           expn = exp.size() == 1 ? exp[0] : nm->mkNode(AND, exp);
         }
@@ -157,13 +155,13 @@ void SygusEvalUnfold::registerModelValue(Node a,
           EvalSygusInvarianceTest esit;
           eval_children.insert(
               eval_children.end(), it->second[i].begin(), it->second[i].end());
-          Node conj = nm->mkNode(APPLY_UF, eval_children);
-          eval_children[1] = vn;
-          Node eval_fun = nm->mkNode(APPLY_UF, eval_children);
+          Node conj = datatypes::DatatypesRewriter::mkSygusEvalApp(dt,eval_children);
+          eval_children[0] = vn;
+          Node eval_fun = datatypes::DatatypesRewriter::mkSygusEvalApp(dt,eval_children);
           res = d_tds->evaluateWithUnfolding(eval_fun);
           esit.init(conj, n, res);
-          eval_children.resize(2);
-          eval_children[1] = n;
+          eval_children.resize(1);
+          eval_children[0] = n;
 
           // evaluate with minimal explanation
           std::vector<Node> mexp;
