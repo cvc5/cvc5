cee6183f49f37960cfb62bab58f08afcf2ff3c99
ajreynol <andrew.j.reynolds@gmail.com>
2018-08-07 14:04:26 -0500

    Fix simple reg exp consume rewriter.

--- src/theory/strings/theory_strings_rewriter.cpp
+++ src/theory/strings/theory_strings_rewriter.cpp
@@ -983,12 +983,15 @@ Node TheoryStringsRewriter::rewriteMembership(TNode node) {
         return scn;
       }else{
         if( (children.size() + mchildren.size())!=prevSize ){
+          Node xn = mkConcat( kind::STRING_CONCAT, mchildren );
+          Node emptyStr = nm->mkConst( String("") );
           if( children.empty() ){
-            retNode = NodeManager::currentNM()->mkConst( mchildren.empty() );
+            retNode = xn.eqNode(emptyStr);
           }else{
-            retNode = NodeManager::currentNM()->mkNode( kind::STRING_IN_REGEXP, mkConcat( kind::STRING_CONCAT, mchildren ), mkConcat( kind::REGEXP_CONCAT, children ) );
+            retNode = nm->mkNode( STRING_IN_REGEXP, xn, mkConcat( REGEXP_CONCAT, children ) );
           }
           Trace("regexp-ext-rewrite") << "Regexp : rewrite : " << node << " -> " << retNode << std::endl;
+          return returnRewrite(node,retNode,"re-simple-consume");
         }
       }
     }
