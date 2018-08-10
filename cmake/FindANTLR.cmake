424923f1317f3182574ebe730ebe0c81b7dbf494
Mathias Preiner <mathias.preiner@gmail.com>
2018-08-10 16:10:23 -0700

    cmake: Antlr parser generation done.

--- cmake/FindANTLR.cmake
+++ cmake/FindANTLR.cmake
@@ -1,21 +1,26 @@
 # Find ANTLR
 # ANTLR_FOUND - system has ANTLR lib
+# ANTLR_BINARY - the ANTLR binary
 # ANTLR_INCLUDE_DIR - the ANTLR include directory
 # ANTLR_LIBRARIES - Libraries needed to use ANTLR
+
+find_program(ANTLR_BINARY
+  NAMES antlr3
+  PATHS "${PROJECT_SOURCE_DIR}/antlr-3.4/bin"
+  )
 
 find_path(ANTLR_INCLUDE_DIR
   NAMES antlr3.h
   PATHS "${PROJECT_SOURCE_DIR}/antlr-3.4/include"
-  NO_DEFAULT_PATH
   )
 
 find_library(ANTLR_LIBRARIES
   NAMES antlr3c libantlr3c
   PATHS "${PROJECT_SOURCE_DIR}/antlr-3.4/lib"
-  NO_DEFAULT_PATH
   )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ANTLR DEFAULT_MSG ANTLR_INCLUDE_DIR ANTLR_LIBRARIES)
+find_package_handle_standard_args(
+  ANTLR DEFAULT_MSG ANTLR_BINARY ANTLR_INCLUDE_DIR ANTLR_LIBRARIES)
 
-mark_as_advanced(ANTLR_INCLUDE_DIR ANTLR_LIBRARIES)
+mark_as_advanced(ANTLR_BINARY ANTLR_INCLUDE_DIR ANTLR_LIBRARIES)
