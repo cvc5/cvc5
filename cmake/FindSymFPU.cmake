9109f7fd60621562651dad2d507b80cf7da74a62
Mathias Preiner <mathias.preiner@gmail.com>
2018-08-13 15:24:46 -0700

    cmake: Add module finder for SymFPU.

---
+++ cmake/FindSymFPU.cmake
@@ -0,0 +1,12 @@
+# Find SymFPU
+# SymFPU_FOUND - system has SymFPU lib
+# SymFPU_INCLUDE_DIR - the SymFPU include directory
+
+find_path(SymFPU_INCLUDE_DIR
+          NAMES symfpu/core/unpackedFloat.h
+          PATHS "${PROJECT_SOURCE_DIR}/symfpu-CVC4")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SymFPU DEFAULT_MSG SymFPU_INCLUDE_DIR)
+
+mark_as_advanced(SymFPU_INCLUDE_DIR)
