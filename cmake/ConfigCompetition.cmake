7023f79262f8fbab78163f4dd5778bd2c62bc2c1
Aina Niemetz <aina.niemetz@gmail.com>
2018-08-14 13:22:15 -0700

    cmake: Add build configurations.

---
+++ cmake/ConfigCompetition.cmake
@@ -0,0 +1,18 @@
+set(CVC4_BUILD_PROFILE_COMPETITION 1)
+add_definitions(-DCVC4_COMPETITION_MODE)
+add_check_c_cxx_flag("-funroll-all-loops")
+add_check_c_cxx_flag("-fexpensive-optimizations")
+add_check_c_cxx_flag("-fno-enforce-eh-specs")
+# OPTLEVEL=9
+# enable_optimized=yes
+set(OPTIMIZATION_LEVEL, 9)
+# enable_debug_symbols=no
+# enable_statistics=no
+# enable_replay=no
+# enable_assertions=no
+# enable_proof=no
+# enable_tracing=no
+# enable_dumping=no
+# enable_muzzle=yes
+# enable_valgrind=no
+# enable_shared=no
---
+++ cmake/ConfigProduction.cmake
@@ -0,0 +1,16 @@
+set(CVC4_BUILD_PROFILE_PRODUCTION 1)
+# OPTLEVEL=3
+# enable_optimized=yes
+set(OPTIMIZATION_LEVEL, 3)
+# enable_debug_symbols=no
+# enable_statistics=yes
+set(ENABLE_STATISTICS ON)
+# enable_replay=no
+# enable_assertions=no
+# enable_proof=yes
+set(ENABLE_PROOFS, ON)
+# enable_tracing=no
+# enable_dumping=yes
+set(ENABLE_DUMPING ON)
+# enable_muzzle=no
+# enable_valgrind=no
