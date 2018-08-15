8ac674d87cb7c3007eb19e0a15691d3710ffb894
Aina Niemetz <aina.niemetz@gmail.com>
2018-08-15 14:00:33 -0700

    cmake: Added 3-valued option handling (to enable detection of user-set options).

--- cmake/ConfigCompetition.cmake
+++ cmake/ConfigCompetition.cmake
@@ -5,15 +5,25 @@ add_check_c_cxx_flag("-fexpensive-optimizations")
 add_check_c_cxx_flag("-fno-enforce-eh-specs")
 # OPTLEVEL=9
 # enable_optimized=yes
-set(OPTIMIZATION_LEVEL, 9)
+cvc4_set_option(ENABLE_OPTIMIZED ON)
+set(OPTIMIZATION_LEVEL 9)
 # enable_debug_symbols=no
+cvc4_set_option(ENABLE_DEBUG_SYMBOLS OFF)
 # enable_statistics=no
+cvc4_set_option(ENABLE_STATISTICS OFF)
 # enable_replay=no
+cvc4_set_option(ENABLE_REPLAY OFF)
 # enable_assertions=no
+cvc4_set_option(ENABLE_ASSERTIONS OFF)
 # enable_proof=no
+cvc4_set_option(ENABLE_PROOFS, OFF)
 # enable_tracing=no
+cvc4_set_option(ENABLE_TRACING OFF)
 # enable_dumping=no
+cvc4_set_option(ENABLE_DUMPING OFF)
 # enable_muzzle=yes
-set(ENABLE_MUZZLE ON)
+cvc4_set_option(ENABLE_MUZZLE ON)
 # enable_valgrind=no
+cvc4_set_option(ENABLE_VALGRIND OFF)
 # enable_shared=no
+set(BUILD_SHARED_LIBS OFF)
