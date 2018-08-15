1931abcccc9b1c7aff3dfbb6b09aaaa7548fba55
Aina Niemetz <aina.niemetz@gmail.com>
2018-08-15 14:00:33 -0700

    cmake: Added 3-valued option handling (to enable detection of user-set options).

--- cmake/ConfigDebug.cmake
+++ cmake/ConfigDebug.cmake
@@ -3,20 +3,23 @@ add_definitions(-DCVC4_DEBUG)
 set(CVC4_DEBUG 1)
 add_check_c_cxx_flag("-fno-inline")
 # enable_optimized=no
+cvc4_set_option(ENABLE_OPTIMIZED OFF)
+set(OPTIMIZATION_LEVEL 0)
 add_c_cxx_flag("-Og")
 # enable_debug_symbols=yes
-set(ENABLE_DEBUG_SYMBOLS ON)
+cvc4_set_option(ENABLE_DEBUG_SYMBOLS ON)
 # enable_statistics=yes
-set(ENABLE_STATISTICS ON)
+cvc4_set_option(ENABLE_STATISTICS ON)
 # enable_replay=yes
-set(ENABLE_REPLAY ON)
+cvc4_set_option(ENABLE_REPLAY ON)
 # enable_assertions=yes
-set(ENABLE_ASSERTIONS ON)
+cvc4_set_option(ENABLE_ASSERTIONS ON)
 # enable_proof=yes
-set(ENABLE_PROOFS, ON)
+cvc4_set_option(ENABLE_PROOFS ON)
 # enable_tracing=yes
-set(ENABLE_TRACING ON)
+cvc4_set_option(ENABLE_TRACING ON)
 # enable_dumping=yes
-set(ENABLE_DUMPING ON)
+cvc4_set_option(ENABLE_DUMPING ON)
 # enable_muzzle=no
+cvc4_set_option(ENABLE_MUZZLE OFF)
 # enable_valgrind=optional
