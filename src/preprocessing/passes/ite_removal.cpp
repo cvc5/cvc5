e6fd3c70f8651c6a9055fad8933caf2596b2b651
Andres Noetzli <andres.noetzli@gmail.com>
2018-08-16 18:57:24 -0700

    Refactor IteRemoval preprocessing pass (#1793)
    This commit refactors the IteRemoval pass to follow the new format.
    In addition to moving the code, this entails the following changes:
    
    - The timer for the ITE removal is now called differently (the default
      timer of PreprocessingPass is used) and measures just the
      preprocessing pass without applySubstitutions(). It also measures the
      time used by both invocations of the ITE removal pass.
    - Debug output will be slightly different because we now just rely on
      the default functionality of PreprocessingPass.
    - d_iteRemover is now passed into the PreprocessingPassContext.
    - AssertionPipeline now owns the d_iteSkolemMap, which makes it
      accessible by preprocessing passes. The idea behind this is that the
      preprocessing passes collect information in AssertionPipeline and
      d_iteSkolemMap fits that pattern.

---
+++ src/preprocessing/passes/ite_removal.cpp
@@ -0,0 +1,49 @@
+/*********************                                                        */
+/*! \file ite_removal.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Tim King, Paul Meng
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Remove ITEs from the assertions
+ **
+ ** [[ Add lengthier description here ]]
+ ** \todo document this file
+ **/
+
+#include "preprocessing/passes/ite_removal.h"
+#include "theory/rewriter.h"
+
+namespace CVC4 {
+namespace preprocessing {
+namespace passes {
+
+using namespace CVC4::theory;
+
+IteRemoval::IteRemoval(PreprocessingPassContext* preprocContext)
+    : PreprocessingPass(preprocContext, "ite-removal")
+{
+}
+
+PreprocessingPassResult IteRemoval::applyInternal(AssertionPipeline* assertions)
+{
+  d_preprocContext->spendResource(options::preprocessStep());
+
+  // Remove all of the ITE occurrences and normalize
+  d_preprocContext->getIteRemover()->run(
+      assertions->ref(), assertions->getIteSkolemMap(), true);
+  for (unsigned i = 0, size = assertions->size(); i < size; ++i)
+  {
+    assertions->replace(i, Rewriter::rewrite((*assertions)[i]));
+  }
+
+  return PreprocessingPassResult::NO_CONFLICT;
+}
+
+}  // namespace passes
+}  // namespace preprocessing
+}  // namespace CVC4
