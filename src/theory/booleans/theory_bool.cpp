c75ee1f48a30456ce5bd0b4e406fd47fff8014f0
ajreynol <andrew.j.reynolds@gmail.com>
2018-08-21 18:05:49 -0500

    Boolean equalities between variables

--- src/theory/booleans/theory_bool.cpp
+++ src/theory/booleans/theory_bool.cpp
@@ -40,12 +40,12 @@ Theory::PPAssertStatus TheoryBool::ppAssert(TNode in, SubstitutionMap& outSubsti
 
   // Add the substitution from the variable to its value
   if (in.getKind() == kind::NOT) {
-    if (in[0].getKind() == kind::VARIABLE) {
+    if (in[0].isVar()) {
       outSubstitutions.addSubstitution(in[0], NodeManager::currentNM()->mkConst<bool>(false));
       return PP_ASSERT_STATUS_SOLVED;
     }
   } else {
-    if (in.getKind() == kind::VARIABLE) {
+    if (in.isVar()) {
       outSubstitutions.addSubstitution(in, NodeManager::currentNM()->mkConst<bool>(true));
       return PP_ASSERT_STATUS_SOLVED;
     }
