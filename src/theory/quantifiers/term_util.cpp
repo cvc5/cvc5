5d8d625ebd5c54520fbef3c09034914a5ef37e2e
ajreynol <andrew.j.reynolds@gmail.com>
2018-08-23 15:59:16 -0500

    Working on relevancy.

--- src/theory/quantifiers/term_util.cpp
+++ src/theory/quantifiers/term_util.cpp
@@ -1043,7 +1043,7 @@ bool TermUtil::isIdempotentArg(Node n, Kind ik, int arg)
   return false;
 }
 
-Node TermUtil::isSingularArg(Node n, Kind ik, int arg)
+Node TermUtil::isSingularArg(Node n, Kind ik, unsigned arg)
 {
   TypeNode tn = n.getType();
   if (n == getTypeValue(tn, 0))
@@ -1081,10 +1081,6 @@ Node TermUtil::isSingularArg(Node n, Kind ik, int arg)
       if (arg == 0)
       {
         return n;
-      }
-      else
-      {
-        // TODO?
       }
     }
     else if (ik == STRING_SUBSTR)
--- src/theory/subs_minimize.cpp
+++ src/theory/subs_minimize.cpp
@@ -100,7 +100,127 @@ bool SubstitutionMinimize::find(Node n,
   } while (!visit.empty());
   Assert(value.find(n) != value.end());
   Assert(!value.find(n)->second.isNull());
+  
+  if(n!=target)
+  {
+    return false;
+  }
+  
+  // only variables that occur in assertions are relevant
+  std::map< Node, bool > rlvVar;
+  
+  visit.push_back(n);
+  std::unordered_set<TNode, TNodeHashFunction> visited;
+  do {
+    cur = visit.back();
+    visit.pop_back();
+    if (visited.find(cur) == visited.end()) {
+      visited.insert(cur);
+      if( cur.isVar() )
+      {
+        if( value[cur]!=cur )
+        {
+          rlvVar[cur] = true;
+        }
+      }
+      else if( cur.getKind()==ITE )
+      {
+        // only recurse on relevant branch
+        Node bval = value[cur[0]];
+        Assert( !bval.isNull() && bval.isConst() );
+        unsigned cindex = bval.getConst<bool>() ? 1 : 2;
+        visit.push_back(cur[0]);
+        visit.push_back(cur[cindex]);
+      }
+      else if( cur.getNumChildren()>0 )
+      {
+        // if the operator is a variable, expand first
+      
+        // see if there are any singular arguments
+        Kind ck = cur.getKind();
+        std::vector< unsigned > singularArg;
+        if( cur.getNumChildren()>1 )
+        {
+          for (unsigned i=0, size=cur.getNumChildren(); i<size; i++ )
+          {
+            Node cn = cur[i];
+            it = value.find(cn);
+            Assert(it != value.end());
+            Assert(!it->second.isNull());
+            if (isSingularArg(cn,ck,i) )
+            {
+              singularArg.push_back(i);
+            }
+          }
+        }
+        // we need to recurse on at most one child
+        if( !singularArg.empty() )
+        {
+          
+        }
+        else
+        {
+          // recurse on all arguments
+        }
+      }
+    }
+  } while (!visit.empty());
+  
+  
+  return true;
+}
 
+
+bool SubstitutionMinimize::isSingularArg(Node n, Kind k, unsigned arg)
+{
+  Assert( n.isConst() );
+  if( k==AND )
+  {
+    return !n.getConst<bool>();
+  }
+  else if( k==OR )
+  {
+    return n.getConst<bool>();
+  }
+  else if( k==IMPLIES )
+  {
+    return arg==(n.getConst<bool>() ? 1 : 0);
+  }
+  if( k==MULT || ( arg==0 && ( k == DIVISION_TOTAL || k == INTS_DIVISION_TOTAL || k==INTS_MODULUS_TOTAL ) ) )
+  {
+    // zero
+    if( n.getConst<Rational>().sgn()==0 )
+    {
+      return true;
+    }
+  }
+  if (k == BITVECTOR_AND || k == BITVECTOR_MULT 
+             || k == BITVECTOR_UDIV_TOTAL
+             || k == BITVECTOR_UREM_TOTAL ||
+     ( arg==0 && (
+      k == BITVECTOR_SHL || k == BITVECTOR_LSHR || k == BITVECTOR_ASHR)))
+  {
+    // bit-vector zero
+    //Node cmpz = bv::utils::mkZero(
+  }
+  if( k==BITVECTOR_OR )
+  {
+    // bit-vector ones
+  }
+  
+  if( ( arg==1 && k==STRING_STRCTN ) || ( arg==0 && k==STRING_SUBSTR ) )
+  {
+    // empty string 
+    
+  }
+  if( ( arg!=0 && k==STRING_SUBSTR ) || ( arg==2 && k==STRING_STRIDOF ) )
+  {
+    // negative integer
+    if( n.getConst<Rational>().sgn() < 0 )
+    {
+      return true;
+    }
+  }
   return false;
 }
 
