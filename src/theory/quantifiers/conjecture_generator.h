9a569df90942b047c081a99af3503f3ab78f10ba
ajreynol <andrew.j.reynolds@gmail.com>
2018-08-24 11:07:18 -0500

    Minor

--- src/theory/quantifiers/conjecture_generator.h
+++ src/theory/quantifiers/conjecture_generator.h
@@ -70,9 +70,9 @@ class TermGenEnv;
 
 class TermGenerator
 {
-private:
+ private:
   unsigned calculateGeneralizationDepth( TermGenEnv * s, std::map< TypeNode, std::vector< int > >& fvs );
-public:
+ public:
   TermGenerator() : d_id(0), d_status(0), d_status_num(0), d_match_status(0), d_match_status_child_num(0), d_match_node(0){}
   TypeNode d_typ;
   unsigned d_id;
