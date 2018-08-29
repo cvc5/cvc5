a4298271b17c42af3121279f97692c9de7a5f5a6
Mathias Preiner <mathias.preiner@gmail.com>
2018-08-29 08:20:31 -0700

    cmake: Add options for specifying install directories for dependencies.

--- cmake/FindABC.cmake
+++ cmake/FindABC.cmake
@@ -4,17 +4,26 @@
 # ABC_LIBRARIES - Libraries needed to use ABC
 # ABC_ARCH_FLAGS - Platform specific compile flags
 
-set(ABC_DEFAULT_HOME "${PROJECT_SOURCE_DIR}/abc/alanmi-abc-53f39c11b58d")
 
+# Check default location of ABC built with contrib/get-abc.
+if(NOT ABC_HOME)
+  set(ABC_HOME ${PROJECT_SOURCE_DIR}/abc/alanmi-abc-53f39c11b58d)
+endif()
+
+# Note: We don't check the system version since ABC does not provide a default
+# install rule.
 find_path(ABC_INCLUDE_DIR
           NAMES base/abc/abc.h
-          PATHS ${ABC_DEFAULT_HOME}/src)
+          PATHS ${ABC_HOME}/src
+          NO_DEFAULT_PATH)
 find_library(ABC_LIBRARIES
              NAMES abc
-             PATHS ${ABC_DEFAULT_HOME})
+             PATHS ${ABC_HOME}
+             NO_DEFAULT_PATH)
 find_program(ABC_ARCH_FLAGS_PROG
              NAMES arch_flags
-             PATHS ${ABC_DEFAULT_HOME})
+             PATHS ${ABC_HOME}
+             NO_DEFAULT_PATH)
 
 if(ABC_ARCH_FLAGS_PROG)
   execute_process(COMMAND ${ABC_ARCH_FLAGS_PROG}
--- cmake/FindANTLR.cmake
+++ cmake/FindANTLR.cmake
@@ -4,20 +4,33 @@
 # ANTLR_INCLUDE_DIR - the ANTLR include directory
 # ANTLR_LIBRARIES - Libraries needed to use ANTLR
 
+
+# Check default location of ANTLR built with contrib/get-antlr-3.4.
+# If the user provides a directory we will not search the default paths and
+# fail if ANTLR was not found in the specified directory.
+if(NOT ANTLR_HOME)
+  set(ANTLR_HOME ${PROJECT_SOURCE_DIR}/antlr-3.4)
+  set(CHECK_SYSTEM_VERSION TRUE)
+endif()
+
 find_program(ANTLR_BINARY
   NAMES antlr3
-  PATHS "${PROJECT_SOURCE_DIR}/antlr-3.4/bin"
-  )
-
+  PATHS ${ANTLR_HOME}/bin
+  NO_DEFAULT_PATH)
 find_path(ANTLR_INCLUDE_DIR
   NAMES antlr3.h
-  PATHS "${PROJECT_SOURCE_DIR}/antlr-3.4/include"
-  )
-
+  PATHS ${ANTLR_HOME}/include
+  NO_DEFAULT_PATH)
 find_library(ANTLR_LIBRARIES
-  NAMES antlr3c libantlr3c
-  PATHS "${PROJECT_SOURCE_DIR}/antlr-3.4/lib"
-  )
+  NAMES antlr3c antlr3c-static
+  PATHS ${ANTLR_HOME}/lib
+  NO_DEFAULT_PATH)
+
+if(CHECK_SYSTEM_VERSION)
+  find_program(ANTLR_BINARY NAMES antlr3)
+  find_path(ANTLR_INCLUDE_DIR NAMES antlr3.h)
+  find_library(ANTLR_LIBRARIES NAMES antlr3c)
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
