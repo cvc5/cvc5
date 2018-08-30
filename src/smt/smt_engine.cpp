f4901471839201bc0c7a630e3e2e7df9035ea9a2
ajreynol <andrew.j.reynolds@gmail.com>
2018-08-30 09:06:21 -0500

    Format

--- src/smt/smt_engine.cpp
+++ src/smt/smt_engine.cpp
@@ -522,7 +522,7 @@ class SmtEnginePrivate : public NodeManagerListener {
    */
   SubstitutionMap d_abstractValueMap;
 
-  /** 
+  /**
    * The (user-context-dependent) set of symbols that occur in at least one
    * assertion in the current user context. This is used by the
    * nonClausalSimplify pass.
@@ -570,8 +570,8 @@ class SmtEnginePrivate : public NodeManagerListener {
    * Returns false if the formula simplifies to "false"
    */
   bool nonClausalSimplify();
-  
-  /** record symbols in assertions 
+
+  /** record symbols in assertions
    *
    * This method is called when a set of assertions is finalized. It adds
    * the symbols to d_symsInAssertions that occur in assertions.
@@ -3874,7 +3874,8 @@ void SmtEnginePrivate::collectSkolems(TNode n, set<TNode>& skolemSet, unordered_
   cache[n] = true;
 }
 
-void SmtEnginePrivate::recordSymbolsInAssertions(const std::vector<Node>& assertions)
+void SmtEnginePrivate::recordSymbolsInAssertions(
+    const std::vector<Node>& assertions)
 {
   std::unordered_set<TNode, TNodeHashFunction> visited;
   std::unordered_set<Node, NodeHashFunction> syms;
