e8d80f43336c5943f7063c49609cad3b2a0e516c
ajreynol <andrew.j.reynolds@gmail.com>
2018-08-30 10:00:10 -0500

    Minor

--- src/smt/model_core_builder.h
+++ src/smt/model_core_builder.h
@@ -2,7 +2,7 @@
 /*! \file model_core_builder.h
  ** \verbatim
  ** Top contributors (to current version):
- **   Andrew Reynolds, Mathias Preiner
+ **   Andrew Reynolds
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
