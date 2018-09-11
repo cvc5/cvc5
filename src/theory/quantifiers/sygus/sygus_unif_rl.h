5af505f65c39a9c65d820505525b862949a0e684
Haniel Babosa <hanielbbarbosa@gmail.com>
2018-09-11 01:11:22 -0500

    clean

--- src/theory/quantifiers/sygus/sygus_unif_rl.h
+++ src/theory/quantifiers/sygus/sygus_unif_rl.h
@@ -274,28 +274,6 @@ class SygusUnifRl : public SygusUnif
      * decision tree.
      */
     Node d_cond_enum;
-    /** size to which explanation should be backtracked to
-     *
-     * When the condition pool is used and we still fail to produce a solution,
-     * the separation lemma should reflect the failure independent of the
-     * repairs from the condition pool. To do this we discard everything after
-     * the first conflict that could not have been resolved with an original
-     * currently enumerated condition.
-     *
-     * the initial value is set to -1, meaning no size has been set yet
-     */
-    int d_exp_backtrack_size;
-    bool pickCondition(unsigned c_counter,
-                       Node e1,
-                       Node e2,
-                       std::vector<Node>& exp);
-    /** try separating e1 from e2 with a value from condition pool
-     *
-     * if it succeeds it changes the saved value in d_conds to the respective
-     * index
-     */
-    bool pickConditionFromPool(unsigned c_counter, Node e1, Node e2);
-
     /** Classifies evaluation points according to enumerated condition values
      *
      * Maintains the invariant that points evaluated in the same way in the
@@ -338,8 +316,6 @@ class SygusUnifRl : public SygusUnif
                         int ind);
       Node computeCond(Node cond, Node hd);
     };
-    /** repair condition to separate */
-    Node repairConditionToSeparate(Node cv, Node e1, Node e2);
     /**
      * Utility for determining how evaluation points are separated by currently
      * enumerated condiotion values
