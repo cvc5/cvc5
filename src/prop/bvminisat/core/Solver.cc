b2651647f815fe1f0d9a467b4e397705f95cf0e5
Mathias Preiner <mathias.preiner@gmail.com>
2018-09-13 14:45:54 -0700

    Fix #include for minisat headers in bvminisat.

--- src/prop/bvminisat/core/Solver.cc
+++ src/prop/bvminisat/core/Solver.cc
@@ -18,7 +18,7 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **************************************************************************************************/
 
-#include "core/Solver.h"
+#include "prop/bvminisat/core/Solver.h"
 
 #include <math.h>
 
@@ -27,7 +27,6 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 
 #include "base/exception.h"
 #include "base/output.h"
-#include "mtl/Sort.h"
 #include "options/bv_options.h"
 #include "options/smt_options.h"
 #include "proof/bitvector_proof.h"
@@ -35,8 +34,9 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include "proof/proof_manager.h"
 #include "proof/sat_proof.h"
 #include "proof/sat_proof_implementation.h"
-#include "theory/interrupted.h"
+#include "prop/bvminisat/mtl/Sort.h"
 #include "util/utility.h"
+#include "theory/interrupted.h"
 
 namespace CVC4 {
 namespace BVMinisat {
