fec2bdddf9b77e3bfc9c6b0be8376c24f10fefa3
Haniel Babosa <hanielbbarbosa@gmail.com>
2018-09-24 21:40:01 -0500

    better debug tracing

--- src/theory/quantifiers/sygus/enum_stream_concrete.cpp
+++ src/theory/quantifiers/sygus/enum_stream_concrete.cpp
@@ -229,6 +229,10 @@ Node StreamCombination::getNext()
     {
       Trace("synth-stream-concrete")
           << " ..no new comb, get next permutation\n";
+#ifdef CVC4_ASSERTIONS
+      Trace("synth-stream-concrete")
+          << " ....total combs until here : " << d_comb_values.size() << "\n";
+#endif
       d_last = d_stream_permutations.getNext();
       // exhausted permutations
       if (d_last.isNull())
