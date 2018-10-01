48ea68aa581d492c48fe9e08b54e9ce26f3508b9
Andres Noetzli <andres.noetzli@gmail.com>
2018-10-01 10:06:38 -0700

    Refactor preprocessing pass registration (#2468)
    This commit refactors how preprocessing pass registration works,
    inspired by LLVM's approach [0]. The basic idea is that every
    preprocessing pass declares a static variable of type `RegisterPass` in
    its source file that registers the pass with the
    `PreprocessingPassRegistry` when starting the program. The registry is a
    singleton that keeps track of all the available passes and allows other
    code to create instances of the passes (note: previously the registry
    itself was owning the passes but this is no longer the case). One of the
    advantages of this solution is that we have a list of available passes
    directly at the beginning of the program, which is useful for example
    when parsing options.
    
    As a side effect, this commit also fixes the SortInference pass, which
    was expecting arguments other than the preprocessing pass context in its
    constructor.
    
    This commit is required for fixing dumping pre/post preprocessing
    passes. It is also the ground work for allowing the user to specify a
    preprocessing pipeline using command-line arguments.
    
    [0] https://llvm.org/docs/WritingAnLLVMPass.html

--- src/preprocessing/passes/quantifier_macros.cpp
+++ src/preprocessing/passes/quantifier_macros.cpp
@@ -20,6 +20,7 @@
 
 #include "options/quantifiers_modes.h"
 #include "options/quantifiers_options.h"
+#include "preprocessing/preprocessing_pass_registry.h"
 #include "proof/proof_manager.h"
 #include "smt/smt_engine.h"
 #include "smt/smt_engine_scope.h"
@@ -544,6 +545,8 @@ void QuantifierMacros::addMacro( Node op, Node n, std::vector< Node >& opc ) {
     }
   }
 }
+
+static RegisterPass<QuantifierMacros> X("quantifier-macros");
 
 }  // passes
 }  // preprocessing
