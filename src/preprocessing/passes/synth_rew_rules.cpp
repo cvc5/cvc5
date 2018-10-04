bd275bab2dfcba5edb85634fdbe87a4631448683
ajreynol <andrew.j.reynolds@gmail.com>
2018-10-04 12:28:20 -0500

    Sketch

--- src/preprocessing/passes/synth_rew_rules.cpp
+++ src/preprocessing/passes/synth_rew_rules.cpp
@@ -47,9 +47,7 @@ PreprocessingPassResult SynthRewRulesPass::applyInternal(
                          << std::endl;
   std::vector<Node>& assertions = assertionsToPreprocess->ref();
 
-  // compute the variables we will be sampling
-  std::vector<Node> vars;
-  unsigned nsamples = options::sygusSamples();
+  NodeManager * nm = NodeManager::currentNM();
 
   Options& nodeManagerOptions = NodeManager::currentNM()->getOptions();
 
@@ -57,103 +55,166 @@ PreprocessingPassResult SynthRewRulesPass::applyInternal(
   SynthRrComputedAttribute srrca;
 
   // initialize the candidate rewrite
-  std::unique_ptr<theory::quantifiers::CandidateRewriteDatabaseGen> crdg;
   std::unordered_map<TNode, bool, TNodeHashFunction> visited;
   std::unordered_map<TNode, bool, TNodeHashFunction>::iterator it;
   std::vector<TNode> visit;
-  // two passes: the first collects the variables, the second registers the
-  // terms
-  for (unsigned r = 0; r < 2; r++)
+  // Get all usable terms from the input. A term is usable if it does not
+  // contain a quantified subterm
+  std::vector<Node> terms;
+  TNode cur;
+  for (const Node& a : assertions)
   {
-    visited.clear();
-    visit.clear();
-    TNode cur;
-    for (const Node& a : assertions)
+    visit.push_back(a);
+    do
     {
-      visit.push_back(a);
-      do
+      cur = visit.back();
+      visit.pop_back();
+      it = visited.find(cur);
+      // if already processed, ignore
+      if (cur.getAttribute(SynthRrComputedAttribute()))
       {
-        cur = visit.back();
-        visit.pop_back();
-        it = visited.find(cur);
-        // if already processed, ignore
-        if (cur.getAttribute(SynthRrComputedAttribute()))
+        Trace("synth-rr-pass-debug")
+            << "...already processed " << cur << std::endl;
+      }
+      else if (it == visited.end())
+      {
+        Trace("synth-rr-pass-debug") << "...preprocess " << cur << std::endl;
+        visited[cur] = false;
+        Kind k = cur.getKind();
+        bool isQuant = k == kind::FORALL || k == kind::EXISTS
+                        || k == kind::LAMBDA || k == kind::CHOICE;
+        // we recurse on this node if it is not a quantified formula
+        if (!isQuant)
         {
-          Trace("synth-rr-pass-debug")
-              << "...already processed " << cur << std::endl;
-        }
-        else if (it == visited.end())
-        {
-          Trace("synth-rr-pass-debug") << "...preprocess " << cur << std::endl;
-          visited[cur] = false;
-          Kind k = cur.getKind();
-          bool isQuant = k == kind::FORALL || k == kind::EXISTS
-                         || k == kind::LAMBDA || k == kind::CHOICE;
-          // we recurse on this node if it is not a quantified formula
-          if (!isQuant)
-          {
-            visit.push_back(cur);
-            for (const Node& cc : cur)
-            {
-              visit.push_back(cc);
-            }
-          }
-        }
-        else if (!it->second)
-        {
-          Trace("synth-rr-pass-debug") << "...postprocess " << cur << std::endl;
-          // check if all of the children are valid
-          // this ensures we do not register terms that have e.g. quantified
-          // formulas as subterms
-          bool childrenValid = true;
+          visit.push_back(cur);
           for (const Node& cc : cur)
           {
-            Assert(visited.find(cc) != visited.end());
-            if (!visited[cc])
-            {
-              childrenValid = false;
-            }
+            visit.push_back(cc);
           }
-          if (childrenValid)
-          {
-            Trace("synth-rr-pass-debug")
-                << "...children are valid, check rewrites..." << std::endl;
-            if (r == 0)
-            {
-              if (cur.isVar())
-              {
-                vars.push_back(cur);
-              }
-            }
-            else
-            {
-              Trace("synth-rr-pass-debug") << "Add term " << cur << std::endl;
-              // mark as processed
-              cur.setAttribute(srrca, true);
-              bool ret = crdg->addTerm(cur, *nodeManagerOptions.getOut());
-              Trace("synth-rr-pass-debug") << "...return " << ret << std::endl;
-              // if we want only rewrites of minimal size terms, we would set
-              // childrenValid to false if ret is false here.
-            }
-          }
-          visited[cur] = childrenValid;
         }
-      } while (!visit.empty());
-    }
-    if (r == 0)
+      }
+      else if (!it->second)
+      {
+        Trace("synth-rr-pass-debug") << "...postprocess " << cur << std::endl;
+        // check if all of the children are valid
+        // this ensures we do not register terms that have e.g. quantified
+        // formulas as subterms
+        bool childrenValid = true;
+        for (const Node& cc : cur)
+        {
+          Assert(visited.find(cc) != visited.end());
+          if (!visited[cc])
+          {
+            childrenValid = false;
+          }
+        }
+        if (childrenValid)
+        {
+          Trace("synth-rr-pass-debug") << "...children are valid" << std::endl;
+          Trace("synth-rr-pass-debug") << "Add term " << cur << std::endl;
+          terms.push_back(cur);
+          // mark as processed
+          cur.setAttribute(srrca, true);
+        }
+        visited[cur] = childrenValid;
+      }
+    } while (!visit.empty());
+  }
+  // We've collected all terms in the input. We will produce skeletons from
+  // these terms. We start by constructing a fixed number of variables per
+  // type.
+  unsigned nvars = 3;
+  std::map< TypeNode, std::vector< Node > > tvars;
+  std::vector< Node > allVars;
+  for( const Node& n : terms )
+  {
+    TypeNode tn = n.getType();
+    if( tvars.find(tn)==tvars.end() )
     {
-      Trace("synth-rr-pass-debug")
-          << "Initialize with " << nsamples
-          << " samples and variables : " << vars << std::endl;
-      crdg = std::unique_ptr<theory::quantifiers::CandidateRewriteDatabaseGen>(
-          new theory::quantifiers::CandidateRewriteDatabaseGen(vars, nsamples));
+      for( unsigned i=0; i<nvars; i++ )
+      {
+        Node v = nm->mkBoundVar(tn);
+        tvars[tn].push_back(v);
+        allVars.push_back(v);
+      }
     }
   }
+  Trace("synth-rr-pass") << "Initialize the candidate rewrite database generator..." << std::endl;
+  unsigned nsamples = options::sygusSamples();
+  std::unique_ptr<theory::quantifiers::CandidateRewriteDatabaseGen> crdg;
+  crdg.reset(new theory::quantifiers::CandidateRewriteDatabaseGen(allVars, nsamples));
 
+  
+  // now, we increment skeleton sizes until a fixed point is reached
+  bool success = false;
+  unsigned skSize = 0;
+  std::vector< Node > termsNext;
+  do
+  {
+    success = false;
+    std::unordered_set< Node, NodeHashFunction > cacheGenTerms;
+    if( skSize==0 )
+    {
+      // optimization: just take the variables directly
+      for( const Node& v : allVars )
+      {
+        if( crdg->addTerm(v, *nodeManagerOptions.getOut()) )
+        {
+          success = true;
+        }
+      }
+    }
+    else
+    {
+      for( const Node& t : terms )
+      {
+        std::unordered_set< Node, NodeHashFunction > genTerms;
+        if( getTermSkeletons(t,skSize,tvars,cacheGenTerms,genTerms) )
+        {
+          // Only if term skeletons were possible to generate do we keep t
+          // for the next round. This ensures we do not keep retrying to
+          // generate skeletons from terms whose size is less than skSize.
+          termsNext.push_back(t);
+        }
+        for( const Node& cgt : genTerms )
+        {
+          if( crdg->addTerm(cgt, *nodeManagerOptions.getOut()) )
+          {
+            success = true;
+          }
+          cacheGenTerms.insert(cgt);
+        }
+      }
+    }    
+    if( success )
+    {
+      skSize++;
+      terms.clear();
+      terms.insert(terms.end(),termsNext.begin(),termsNext.end());
+      termsNext.clear();
+    }
+  }while( success );
+  
+
+  /*
+  bool ret = crdg->addTerm(cur, *nodeManagerOptions.getOut());
+  Trace("synth-rr-pass-debug") << "...return " << ret << std::endl;
+  // if we want only rewrites of minimal size terms, we would set
+  // childrenValid to false if ret is false here.
+  */
+  
+  
   Trace("synth-rr-pass") << "...finished " << std::endl;
   return PreprocessingPassResult::NO_CONFLICT;
 }
 
+bool SynthRewRulesPass::getTermSkeletons( Node t, unsigned tsize, const std::map< TypeNode, std::vector< Node > >& tvars, 
+      const std::unordered_set< Node, NodeHashFunction >& cacheGenTerms,
+      std::unordered_set< Node, NodeHashFunction >& genTerms
+      )
+{
+  return false;
+}
 
 }  // namespace passes
 }  // namespace preprocessing
--- src/preprocessing/passes/synth_rew_rules.h
+++ src/preprocessing/passes/synth_rew_rules.h
@@ -39,6 +39,13 @@ class SynthRewRulesPass : public PreprocessingPass
  protected:
   PreprocessingPassResult applyInternal(
       AssertionPipeline* assertionsToPreprocess) override;
+  /** get term skeletons 
+   * 
+   */
+  bool getTermSkeletons( Node t, unsigned tsize, const std::map< TypeNode, std::vector< Node > >& tvars, 
+        const std::unordered_set< Node, NodeHashFunction >& cacheGenTerms,
+        std::unordered_set< Node, NodeHashFunction >& genTerms
+        );
 };
 
 }  // namespace passes
