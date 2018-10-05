653a8c1302df6a09c9b1522aa629862c0273a133
ajreynol <andrew.j.reynolds@gmail.com>
2018-10-04 23:18:36 -0500

    More

--- src/theory/quantifiers/candidate_rewrite_database.cpp
+++ src/theory/quantifiers/candidate_rewrite_database.cpp
@@ -64,7 +64,7 @@ void CandidateRewriteDatabase::initializeSygus(const std::vector<Node>& vars,
   d_qe = qe;
   d_tds = d_qe->getTermDatabaseSygus();
   d_ext_rewrite = nullptr;
-  d_crewrite_filter.initialize(ss, d_tds, false);
+  d_crewrite_filter.initialize(ss, d_tds, d_using_sygus);
   ExprMiner::initialize(vars, ss);
 }
 
