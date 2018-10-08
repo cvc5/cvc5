54899234c3bdf59781a7c8d0deaee458a0697017
ajreynol <andrew.j.reynolds@gmail.com>
2018-10-08 09:06:33 -0500

    Document

--- src/preprocessing/passes/synth_rew_rules.h
+++ src/preprocessing/passes/synth_rew_rules.h
@@ -24,12 +24,27 @@ namespace preprocessing {
 namespace passes {
 
 /**
- * This class computes candidate rewrite rules of the form t1 = t2, where
- * t1 and t2 are subterms of assertionsToPreprocess. It prints
- * "candidate-rewrite" messages on the output stream of options.
- *
- * In contrast to other preprocessing passes, this pass does not modify
- * the set of assertions.
+ * This class rewrites the input assertions into a sygus conjecture over a
+ * grammar whose terms are "abstractions" of the subterms of
+ * assertionsToPreprocess. In detail, assume our input was 
+ *    bvadd( bvshlr( bvadd( a, 4 ), 1 ), b ) = 1
+ * This class constructs this grammar:
+ *    A -> T1 | T2 | T3 | T4 | Tv
+ *    T1 -> bvadd( T2, Tv ) | x | y
+ *    T2 -> bvshlr( T3, T4 ) | x | y
+ *    T3 -> bvadd( Tv, T5 ) | x | y
+ *    T4 -> 1 | x | y
+ *    T5 -> 4 | x | y
+ *    Tv -> x | y
+ * Notice that this grammar generates all subterms of the input where leaves
+ * are replaced by the variables x and/or y (the number of variables allocated
+ * by this class is configurable via sygus-rr-synth-input-nvars).
+ * 
+ * It then rewrites the input into the negated sygus conjecture
+ *   forall x : ( BV_n x BV_n ) -> BV_n. false
+ * where x has the sygus grammar restriction A from above. This conjecture can
+ * then be processed using --sygus-rr-synth in the standard way, which will
+ * cause candidate rewrites to be printed on the output stream.
  */
 class SynthRewRulesPass : public PreprocessingPass
 {
