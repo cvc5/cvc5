e77a66013061cb71898b1f3d7e62710b97aff946
ajreynol <andrew.j.reynolds@gmail.com>
2018-10-15 10:28:02 -0500

    One more

--- test/unit/theory/theory_white.h
+++ test/unit/theory/theory_white.h
@@ -166,7 +166,9 @@ class TheoryBlack : public CxxTest::TestSuite {
     d_logicInfo = new LogicInfo();
     d_logicInfo->lock();
 
-    // must initialize here
+    // Notice that this unit test uses the theory engine of a created SMT
+    // engine d_smt. We must ensure that d_smt is properly initialized via
+    // the following call, which constructs its underlying theory engine.
     d_smt->finalOptionsAreSet();
     // guard against duplicate statistics assertion errors
     delete d_smt->d_theoryEngine->d_theoryTable[THEORY_BUILTIN];
