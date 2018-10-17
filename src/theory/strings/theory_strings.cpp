1823d6d537a59d85a17f09f53c8128d934c420a3
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2018-10-17 11:14:49 -0500

    Fix context-dependent for positive contains reduction (#2644)

--- src/theory/strings/theory_strings.cpp
+++ src/theory/strings/theory_strings.cpp
@@ -493,6 +493,10 @@ bool TheoryStrings::doReduction(int effort, Node n, bool& isCd)
     Trace("strings-extf-debug")
         << "  resolve extf : " << n << " based on positive contain reduction."
         << std::endl;
+    Trace("strings-red-lemma") << "Reduction (positive contains) lemma : " << n
+                               << " => " << eq << std::endl;
+    // context-dependent because it depends on the polarity of n itself
+    isCd = true;
   }
   else if (k != kind::STRING_CODE)
   {
@@ -513,8 +517,8 @@ bool TheoryStrings::doReduction(int effort, Node n, bool& isCd)
     sendInference(d_empty_vec, nnlem, "Reduction", true);
     Trace("strings-extf-debug")
         << "  resolve extf : " << n << " based on reduction." << std::endl;
+    isCd = false;
   }
-  isCd = false;
   return true;
 }
 
@@ -1001,6 +1005,7 @@ void TheoryStrings::checkExtfReductions( int effort ) {
   Trace("strings-process") << "  checking " << extf.size() << " active extf"
                            << std::endl;
   for( unsigned i=0; i<extf.size(); i++ ){
+    Assert(!d_conflict);
     Node n = extf[i];
     Trace("strings-process") << "  check " << n << ", active in model="
                              << d_extf_info_tmp[n].d_model_active << std::endl;
