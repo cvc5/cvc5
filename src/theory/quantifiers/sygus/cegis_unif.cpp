a545134bd04eaf7a6586f09ccb59a297ff49723c
ajreynol <andrew.j.reynolds@gmail.com>
2018-10-24 16:42:58 -0500

    Revert

--- src/theory/quantifiers/sygus/cegis_unif.cpp
+++ src/theory/quantifiers/sygus/cegis_unif.cpp
@@ -614,11 +614,7 @@ void CegisUnifEnumDecisionStrategy::setUpEnumerator(Node e,
   if (options::sygusUnifCondIndependent() && index == 1)
   {
     mkActiveGuard = true;
-    // it is only compatible with variable agnostic enumeration, since the
-    // other kinds of enumeration (basic, enum) may be non-terminating for a
-    // given abstract value.
-    isActiveGen =
-        options::sygusActiveGenMode() == SYGUS_ACTIVE_GEN_VAR_AGNOSTIC;
+    isActiveGen = options::sygusActiveGenMode() != SYGUS_ACTIVE_GEN_NONE;
   }
   Trace("cegis-unif-enum") << "* Registering new enumerator " << e
                            << " to strategy point " << si.d_pt << "\n";
