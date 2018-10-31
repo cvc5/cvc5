691a5d2de7e231027dda6d57f3fc9ba277f5073f
anwu1219 <haozewu@stanford.edu>
2018-10-31 14:44:54 -0700

    revert

--- src/options/decision_weight.h
+++ src/options/decision_weight.h
@@ -1,18 +1,18 @@
 /*********************                                                        */
 /*! \file decision_weight.h
-** \verbatim
-** Top contributors (to current version):
-**   Kshitij Bansal, Tim King, Morgan Deters
-** This file is part of the CVC4 project.
-** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
-** in the top-level source directory) and their institutional affiliations.
-** All rights reserved.  See the file COPYING in the top-level source
-** directory for licensing information.\endverbatim
-**
-** \brief Rewriter attributes
-**
-** Rewriter attributes.
-**/
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Kshitij Bansal, Tim King, Morgan Deters
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Rewriter attributes
+ **
+ ** Rewriter attributes.
+ **/
 
 #include "cvc4_private.h"
 
@@ -24,7 +24,7 @@ namespace decision {
 
 typedef uint64_t DecisionWeight;
 
-}  // namespace decision
+}/* CVC4::decision namespace */
 }/* CVC4 namespace */
 
 #endif /* __CVC4__OPTIONS__DECISION_WEIGHT_H */
