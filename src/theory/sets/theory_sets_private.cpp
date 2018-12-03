b6e8c134dcde9c3a7fc6fadd90206823c231a47e
ajreynol <andrew.j.reynolds@gmail.com>
2018-12-03 16:46:14 -0600

    Fix type

--- src/theory/sets/theory_sets_private.cpp
+++ src/theory/sets/theory_sets_private.cpp
@@ -1543,7 +1543,7 @@ void TheorySetsPrivate::checkMinCard( std::vector< Node >& lemmas ) {
 
   for( int i=(int)(d_set_eqc.size()-1); i>=0; i-- ){
     Node eqc = d_set_eqc[i];
-    TypeNode tn = eqc.getType();
+    TypeNode tn = eqc.getType().getSetElementType();
     if (d_t_card_enabled.find(tn) == d_t_card_enabled.end())
     {
       // cardinality is not enabled for this type, skip
