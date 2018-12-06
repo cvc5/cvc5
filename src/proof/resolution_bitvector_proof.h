cvc5/cvc5

commit aa0a875dfd40bd9dfa810238327db51498b74677

Bit vector proof superclass (#2599)
* Split BitvectorProof into a sub/superclass

The superclass contains general printing knowledge.

The subclass contains CNF or Resolution-specific knowledge.

* Renames & code moves

* Nits cleaned in prep for PR

* Moved CNF-proof from ResolutionBitVectorProof to BitVectorProof

Since DRAT BV proofs will also contain a CNF-proof, the CNF proof should
be stored in `BitVectorProof`.

* Unique pointers, comments, and code movement.

Adjusted the distribution of code between BVP and RBVP.
  Notably, put the CNF proof in BVP because it isn't
  resolution-specific.
Added comments to the headers of both files -- mostly BVP.
Changed two owned pointers into unique_ptr.
  BVP's pointer to a CNF proof
  RBVP's pointer to a resolution proof

BVP: `BitVectorProof`
RBVP: `ResolutionBitVectorProof`

* clang-format

* Undo manual copyright modification

* s/superclass/base class/

Co-Authored-By: alex-ozdemir <aozdemir@hmc.edu>

* make LFSCBitVectorProof::printOwnedSort public

* Andres's Comments

Mostly cleaning up (or trying to clean up) includes.

* Cleaned up one header cycle

However, this only allowed me to move the forward-decl, not eliminate
it, because there were actually two underlying include cycles that the
forward-decl solved.

* Added single _s to header gaurds

* Fix Class name in debug output

Credits to Andres

Co-Authored-By: alex-ozdemir <aozdemir@hmc.edu>

* Reordered methods in BitVectorProof per original  ordering

src/proof/resolution_bitvector_proof.h
@@ -1,0 +1,132 @@
+/*********************                                                        */
+/*! \file resolution_bitvector_proof.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Liana Hadarean, Mathias Preiner, Guy Katz
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Bitvector proof
+ **
+ ** Bitvector proof
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef __CVC4__PROOF__RESOLUTION_BITVECTOR_PROOF_H
+#define __CVC4__PROOF__RESOLUTION_BITVECTOR_PROOF_H
+
+#include <iosfwd>
+
+#include "context/context.h"
+#include "expr/expr.h"
+#include "proof/bitvector_proof.h"
+#include "proof/theory_proof.h"
+#include "prop/bvminisat/core/Solver.h"
+
+namespace CVC4 {
+
+namespace theory {
+namespace bv {
+class TheoryBV;
+template <class T>
+class TBitblaster;
+}  // namespace bv
+}  // namespace theory
+
+// TODO(aozdemir) break the sat_solver - resolution_bitvectorproof - cnf_stream
+// header cycle and remove this.
+namespace prop {
+class CnfStream;
+}
+
+} /* namespace CVC4 */
+
+
+namespace CVC4 {
+
+template <class Solver>
+class TSatProof;
+typedef TSatProof<CVC4::BVMinisat::Solver> BVSatProof;
+
+namespace proof {
+
+/**
+ * Represents a bitvector proof which is backed by
+ * (a) bitblasting and
+ * (b) a resolution unsat proof.
+ *
+ * Contains tools for constructing BV conflicts
+ */
+class ResolutionBitVectorProof : public BitVectorProof
+{
+ public:
+  ResolutionBitVectorProof(theory::bv::TheoryBV* bv,
+                           TheoryProofEngine* proofEngine);
+
+  /**
+   * Create an (internal) SAT proof object
+   * Must be invoked before manipulating BV conflicts,
+   * or initializing a BNF proof
+   */
+  void initSatProof(CVC4::BVMinisat::Solver* solver);
+
+  BVSatProof* getSatProof();
+
+  /**
+   * Kind of a mess.
+   * In eager mode this must be invoked before printing a proof of the empty
+   * clause. In lazy mode the behavior is ???
+   * TODO(aozdemir) clean this up.
+   */
+  void finalizeConflicts(std::vector<Expr>& conflicts);
+
+  void startBVConflict(CVC4::BVMinisat::Solver::TCRef cr);
+  void startBVConflict(CVC4::BVMinisat::Solver::TLit lit);
+  void endBVConflict(const BVMinisat::Solver::TLitVec& confl);
+
+  void markAssumptionConflict() { d_isAssumptionConflict = true; }
+  bool isAssumptionConflict() const { return d_isAssumptionConflict; }
+
+  virtual void printResolutionProof(std::ostream& os,
+                                    std::ostream& paren,
+                                    ProofLetMap& letMap) = 0;
+
+  void initCnfProof(prop::CnfStream* cnfStream, context::Context* cnf) override;
+
+ protected:
+  // The CNF formula that results from bit-blasting will need a proof.
+  // This is that proof.
+  std::unique_ptr<BVSatProof> d_resolutionProof;
+
+  bool d_isAssumptionConflict;
+
+  theory::TheoryId getTheoryId() override;
+  context::Context d_fakeContext;
+};
+
+class LFSCBitVectorProof : public ResolutionBitVectorProof
+{
+ public:
+  LFSCBitVectorProof(theory::bv::TheoryBV* bv, TheoryProofEngine* proofEngine)
+      : ResolutionBitVectorProof(bv, proofEngine)
+  {
+  }
+  void printTheoryLemmaProof(std::vector<Expr>& lemma,
+                             std::ostream& os,
+                             std::ostream& paren,
+                             const ProofLetMap& map) override;
+  void printResolutionProof(std::ostream& os,
+                            std::ostream& paren,
+                            ProofLetMap& letMap) override;
+  void calculateAtomsInBitblastingProof() override;
+};
+
+}  // namespace proof
+
+}  // namespace CVC4
+
+#endif /* __CVC4__PROOF__RESOLUTIONBITVECTORPROOF_H */

src/CMakeLists.txt
@@ -142,6 +142,8 @@ libcvc4_add_sources(
   proof/proof_output_channel.h
   proof/proof_utils.cpp
   proof/proof_utils.h
+  proof/resolution_bitvector_proof.cpp
+  proof/resolution_bitvector_proof.h
   proof/sat_proof.h
   proof/sat_proof_implementation.h
   proof/simplify_boolean_node.cpp
@@ -202,6 +204,7 @@ libcvc4_add_sources(
   prop/sat_solver.h
   prop/sat_solver_factory.cpp
   prop/sat_solver_factory.h
+  prop/bv_sat_solver_notify.h
   prop/sat_solver_types.h
   prop/theory_proxy.cpp
   prop/theory_proxy.h

src/proof/bitvector_proof.h
@@ -9,48 +9,28 @@
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
- ** \brief Bitvector proof
+ ** \brief Bitvector proof base class
  **
- ** Bitvector proof
+ ** Contains code (e.g. proof printing code) which is common to all bitvector
+ *proofs.
  **/
 
 #include "cvc4_private.h"
 
-#ifndef __CVC4__BITVECTOR__PROOF_H
-#define __CVC4__BITVECTOR__PROOF_H
+#ifndef __CVC4__BITVECTOR_PROOF_H
+#define __CVC4__BITVECTOR_PROOF_H
 
-#include <iostream>
 #include <set>
-#include <sstream>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-
 #include "expr/expr.h"
+#include "proof/cnf_proof.h"
 #include "proof/theory_proof.h"
-#include "prop/bvminisat/core/Solver.h"
-
-
-namespace CVC4 {
-
-namespace prop {
-class CnfStream;
-} /* namespace CVC4::prop */
-
-namespace theory {
-namespace bv {
-class TheoryBV;
-template <class T> class TBitblaster;
-} /* namespace CVC4::theory::bv */
-} /* namespace CVC4::theory */
-
-class CnfProof;
-} /* namespace CVC4 */
+#include "theory/bv/bitblast/bitblaster.h"
+#include "theory/bv/theory_bv.h"
 
 namespace CVC4 {
-
-template <class Solver> class TSatProof;
-typedef TSatProof< CVC4::BVMinisat::Solver> BVSatProof;
 
 typedef std::unordered_set<Expr, ExprHashFunction> ExprSet;
 typedef std::unordered_map<Expr, ClauseId, ExprHashFunction> ExprToClauseId;
@@ -58,69 +38,137 @@ typedef std::unordered_map<Expr, unsigned, ExprHashFunction> ExprToId;
 typedef std::unordered_map<Expr, Expr, ExprHashFunction> ExprToExpr;
 typedef std::unordered_map<Expr, std::string, ExprHashFunction> ExprToString;
 
-class BitVectorProof : public TheoryProof {
-protected:
+/**
+ * A bitvector proof is best understood as having
+ *
+ *    1. A declaration of a "bitblasted formulas" -- boolean formulas
+ *       that are each translations of a BV-literal (a comparison between BVs).
+ *
+ *       (and a proof that each "bitblasted formula" is implied by the
+ *       corresponding BV literal)
+ *
+ *    2. A declaration of a cnf formula equisatisfiable to the bitblasted
+ *       formula
+ *
+ *       (and a proof that each clause is implied by some bitblasted formula)
+ *
+ *    3. A proof of UNSAT from the clauses.
+ *
+ * This class is responsible for 1 & 2. The proof of UNSAT is delegated to a
+ * subclass.
+ */
+class BitVectorProof : public TheoryProof
+{
+ protected:
+  BitVectorProof(theory::bv::TheoryBV* bv, TheoryProofEngine* proofEngine);
+  virtual ~BitVectorProof(){};
+
+  // Set of BV variables in the input. (e.g. "a" in [ a = 000 ] ^ [ a == 001 ])
   ExprSet d_declarations;
 
-  ExprSet d_usedBB; // terms and formulas that are actually relevant to the proof
+  // terms and formulas that are actually relevant to the proof
+  ExprSet d_usedBB;
 
-  ExprSet d_seenBBTerms; // terms that need to be bit-blasted
-  std::vector<Expr> d_bbTerms; // order of bit-blasting
-  ExprToExpr d_bbAtoms; // atoms that need to be bit-blasted
+  ExprSet d_seenBBTerms;        // terms that need to be bit-blasted
+  std::vector<Expr> d_bbTerms;  // order of bit-blasting
+
+  /** atoms that need to be bit-blasted,
+   * BV-literals -> (BV-literals <=> bool formula)
+   * where a BV literal is a signed or unsigned comparison.
+   */
+  ExprToExpr d_bbAtoms;
 
   // map from Expr representing normalized lemma to ClauseId in SAT solver
   ExprToClauseId d_bbConflictMap;
-  BVSatProof* d_resolutionProof;
 
-  CnfProof* d_cnfProof;
-
-  bool d_isAssumptionConflict;
   theory::bv::TBitblaster<Node>* d_bitblaster;
+
+  /** In an LFSC proof the manifestation of this expression bit-level
+   * representation will have a string name. This method returns that name.
+   */
   std::string getBBTermName(Expr expr);
 
-  std::map<Expr,std::string> d_constantLetMap;
-  bool d_useConstantLetification;
-  theory::TheoryId getTheoryId() override;
-  context::Context d_fakeContext;
-public:
-  BitVectorProof(theory::bv::TheoryBV* bv, TheoryProofEngine* proofEngine);
-
-  void initSatProof(CVC4::BVMinisat::Solver* solver);
-  void initCnfProof(prop::CnfStream* cnfStream, context::Context* ctx);
-  void setBitblaster(theory::bv::TBitblaster<Node>* bb);
-
-  BVSatProof* getSatProof();
-  CnfProof* getCnfProof() {return d_cnfProof; }
-  void finalizeConflicts(std::vector<Expr>& conflicts);
-
-  void startBVConflict(CVC4::BVMinisat::Solver::TCRef cr);
-  void startBVConflict(CVC4::BVMinisat::Solver::TLit lit);
-  /**
-   * All the
-   *
-   * @param confl an inconsistent set of bv literals
+  /** A mapping from constant BV terms to identifiers that will refer to them in
+   * an LFSC proof, if constant-letification is enabled.
    */
-  void endBVConflict(const BVMinisat::Solver::TLitVec& confl);
-  void markAssumptionConflict() { d_isAssumptionConflict = true; }
-  bool isAssumptionConflict() { return d_isAssumptionConflict; }
+  std::map<Expr, std::string> d_constantLetMap;
+
+  /** Should we introduced identifiers to refer to BV constant terms?  It may
+   * reduce the textual size of a proof!
+   */
+  bool d_useConstantLetification;
+
+  /** Temporary storage for the set of nodes in the bitblasted formula which
+   * correspond to CNF variables eventually used in the proof of unsat on the
+   * CNF formula
+   */
+  std::set<Node> d_atomsInBitblastingProof;
+
+  /**
+   * Prints out
+   *   (a) a declaration of bit-level interpretations corresponding to bits in
+   *       the input BV terms.
+   *   (b) a proof that the each BV literal entails a boolean formula on
+   *       bitof expressions.
+   */
+  void printBitblasting(std::ostream& os, std::ostream& paren);
+
+  /**
+   * The proof that the bit-blasted SAT formula is correctly converted to CNF
+   */
+  std::unique_ptr<CnfProof> d_cnfProof;
+
+ public:
+  void printOwnedTerm(Expr term,
+                      std::ostream& os,
+                      const ProofLetMap& map) override;
+
+  void printOwnedSort(Type type, std::ostream& os) override;
+
+  /**
+   * Populate the d_atomsInBitblastingProof member.
+   * See its documentation
+   */
+  virtual void calculateAtomsInBitblastingProof() = 0;
+
+  /**
+   * Read the d_atomsInBitblastingProof member.
+   * See its documentation.
+   */
+  const std::set<Node>* getAtomsInBitblastingProof();
 
   void registerTermBB(Expr term);
+
+  /**
+   * Informs the proof that the `atom` predicate was bitblasted into the
+   * `atom_bb` term.
+   *
+   * The `atom` term must be a comparison of bitvectors, and the `atom_bb` term
+   * a boolean formula on bitof expressions
+   */
   void registerAtomBB(Expr atom, Expr atom_bb);
 
   void registerTerm(Expr term) override;
 
-  virtual void printTermBitblasting(Expr term, std::ostream& os) = 0;
-  virtual void printAtomBitblasting(Expr term, std::ostream& os, bool swap) = 0;
-  virtual void printAtomBitblastingToFalse(Expr term, std::ostream& os) = 0;
+  /**
+   * This must be done before registering any terms or atoms, since the CNF
+   * proof must reflect the result of bitblasting those
+   */
+  virtual void initCnfProof(prop::CnfStream* cnfStream, context::Context* ctx);
 
-  virtual void printBitblasting(std::ostream& os, std::ostream& paren) = 0;
-  virtual void printResolutionProof(std::ostream& os, std::ostream& paren, ProofLetMap& letMap) = 0;
-  virtual const std::set<Node>* getAtomsInBitblastingProof() = 0;
-  virtual void calculateAtomsInBitblastingProof() = 0;
-};
+  CnfProof* getCnfProof() { return d_cnfProof.get(); }
 
-class LFSCBitVectorProof: public BitVectorProof {
+  void setBitblaster(theory::bv::TBitblaster<Node>* bb);
 
+ private:
+  ExprToString d_exprToVariableName;
+
+  ExprToString d_assignedAliases;
+  std::map<std::string, std::string> d_aliasToBindDeclaration;
+  std::string assignAlias(Expr expr);
+  bool hasAlias(Expr expr);
+
+  // Functions for printing various BV terms. Helpers for BV's `printOwnedTerm`
   void printConstant(Expr term, std::ostream& os);
   void printOperatorNary(Expr term, std::ostream& os, const ProofLetMap& map);
   void printOperatorUnary(Expr term, std::ostream& os, const ProofLetMap& map);
@@ -128,29 +176,19 @@ class LFSCBitVectorProof: public BitVectorProof {
   void printOperatorParametric(Expr term, std::ostream& os, const ProofLetMap& map);
   void printBitOf(Expr term, std::ostream& os, const ProofLetMap& map);
 
-  ExprToString d_exprToVariableName;
-  ExprToString d_assignedAliases;
-  std::map<std::string, std::string> d_aliasToBindDeclaration;
-  std::string assignAlias(Expr expr);
-  bool hasAlias(Expr expr);
+  /**
+   * Prints the LFSC construction of a bblast_term for `term`
+   */
+  void printTermBitblasting(Expr term, std::ostream& os);
 
-  std::set<Node> d_atomsInBitblastingProof;
+  /**
+   * For a given BV-atom (a comparison), prints a proof that that comparison
+   * holds iff the bitblasted equivalent of it holds.
+   * Uses a side-condidition to do the bit-blasting.
+   */
+  void printAtomBitblasting(Expr term, std::ostream& os, bool swap);
+  void printAtomBitblastingToFalse(Expr term, std::ostream& os);
 
-public:
-  LFSCBitVectorProof(theory::bv::TheoryBV* bv, TheoryProofEngine* proofEngine)
-    :BitVectorProof(bv, proofEngine)
-  {}
-  void printOwnedTerm(Expr term,
-                      std::ostream& os,
-                      const ProofLetMap& map) override;
-  void printOwnedSort(Type type, std::ostream& os) override;
-  void printTermBitblasting(Expr term, std::ostream& os) override;
-  void printAtomBitblasting(Expr term, std::ostream& os, bool swap) override;
-  void printAtomBitblastingToFalse(Expr term, std::ostream& os) override;
-  void printTheoryLemmaProof(std::vector<Expr>& lemma,
-                             std::ostream& os,
-                             std::ostream& paren,
-                             const ProofLetMap& map) override;
   void printSortDeclarations(std::ostream& os, std::ostream& paren) override;
   void printTermDeclarations(std::ostream& os, std::ostream& paren) override;
   void printDeferredDeclarations(std::ostream& os,
@@ -158,12 +196,7 @@ public:
   void printAliasingDeclarations(std::ostream& os,
                                  std::ostream& paren,
                                  const ProofLetMap& globalLetMap) override;
-  void printBitblasting(std::ostream& os, std::ostream& paren) override;
-  void printResolutionProof(std::ostream& os,
-                            std::ostream& paren,
-                            ProofLetMap& letMap) override;
-  void calculateAtomsInBitblastingProof() override;
-  const std::set<Node>* getAtomsInBitblastingProof() override;
+
   void printConstantDisequalityProof(std::ostream& os,
                                      Expr c1,
                                      Expr c2,

src/proof/proof_manager.cpp
@@ -21,11 +21,11 @@
 #include "context/context.h"
 #include "options/bv_options.h"
 #include "options/proof_options.h"
-#include "proof/bitvector_proof.h"
 #include "proof/clause_id.h"
 #include "proof/cnf_proof.h"
 #include "proof/lfsc_proof_printer.h"
 #include "proof/proof_utils.h"
+#include "proof/resolution_bitvector_proof.h"
 #include "proof/sat_proof_implementation.h"
 #include "proof/theory_proof.h"
 #include "smt/smt_engine.h"
@@ -116,10 +116,11 @@ UFProof* ProofManager::getUfProof() {
   return (UFProof*)pf;
 }
 
-BitVectorProof* ProofManager::getBitVectorProof() {
+proof::ResolutionBitVectorProof* ProofManager::getBitVectorProof()
+{
   Assert (options::proof());
   TheoryProof* pf = getTheoryProofEngine()->getTheoryProof(theory::THEORY_BV);
-  return (BitVectorProof*)pf;
+  return static_cast<proof::ResolutionBitVectorProof*>(pf);
 }
 
 ArrayProof* ProofManager::getArrayProof() {

src/proof/proof_manager.h
@@ -69,7 +69,10 @@ class TheoryProof;
 class UFProof;
 class ArithProof;
 class ArrayProof;
-class BitVectorProof;
+
+namespace proof {
+class ResolutionBitVectorProof;
+}
 
 template <class Solver> class LFSCSatProof;
 typedef TSatProof<CVC4::Minisat::Solver> CoreSatProof;
@@ -77,7 +80,6 @@ typedef TSatProof<CVC4::Minisat::Solver> CoreSatProof;
 class LFSCCnfProof;
 class LFSCTheoryProofEngine;
 class LFSCUFProof;
-class LFSCBitVectorProof;
 class LFSCRewriterProof;
 
 namespace prop {
@@ -189,7 +191,7 @@ public:
   static TheoryProofEngine* getTheoryProofEngine();
   static TheoryProof* getTheoryProof( theory::TheoryId id );
   static UFProof* getUfProof();
-  static BitVectorProof* getBitVectorProof();
+  static proof::ResolutionBitVectorProof* getBitVectorProof();
   static ArrayProof* getArrayProof();
   static ArithProof* getArithProof();
 

commit f541cfd053ab8d0feb52a0fc2784503c15593f37

Fix use-after-free due to destruction order
A test for PR #2737 was failing even though the PR only added dead code.
This PR fixes the issue by fixing two use-after-free bugs:

- `ResolutionBitVectorProof` has a `Context` and a
`std::unique_ptr<BVSatProof>` member. The `BVSatProof` depends on the
`Context` and tries to access it (indirectly) in its constructor but
because the context was declared after the proof, the context was
destroyed before the proof, leading to a use-after-free in a method
called from the proof's destructor. This commit reorders the two
members.
- `TLazyBitblaster` was destroyed before the `LFSCCnfProof` in
`BitVectorProof` because `SmtEngine`'s destructor first destroyed the
theory engine and then the proof manager. This lead to a use-after-free
because `LFSCCnfProof` was using the `d_nullContext` of
`TLazyBitblaster`, which got indirectly accessed in `LFSCCnfProof`'s
destructor. This commit moves the destruction of `ProofManager` above
the destruction of the theory engine.

The issues were likely introduced by #2599. They went undetected because
our nightlies' ASAN check does not use proofs due to known memory leaks
in the proof module of CVC4.

I have tested this PR up to regression level 2 with ASAN with leak
detection disabled.

src/proof/resolution_bitvector_proof.h
@@ -98,6 +98,8 @@ class ResolutionBitVectorProof : public BitVectorProof
   void initCnfProof(prop::CnfStream* cnfStream, context::Context* cnf) override;
 
  protected:
+  context::Context d_fakeContext;
+
   // The CNF formula that results from bit-blasting will need a proof.
   // This is that proof.
   std::unique_ptr<BVSatProof> d_resolutionProof;
@@ -105,7 +107,6 @@ class ResolutionBitVectorProof : public BitVectorProof
   bool d_isAssumptionConflict;
 
   theory::TheoryId getTheoryId() override;
-  context::Context d_fakeContext;
 };
 
 class LFSCBitVectorProof : public ResolutionBitVectorProof

src/smt/smt_engine.cpp
@@ -1071,21 +1071,24 @@ SmtEngine::~SmtEngine()
     //destroy all passes before destroying things that they refer to
     d_private->cleanupPreprocessingPasses();
 
+    // d_proofManager is always created when proofs are enabled at configure
+    // time.  Because of this, this code should not be wrapped in PROOF() which
+    // additionally checks flags such as options::proof().
+    //
+    // Note: the proof manager must be destroyed before the theory engine.
+    // Because the destruction of the proofs depends on contexts owned be the
+    // theory solvers.
+#ifdef CVC4_PROOF
+    delete d_proofManager;
+    d_proofManager = NULL;
+#endif
+
     delete d_theoryEngine;
     d_theoryEngine = NULL;
     delete d_propEngine;
     d_propEngine = NULL;
     delete d_decisionEngine;
     d_decisionEngine = NULL;
-
-
-// d_proofManager is always created when proofs are enabled at configure time.
-// Becuase of this, this code should not be wrapped in PROOF() which
-// additionally checks flags such as options::proof().
-#ifdef CVC4_PROOF
-    delete d_proofManager;
-    d_proofManager = NULL;
-#endif
 
     delete d_stats;
     d_stats = NULL;