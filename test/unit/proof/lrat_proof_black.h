6040187a4e9405ff1108b152d179af325335eff2
Andres Noetzli <andres.noetzli@gmail.com>
2019-01-12 08:14:53 -0800

    s/DRAT/LRAT/
    Thanks Andres!
    
    Co-Authored-By: alex-ozdemir <aozdemir@hmc.edu>

--- test/unit/proof/lrat_proof_black.h
+++ test/unit/proof/lrat_proof_black.h
@@ -4,14 +4,14 @@
  ** Top contributors (to current version):
  **   Alex Ozdemir
  ** This file is part of the CVC4 project.
- ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
- ** \brief Black box testing of the DRAT proof class
+ ** \brief Black box testing of the LRAT proof class
  **
- ** In particular, tests DRAT binary parsing.
+ ** In particular, tests LRAT LFSC output.
  **/
 
 #include <cxxtest/TestSuite.h>
