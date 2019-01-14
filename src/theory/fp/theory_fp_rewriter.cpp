4e350af3ab513118fcb0d2927ad99113d0c6eae6
ajreynol <andrew.j.reynolds@gmail.com>
2019-01-14 17:22:44 -0600

    Format

--- src/theory/fp/theory_fp_rewriter.cpp
+++ src/theory/fp/theory_fp_rewriter.cpp
@@ -76,7 +76,8 @@ namespace rewrite {
 
   RewriteResponse compactAbs (TNode node, bool) {
     Assert(node.getKind() == kind::FLOATINGPOINT_ABS);
-    if (node[0].getKind() == kind::FLOATINGPOINT_NEG) {
+    if (node[0].getKind() == kind::FLOATINGPOINT_NEG)
+    {
       return RewriteResponse(REWRITE_AGAIN, node[0][0]);
     }
 
