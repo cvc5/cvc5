79cda178c463294e98492b9f1127ba764974fc76
ajreynol <andrew.j.reynolds@gmail.com>
2019-03-12 14:13:30 -0500

    Temp move

--- src/theory/datatypes/theory_datatypes.cpp
+++ src/theory/datatypes/theory_datatypes.cpp
@@ -553,102 +553,102 @@ void TheoryDatatypes::finishInit() {
 
 Node TheoryDatatypes::expandDefinition(LogicRequest &logicRequest, Node n) {
   NodeManager* nm = NodeManager::currentNM();
-  switch( n.getKind() ){
-  case kind::APPLY_SELECTOR: {
-    Trace("dt-expand") << "Dt Expand definition : " << n << std::endl;
-    Node selector = n.getOperator();
-    Expr selectorExpr = selector.toExpr();
-    // APPLY_SELECTOR always applies to an external selector, cindexOf is legal here
-    size_t cindex = Datatype::cindexOf(selectorExpr);
-    const Datatype& dt = Datatype::datatypeOf(selectorExpr);
-    const DatatypeConstructor& c = dt[cindex];
-    Node selector_use;
-    TypeNode ndt = n[0].getType();
-    if( options::dtSharedSelectors() ){
-      size_t selectorIndex = DatatypesRewriter::indexOf(selector);
-      Trace("dt-expand") << "...selector index = " << selectorIndex << std::endl;
-      Assert( selectorIndex<c.getNumArgs() );
-      selector_use = Node::fromExpr( c.getSelectorInternal( ndt.toType(), selectorIndex ) );
-    }else{
-      selector_use = selector;
-    }
-    Node sel = NodeManager::currentNM()->mkNode( kind::APPLY_SELECTOR_TOTAL, selector_use, n[0] );
-    if( options::dtRewriteErrorSel() ){
-      return sel;
-    }else{
-      Expr tester = c.getTester();
-      Node tst = NodeManager::currentNM()->mkNode( kind::APPLY_TESTER, Node::fromExpr( tester ), n[0] );
-      tst = Rewriter::rewrite( tst );
-      Node n_ret;
-      if( tst==d_true ){
-        n_ret = sel;
+    switch( n.getKind() ){
+    case kind::APPLY_SELECTOR: {
+      Trace("dt-expand") << "Dt Expand definition : " << n << std::endl;
+      Node selector = n.getOperator();
+      Expr selectorExpr = selector.toExpr();
+      // APPLY_SELECTOR always applies to an external selector, cindexOf is legal here
+      size_t cindex = Datatype::cindexOf(selectorExpr);
+      const Datatype& dt = Datatype::datatypeOf(selectorExpr);
+      const DatatypeConstructor& c = dt[cindex];
+      Node selector_use;
+      TypeNode ndt = n[0].getType();
+      if( options::dtSharedSelectors() ){
+        size_t selectorIndex = DatatypesRewriter::indexOf(selector);
+        Trace("dt-expand") << "...selector index = " << selectorIndex << std::endl;
+        Assert( selectorIndex<c.getNumArgs() );
+        selector_use = Node::fromExpr( c.getSelectorInternal( ndt.toType(), selectorIndex ) );
       }else{
-        mkExpDefSkolem( selector, ndt, n.getType() );
-        Node sk = NodeManager::currentNM()->mkNode( kind::APPLY_UF, d_exp_def_skolem[ndt][ selector ], n[0]  );
-        if( tst==NodeManager::currentNM()->mkConst( false ) ){
-          n_ret = sk;
+        selector_use = selector;
+      }
+      Node sel = NodeManager::currentNM()->mkNode( kind::APPLY_SELECTOR_TOTAL, selector_use, n[0] );
+      if( options::dtRewriteErrorSel() ){
+        return sel;
+      }else{
+        Expr tester = c.getTester();
+        Node tst = NodeManager::currentNM()->mkNode( kind::APPLY_TESTER, Node::fromExpr( tester ), n[0] );
+        tst = Rewriter::rewrite( tst );
+        Node n_ret;
+        if( tst==d_true ){
+          n_ret = sel;
         }else{
-          n_ret = NodeManager::currentNM()->mkNode( kind::ITE, tst, sel, sk );
+          mkExpDefSkolem( selector, ndt, n.getType() );
+          Node sk = NodeManager::currentNM()->mkNode( kind::APPLY_UF, d_exp_def_skolem[ndt][ selector ], n[0]  );
+          if( tst==NodeManager::currentNM()->mkConst( false ) ){
+            n_ret = sk;
+          }else{
+            n_ret = NodeManager::currentNM()->mkNode( kind::ITE, tst, sel, sk );
+          }
         }
+        //n_ret = Rewriter::rewrite( n_ret );
+        Trace("dt-expand") << "Expand def : " << n << " to " << n_ret << std::endl;
+        return n_ret;
       }
-      //n_ret = Rewriter::rewrite( n_ret );
-      Trace("dt-expand") << "Expand def : " << n << " to " << n_ret << std::endl;
-      return n_ret;
     }
-  }
-    break;
-    case TUPLE_UPDATE:
-    case RECORD_UPDATE:
-    {
-      TypeNode t = n.getType();
-      Assert(t.isDatatype());
-      const Datatype& dt = DatatypeType(t.toType()).getDatatype();
-      NodeBuilder<> b(APPLY_CONSTRUCTOR);
-      b << Node::fromExpr(dt[0].getConstructor());
-      size_t size, updateIndex;
-      if (n.getKind() == TUPLE_UPDATE)
+      break;
+      case TUPLE_UPDATE:
+      case RECORD_UPDATE:
       {
-        Assert(t.isTuple());
-        size = t.getTupleLength();
-        updateIndex = n.getOperator().getConst<TupleUpdate>().getIndex();
-      }
-      else
-      {
-        Assert(t.isRecord());
-        const Record& record = t.getRecord();
-        size = record.getNumFields();
-        updateIndex = record.getIndex(
-            n.getOperator().getConst<RecordUpdate>().getField());
-      }
-      Debug("tuprec") << "expr is " << n << std::endl;
-      Debug("tuprec") << "updateIndex is " << updateIndex << std::endl;
-      Debug("tuprec") << "t is " << t << std::endl;
-      Debug("tuprec") << "t has arity " << size << std::endl;
-      for (size_t i = 0; i < size; ++i)
-      {
-        if (i == updateIndex)
+        TypeNode t = n.getType();
+        Assert(t.isDatatype());
+        const Datatype& dt = DatatypeType(t.toType()).getDatatype();
+        NodeBuilder<> b(APPLY_CONSTRUCTOR);
+        b << Node::fromExpr(dt[0].getConstructor());
+        size_t size, updateIndex;
+        if (n.getKind() == TUPLE_UPDATE)
         {
-          b << n[1];
-          Debug("tuprec") << "arg " << i << " gets updated to " << n[1]
-                          << std::endl;
+          Assert(t.isTuple());
+          size = t.getTupleLength();
+          updateIndex = n.getOperator().getConst<TupleUpdate>().getIndex();
         }
         else
         {
-          b << nm->mkNode(
-              APPLY_SELECTOR_TOTAL,
-              Node::fromExpr(dt[0].getSelectorInternal(t.toType(), i)),
-              n[0]);
-          Debug("tuprec") << "arg " << i << " copies "
-                          << b[b.getNumChildren() - 1] << std::endl;
+          Assert(t.isRecord());
+          const Record& record = t.getRecord();
+          size = record.getNumFields();
+          updateIndex = record.getIndex(
+              n.getOperator().getConst<RecordUpdate>().getField());
         }
+        Debug("tuprec") << "expr is " << n << std::endl;
+        Debug("tuprec") << "updateIndex is " << updateIndex << std::endl;
+        Debug("tuprec") << "t is " << t << std::endl;
+        Debug("tuprec") << "t has arity " << size << std::endl;
+        for (size_t i = 0; i < size; ++i)
+        {
+          if (i == updateIndex)
+          {
+            b << n[1];
+            Debug("tuprec") << "arg " << i << " gets updated to " << n[1]
+                            << std::endl;
+          }
+          else
+          {
+            b << nm->mkNode(
+                APPLY_SELECTOR_TOTAL,
+                Node::fromExpr(dt[0].getSelectorInternal(t.toType(), i)),
+                n[0]);
+            Debug("tuprec") << "arg " << i << " copies "
+                            << b[b.getNumChildren() - 1] << std::endl;
+          }
+        }
+        Node n_ret = b;
+        Debug("tuprec") << "return " << n_ret << std::endl;
+        return n_ret;
       }
-      Node n_ret = b;
-      Debug("tuprec") << "return " << n_ret << std::endl;
-      return n_ret;
+      break;
+      default: return n; break;
     }
-    break;
-    default: return n; break;
-  }
   Unreachable();
 }
 
