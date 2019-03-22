31aac955bc714d5f0abaf3527c109971a2f01d89
ajreynol <andrew.j.reynolds@gmail.com>
2019-03-22 09:02:36 -0500

    Fix doc

--- src/theory/strings/regexp_solver.cpp
+++ src/theory/strings/regexp_solver.cpp
@@ -4,13 +4,14 @@
  ** Top contributors (to current version):
  **   Andrew Reynolds
  ** This file is part of the CVC4 project.
- ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
  ** \brief Implementation of the regular expression solver for the theory of
  *strings.
+ **
  **/
 
 #include "theory/strings/regexp_solver.h"
