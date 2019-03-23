60ffbbca8951a9ee5b61543cd6ad6b10f00f6f19
Aina Niemetz <aina.niemetz@gmail.com>
2019-03-23 16:21:28 -0700

    BV: Fix typerules for rotate operators.

--- test/unit/api/solver_black.h
+++ test/unit/api/solver_black.h
@@ -451,6 +451,8 @@ void SolverBlack::testMkOpTerm()
 
   // mkOpTerm(Kind kind, uint32_t arg)
   TS_ASSERT_THROWS_NOTHING(d_solver->mkOpTerm(DIVISIBLE_OP, 1));
+  TS_ASSERT_THROWS_NOTHING(d_solver->mkOpTerm(BITVECTOR_ROTATE_LEFT_OP, 1));
+  TS_ASSERT_THROWS_NOTHING(d_solver->mkOpTerm(BITVECTOR_ROTATE_RIGHT_OP, 1));
   TS_ASSERT_THROWS(d_solver->mkOpTerm(BITVECTOR_EXTRACT_OP, 1),
                    CVC4ApiException&);
 
