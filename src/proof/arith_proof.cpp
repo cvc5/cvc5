34c1f3fd3a581c334b3945cc40525ee4f5ba20c1
yoni206 <yoni206@gmail.com>
2019-03-24 23:49:39 -0700

    added holes to some places. still one bug.

--- src/proof/arith_proof.cpp
+++ src/proof/arith_proof.cpp
@@ -33,6 +33,22 @@ inline static Node eqNode(TNode n1, TNode n2) {
   return NodeManager::currentNM()->mkNode(kind::EQUAL, n1, n2);
 }
 
+inline static bool all_children_geq(const Node& conflict) {
+	bool result = true;
+	size_t nChildren = conflict.getNumChildren();
+	for (size_t i=0; i < nChildren; i++) {
+	  const Node& n = conflict[i];
+	  const Node& nonneg = n.getKind() == kind::NOT ? n[0] : n;
+	  if (nonneg.getKind() == kind::GEQ) {
+	    continue;
+	  } else {
+	    result = false;
+	    break;
+	  }
+	}
+	return result;
+}
+
 // congrence matching term helper
 inline static bool match(TNode n1, TNode n2) {
   Debug("pf::arith") << "match " << n1 << " " << n2 << std::endl;
@@ -973,9 +989,6 @@ void LFSCArithProof::printVariableNormalizer(std::ostream& o, const Node& n)
 void LFSCArithProof::printLinearPolynomialPredicateNormalizer(std::ostream& o,
                                                               const Node& n)
 {
-  std::cout << "panda " << n.getKind() << std::endl;
-  Assert(n.getKind() == kind::GEQ,
-         "can only print normalization witnesses for (>=) nodes");
   Assert(n[1].getKind() == kind::CONST_RATIONAL);
   o << "(poly_formula_norm_>= _ _ _ ";
   o << "\n    (pn_- _ _ _ _ _ ";
@@ -1007,152 +1020,151 @@ void LFSCArithProof::printTheoryLemmaProof(std::vector<Expr>& lemma,
 
   // If we have Farkas coefficients stored for this lemma, use them to write a
   // proof. Otherwise, just `trust` the lemma.
-  if (d_recorder.hasFarkasCoefficients(conflictSet))
+  // Get farkas coefficients & literal order
+  const auto& farkasInfo = d_recorder.getFarkasCoefficients(conflictSet);
+  const Node& conflict = farkasInfo.first;
+  theory::arith::RationalVectorCP farkasCoefficients = farkasInfo.second;
+  const size_t nAntecedents = conflict.getNumChildren();
+  if (d_recorder.hasFarkasCoefficients(conflictSet) && all_children_geq(conflict))
   {
-    // Get farkas coefficients & literal order
-    const auto& farkasInfo = d_recorder.getFarkasCoefficients(conflictSet);
-    const Node& conflict = farkasInfo.first;
-    theory::arith::RationalVectorCP farkasCoefficients = farkasInfo.second;
-    Assert(farkasCoefficients != theory::arith::RationalVectorCPSentinel);
-    Assert(conflict.getNumChildren() == farkasCoefficients->size());
-    const size_t nAntecedents = conflict.getNumChildren();
+      Assert(farkasCoefficients != theory::arith::RationalVectorCPSentinel);
+      Assert(conflict.getNumChildren() == farkasCoefficients->size());
+      // Print proof
+      os << "\n;; Farkas Proof" << std::endl;
 
-    // Print proof
-    os << "\n;; Farkas Proof" << std::endl;
-
-    // Construct witness that the literals are linear polynomials
-    os << ";  Linear Polynomial Normalization Witnesses" << std::endl;
-    for (size_t i = 0; i != nAntecedents; ++i)
-    {
-      const Node& antecedent = conflict[i];
-      const Rational farkasC = (*farkasCoefficients)[i];
-      os << "\n; " << antecedent << " w/ farkas c = " << farkasC << std::endl;
-      os << "  (@ "
-         << ProofManager::getLitName(antecedent.negate(),
-                                     linearityWitnessPrefix)
-         << " ";
-      const Node& nonneg =
-          antecedent.getKind() == kind::NOT ? antecedent[0] : antecedent;
-      printLinearPolynomialPredicateNormalizer(os, nonneg);
-      lemmaParen << ")";
-    }
-
-    // Prove linear polynomial constraints
-    os << "\n;  Linear Polynomial Proof Conversions";
-    for (size_t i = 0; i != nAntecedents; ++i)
-    {
-      const Node& antecedent = conflict[i];
-      os << "\n  (@ "
-         << ProofManager::getLitName(antecedent.negate(), linearizedProofPrefix)
-         << " ";
-      lemmaParen << ")";
-      switch (conflict[i].getKind())
+      // Construct witness that the literals are linear polynomials
+      os << ";  Linear Polynomial Normalization Witnesses" << std::endl;
+      for (size_t i = 0; i != nAntecedents; ++i)
       {
-        case kind::NOT:
+        const Node& antecedent = conflict[i];
+        const Rational farkasC = (*farkasCoefficients)[i];
+        os << "\n; " << antecedent << " w/ farkas c = " << farkasC << std::endl;
+        os << "  (@ "
+           << ProofManager::getLitName(antecedent.negate(),
+                                       linearityWitnessPrefix)
+           << " ";
+        const Node& nonneg =
+            antecedent.getKind() == kind::NOT ? antecedent[0] : antecedent;
+        printLinearPolynomialPredicateNormalizer(os, nonneg);
+        lemmaParen << ")";
+      }
+
+      // Prove linear polynomial constraints
+      os << "\n;  Linear Polynomial Proof Conversions";
+      for (size_t i = 0; i != nAntecedents; ++i)
+      {
+        const Node& antecedent = conflict[i];
+        os << "\n  (@ "
+           << ProofManager::getLitName(antecedent.negate(), linearizedProofPrefix)
+           << " ";
+        lemmaParen << ")";
+        switch (conflict[i].getKind())
         {
-          Assert(conflict[i][0].getKind() == kind::GEQ);
-          os << "(poly_flip_not_>= _ _ "
-             << "(poly_form_not _ _ "
-             << ProofManager::getLitName(antecedent.negate(),
-                                         linearityWitnessPrefix)
-             << " " << ProofManager::getLitName(antecedent.negate(), "")
-             << "))";
-          break;
+          case kind::NOT:
+          {
+            Assert(conflict[i][0].getKind() == kind::GEQ);
+            os << "(poly_flip_not_>= _ _ "
+               << "(poly_form_not _ _ "
+               << ProofManager::getLitName(antecedent.negate(),
+                                           linearityWitnessPrefix)
+               << " " << ProofManager::getLitName(antecedent.negate(), "")
+               << "))";
+            break;
+          }
+          case kind::GEQ:
+          {
+            os << "(poly_form _ _ "
+               << ProofManager::getLitName(antecedent.negate(),
+                                           linearityWitnessPrefix)
+               << " " << ProofManager::getLitName(antecedent.negate(), "") << ")";
+            break;
+          }
+          default: Unreachable();
         }
-        case kind::GEQ:
+      }
+
+      /* Combine linear polynomial constraints to derive a contradiction.
+       *
+       * The linear polynomial constraints are refered to as **antecedents**,
+       * since they are antecedents to the contradiction.
+       *
+       * The structure of the combination is a tree
+       *
+       *   (=> <=)
+       *      |
+       *      +                   0
+       *     / \
+       *    *   +                 1
+       *       / \
+       *      *   +               2
+       *         / \
+       *        *  ...            i
+       *             \
+       *              +           n-1
+       *             / \
+       *            *   (0 >= 0)
+       *
+       * Where each * is a linearized antecedant being scaled by a farkas
+       * coefficient and each + is the sum of inequalities. The tricky bit is that
+       * each antecedent can be strict (>) or relaxed (>=) and the axiom used for
+       * each * and + depends on this... The axiom for * depends on the
+       * strictness of its linear polynomial input, and the axiom for + depends
+       * on the strictness of **both** its inputs. The contradiction axiom is
+       * also a function of the strictness of its input.
+       *
+       * There are n *s and +s and we precompute
+       *    1. The strictness of the ith antecedant  (`ith_antecedent_is_strict`)
+       *    2. The strictness of the right argument of the ith sum
+       * (`ith_acc_is_strict`)
+       *    3. The strictness of the final result (`strict_contradiction`)
+       *
+       * Precomupation is helpful since
+       *    the computation is post-order,
+       *    but printing is pre-order.
+       */
+      std::vector<bool> ith_antecedent_is_strict(nAntecedents, false);
+      std::vector<bool> ith_acc_is_strict(nAntecedents, false);
+      for (int i = nAntecedents - 1; i >= 0; --i)
+      {
+        ith_antecedent_is_strict[i] = conflict[i].getKind() == kind::NOT;
+        if (i == (int)nAntecedents - 1)
         {
-          os << "(poly_form _ _ "
-             << ProofManager::getLitName(antecedent.negate(),
-                                         linearityWitnessPrefix)
-             << " " << ProofManager::getLitName(antecedent.negate(), "") << ")";
-          break;
+          ith_acc_is_strict[i] = false;
         }
-        default: Unreachable();
+        else
+        {
+          ith_acc_is_strict[i] =
+              ith_acc_is_strict[i + 1] || ith_antecedent_is_strict[i + 1];
+        }
       }
-    }
+      bool strict_contradiction =
+          ith_acc_is_strict[0] || ith_antecedent_is_strict[0];
 
-    /* Combine linear polynomial constraints to derive a contradiction.
-     *
-     * The linear polynomial constraints are refered to as **antecedents**,
-     * since they are antecedents to the contradiction.
-     *
-     * The structure of the combination is a tree
-     *
-     *   (=> <=)
-     *      |
-     *      +                   0
-     *     / \
-     *    *   +                 1
-     *       / \
-     *      *   +               2
-     *         / \
-     *        *  ...            i
-     *             \
-     *              +           n-1
-     *             / \
-     *            *   (0 >= 0)
-     *
-     * Where each * is a linearized antecedant being scaled by a farkas
-     * coefficient and each + is the sum of inequalities. The tricky bit is that
-     * each antecedent can be strict (>) or relaxed (>=) and the axiom used for
-     * each * and + depends on this... The axiom for * depends on the
-     * strictness of its linear polynomial input, and the axiom for + depends
-     * on the strictness of **both** its inputs. The contradiction axiom is
-     * also a function of the strictness of its input.
-     *
-     * There are n *s and +s and we precompute
-     *    1. The strictness of the ith antecedant  (`ith_antecedent_is_strict`)
-     *    2. The strictness of the right argument of the ith sum
-     * (`ith_acc_is_strict`)
-     *    3. The strictness of the final result (`strict_contradiction`)
-     *
-     * Precomupation is helpful since
-     *    the computation is post-order,
-     *    but printing is pre-order.
-     */
-    std::vector<bool> ith_antecedent_is_strict(nAntecedents, false);
-    std::vector<bool> ith_acc_is_strict(nAntecedents, false);
-    for (int i = nAntecedents - 1; i >= 0; --i)
-    {
-      ith_antecedent_is_strict[i] = conflict[i].getKind() == kind::NOT;
-      if (i == (int)nAntecedents - 1)
+      // Now, print the proof
+      os << "\n;  Farkas Combination";
+      // Choose the appropriate contradiction axiom
+      os << "\n  (lra_contra_" << (strict_contradiction ? ">" : ">=") << " _ ";
+      for (size_t i = 0; i != nAntecedents; ++i)
       {
-        ith_acc_is_strict[i] = false;
+        const Node& lit = conflict[i];
+        const char* ante_op = ith_antecedent_is_strict[i] ? ">" : ">=";
+        const char* acc_op = ith_acc_is_strict[i] ? ">" : ">=";
+        os << "\n    (lra_add_" << ante_op << "_" << acc_op << " _ _ _ ";
+        os << "\n       (lra_mul_c_" << ante_op << " _ _ ";
+        printRational(os, (*farkasCoefficients)[i].abs());
+        os << " " << ProofManager::getLitName(lit.negate(), linearizedProofPrefix)
+           << ")"
+           << " ; " << lit;
       }
-      else
-      {
-        ith_acc_is_strict[i] =
-            ith_acc_is_strict[i + 1] || ith_antecedent_is_strict[i + 1];
-      }
-    }
-    bool strict_contradiction =
-        ith_acc_is_strict[0] || ith_antecedent_is_strict[0];
 
-    // Now, print the proof
-    os << "\n;  Farkas Combination";
-    // Choose the appropriate contradiction axiom
-    os << "\n  (lra_contra_" << (strict_contradiction ? ">" : ">=") << " _ ";
-    for (size_t i = 0; i != nAntecedents; ++i)
-    {
-      const Node& lit = conflict[i];
-      const char* ante_op = ith_antecedent_is_strict[i] ? ">" : ">=";
-      const char* acc_op = ith_acc_is_strict[i] ? ">" : ">=";
-      os << "\n    (lra_add_" << ante_op << "_" << acc_op << " _ _ _ ";
-      os << "\n       (lra_mul_c_" << ante_op << " _ _ ";
-      printRational(os, (*farkasCoefficients)[i].abs());
-      os << " " << ProofManager::getLitName(lit.negate(), linearizedProofPrefix)
-         << ")"
-         << " ; " << lit;
-    }
+      // The basis, at least, is always the same...
+      os << "\n    (lra_axiom_>= 0/1)";
+      std::fill_n(std::ostream_iterator<char>(os),
+                  nAntecedents,
+                  ')');  // close lra_add_*_*
+      os << ")";         // close lra_contra_*
 
-    // The basis, at least, is always the same...
-    os << "\n    (lra_axiom_>= 0/1)";
-    std::fill_n(std::ostream_iterator<char>(os),
-                nAntecedents,
-                ')');  // close lra_add_*_*
-    os << ")";         // close lra_contra_*
-
-    os << lemmaParen.str();  // close normalizers and proof-normalizers
+      os << lemmaParen.str();  // close normalizers and proof-normalizers 
   }
   else
   {
