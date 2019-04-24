1be47ce0927c5b5277d0d854b7b754c4bd54c289
ajreynol <andrew.j.reynolds@gmail.com>
2019-04-24 13:36:23 -0500

    Towards elimination of APPLY kind.

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -131,7 +131,7 @@ Expr Parser::getExpressionForNameAndType(const std::string& name, Type t) {
   if(isDefinedFunction(expr)) {
     // defined functions/constants are wrapped in an APPLY so that they are
     // expanded into their definition, e.g. during SmtEnginePrivate::expandDefinitions
-    expr = getExprManager()->mkExpr(CVC4::kind::APPLY, expr);
+    //expr = getExprManager()->mkExpr(CVC4::kind::APPLY, expr);
   }else{
     Type te = expr.getType();
     if(te.isConstructor() && ConstructorType(te).getArity() == 0) {
--- src/smt/smt_engine.cpp
+++ src/smt/smt_engine.cpp
@@ -2775,7 +2775,7 @@ Node SmtEnginePrivate::expandDefinitions(TNode n, unordered_map<Node, Node, Node
         {
           doExpand = true;
         }
-        else if (options::macrosQuant() || options::sygusInference())
+        else
         {
           // The above options assign substitutions to APPLY_UF, thus we check
           // here and expand if this operator corresponds to a defined function.
