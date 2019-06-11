d49acba9086a348f5309d289eb9fad9e6c6bf6a2
ajreynol <andrew.j.reynolds@gmail.com>
2019-06-11 16:36:11 -0500

    Minor

--- src/theory/quantifiers/quant_conflict_find.cpp
+++ src/theory/quantifiers/quant_conflict_find.cpp
@@ -1963,7 +1963,6 @@ void QuantConflictFind::check(Theory::Effort level, QEffort quant_e)
     if (level >= Theory::EFFORT_FULL)
     {
       Trace("qcf-warn") << "ALREADY IN CONFLICT? " << level << std::endl;
-      // Assert( false );
     }
     return;
   }
