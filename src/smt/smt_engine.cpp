2f12653c8c2179987bc8eea531fe41e158cb37cf
ajreynol <andrew.j.reynolds@gmail.com>
2019-07-30 11:11:49 -0500

    Format

--- src/smt/smt_engine.cpp
+++ src/smt/smt_engine.cpp
@@ -5055,9 +5055,7 @@ Expr SmtEngine::doQuantifierElimination(const Expr& e, bool doFull, bool strict)
   }
 }
 
-bool SmtEngine::getAbduct(const Expr& conj,
-                          const Type& grammarType,
-                          Expr& abd)
+bool SmtEngine::getAbduct(const Expr& conj, const Type& grammarType, Expr& abd)
 {
   SmtScope smts(this);
 
