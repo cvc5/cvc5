011a5ed895cd6bc655e314156cb9f78260228e84
ajreynol <andrew.j.reynolds@gmail.com>
2019-08-02 11:37:15 -0500

    Minor

--- src/theory/quantifiers/sygus/sygus_enumerator_basic.h
+++ src/theory/quantifiers/sygus/sygus_enumerator_basic.h
@@ -9,7 +9,7 @@
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
- ** \brief sygus_enumerator
+ ** \brief Basic sygus enumerator class
  **/
 
 #include "cvc4_private.h"
