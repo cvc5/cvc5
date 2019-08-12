763d7c6610a8da7e1eca6a55b1cfb3bd928a651b
ajreynol <andrew.j.reynolds@gmail.com>
2019-08-12 16:15:33 -0500

    Minor

--- src/theory/strings/solver_state.cpp
+++ src/theory/strings/solver_state.cpp
@@ -2,16 +2,14 @@
 /*! \file solver_state.cpp
  ** \verbatim
  ** Top contributors (to current version):
- **   Andrew Reynolds, Tianyi Liang, Morgan Deters
+ **   Andrew Reynolds
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
- ** \brief Implementation of the theory of strings.
- **
- ** Implementation of the theory of strings.
+ ** \brief Implementation of the solver state of the theory of strings.
  **/
 
 #include "theory/strings/solver_state.h"
