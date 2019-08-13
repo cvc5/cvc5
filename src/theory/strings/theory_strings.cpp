d9a29fd74eb024b9b9ca7158af0334fb8b7d03ea
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2019-08-13 09:51:37 -0500

    Update src/theory/strings/theory_strings.cpp
    Co-Authored-By: Andres Noetzli <andres.noetzli@gmail.com>

--- src/theory/strings/theory_strings.cpp
+++ src/theory/strings/theory_strings.cpp
@@ -1017,7 +1017,7 @@ void TheoryStrings::check(Effort e) {
         Trace("strings-check") << "  ...finish run strategy: ";
         Trace("strings-check") << (addedFact ? "addedFact " : "");
         Trace("strings-check") << (addedLemma ? "addedLemma " : " ");
-        Trace("strings-check") << (d_conflict ? "conflict " : " ");
+        Trace("strings-check") << (d_conflict ? "conflict " : "");
         if (!addedFact && !addedLemma && !d_conflict)
         {
           Trace("strings-check") << "(none)";
