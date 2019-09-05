0f5a80ac71b537a70e7cf70c3ed0b8fa91054125
ajreynol <andrew.j.reynolds@gmail.com>
2019-09-05 10:04:12 -0500

    Make CEGQI term type to enum

--- src/theory/quantifiers/cegqi/ceg_arith_instantiator.h
+++ src/theory/quantifiers/cegqi/ceg_arith_instantiator.h
@@ -150,8 +150,13 @@ class ArithInstantiator : public Instantiator
    *    veq_c * pv <> val + vts_coeff_delta * delta + vts_coeff_inf * inf
    * where we ensure val has Int type if pv has Int type, and val does not
    * contain vts symbols.
+   * 
+   * It returns a CegTermType:
+   *   CEG_TT_INVALID if it was not possible to put atom into a solved form,
+   *   CEG_TT_LOWER if <> in the above equation is >=, or
+   *   CEG_TT_UPPER if <> in the above equation is <=.
    */
-  int solve_arith(CegInstantiator* ci,
+  CegTermType solve_arith(CegInstantiator* ci,
                   Node v,
                   Node atom,
                   Node& veq_c,
