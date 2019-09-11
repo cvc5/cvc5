6bc00ca1d1854c4025b740a28a7af8b63f41f93f
ajreynol <andrew.j.reynolds@gmail.com>
2019-09-11 18:32:41 -0500

    Format

--- src/theory/quantifiers/cegqi/vts_term_cache.h
+++ src/theory/quantifiers/cegqi/vts_term_cache.h
@@ -22,38 +22,42 @@
 
 namespace CVC4 {
 namespace theory {
-  
+
 class QuantifiersEngine;
-  
+
 namespace quantifiers {
 
-/** Virtual term substitution term cache 
+/** Virtual term substitution term cache
  *
  * This class stores skolems corresponding to virtual terms (e.g. delta and
  * infinity) and has methods for performing virtual term substitution.
- */ 
+ */
 class VtsTermCache
 {
-public:
-  VtsTermCache(QuantifiersEngine * qe);
-  ~VtsTermCache(){}
+ public:
+  VtsTermCache(QuantifiersEngine* qe);
+  ~VtsTermCache() {}
   /** get vts delta */
-  Node getVtsDelta( bool isFree = false, bool create = true );
+  Node getVtsDelta(bool isFree = false, bool create = true);
   /** get vts infinity */
-  Node getVtsInfinity( TypeNode tn, bool isFree = false, bool create = true );
+  Node getVtsInfinity(TypeNode tn, bool isFree = false, bool create = true);
   /** get all vts terms */
-  void getVtsTerms( std::vector< Node >& t, bool isFree = false, bool create = true, bool inc_delta = true );
+  void getVtsTerms(std::vector<Node>& t,
+                   bool isFree = false,
+                   bool create = true,
+                   bool inc_delta = true);
   /** rewrite delta */
-  Node rewriteVtsSymbols( Node n );
+  Node rewriteVtsSymbols(Node n);
   /** simple check for contains term */
-  bool containsVtsTerm( Node n, bool isFree = false );
+  bool containsVtsTerm(Node n, bool isFree = false);
   /** simple check for contains term */
-  bool containsVtsTerm( std::vector< Node >& n, bool isFree = false );
+  bool containsVtsTerm(std::vector<Node>& n, bool isFree = false);
   /** simple check for contains term */
-  bool containsVtsInfinity( Node n, bool isFree = false );
-private:
+  bool containsVtsInfinity(Node n, bool isFree = false);
+
+ private:
   /** pointer to the quantifiers engine */
-  QuantifiersEngine * d_qe;
+  QuantifiersEngine* d_qe;
   /** constants */
   Node d_zero;
   /** The virtual term substitution delta */
@@ -61,15 +65,15 @@ private:
   /** The virtual term substitution "free delta" */
   Node d_vts_delta_free;
   /** The virtual term substitution infinities for int/real types */
-  std::map< TypeNode, Node > d_vts_inf;
+  std::map<TypeNode, Node> d_vts_inf;
   /** The virtual term substitution "free infinities" for int/real types */
-  std::map< TypeNode, Node > d_vts_inf_free;
+  std::map<TypeNode, Node> d_vts_inf_free;
   /** substitute vts free terms */
-  Node substituteVtsFreeTerms( Node n );
-};/* class TermUtil */
+  Node substituteVtsFreeTerms(Node n);
+}; /* class TermUtil */
 
-}/* CVC4::theory::quantifiers namespace */
-}/* CVC4::theory namespace */
-}/* CVC4 namespace */
+}  // namespace quantifiers
+}  // namespace theory
+}  // namespace CVC4
 
 #endif /* CVC4__THEORY__QUANTIFIERS__CEGQI__VTS_TERM_CACHE_H */
