4f4823ac97c5889880e10a75d860bea3783219ff
ajreynol <andrew.j.reynolds@gmail.com>
2019-09-11 18:47:19 -0500

    Format

--- src/theory/quantifiers/cegqi/vts_term_cache.h
+++ src/theory/quantifiers/cegqi/vts_term_cache.h
@@ -31,14 +31,14 @@ namespace quantifiers {
  *
  * This class stores skolems corresponding to virtual terms (e.g. delta and
  * infinity) and has methods for performing virtual term substitution.
- * 
+ *
  * In detail, there are three kinds of virtual terms:
  * (1) delta, of Real type, representing a infinitessimally small real value,
  * (2) infinity, of Real type, representing an infinitely large real value,
  * (3) infinite, of Int type, representing an infinitely large integer value.
- * 
- * For each of the above three virtual terms, we have 2 variants. 
- * 
+ *
+ * For each of the above three virtual terms, we have 2 variants.
+ *
  * The first variant we simply call "virtual terms". These terms are intended
  * to never appear in assertions and are simply used by algorithms e.g. CEGQI
  * for specifying instantiations. They are eliminated by the standard rules of
@@ -49,7 +49,7 @@ namespace quantifiers {
  *   t <= s - delta -----> t < s
  *   t <= s + inf ----> true
  *   t <= s - inf ----> false
- * 
+ *
  * The second variant we call "free virtual terms". These terms are intended
  * to appear in assertions and are constrained to have the semantics of the
  * above virtual terms, e.g.:
@@ -64,21 +64,21 @@ class VtsTermCache
  public:
   VtsTermCache(QuantifiersEngine* qe);
   ~VtsTermCache() {}
-  /** 
+  /**
    * Get vts delta. The argument isFree indicates if we are getting the
    * free variant of delta. If create is false, this method returns Node::null
    * if the requested delta has not already been created.
    */
   Node getVtsDelta(bool isFree = false, bool create = true);
-  /** 
-   * Get vts infinity of type tn, where tn should be Int or Real. 
+  /**
+   * Get vts infinity of type tn, where tn should be Int or Real.
    * The argument isFree indicates if we are getting the free variant of
    * infinity. If create is false, this method returns Node::null if the
    * requested infinity has not already been created.
    */
   Node getVtsInfinity(TypeNode tn, bool isFree = false, bool create = true);
-  /** 
-   * Get all vts terms and add them to vector t. 
+  /**
+   * Get all vts terms and add them to vector t.
    * If the argument isFree is true, we return the free variant of all virtual
    * terms. If create is false, we do not add virtual terms that have not
    * already been created. If inc_delta is false, we do not include delta.
@@ -87,27 +87,27 @@ class VtsTermCache
                    bool isFree = false,
                    bool create = true,
                    bool inc_delta = true);
-  /** 
+  /**
    * Rewrite virtual terms in node n. This returns the rewritten form of n
    * after virtual term substitution.
-   * 
+   *
    * This method ensures that the returned node is equivalent to n and does
    * not contain free occurrences of the virtual terms.
    */
   Node rewriteVtsSymbols(Node n);
-  /** 
+  /**
    * This method returns true iff n contains a virtual term. If isFree is true,
    * if returns true iff n contains a free virtual term.
    */
   bool containsVtsTerm(Node n, bool isFree = false);
-  /** 
-   * This method returns true iff any term in vector n contains a virtual term. 
+  /**
+   * This method returns true iff any term in vector n contains a virtual term.
    * If isFree is true, if returns true iff any term in vector n contains a
    * free virtual term.
    */
   bool containsVtsTerm(std::vector<Node>& n, bool isFree = false);
-  /** 
-   * This method returns true iff n contains an occurence of the virtual term 
+  /**
+   * This method returns true iff n contains an occurence of the virtual term
    * infinity. If isFree is true, if returns true iff n contains the free
    * virtual term infinity.
    */
