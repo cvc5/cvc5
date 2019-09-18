76c27eead5046f5eac212f58c6755544034ca5e1
Makai Mann <makaim@stanford.edu>
2019-09-18 13:54:21 -0700

    Add test for null term

--- test/unit/api/solver_black.h
+++ test/unit/api/solver_black.h
@@ -764,6 +764,7 @@ void SolverBlack::testMkConstArray()
   Term constArr = d_solver->mkConstArray(arrSort, zero);
 
   TS_ASSERT_THROWS_NOTHING(d_solver->mkConstArray(arrSort, zero));
+  TS_ASSERT_THROWS(d_solver->mkConstArray(arrSort, Term()), CVC4ApiException&);
   TS_ASSERT_THROWS(d_solver->mkConstArray(arrSort, d_solver->mkBitVector(1, 1)), CVC4ApiException&);
   TS_ASSERT_THROWS(d_solver->mkConstArray(intSort, zero), CVC4ApiException&);
 }
