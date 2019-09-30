5796c91ca1a258db6d0d3ceab37b3f7dc2f87f2e
Ying Sheng <Ying@DN0a1ea3f3.SUNet>
2019-09-29 22:42:23 -0700

    re-order CMakeList in regress

--- src/smt/smt_engine.cpp
+++ src/smt/smt_engine.cpp
@@ -1238,12 +1238,12 @@ void SmtEngine::setDefaults() {
         d_logic.disableTheory(THEORY_UF);
         d_logic.lock();
       }
-	  if (d_logic.isTheoryEnabled(THEORY_ARRAYS))
-	  {
-		d_logic = d_logic.getUnlockedCopy();
-		d_logic.disableTheory(THEORY_ARRAYS);
-		d_logic.lock();
-	  }
+      if (d_logic.isTheoryEnabled(THEORY_ARRAYS))
+      {
+        d_logic = d_logic.getUnlockedCopy();
+        d_logic.disableTheory(THEORY_ARRAYS);
+        d_logic.lock();
+      }
     }
   }
 
