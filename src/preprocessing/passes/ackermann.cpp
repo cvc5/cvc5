20b5907ad7e0406a75877df5d976d4cdc8aa719f
Ying Sheng <Ying@Yings-MacBook-Pro.local>
2019-09-30 13:16:23 -0700

    comments modified

--- src/preprocessing/passes/ackermann.cpp
+++ src/preprocessing/passes/ackermann.cpp
@@ -170,7 +170,7 @@ void collectFunctionsAndLemmas(FunctionToArgsMap& fun_to_args,
       {
         AlwaysAssert(
             term.getKind() != kind::STORE,
-            "Cannot use Ackermannization on QF_ABV formula with stores");
+            "Cannot use Ackermannization on formula with stores to arrays");
         /* add children to the vector, so that they are processed later */
         for (TNode n : term)
         {
