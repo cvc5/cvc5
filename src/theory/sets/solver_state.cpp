652bce8281f92a0fd8dccc7a4673826d70694492
ajreynol <andrew.j.reynolds@gmail.com>
2019-10-10 12:08:48 -0500

    Working

--- src/theory/sets/solver_state.cpp
+++ src/theory/sets/solver_state.cpp
@@ -137,10 +137,10 @@ void SolverState::registerTerm(Node r, TypeNode tnn, Node n)
     d_nvar_sets[r].push_back(n);
     Trace("sets-debug2") << "Non-var-set[" << r << "] : " << n << std::endl;
   }
-  else if (nk == VARIABLE)
+  else if (n.isVar() && !d_skCache.isSkolem(n))
   {
-    // it is important that we check kind VARIABLE, due to the semantics of the
-    // universe set.
+    // it is important that we check it is a variable, but not an internally
+    // introduced skolem, due to the semantics of the universe set.
     if (tnn.isSet())
     {
       if (d_var_set.find(r) == d_var_set.end())
