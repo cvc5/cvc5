79794875822dd08c1a11bf4f261857ae0b06efa8
Ying Sheng <sqy1415@gmail.com>
2019-10-10 16:21:31 -0700

    Update src/preprocessing/passes/ackermann.cpp
    Co-Authored-By: Andres Noetzli <andres.noetzli@gmail.com>

--- src/preprocessing/passes/ackermann.cpp
+++ src/preprocessing/passes/ackermann.cpp
@@ -241,7 +241,7 @@ void collectUSortsToBV(std::unordered_set<unsigned>& used,
  * We use BFS to get all terms without duplications, and count the number of
  * different terms for each uninterpreted sort. Then for each sort, we will
  * assign a new bit-vector type with a unique size. The unique size ensures
- * that, after the replacement, the different sorts will be converted into bit
+ * that, after the replacement, the different sorts will be converted into bit-
  * vectors with different size.
  * The size is calculated to have enough capacity, that can accommodate the
  * variables occured in the original formula. */
