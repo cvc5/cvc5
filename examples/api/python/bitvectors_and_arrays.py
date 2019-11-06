9f58bd38d8409c27afd69c0427ef907d44a28e01
Makai Mann <makaim@stanford.edu>
2019-11-06 09:26:28 -0800

    Don't use specific python version in examples

--- examples/api/python/bitvectors_and_arrays.py
+++ examples/api/python/bitvectors_and_arrays.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 #####################
 #! \file bitvectors_and_arrays.py
--- examples/api/python/linear_arith.py
+++ examples/api/python/linear_arith.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 #####################
 #! \file linear_arith.py
