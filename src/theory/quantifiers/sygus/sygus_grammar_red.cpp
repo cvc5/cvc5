82c4070481ef4bdf3b55f0ac8db98fb8a97454c7
ajreynol <andrew.j.reynolds@gmail.com>
2019-11-17 10:03:32 -0600

    Improve interface for sygus datatype, fix utilities

--- src/theory/quantifiers/sygus/sygus_grammar_red.cpp
+++ src/theory/quantifiers/sygus/sygus_grammar_red.cpp
@@ -14,6 +14,7 @@
 
 #include "theory/quantifiers/sygus/sygus_grammar_red.h"
 
+#include "expr/sygus_datatype.h"
 #include "options/quantifiers_options.h"
 #include "theory/quantifiers/sygus/term_database_sygus.h"
 #include "theory/quantifiers/term_util.h"
@@ -41,12 +42,20 @@ void SygusRedundantCons::initialize(QuantifiersEngine* qe, TypeNode tn)
   {
     Trace("sygus-red") << "  Is " << dt[i].getName() << " a redundant operator?"
                        << std::endl;
+    Node sop = Node::fromExpr(dt[i].getSygusOp());
+    if (sop.getAttribute(SygusAnyConstAttribute()))
+    {
+      // the any constant constructor is never redundant
+      d_sygus_red_status.push_back(0);
+      continue;
+    }
     std::map<int, Node> pre;
     // We do not do beta reduction, since we want the arguments to match the
     // the types of the datatype.
     Node g = tds->mkGeneric(dt, i, pre, false);
     Trace("sygus-red-debug") << "  ...pre-rewrite : " << g << std::endl;
     d_gen_terms[i] = g;
+    Assert(g.getNumChildren() == dt[i].getNumArgs());
     for (unsigned j = 0, nargs = dt[i].getNumArgs(); j < nargs; j++)
     {
       pre[j] = g[j];
