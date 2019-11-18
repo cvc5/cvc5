0ca36a4c03c08fe7c017550ebecbfbd8690d65aa
Andres Noetzli <andres.noetzli@gmail.com>
2019-11-18 12:28:54 -0800

    Use -Wimplicit-fallthrough (#3464)
    This commit enables compiler warnings for implicit fallthroughs in
    switch statements that are not explicitly marked as such. The commit
    introduces a new macro `CVC4_FALLTHROUGH` that can be used to indicate
    that a fallthrough is intentional. The commit fixes existing warnings
    and a bug in the arithmetic rewriter for `abs` (the bug likely couldn't
    be triggered easily because we rewrite `abs` to an `ite` while expanding
    definitions).
    
    To have the new macro also available in the parser, the commit changes
    `src/base/check.h` to be visible to the parser (it includes
    `cvc4_private_library.h` now instead of `cvc4_private.h`).

--- src/printer/smt2/smt2_printer.cpp
+++ src/printer/smt2/smt2_printer.cpp
@@ -615,7 +615,7 @@ void Smt2Printer::toStream(std::ostream& out,
 
     // arrays theory
   case kind::SELECT:
-  case kind::STORE: typeChildren = true;
+  case kind::STORE: typeChildren = true; CVC4_FALLTHROUGH;
   case kind::PARTIAL_SELECT_0:
   case kind::PARTIAL_SELECT_1:
   case kind::ARRAY_TYPE:
@@ -751,7 +751,7 @@ void Smt2Printer::toStream(std::ostream& out,
     parametricTypeChildren = true;
     out << smtKindString(k, d_variant) << " ";
     break;
-  case kind::MEMBER: typeChildren = true;
+  case kind::MEMBER: typeChildren = true; CVC4_FALLTHROUGH;
   case kind::INSERT:
   case kind::SET_TYPE:
   case kind::SINGLETON:
