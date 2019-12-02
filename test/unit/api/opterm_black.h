207de293b26cf7771814d3cf421e64fc6116434e
makaimann <makaim@stanford.edu>
2019-12-02 13:36:19 -0800

    OpTerm Refactor: Allow retrieving OpTerm used to create Term in public C++ API (#3355)
    * Treat uninterpreted functions as a child in Term iteration
    
    * Remove unnecessary const_iterator constructor
    
    * Add parameter comments to const_iterator constructor
    
    * Use operator[] instead of storing a vector of Expr children
    
    * Switch pos member variable from int to uint32_t
    
    * Add comment about how UFs are treated in iteration
    
    * Allow OpTerm to contain a single Kind, update OpTerm construction
    
    * Update mkTerm to use only an OpTerm (and not also a Kind)
    
    * Remove unnecessary function checkMkOpTerm
    
    * Update mkOpTerm comments to not use _OP Kinds
    
    * Update examples to use new mkTerm
    
    * First pass on fixing unit test
    
    * Override kind for Constructor and Selector Terms
    
    * More fixes to unit tests
    
    * Updates to parser
    
    * Remove old assert (for Kind, OpTerm pattern which was removed)
    
    * Remove *_OP kinds from public API
    
    * Add hasOpTerm and getOpTerm methods to Term
    
    * Add test for UF iteration
    
    * Add unit test for getOpTerm
    
    * Move OpTerm implementation above Term implemenation to match header file
    
    Moved in header because Term::getOpTerm() returns an OpTerm and the compiler complains
    if OpTerm is not defined earlier. Simply moving the declaration is easier/cleaner than
    forward declaring within the same file that it's declared.
    
    * Fix mkTerm in datatypes-new.cpp example
    
    * Use helper function for creating term from Kind to avoid nested API calls
    
    * Rename: OpTerm->Op in API
    
    * Update OpTerm->Op in examples/tests/parser
    
    * Add case for APPLY_TESTER
    
    * operator term -> operator
    
    * Update src/api/cvc4cpp.h
    
    Co-Authored-By: Aina Niemetz <aina.niemetz@gmail.com>
    
    * Comment comment suggestion
    
    Co-Authored-By: Aina Niemetz <aina.niemetz@gmail.com>
    
    * Add not-null checks and implement Op from a single Kind constructor
    
    * Undo sed mistake for OpTerm replacement
    
    * Add 'd_' prefix to member vars
    
    * Fix comment and remove old commented-out code
    
    * Formatting
    
    * Revert "Formatting"
    
    This reverts commit d1d5fc1fb71496daeba668e97cad84c213200ba9.
    
    * More fixes for sed mistakes
    
    * Minor formatting
    
    * Undo changes in CVC parser
    
    * Add isIndexed and prefix with d_
    
    * Create helper function for isIndexed to avoid calling API functions in other API functions

--- test/unit/api/opterm_black.h
+++ test/unit/api/opterm_black.h
@@ -18,7 +18,7 @@
 
 using namespace CVC4::api;
 
-class OpTermBlack : public CxxTest::TestSuite
+class OpBlack : public CxxTest::TestSuite
 {
  public:
   void setUp() override {}
@@ -27,6 +27,7 @@ class OpTermBlack : public CxxTest::TestSuite
   void testGetKind();
   void testGetSort();
   void testIsNull();
+  void testOpTermFromKind();
   void testGetIndicesString();
   void testGetIndicesKind();
   void testGetIndicesUint();
@@ -36,168 +37,173 @@ class OpTermBlack : public CxxTest::TestSuite
   Solver d_solver;
 };
 
-void OpTermBlack::testGetKind()
+void OpBlack::testGetKind()
 {
-  OpTerm x;
+  Op x;
   TS_ASSERT_THROWS(x.getSort(), CVC4ApiException&);
-  x = d_solver.mkOpTerm(BITVECTOR_EXTRACT_OP, 31, 1);
+  x = d_solver.mkOp(BITVECTOR_EXTRACT, 31, 1);
   TS_ASSERT_THROWS_NOTHING(x.getKind());
 }
 
-void OpTermBlack::testGetSort()
+void OpBlack::testGetSort()
 {
-  OpTerm x;
+  Op x;
   TS_ASSERT_THROWS(x.getSort(), CVC4ApiException&);
-  x = d_solver.mkOpTerm(BITVECTOR_EXTRACT_OP, 31, 1);
+  x = d_solver.mkOp(BITVECTOR_EXTRACT, 31, 1);
   TS_ASSERT_THROWS_NOTHING(x.getSort());
 }
 
-void OpTermBlack::testIsNull()
+void OpBlack::testIsNull()
 {
-  OpTerm x;
+  Op x;
   TS_ASSERT(x.isNull());
-  x = d_solver.mkOpTerm(BITVECTOR_EXTRACT_OP, 31, 1);
+  x = d_solver.mkOp(BITVECTOR_EXTRACT, 31, 1);
   TS_ASSERT(!x.isNull());
 }
 
-void OpTermBlack::testGetIndicesString()
+void OpBlack::testOpTermFromKind()
 {
-  OpTerm x;
+  Op plus(PLUS);
+  TS_ASSERT(!plus.isIndexed());
+  TS_ASSERT_THROWS(plus.getIndices<uint32_t>(), CVC4ApiException&);
+}
+
+void OpBlack::testGetIndicesString()
+{
+  Op x;
   TS_ASSERT_THROWS(x.getIndices<std::string>(), CVC4ApiException&);
 
-  OpTerm divisible_ot = d_solver.mkOpTerm(DIVISIBLE_OP, 4);
+  Op divisible_ot = d_solver.mkOp(DIVISIBLE, 4);
+  TS_ASSERT(divisible_ot.isIndexed());
   std::string divisible_idx = divisible_ot.getIndices<std::string>();
   TS_ASSERT(divisible_idx == "4");
 
-  OpTerm record_update_ot = d_solver.mkOpTerm(RECORD_UPDATE_OP, "test");
+  Op record_update_ot = d_solver.mkOp(RECORD_UPDATE, "test");
   std::string record_update_idx = record_update_ot.getIndices<std::string>();
   TS_ASSERT(record_update_idx == "test");
   TS_ASSERT_THROWS(record_update_ot.getIndices<uint32_t>(), CVC4ApiException&);
 }
 
-void OpTermBlack::testGetIndicesKind()
+void OpBlack::testGetIndicesKind()
 {
-  OpTerm chain_ot = d_solver.mkOpTerm(CHAIN_OP, AND);
+  Op chain_ot = d_solver.mkOp(CHAIN, AND);
+  TS_ASSERT(chain_ot.isIndexed());
   Kind chain_idx = chain_ot.getIndices<Kind>();
   TS_ASSERT(chain_idx == AND);
 }
 
-void OpTermBlack::testGetIndicesUint()
+void OpBlack::testGetIndicesUint()
 {
-  OpTerm bitvector_repeat_ot = d_solver.mkOpTerm(BITVECTOR_REPEAT_OP, 5);
+  Op bitvector_repeat_ot = d_solver.mkOp(BITVECTOR_REPEAT, 5);
+  TS_ASSERT(bitvector_repeat_ot.isIndexed());
   uint32_t bitvector_repeat_idx = bitvector_repeat_ot.getIndices<uint32_t>();
   TS_ASSERT(bitvector_repeat_idx == 5);
   TS_ASSERT_THROWS(
       (bitvector_repeat_ot.getIndices<std::pair<uint32_t, uint32_t>>()),
       CVC4ApiException&);
 
-  OpTerm bitvector_zero_extend_ot =
-      d_solver.mkOpTerm(BITVECTOR_ZERO_EXTEND_OP, 6);
+  Op bitvector_zero_extend_ot = d_solver.mkOp(BITVECTOR_ZERO_EXTEND, 6);
   uint32_t bitvector_zero_extend_idx =
       bitvector_zero_extend_ot.getIndices<uint32_t>();
   TS_ASSERT(bitvector_zero_extend_idx == 6);
 
-  OpTerm bitvector_sign_extend_ot =
-      d_solver.mkOpTerm(BITVECTOR_SIGN_EXTEND_OP, 7);
+  Op bitvector_sign_extend_ot = d_solver.mkOp(BITVECTOR_SIGN_EXTEND, 7);
   uint32_t bitvector_sign_extend_idx =
       bitvector_sign_extend_ot.getIndices<uint32_t>();
   TS_ASSERT(bitvector_sign_extend_idx == 7);
 
-  OpTerm bitvector_rotate_left_ot =
-      d_solver.mkOpTerm(BITVECTOR_ROTATE_LEFT_OP, 8);
+  Op bitvector_rotate_left_ot = d_solver.mkOp(BITVECTOR_ROTATE_LEFT, 8);
   uint32_t bitvector_rotate_left_idx =
       bitvector_rotate_left_ot.getIndices<uint32_t>();
   TS_ASSERT(bitvector_rotate_left_idx == 8);
 
-  OpTerm bitvector_rotate_right_ot =
-      d_solver.mkOpTerm(BITVECTOR_ROTATE_RIGHT_OP, 9);
+  Op bitvector_rotate_right_ot = d_solver.mkOp(BITVECTOR_ROTATE_RIGHT, 9);
   uint32_t bitvector_rotate_right_idx =
       bitvector_rotate_right_ot.getIndices<uint32_t>();
   TS_ASSERT(bitvector_rotate_right_idx == 9);
 
-  OpTerm int_to_bitvector_ot = d_solver.mkOpTerm(INT_TO_BITVECTOR_OP, 10);
+  Op int_to_bitvector_ot = d_solver.mkOp(INT_TO_BITVECTOR, 10);
   uint32_t int_to_bitvector_idx = int_to_bitvector_ot.getIndices<uint32_t>();
   TS_ASSERT(int_to_bitvector_idx == 10);
 
-  OpTerm floatingpoint_to_ubv_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_UBV_OP, 11);
+  Op floatingpoint_to_ubv_ot = d_solver.mkOp(FLOATINGPOINT_TO_UBV, 11);
   uint32_t floatingpoint_to_ubv_idx =
       floatingpoint_to_ubv_ot.getIndices<uint32_t>();
   TS_ASSERT(floatingpoint_to_ubv_idx == 11);
 
-  OpTerm floatingpoint_to_ubv_total_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_UBV_TOTAL_OP, 12);
+  Op floatingpoint_to_ubv_total_ot =
+      d_solver.mkOp(FLOATINGPOINT_TO_UBV_TOTAL, 12);
   uint32_t floatingpoint_to_ubv_total_idx =
       floatingpoint_to_ubv_total_ot.getIndices<uint32_t>();
   TS_ASSERT(floatingpoint_to_ubv_total_idx == 12);
 
-  OpTerm floatingpoint_to_sbv_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_SBV_OP, 13);
+  Op floatingpoint_to_sbv_ot = d_solver.mkOp(FLOATINGPOINT_TO_SBV, 13);
   uint32_t floatingpoint_to_sbv_idx =
       floatingpoint_to_sbv_ot.getIndices<uint32_t>();
   TS_ASSERT(floatingpoint_to_sbv_idx == 13);
 
-  OpTerm floatingpoint_to_sbv_total_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_SBV_TOTAL_OP, 14);
+  Op floatingpoint_to_sbv_total_ot =
+      d_solver.mkOp(FLOATINGPOINT_TO_SBV_TOTAL, 14);
   uint32_t floatingpoint_to_sbv_total_idx =
       floatingpoint_to_sbv_total_ot.getIndices<uint32_t>();
   TS_ASSERT(floatingpoint_to_sbv_total_idx == 14);
 
-  OpTerm tuple_update_ot = d_solver.mkOpTerm(TUPLE_UPDATE_OP, 5);
+  Op tuple_update_ot = d_solver.mkOp(TUPLE_UPDATE, 5);
   uint32_t tuple_update_idx = tuple_update_ot.getIndices<uint32_t>();
   TS_ASSERT(tuple_update_idx == 5);
   TS_ASSERT_THROWS(tuple_update_ot.getIndices<std::string>(),
                    CVC4ApiException&);
 }
 
-void OpTermBlack::testGetIndicesPairUint()
+void OpBlack::testGetIndicesPairUint()
 {
-  OpTerm bitvector_extract_ot = d_solver.mkOpTerm(BITVECTOR_EXTRACT_OP, 4, 0);
+  Op bitvector_extract_ot = d_solver.mkOp(BITVECTOR_EXTRACT, 4, 0);
+  TS_ASSERT(bitvector_extract_ot.isIndexed());
   std::pair<uint32_t, uint32_t> bitvector_extract_indices =
       bitvector_extract_ot.getIndices<std::pair<uint32_t, uint32_t>>();
   TS_ASSERT((bitvector_extract_indices == std::pair<uint32_t, uint32_t>{4, 0}));
 
-  OpTerm floatingpoint_to_fp_ieee_bitvector_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_FP_IEEE_BITVECTOR_OP, 4, 25);
+  Op floatingpoint_to_fp_ieee_bitvector_ot =
+      d_solver.mkOp(FLOATINGPOINT_TO_FP_IEEE_BITVECTOR, 4, 25);
   std::pair<uint32_t, uint32_t> floatingpoint_to_fp_ieee_bitvector_indices =
       floatingpoint_to_fp_ieee_bitvector_ot
           .getIndices<std::pair<uint32_t, uint32_t>>();
   TS_ASSERT((floatingpoint_to_fp_ieee_bitvector_indices
              == std::pair<uint32_t, uint32_t>{4, 25}));
 
-  OpTerm floatingpoint_to_fp_floatingpoint_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_FP_FLOATINGPOINT_OP, 4, 25);
+  Op floatingpoint_to_fp_floatingpoint_ot =
+      d_solver.mkOp(FLOATINGPOINT_TO_FP_FLOATINGPOINT, 4, 25);
   std::pair<uint32_t, uint32_t> floatingpoint_to_fp_floatingpoint_indices =
       floatingpoint_to_fp_floatingpoint_ot
           .getIndices<std::pair<uint32_t, uint32_t>>();
   TS_ASSERT((floatingpoint_to_fp_floatingpoint_indices
              == std::pair<uint32_t, uint32_t>{4, 25}));
 
-  OpTerm floatingpoint_to_fp_real_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_FP_REAL_OP, 4, 25);
+  Op floatingpoint_to_fp_real_ot =
+      d_solver.mkOp(FLOATINGPOINT_TO_FP_REAL, 4, 25);
   std::pair<uint32_t, uint32_t> floatingpoint_to_fp_real_indices =
       floatingpoint_to_fp_real_ot.getIndices<std::pair<uint32_t, uint32_t>>();
   TS_ASSERT((floatingpoint_to_fp_real_indices
              == std::pair<uint32_t, uint32_t>{4, 25}));
 
-  OpTerm floatingpoint_to_fp_signed_bitvector_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_FP_SIGNED_BITVECTOR_OP, 4, 25);
+  Op floatingpoint_to_fp_signed_bitvector_ot =
+      d_solver.mkOp(FLOATINGPOINT_TO_FP_SIGNED_BITVECTOR, 4, 25);
   std::pair<uint32_t, uint32_t> floatingpoint_to_fp_signed_bitvector_indices =
       floatingpoint_to_fp_signed_bitvector_ot
           .getIndices<std::pair<uint32_t, uint32_t>>();
   TS_ASSERT((floatingpoint_to_fp_signed_bitvector_indices
              == std::pair<uint32_t, uint32_t>{4, 25}));
 
-  OpTerm floatingpoint_to_fp_unsigned_bitvector_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_FP_UNSIGNED_BITVECTOR_OP, 4, 25);
+  Op floatingpoint_to_fp_unsigned_bitvector_ot =
+      d_solver.mkOp(FLOATINGPOINT_TO_FP_UNSIGNED_BITVECTOR, 4, 25);
   std::pair<uint32_t, uint32_t> floatingpoint_to_fp_unsigned_bitvector_indices =
       floatingpoint_to_fp_unsigned_bitvector_ot
           .getIndices<std::pair<uint32_t, uint32_t>>();
   TS_ASSERT((floatingpoint_to_fp_unsigned_bitvector_indices
              == std::pair<uint32_t, uint32_t>{4, 25}));
 
-  OpTerm floatingpoint_to_fp_generic_ot =
-      d_solver.mkOpTerm(FLOATINGPOINT_TO_FP_GENERIC_OP, 4, 25);
+  Op floatingpoint_to_fp_generic_ot =
+      d_solver.mkOp(FLOATINGPOINT_TO_FP_GENERIC, 4, 25);
   std::pair<uint32_t, uint32_t> floatingpoint_to_fp_generic_indices =
       floatingpoint_to_fp_generic_ot
           .getIndices<std::pair<uint32_t, uint32_t>>();
