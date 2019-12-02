a055db8adb1f9f367cd4303df584a624c4cfc810
ajreynol <andrew.j.reynolds@gmail.com>
2019-12-02 13:24:49 -0600

    Format

--- src/theory/arith/nonlinear_extension.cpp
+++ src/theory/arith/nonlinear_extension.cpp
@@ -544,7 +544,7 @@ void NonlinearExtension::sendLemmas(const std::vector<Node>& out,
                                     bool preprocess)
 {
   for (const Node& lem : out)
-  { 
+  {
     Trace("nl-ext-lemma") << "NonlinearExtension::Lemma : " << lem << std::endl;
     d_containing.getOutputChannel().lemma(lem, false, preprocess);
     // add to cache if not preprocess
@@ -1286,14 +1286,14 @@ void NonlinearExtension::check(Theory::Effort e) {
         return;
       }
     }
-    // get the values that should be replaced in the model	
-    d_model.getModelValueRepair(arithModel, approximations);	
-    // those that are exact are written as exact approximations to the model	
-    for (std::pair<const Node, Node>& r : arithModel)	
-    {	
-      Node eq = r.first.eqNode(r.second);	
-      eq = Rewriter::rewrite(eq);	
-      tm->recordApproximation(r.first, eq);	
+    // get the values that should be replaced in the model
+    d_model.getModelValueRepair(arithModel, approximations);
+    // those that are exact are written as exact approximations to the model
+    for (std::pair<const Node, Node>& r : arithModel)
+    {
+      Node eq = r.first.eqNode(r.second);
+      eq = Rewriter::rewrite(eq);
+      tm->recordApproximation(r.first, eq);
     }
     // those that are approximate are recorded as approximations
     for (std::pair<const Node, Node>& a : approximations)
