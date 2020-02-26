f053346f5c779053dfb177015301cb56f4e71932
ajreynol <andrew.j.reynolds@gmail.com>
2020-02-25 21:18:25 -0600

    Split

--- src/expr/dtype.cpp
+++ src/expr/dtype.cpp
@@ -641,13 +641,6 @@ const DTypeConstructor& DType::operator[](size_t index) const
   return *d_constructors[index];
 }
 
-DTypeConstructor DType::getConstructorForTerm(Node cons) const
-{
-  size_t index = indexOf(cons);
-  Assert(index < getNumConstructors());
-  return *d_constructors[index];
-}
-
 Node DType::getSharedSelector(TypeNode dtt, TypeNode t, size_t index) const
 {
   Assert(isResolved());
