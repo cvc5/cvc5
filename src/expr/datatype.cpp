e914d6dcb17edc3f1d3b838533f4613c2370ddb3
ajreynol <andrew.j.reynolds@gmail.com>
2020-02-27 08:54:30 -0600

    Fix comment

--- src/expr/datatype.cpp
+++ src/expr/datatype.cpp
@@ -223,15 +223,18 @@ void Datatype::addConstructor(const DatatypeConstructor& c) {
 void Datatype::setSygus( Type st, Expr bvl, bool allow_const, bool allow_all ){
   PrettyCheckArgument(
       !isResolved(), this, "cannot set sygus type to a finalized Datatype");
-  // We can be in a case where the only rule specified was (Variable T)
-  // and there are no variables of type T, in which case this is a bogus
-  // grammar. This results in the error below.
-  // We can also be in a case where the only rule specified was
+  // We can be in a case where the only rule specified was
   // (Constant T), in which case we have not yet added a constructor. We
   // ensure an arbitrary constant is added in this case. We additionally
-  // add a constant if the grammar allows it regardless of whether the
-  // datatype has other constructors, since this ensures the datatype is
-  // well-founded (see 3423).
+  // add a constant if the grammar has only non-nullary constructors, since this
+  // ensures the datatype is well-founded (see 3423).
+  // Notice we only want to do this for sygus datatypes that are user-provided.
+  // At the moment, the condition !allow_all implies the grammar is
+  // user-provided and hence may require a default constant.
+  // TODO (https://github.com/CVC4/cvc4-projects/issues/38):
+  // In an API for SyGuS, it probably makes more sense for the user to
+  // explicitly add the "any constant" constructor with a call instead of
+  // passing a flag. This would make the block of code unnecessary.
   if (allow_const && !allow_all)
   {
     // if i don't already have a constant (0-ary constructor)
