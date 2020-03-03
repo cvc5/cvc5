78e5181236d5b8c8bd052c2b05bdd8612011d5d8
mudathirmahgoub <mudathirmahgoub@gmail.com>
2020-03-03 01:43:48 -0600

    used binary numbers for sets

--- src/theory/sets/theory_sets_type_enumerator.h
+++ src/theory/sets/theory_sets_type_enumerator.h
@@ -32,25 +32,25 @@ namespace sets {
 
 class SetEnumerator : public TypeEnumeratorBase<SetEnumerator>
 {
- private:
-  NodeManager* d_nm;
-  TypeEnumerator d_elementTypeEnumerator;
-  bool d_finished;
-
  public:
   SetEnumerator(TypeNode type, TypeEnumeratorProperties* tep = nullptr)
       : TypeEnumeratorBase<SetEnumerator>(type),
-        d_nm(NodeManager::currentNM()),
-        d_elementTypeEnumerator(type.getSetElementType(), tep),
-        d_finished(false)
+        d_nodeManager(NodeManager::currentNM()),
+        d_elementEnumerator(type.getSetElementType(), tep),
+        d_isElementEnumeratorFinished(false),
+        d_currentSetIndex(0),
+        d_currentSet()
   {
+    d_currentSet = d_nodeManager->mkConst(EmptySet(type.toType()));
   }
 
   SetEnumerator(const SetEnumerator& enumerator)
       : TypeEnumeratorBase<SetEnumerator>(enumerator.getType()),
-        d_nm(enumerator.d_nm),
-        d_elementTypeEnumerator(enumerator.d_elementTypeEnumerator),
-        d_finished(enumerator.d_finished)
+        d_nodeManager(enumerator.d_nodeManager),
+        d_elementEnumerator(enumerator.d_elementEnumerator),
+        d_isElementEnumeratorFinished(enumerator.d_isElementEnumeratorFinished),
+        d_currentSetIndex(enumerator.d_currentSetIndex),
+        d_currentSet(enumerator.d_currentSet)
   {
   }
 
@@ -58,56 +58,86 @@ class SetEnumerator : public TypeEnumeratorBase<SetEnumerator>
 
   Node operator*() override
   {
-    if (d_finished)
+    if (d_isElementEnumeratorFinished)
     {
       throw NoMoreValuesException(getType());
     }
 
-    Trace("set-type-enum") << "elements: " << *d_elementTypeEnumerator
-                           << std::endl;
-
-    Node n = d_nm->mkNode(Kind::SINGLETON, *d_elementTypeEnumerator);
-
-    Assert(n.isConst());
-    Assert(n == Rewriter::rewrite(n));
-
-    return n;
+    return d_currentSet;
   }
 
   SetEnumerator& operator++() override
   {
-    Trace("set-type-enum") << "operator++ called, **this = " << **this
-                           << std::endl;
-
-    if (d_finished)
+    if (d_isElementEnumeratorFinished)
     {
       Trace("set-type-enum") << "operator++ finished!" << std::endl;
       return *this;
     }
 
-    ++d_elementTypeEnumerator;
+    d_currentSetIndex++;
 
-    TypeEnumerator newEnumerator(d_elementTypeEnumerator);
-    ++newEnumerator;
-    if (newEnumerator.isFinished())
+    // generate new element
+    if (d_currentSetIndex == (unsigned)(1 << d_elementsSoFar.size()))
     {
-      Trace("set-type-enum") << "operator++ finished!" << std::endl;
-      d_finished = true;
-      return *this;
+      Node element = *d_elementEnumerator;
+      d_elementsSoFar.push_back(element);
+      d_elementEnumerator++;
+      d_currentSet = d_nodeManager->mkNode(Kind::SINGLETON, element);
+
+      TypeEnumerator newEnumerator(d_elementEnumerator);
+      newEnumerator++;
+      if (newEnumerator.isFinished())
+      {
+        d_isElementEnumeratorFinished = true;
+      }
+    }
+    else
+    {
+      std::vector<unsigned> indices = fromDecimalToBinary(d_currentSetIndex);
+      std::vector<Node> elements;
+      for (unsigned i : indices)
+      {
+        elements.push_back(d_elementsSoFar[i - 1]);
+      }
+      d_currentSet = NormalForm::elementsToSet(
+          std::set<TNode>(elements.begin(), elements.end()), getType());
     }
 
-    Trace("set-type-enum") << "operator++ returning, **this = " << **this
-                           << std::endl;
+    Assert(d_currentSet.isConst());
+    Assert(d_currentSet == Rewriter::rewrite(d_currentSet));
     return *this;
   }
 
   bool isFinished() override
   {
-    Trace("set-type-enum") << "isFinished returning: " << d_finished
-                           << std::endl;
-    return d_finished;
+    Trace("set-type-enum") << "isFinished returning: "
+                           << d_isElementEnumeratorFinished << std::endl;
+    return d_isElementEnumeratorFinished;
   }
 
+ private:
+  std::vector<unsigned> fromDecimalToBinary(unsigned decimal)
+  {
+    std::vector<unsigned> binary;
+    int i = 0;
+    while (decimal > 0)
+    {
+      i++;
+      if (decimal % 2 == 1)
+      {
+        binary.push_back(i);
+      }
+      decimal = (unsigned)decimal >> 1;
+    }
+    return std::move(binary);
+  }
+
+  NodeManager* d_nodeManager;
+  TypeEnumerator d_elementEnumerator;
+  bool d_isElementEnumeratorFinished;
+  std::vector<Node> d_elementsSoFar;
+  unsigned int d_currentSetIndex;
+  Node d_currentSet;
 }; /* class SetEnumerator */
 
 }  // namespace sets
