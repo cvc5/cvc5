833c274debd7a544d16d2c4480465aa4311ebda5
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2020-03-06 11:46:45 -0600

    Update src/printer/ast/ast_printer.cpp
    Co-Authored-By: Andres Noetzli <andres.noetzli@gmail.com>

--- src/printer/ast/ast_printer.cpp
+++ src/printer/ast/ast_printer.cpp
@@ -393,12 +393,9 @@ static void toStream(std::ostream& out, const DatatypeDeclarationCommand* c)
 {
   const vector<Type>& datatypes = c->getDatatypes();
   out << "DatatypeDeclarationCommand([";
-  for (vector<Type>::const_iterator i = datatypes.begin(),
-                                    i_end = datatypes.end();
-       i != i_end;
-       ++i)
+  for (const Type& t : datatypes)
   {
-    out << *i << ";" << endl;
+    out << t << ";" << endl;
   }
   out << "])";
 }
