4a7565da0288045e633bb533e9db5baa099d9d72
Aina Niemetz <aina.niemetz@gmail.com>
2020-03-11 20:41:08 -0700

    Addressed

--- src/main/command_executor.cpp
+++ src/main/command_executor.cpp
@@ -49,15 +49,13 @@ void setNoLimitCPU() {
 void printStatsIncremental(std::ostream& out, const std::string& prvsStatsString, const std::string& curStatsString);
 
 CommandExecutor::CommandExecutor(Options& options)
-    : d_solver(nullptr),
-      d_smtEngine(nullptr),
+    : d_solver(new api::Solver(&options)),
+      d_smtEngine(d_solver->getSmtEngine()),
       d_options(options),
       d_stats("driver"),
       d_result(),
       d_replayStream(nullptr)
 {
-  d_solver.reset(new api::Solver(&options));
-  d_smtEngine = d_solver->getSmtEngine();
 }
 
 void CommandExecutor::flushStatistics(std::ostream& out) const
