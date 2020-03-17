b3eb1e2bff055a9b3b41c280c97a6b2fe5eefab4
Alex Ozdemir <aozdemir@hmc.edu>
2020-03-17 12:02:35 -0700

    Add copywrite notice

--- src/expr/node_traversal.cpp
+++ src/expr/node_traversal.cpp
@@ -1,3 +1,17 @@
+/*********************                                                        */
+/*! \file node_traversal.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Alex Ozdemir
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Iterators for traversing nodes.
+ **/
+
 #include "node_traversal.h"
 
 #include <iostream>
