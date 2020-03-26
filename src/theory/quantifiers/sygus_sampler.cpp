f7eed236ba9fe2e36748b035bd0ab67d5ebf8e2f
ajreynol <andrew.j.reynolds@gmail.com>
2020-03-26 08:51:15 -0500

    Fix

--- src/theory/quantifiers/sygus_sampler.cpp
+++ src/theory/quantifiers/sygus_sampler.cpp
@@ -561,7 +561,7 @@ Node SygusSampler::getRandomValue(TypeNode tn)
       {
         d_rstring_alphabet.push_back(ch);
         Trace("sygus-sample-str-alpha")
-            << " \"" << static_cast<char>(ch) << "\"";
+            << " \\u" << ch;
       }
       Trace("sygus-sample-str-alpha") << std::endl;
     }
