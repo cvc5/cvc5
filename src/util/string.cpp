1644219297df865e54dfba6721ba6c0eaef676fe
ajreynol <andrew.j.reynolds@gmail.com>
2020-03-27 13:29:43 -0500

    Dummy commit

--- src/util/string.cpp
+++ src/util/string.cpp
@@ -9,7 +9,7 @@
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
- ** \brief Implementation of the string data type
+ ** \brief Implementation of the string data type.
  **/
 
 #include "util/string.h"
