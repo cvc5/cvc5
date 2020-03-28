357f181ad957c0f2939658ffb6e402367e815999
Abdalrhman Mohamed <abdalrhman-mohamed@uiowa.edu>
2020-03-28 13:51:56 -0500

    Follow v2_6 policy.

--- src/parser/smt2/smt2.cpp
+++ src/parser/smt2/smt2.cpp
@@ -330,7 +330,7 @@ api::Term Smt2::getExpressionForNameAndType(const std::string& name,
 
 bool Smt2::getTesterName(api::Term cons, std::string& name)
 {
-  if ((v2_6() && strictModeEnabled()) || sygus_v2())
+  if ((v2_6() || sygus_v2()) && strictModeEnabled())
   {
     // 2.6 or above uses indexed tester symbols, if we are in strict mode,
     // we do not automatically define is-cons for constructor cons.
