833f4a2783d8fb52d0cde0415aa226f0445c3b59
ajreynol <andrew.j.reynolds@gmail.com>
2020-04-04 10:59:27 -0500

    Fix

--- src/theory/quantifiers/theory_quantifiers.cpp
+++ src/theory/quantifiers/theory_quantifiers.cpp
@@ -38,6 +38,7 @@ using namespace CVC4::theory::quantifiers;
 TheoryQuantifiers::TheoryQuantifiers(Context* c, context::UserContext* u, OutputChannel& out, Valuation valuation, const LogicInfo& logicInfo) :
     Theory(THEORY_QUANTIFIERS, c, u, out, valuation, logicInfo)
 {
+  out.handleUserAttribute( "fun-def", this );
   out.handleUserAttribute( "sygus", this );
   out.handleUserAttribute("quant-name", this);
   out.handleUserAttribute("sygus-synth-grammar", this);
