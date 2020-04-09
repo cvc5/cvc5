2b8e7216fc5be11dfd84ba2c01c85c6f489bdc0d
ajreynol <andrew.j.reynolds@gmail.com>
2020-04-08 22:26:27 -0500

    Format

--- src/theory/quantifiers/relevant_domain.cpp
+++ src/theory/quantifiers/relevant_domain.cpp
@@ -226,7 +226,7 @@ void RelevantDomain::computeRelevantDomainLit( Node q, bool hasPol, bool pol, No
     d_rel_dom_lit[hasPol][pol][n].d_merge = false;
     int varCount = 0;
     int varCh = -1;
-    TermUtil * tu = d_qe->getTermUtil();
+    TermUtil* tu = d_qe->getTermUtil();
     for( unsigned i=0; i<n.getNumChildren(); i++ ){
       if( n[i].getKind()==INST_CONSTANT ){
         // must get the quantified formula this belongs to, which may be
