cab62d987ca382023e4929d1ecb8629977dd24f6
ajreynol <andrew.j.reynolds@gmail.com>
2020-04-12 12:24:20 -0500

    Use fresh variables when miniscoping

--- src/theory/quantifiers/quantifiers_rewriter.cpp
+++ src/theory/quantifiers/quantifiers_rewriter.cpp
@@ -1664,6 +1664,7 @@ Node QuantifiersRewriter::mkForall( std::vector< Node >& args, Node body, std::v
 
 //computes miniscoping, also eliminates variables that do not occur free in body
 Node QuantifiersRewriter::computeMiniscoping( std::vector< Node >& args, Node body, QAttributes& qa ){
+  NodeManager * nm = NodeManager::currentNM();
   if( body.getKind()==FORALL ){
     //combine prenex
     std::vector< Node > newArgs;
@@ -1679,8 +1680,15 @@ Node QuantifiersRewriter::computeMiniscoping( std::vector< Node >& args, Node bo
     {
       // break apart
       NodeBuilder<> t(kind::AND);
-      for( unsigned i=0; i<body.getNumChildren(); i++ ){
-        t << computeMiniscoping( args, body[i], qa );
+      for (const Node& b : body){
+        // must create fresh copy of args
+        std::vector<Node> argsc;
+        for (const Node& v : args)
+        {
+          argsc.push_back(nm->mkBoundVar(v.getType()));
+        }
+        Node bodyc = b.substitute(args.begin(),args.end(),argsc.begin(), argsc.end());
+        t << computeMiniscoping( argsc, bodyc, qa );
       }
       Node retVal = t;
       return retVal;
