124993296855b7ddcbc005bdb68964f839095cb2
ajreynol <andrew.j.reynolds@gmail.com>
2020-04-14 13:03:49 -0500

    Add

---
+++ src/expr/proof_node.h
@@ -0,0 +1,74 @@
+/*********************                                                        */
+/*! \file proof_node.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Proof node utility
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef CVC4__EXPR__PROOF_NODE_H
+#define CVC4__EXPR__PROOF_NODE_H
+
+#include <vector>
+
+#include "expr/node.h"
+#include "expr/proof_step.h"
+
+namespace CVC4 {
+
+/** A node in a proof
+ *
+ * A ProofNode represents a single step in a proof. It contains:
+ * (1) d_id, an identifier indicating the type of inference,
+ * (2) d_children, the child ProofNode objects indicating its premises,
+ * (3) d_args, additional arguments used to determine the conclusion,
+ * (4) d_proven, cache of the formula that this ProofNode proves.
+ *
+ * Overall, a ProofNode and its children form a directed acyclic graph.
+ *
+ * A ProofNode is intended to be mutable in that (1), (2) and (3) can be
+ * modified. However, (4) is intended to be immutable. The method setValue
+ * is private and can be called by objects that manage ProofNode objects.
+ */
+class ProofNode
+{
+ public:
+  ProofNode(ProofStep id,
+            const std::vector<std::shared_ptr<ProofNode>>& children,
+            const std::vector<Node>& args);
+  ~ProofNode() {}
+  /** get id */
+  ProofStep getId() const;
+  /** get what this node proves, or the null node if this is an invalid proof */
+  Node getResult() const;
+  /** get assumptions */
+  void getAssumptions(std::vector<Node>& assump);
+  /** print debug */
+  void printDebug(std::ostream& os) const;
+
+ private:
+  /** set value, called to overwrite the value */
+  void setValue(ProofStep id,
+                const std::vector<std::shared_ptr<ProofNode>>& children,
+                const std::vector<Node>& args);
+  /** The proof step */
+  ProofStep d_id;
+  /** The children of this node */
+  std::vector<std::shared_ptr<ProofNode>> d_children;
+  /** arguments of this node */
+  std::vector<Node> d_args;
+  /** The cache of the fact that has been proven, modifiable by ProofChecker */
+  Node d_proven;
+};
+
+}  // namespace CVC4
+
+#endif /* CVC4__EXPR__PROOF_NODE_H */
---
+++ src/expr/proof_step.cpp
@@ -0,0 +1,56 @@
+/*********************                                                        */
+/*! \file proof_step.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implementation of proof step
+ **/
+
+#include "expr/proof_step.h"
+
+namespace CVC4 {
+
+const char* toString(ProofStep id)
+{
+  switch (id)
+  {
+    //================================================= CORE
+    case ProofStep::ASSUME: return "ASSUME";
+    case ProofStep::SUBS: return "SUBS";
+    case ProofStep::REWRITE: return "REWRITE";
+    case ProofStep::REWRITE_PRED: return "REWRITE_PRED";
+    case ProofStep::SPLIT: return "SPLIT";
+    //================================================= EQUALITY
+    case ProofStep::REFL: return "REFL";
+    case ProofStep::SYMM: return "SYMM";
+    case ProofStep::TRANS: return "TRANS";
+    case ProofStep::CONG: return "CONG";
+    //================================================= THEORY_STRINGS
+    case ProofStep::CONCAT_ENDP_UNIFY: return "CONCAT_ENDP_UNIFY";
+    case ProofStep::CONCAT_UNIFY: return "CONCAT_UNIFY";
+    case ProofStep::CONCAT_SPLIT: return "CONCAT_SPLIT";
+    case ProofStep::CONCAT_LPROP: return "CONCAT_LPROP";
+    case ProofStep::CONCAT_CPROP: return "CONCAT_CPROP";
+    case ProofStep::CTN_NOT_EQUAL: return "CTN_NOT_EQUAL";
+    case ProofStep::REDUCTION: return "REDUCTION";
+    case ProofStep::RE_INTER: return "RE_INTER";
+    case ProofStep::RE_UNFOLD: return "RE_UNFOLD";
+    //================================================= UNKNOWN
+    case ProofStep::UNKNOWN: return "UNKNOWN";
+    default: return "?";
+  }
+}
+
+std::ostream& operator<<(std::ostream& out, ProofStep id)
+{
+  out << toString(id);
+  return out;
+}
+
+}  // namespace CVC4
