d6843a26cbaf0fb200311d240464aa5ab7eb19f5
ajreynol <andrew.j.reynolds@gmail.com>
2020-04-15 17:12:17 -0500

    Add ProofNodeManager and ProofChecker

---
+++ src/expr/proof_checker.cpp
@@ -0,0 +1,58 @@
+/*********************                                                        */
+/*! \file proof_checker.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implementation of proof checker
+ **/
+
+#include "expr/proof_checker.h"
+
+namespace CVC4 {
+
+Node ProofChecker::check(ProofNode* pn, Node expected)
+{
+  return check(pn->getId(), pn->getChildren(), pn->getArguments(), expected);
+}
+
+Node ProofChecker::check(
+    PfRule id,
+    const std::vector<std::shared_ptr<ProofNode>>& children,
+    const std::vector<Node>& args,
+    Node expected)
+{
+  std::map<PfRule, ProofRuleChecker*>::iterator it = d_checker.find(id);
+  if (it == d_checker.end())
+  {
+    return Node::null();
+  }
+  // check it with the corresponding checker
+  Node res = it->second->check(id, children, args);
+  if (!expected.isNull() && res != expected)
+  {
+    // it did not match the given expectation, fail
+    return Node::null();
+  }
+  return res;
+}
+
+void ProofChecker::registerChecker(PfRule id, ProofRuleChecker* psc)
+{
+  std::map<PfRule, ProofRuleChecker*>::iterator it = d_checker.find(id);
+  if (it != d_checker.end())
+  {
+    // checker is already provided
+    Notice() << "ProofChecker::registerChecker: checker already exists for "
+             << id << std::endl;
+    return;
+  }
+  d_checker[id] = psc;
+}
+
+}  // namespace CVC4
