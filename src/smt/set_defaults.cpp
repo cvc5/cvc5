be6719144c88921fa39823976376961fe03f17a7
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2020-04-14 20:54:54 -0500

    Disable preregistration of instantiations for cegqi in incremental (#4251)
    Fixes #4243.

--- src/smt/set_defaults.cpp
+++ src/smt/set_defaults.cpp
@@ -1118,6 +1118,7 @@ void setDefaults(SmtEngine& smte, LogicInfo& logic)
     {
       // cannot do nested quantifier elimination in incremental mode
       options::cbqiNestedQE.set(false);
+      options::cbqiPreRegInst.set(false);
     }
     if (logic.isPure(THEORY_ARITH) || logic.isPure(THEORY_BV))
     {
