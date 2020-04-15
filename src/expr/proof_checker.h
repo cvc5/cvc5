f880c3c6579e80d7203eac0cedf835e31c4fbeae
ajreynol <andrew.j.reynolds@gmail.com>
2020-04-15 16:44:13 -0500

    More doc

--- src/expr/proof_checker.h
+++ src/expr/proof_checker.h
@@ -30,10 +30,15 @@ class ProofRuleChecker
  public:
   ProofRuleChecker() {}
   virtual ~ProofRuleChecker() {}
-  /** Check
-   *
-   * Return the formula that is proven by proof node with the given id, children
-   * and arguments, or null if such a node is not well-formed.
+  /** 
+   * Return the formula that is proven by a proof node with the given id,
+   * children and arguments, or null if such a proof node is not well-formed.
+   * 
+   * @param id The id of the proof node to check
+   * @param children The children of the proof node to check
+   * @param args The arguments of the proof node to check   
+   * @return The conclusion of the proof node if successful or null if such a
+   * proof node is malformed.
    */
   virtual Node check(PfRule id,
                      const std::vector<std::shared_ptr<ProofNode>>& children,
@@ -46,22 +51,34 @@ class ProofChecker
  public:
   ProofChecker() {}
   ~ProofChecker() {}
-  /** Check
-   *
+  /** 
    * Return the formula that is proven by proof node pn, or null if pn is not
-   * well-formed. The value of pn->d_proven is set to this value.
-   *
-   * If expected is non-null, then we return null if pn does not prove expected.
+   * well-formed. If expected is non-null, then we return null if pn does not
+   * prove expected.
+   * 
+   * @param pn The proof node to check
+   * @param expected The (optional) formula that is the expected conclusion of
+   * the proof node.
+   * @return The conclusion of the proof node if successful or null if the
+   * proof is malformed, or if no checker is available for id.
    */
   Node check(ProofNode* pn, Node expected = Node::null());
-  /** Same as above, with explicit arguments */
+  /** Same as above, with explicit arguments
+   * 
+   * @param id The id of the proof node to check
+   * @param children The children of the proof node to check
+   * @param args The arguments of the proof node to check
+   * @param expected The (optional) formula that is the expected conclusion of
+   * the proof node.
+   * @return The conclusion of the proof node if successful or null if the
+   * proof is malformed, or if no checker is available for id.
+   */
   Node check(PfRule id,
              const std::vector<std::shared_ptr<ProofNode>>& children,
              const std::vector<Node>& args,
              Node expected = Node::null());
-  /** Indicate that psc is the checker for proof step id */
+  /** Indicate that psc is the checker for proof rule id */
   void registerChecker(PfRule id, ProofRuleChecker* psc);
-
  private:
   /** Maps proof steps to their checker */
   std::map<PfRule, ProofRuleChecker*> d_checker;
