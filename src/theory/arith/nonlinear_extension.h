54ae5a1c2c6be601a61a68a82d04e24955defb26
ajreynol <andrew.j.reynolds@gmail.com>
2020-05-27 21:22:26 -0500

    Rename

--- src/theory/arith/nonlinear_extension.h
+++ src/theory/arith/nonlinear_extension.h
@@ -28,7 +28,7 @@
 #include "theory/arith/nl_lemma_utils.h"
 #include "theory/arith/nl_model.h"
 #include "theory/arith/nl_solver.h"
-#include "theory/arith/nln_solver.h"
+#include "theory/arith/cad_solver.h"
 #include "theory/arith/theory_arith.h"
 #include "theory/arith/transcendental_solver.h"
 #include "theory/uf/equality_engine.h"
@@ -311,9 +311,9 @@ class NonlinearExtension {
    * constraints involving nonlinear mulitplication, Cimatti et al., TACAS 2017.
    */
   NlSolver d_nlSlv;
-  /** The non-linear new solver
+  /** The cylindrical algebraic decomposition (CAD) solver
    */
-  NlNewSolver d_nlnSlv;
+  CadSolver d_cadSlv;
   /**
    * The lemmas we computed during collectModelInfo. We store two vectors of
    * lemmas to be sent out on the output channel of TheoryArith. The first
