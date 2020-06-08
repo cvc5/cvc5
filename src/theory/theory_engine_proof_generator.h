84effe77a9970b1506b2421bbd16e1c3effb6cfd
ajreynol <andrew.j.reynolds@gmail.com>
2020-06-08 12:28:28 -0500

    (proof-new) Theory engine proof generator

---
+++ src/theory/theory_engine_proof_generator.h
@@ -0,0 +1,66 @@
+/*********************                                                        */
+/*! \file theory_engine.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **  Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief The theory engine proof generator
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef CVC4__THEORY_ENGINE_PROOF_GENERATOR_H
+#define CVC4__THEORY_ENGINE_PROOF_GENERATOR_H
+
+#include <memory>
+
+#include "context/cdhashmap.h"
+#include "context/context.h"
+#include "expr/lazy_proof.h"
+#include "expr/proof_generator.h"
+#include "expr/proof_node_manager.h"
+#include "theory/trust_node.h"
+
+namespace CVC4 {
+
+/**
+ * A simple proof generator class used by the theory engine. This class
+ * stores proofs for TheoryEngine::getExplanation.
+ */
+class TheoryEngineProofGenerator : public ProofGenerator
+{
+  typedef context::
+      CDHashMap<Node, std::shared_ptr<LazyCDProof>, NodeHashFunction>
+          NodeLazyCDProofMap;
+
+ public:
+  TheoryEngineProofGenerator(ProofNodeManager* pnm, context::UserContext* u);
+  ~TheoryEngineProofGenerator() {}
+  /**
+   * Make trust explanation. Called when lpf has of proof of (=> exp lit). This
+   * stores lpf in the map d_proofs below and returns the trust node for this
+   * propagation, which has TrustNodeKind TrustNodeKind::PROP_EXP.
+   */
+  theory::TrustNode mkTrustExplain(TNode lit,
+                                   Node exp,
+                                   std::shared_ptr<LazyCDProof> lpf);
+  /** Get proof for */
+  std::shared_ptr<ProofNode> getProofFor(Node f) override;
+  /** Identify this generator (for debugging, etc..) */
+  std::string identify() const override;
+
+ private:
+  /** The proof manager, used for allocating new ProofNode objects */
+  ProofNodeManager* d_pnm;
+  /** Map from formulas to lazy CD proofs */
+  NodeLazyCDProofMap d_proofs;
+};
+
+}  // namespace CVC4
+
+#endif /* CVC4__THEORY_ENGINE_PROOF_GENERATOR_H */
