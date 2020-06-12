ac9f57dde136c186559bb126f320a16c3237eaad
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2020-06-11 22:12:19 -0500

    Apply suggestions from code review
    Co-authored-by: Andres Noetzli <andres.noetzli@gmail.com>

--- src/theory/engine_output_channel.h
+++ src/theory/engine_output_channel.h
@@ -32,7 +32,7 @@ namespace theory {
  * An output channel for Theory that passes messages back to a TheoryEngine
  * for a given Theory.
  *
- * Notice that is has interfaces trustedConflict and trustedLemma which are
+ * Notice that it has interfaces trustedConflict and trustedLemma which are
  * used for ensuring that proof generators are associated with the lemmas
  * and conflicts sent on this output channel.
  */
