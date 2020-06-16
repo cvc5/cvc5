e37d0c385d698d46f14fb30e5a44de63c686fadb
Aina Niemetz <aina.niemetz@gmail.com>
2020-06-16 13:48:05 -0700

    Update copyright headers.

--- src/smt/defined_function.h
+++ src/smt/defined_function.h
@@ -2,7 +2,7 @@
 /*! \file defined_function.h
  ** \verbatim
  ** Top contributors (to current version):
- **   Andrew Reynolds
+ **   Andrew Reynolds, Morgan Deters
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
