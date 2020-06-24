ae1e8a32a69909732e7058c995e93a143caf9b87
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-24 21:29:19 +0200

    Use old setitime() instead of newer timer_create() due to missing macos support.

--- src/main/time_limit.cpp
+++ src/main/time_limit.cpp
@@ -1,7 +1,6 @@
 #include "time_limit.h"
 
-#include <signal.h>
-#include <time.h>
+#include <sys/time.h>
 
 #include <cerrno>
 #include <cstring>
@@ -9,41 +8,20 @@
 namespace CVC4 {
 namespace main {
 
-TimeLimitListener::~TimeLimitListener()
-{
-  if (timerid != 0)
-  {
-    timer_delete(timerid);
-  }
-}
-
 void TimeLimitListener::notify()
 {
   unsigned long ms = options.getCumulativeTimeLimit();
 
-  // Check https://linux.die.net/man/7/sigevent
-  // Notify by SIGXCPU
-  struct sigevent sev;
-  sev.sigev_notify = SIGEV_SIGNAL;
-  sev.sigev_signo = SIGXCPU;
-
-  // Check https://linux.die.net/man/2/timer_create
-  if (timer_create(CLOCK_MONOTONIC, &sev, &timerid))
-  {
-    throw Exception(std::string("timer_create() failure: ") + strerror(errno));
-  }
-
-  // Check https://linux.die.net/man/2/timer_settime
-  struct itimerspec timerspec;
+  // Check https://linux.die.net/man/2/setitimer
+  struct itimerval timerspec;
   timerspec.it_value.tv_sec = ms / 1000;
-  timerspec.it_value.tv_nsec = (ms % 1000) * 1000000;
+  timerspec.it_value.tv_usec = (ms % 1000) * 1000;
   timerspec.it_interval.tv_sec = 0;
-  timerspec.it_interval.tv_nsec = 0;
-  // Argument 1: the timer id
-  // Argument 2: flags, we don't need any
-  // Argument 3: timer configuration, relative to current time
-  // Argument 4: old timer configuration, we don't want to know
-  if (timer_settime(timerid, 0, &timerspec, nullptr))
+  timerspec.it_interval.tv_usec = 0;
+  // Argument 1: which timer to set, we want the real time
+  // Argument 2: timer configuration, relative to current time
+  // Argument 3: old timer configuration, we don't want to know
+  if (setitimer(ITIMER_REAL, &timerspec, nullptr))
   {
     throw Exception(std::string("timer_settime() failure: ") + strerror(errno));
   }
--- src/main/time_limit.h
+++ src/main/time_limit.h
@@ -12,24 +12,19 @@ namespace main {
 /** Manages an overall wall-clock time limit for the solver binary.
  * It works as a listener to an Options object.
  * When notified, it retrieves the time limit and creates a POSIX timer (via
- * timer_create() and timer_settime()). This timer is configured to signal its
- * expiration with an SIGXCPU that is handled by timeout_handler() in util.cpp
- * Note that this class expects a reference to an Options object, while other
- * listeners don't, as the OptionsScope is not yet initialized when we set up
- * this timer.
+ * setitimer()). This timer signals its expiration with an SIGALRM that is
+ * handled by timeout_handler() in util.cpp Note that this class expects a
+ * reference to an Options object, while other listeners don't, as the
+ * OptionsScope is not yet initialized when we set up this timer.
  */
 class TimeLimitListener : public CVC4::Listener
 {
   /** A reference to the Options object. */
   const Options& options;
-  /** ID of the POSIX timer. */
-  timer_t timerid = 0;
 
  public:
   /** Create a listener with the given Options object. */
   TimeLimitListener(const Options& opts) : options(opts) {}
-  /** Destruct and possibly delete timer. */
-  ~TimeLimitListener();
   /** Retrieves timeout from Options and creates a timer. */
   void notify() override;
 };
