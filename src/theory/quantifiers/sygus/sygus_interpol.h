0f666d9b9ce79a4f0efddb034382a14fbeb90d5b
Ying Sheng <sqy1415@gmail.com>
2020-06-30 14:24:27 -0700

    format

--- src/theory/quantifiers/sygus/sygus_interpol.h
+++ src/theory/quantifiers/sygus/sygus_interpol.h
@@ -27,8 +27,7 @@ namespace CVC4 {
 namespace theory {
 namespace quantifiers {
 /**
- * A utility that turns a set of quantifier-free assertions into
- * a sygus conjecture that encodes an interpolation problem. In detail, if our
+ * A utility that turns a set of quantifier-free assertions into a sygus conjecture that encodes an interpolation problem. In detail, if our
  * input formula is F( x ) for free symbols x, and is partitioned into axioms Fa
  * and conjecture Fc then the sygus conjecture we construct is:
  *
