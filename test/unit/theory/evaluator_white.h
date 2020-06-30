d490606d5b9b1177eee4a9c7e2f7c0690eb59093
ajreynol <andrew.j.reynolds@gmail.com>
2020-06-30 14:17:50 -0500

    Fix

--- test/unit/theory/evaluator_white.h
+++ test/unit/theory/evaluator_white.h
@@ -49,8 +49,8 @@ class TheoryEvaluatorWhite : public CxxTest::TestSuite
     Options opts;
     opts.setOutputLanguage(language::output::LANG_SMTLIB_V2);
     d_em = new ExprManager;
-    d_nm = NodeManager::fromExprManager(d_em, &opts);
-    d_smt = new SmtEngine(d_em);
+    d_nm = NodeManager::fromExprManager(d_em);
+    d_smt = new SmtEngine(d_em, &opts);
     d_scope = new SmtScope(d_smt);
     d_smt->finalOptionsAreSet();
   }
