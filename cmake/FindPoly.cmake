b86a9d114869485bff4c2f7f9fcf60a872236fdb
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-01 08:37:29 +0200

    Minor cleanup in FindPoly.cmake

--- cmake/FindPoly.cmake
+++ cmake/FindPoly.cmake
@@ -2,7 +2,6 @@
 # POLY_FOUND - system has libpoly
 # POLY_INCLUDE_DIR - the libpoly include directory
 # POLY_LIBRARIES - Libraries needed to use libpoly
-# POLY_ARCH_FLAGS - Platform specific compile flags
 
 # Note: contrib/get-poly copies header files to deps/install/include/poly.
 # However, includes in libpoly headers are not prefixed with "poly/" and therefore
@@ -11,8 +10,8 @@ find_path(POLY_INCLUDE_DIR NAMES poly/poly.h PATH_SUFFIXES poly)
 find_library(POLY_LIB NAMES poly)
 find_library(POLY_LIBXX NAMES polyxx)
 set(POLY_LIBRARIES "${POLY_LIBXX};${POLY_LIB}")
-unset(POLY_LIB)
-unset(POLY_LIBXX)
+unset(POLY_LIB CACHE)
+unset(POLY_LIBXX CACHE)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Poly
