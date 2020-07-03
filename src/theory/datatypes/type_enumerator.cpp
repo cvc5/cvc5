68ed8071a87909339cb8e431c73e16f48054f682
Andres Noetzli <andres.noetzli@gmail.com>
2020-07-02 22:57:50 -0700

    Use TypeNode in UninterpretedConstant
    This commit changes `UninterpretedConstant` to use `TypeNode` instead of
    `Type`.

--- src/theory/datatypes/type_enumerator.cpp
+++ src/theory/datatypes/type_enumerator.cpp
@@ -107,7 +107,7 @@ Node DatatypesEnumerator::getTermEnum( TypeNode tn, unsigned i ){
      if (d_child_enum)
      {
        ret = NodeManager::currentNM()->mkConst(
-           UninterpretedConstant(d_type.toType(), d_size_limit));
+           UninterpretedConstant(d_type, d_size_limit));
      }
      else
      {
