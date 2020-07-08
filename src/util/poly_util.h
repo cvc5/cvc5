da850a03e14db8ae0258d5ab934e423533acd46f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-08 07:03:30 +0200

    Fixed comment formatting.

--- src/util/poly_util.h
+++ src/util/poly_util.h
@@ -16,7 +16,8 @@
 #include <poly/polyxx.h>
 
 namespace CVC4 {
-/** Utilities for working with libpoly.
+/**
+ * Utilities for working with libpoly.
  * This namespace contains various basic conversion routines necessary for the
  * integration of libpoly. Firstly, libpoly is based on GMP and hence we need
  * conversion from and to CLN (in case CLN is enabled). Otherwise, conversion of
@@ -42,21 +43,27 @@ poly::Integer to_integer(const Integer& i);
 std::vector<poly::Integer> to_integer(const std::vector<Integer>& vi);
 /** Converts a CVC4::Rational to a poly::Rational. */
 poly::Rational to_rational(const Rational& r);
-/** Converts a CVC4::Rational to a poly::DyadicRational. If the input is not
- * dyadic, no result is produced. */
+/**
+ * Converts a CVC4::Rational to a poly::DyadicRational. If the input is not
+ * dyadic, no result is produced.
+ */
 Maybe<poly::DyadicRational> to_dyadic_rational(const Rational& r);
-/** Converts a poly::Rational to a poly::DyadicRational. If the input is not
- * dyadic, no result is produced. */
+/**
+ * Converts a poly::Rational to a poly::DyadicRational. If the input is not
+ * dyadic, no result is produced.
+ */
 Maybe<poly::DyadicRational> to_dyadic_rational(const poly::Rational& r);
 
-/** Iteratively approximates a poly::Rational by a dyadic poly::Rational.
+/**
+ * Iteratively approximates a poly::Rational by a dyadic poly::Rational.
  * Assuming that r is dyadic, makes one refinement step to move r closer to
  * original.
  * Assumes one starts with lower(original) or ceil(original) for r.
  */
 void approximate_to_dyadic(poly::Rational& r, const poly::Rational& original);
 
-/** Constructs a poly::AlgebraicNumber, allowing for refinement of the
+/**
+ * Constructs a poly::AlgebraicNumber, allowing for refinement of the
  * CVC4::Rational bounds. As a poly::AlgebraicNumber works on
  * poly::DyadicRationals internally, the bounds are iteratively refined using
  * approximate_to_dyadic until the respective interval is isolating. If the
@@ -66,15 +73,18 @@ poly::AlgebraicNumber to_poly_ran_with_refinement(poly::UPolynomial&& p,
                                                   const Rational& lower,
                                                   const Rational& upper);
 
-/** Constructs a CVC4::RealAlgebraicNumber, simply wrapping
- * to_poly_ran_with_refinement. */
+/**
+ * Constructs a CVC4::RealAlgebraicNumber, simply wrapping
+ * to_poly_ran_with_refinement.
+ */
 RealAlgebraicNumber to_ran_with_refinement(poly::UPolynomial&& p,
                                            const Rational& lower,
                                            const Rational& upper);
 
 std::size_t total_degree(const poly::Polynomial& p);
 
-/** Collects information about a single variable in a set of polynomials.
+/**
+ * Collects information about a single variable in a set of polynomials.
  * Used for determining a variable ordering.
  */
 struct VariableInformation {
