54c2ac3cf447e335c8bc109e16f84f52fbdb3ffa
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-10 19:58:54 +0200

    Add boilerplate comment.

--- src/main/signal_handlers.h
+++ src/main/signal_handlers.h
@@ -1,3 +1,19 @@
+/*********************                                                        */
+/*! \file signal_handlers.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Gereon Kremer
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implementation of signal handlers.
+ **
+ ** Implementation of signal handlers.
+ **/
+
 #ifndef CVC4__MAIN__SIGNAL_HANDLERS_H
 #define CVC4__MAIN__SIGNAL_HANDLERS_H
 
