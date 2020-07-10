4bc926ebc94e4d5d7d1b948d122b267262c93c56
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-10 14:01:09 +0200

    Added boilerplate comment.

--- test/unit/util/real_algebraic_number_black.h
+++ test/unit/util/real_algebraic_number_black.h
@@ -1,3 +1,19 @@
+/*********************                                                        */
+/*! \file real_algebraic_number_black.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Gereon Kremer
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Black box testing of CVC4::RealAlgebraicNumber.
+ **
+ ** Black box testing of CVC4::RealAlgebraicNumber.
+ **/
+
 #include <cxxtest/TestSuite.h>
 
 #include "util/real_algebraic_number.h"
