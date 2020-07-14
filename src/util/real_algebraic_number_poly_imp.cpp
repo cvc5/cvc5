8c2b987ca8e1c1bea842b867970e17975a7e0723
Gereon Kremer <nafur42@gmail.com>
2020-07-14 08:23:07 +0200

    Removed "Not Committed Yet" author.
    Co-authored-by: Aina Niemetz <aina.niemetz@gmail.com>

--- src/util/real_algebraic_number_poly_imp.cpp
+++ src/util/real_algebraic_number_poly_imp.cpp
@@ -2,7 +2,7 @@
 /*! \file real_algebraic_number_poly_imp.cpp
  ** \verbatim
  ** Top contributors (to current version):
- **   Gereon Kremer, Not Committed Yet
+ **   Gereon Kremer
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
