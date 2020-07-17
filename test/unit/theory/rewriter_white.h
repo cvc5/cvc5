dbdec4ceca78c7608c1e76a5ae0ba818f555fd90
ajreynol <andrew.j.reynolds@gmail.com>
2020-07-17 12:02:35 -0500

    Fix unit test

--- test/unit/theory/rewriter_white.h
+++ test/unit/theory/rewriter_white.h
@@ -65,7 +65,7 @@ class RewriterWhite : public CxxTest::TestSuite
     d_smt->finalOptionsAreSet();
     // make a rewriter with proof generation
     d_rewriter = new Rewriter;
-    d_rewriter->setProofChecker(d_smt->getPfManager()->getProofChecker());
+    d_rewriter->setProofNodeManager(d_smt->getPfManager()->getProofNodeManager());
   }
 
   void tearDown() override
