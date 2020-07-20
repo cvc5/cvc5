d95cb71e63eb647f0708b3e3e74138cc9d32ba8c
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-20 12:51:09 +0200

    Added interface for new CAD-based solver.

---
+++ src/theory/arith/nl/cad/cdcac.cpp
@@ -0,0 +1,102 @@
+/*********************                                                        */
+/*! \file cdcac.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Gereon Kremer
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implements the CDCAC approach.
+ **
+ ** Implements the CDCAC approach as described in
+ ** https://arxiv.org/pdf/2003.05633.pdf.
+ **/
+
+#include "cdcac.h"
+
+#include "projections.h"
+#include "variable_ordering.h"
+
+namespace CVC4 {
+namespace theory {
+namespace arith {
+namespace nl {
+namespace cad {
+
+CDCAC::CDCAC() {}
+
+CDCAC::CDCAC(const std::vector<Variable>& ordering)
+    : mVariableOrdering(ordering)
+{
+}
+
+void CDCAC::reset()
+{
+  mConstraints.reset();
+  mAssignment.clear();
+}
+
+void CDCAC::compute_variable_ordering() {}
+
+Constraints& CDCAC::get_constraints() { return mConstraints; }
+const Constraints& CDCAC::get_constraints() const { return mConstraints; }
+
+const Assignment& CDCAC::get_model() const { return mAssignment; }
+
+const std::vector<Variable>& CDCAC::get_variable_ordering() const
+{
+  return mVariableOrdering;
+}
+
+std::vector<CACInterval> CDCAC::get_unsat_intervals(
+    std::size_t cur_variable) const
+{
+  return {};
+}
+
+std::vector<Polynomial> CDCAC::required_coefficients(const Polynomial& p) const
+{
+  return {};
+}
+
+void add_polynomial(
+    std::vector<std::pair<Polynomial, std::vector<Node>>>& polys,
+    const Polynomial& poly,
+    const std::vector<Node>& origin)
+{
+}
+
+std::vector<Polynomial> CDCAC::construct_characterization(
+    std::vector<CACInterval>& intervals)
+{
+  return {};
+}
+
+CACInterval CDCAC::interval_from_characterization(
+    const std::vector<Polynomial>& characterization,
+    std::size_t cur_variable,
+    const Value& sample)
+{
+  return {};
+}
+
+std::vector<CACInterval> CDCAC::get_unsat_cover(std::size_t cur_variable,
+                                                bool return_first_interval)
+{
+  return {};
+}
+
+}  // namespace cad
+}  // namespace nl
+}  // namespace arith
+}  // namespace theory
+}  // namespace CVC4
