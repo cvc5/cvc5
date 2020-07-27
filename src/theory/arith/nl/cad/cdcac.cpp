4f5182611c5dba8dd6c91d795dfe8f3c1b9a6a70
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-27 19:51:10 +0200

    Remove redundant paragraph from copyright headers.

--- src/theory/arith/nl/cad/cdcac.cpp
+++ src/theory/arith/nl/cad/cdcac.cpp
@@ -9,12 +9,6 @@
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
- ** This file is part of the CVC4 project.
- ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
- ** in the top-level source directory) and their institutional affiliations.
- ** All rights reserved.  See the file COPYING in the top-level source
- ** directory for licensing information.\endverbatim
- **
  ** \brief Implements the CDCAC approach.
  **
  ** Implements the CDCAC approach as described in
