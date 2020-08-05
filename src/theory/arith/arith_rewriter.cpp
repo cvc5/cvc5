965ac5aca86f824ec637696d86cbb7630b95d9c1
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-08-05 17:28:33 +0200

    Using expr::NodeValue::MAX_CHILDREN as new upper limit.

--- src/theory/arith/arith_rewriter.cpp
+++ src/theory/arith/arith_rewriter.cpp
@@ -229,8 +229,8 @@ RewriteResponse ArithRewriter::postRewriteTerm(TNode t){
           if(exp.sgn() == 0){
             return RewriteResponse(REWRITE_DONE, mkRationalNode(Rational(1)));
           }else if(exp.sgn() > 0 && exp.isIntegral()){
-            CVC4::Rational r(INT_MAX);
-            if( exp<r ){
+            CVC4::Rational r(expr::NodeValue::MAX_CHILDREN);
+            if( exp <= r ){
               unsigned num = exp.getNumerator().toUnsignedInt();
               if( num==1 ){
                 return RewriteResponse(REWRITE_AGAIN, base);
@@ -249,7 +249,7 @@ RewriteResponse ArithRewriter::postRewriteTerm(TNode t){
 
         // Todo improve the exception thrown
         std::stringstream ss;
-        ss << "The exponent of the POW(^) operator can only be a constant positive integral number below " << INT_MAX << ". ";
+        ss << "The exponent of the POW(^) operator can only be a positive integral constant below " << (expr::NodeValue::MAX_CHILDREN+1) << ". ";
         ss << "Exception occurred in:" << std::endl;
         ss << "  " << t;
         throw LogicException(ss.str());
