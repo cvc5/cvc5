0e9d9926d5212486e3946c7ff16afd6d693209bd
ajreynol <andrew.j.reynolds@gmail.com>
2020-08-06 20:49:55 -0500

    Update proof rules

--- src/expr/proof_rule.cpp
+++ src/expr/proof_rule.cpp
@@ -114,6 +114,8 @@ const char* toString(PfRule id)
       return "RE_UNFOLD_NEG_CONCAT_FIXED";
     case PfRule::RE_ELIM: return "RE_ELIM";
     case PfRule::STRING_CODE_INJ: return "STRING_CODE_INJ";
+    case PfRule::STRING_SEQ_UNIT_INJ: return "STRING_SEQ_UNIT_INJ";
+    case PfRule::STRING_TRUST: return "STRING_TRUST";
     //================================================= Arith rules
     case PfRule::ARITH_SCALE_SUM_UPPER_BOUNDS: return "ARITH_SCALE_SUM_UPPER_BOUNDS";
     case PfRule::ARITH_TRICHOTOMY: return "ARITH_TRICHOTOMY";
