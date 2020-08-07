3b4f875cb9e6be3c5c164fddf03a87913eaded7b
ajreynol <andrew.j.reynolds@gmail.com>
2020-08-06 22:22:54 -0500

    Splitting term context, documentation.

--- src/expr/term_context.cpp
+++ src/expr/term_context.cpp
@@ -9,17 +9,18 @@
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
- ** \brief Term context
+ ** \brief Implementation of term context utilities.
  **/
 
 #include "expr/term_context.h"
 
 namespace CVC4 {
 
-TermContext::TermContext(uint32_t ivalue) : d_initVal(ivalue) {}
-uint32_t TermContext::initialValue() const { return d_initVal; }
-
-RtfTermContext::RtfTermContext() : TermContext(0) {}
+uint32_t RtfTermContext::initialValue() const
+{
+  // by default, not in a term context or a quantifier
+  return 0;
+}
 
 uint32_t RtfTermContext::computeValue(TNode t,
                                       uint32_t tval,
@@ -62,6 +63,12 @@ bool RtfTermContext::hasNestedTermChildren(TNode t)
          && k != kind::BITVECTOR_EAGER_ATOM;
 }
 
+uint32_t PolarityTermContext::initialValue() const
+{
+  // by default, we have true polarity
+  return 2;
+}
+
 uint32_t PolarityTermContext::computeValue(TNode t,
                                            uint32_t tval,
                                            size_t index) const
@@ -101,100 +108,6 @@ void PolarityTermContext::getFlags(uint32_t val, bool& hasPol, bool& pol)
 {
   hasPol = val == 0;
   pol = val == 2;
-}
-
-TCtxNode::TCtxNode(Node n, const TermContext* tctx)
-    : d_node(n), d_val(tctx->initialValue()), d_tctx(tctx)
-{
-}
-
-TCtxNode::TCtxNode(Node n, uint32_t val, const TermContext* tctx)
-    : d_node(n), d_val(val), d_tctx(tctx)
-{
-}
-
-size_t TCtxNode::getNumChildren() const { return d_node.getNumChildren(); }
-
-TCtxNode TCtxNode::getChild(size_t i) const
-{
-  Assert(i < d_node.getNumChildren());
-  return TCtxNode(d_node[i], d_tctx->computeValue(d_node, d_val, i), d_tctx);
-}
-
-Node TCtxNode::getNode() const { return d_node; }
-
-uint32_t TCtxNode::getTermContext() const { return d_val; }
-
-Node TCtxNode::getNodeHash() const { return computeNodeHash(d_node, d_val); }
-
-Node TCtxNode::computeNodeHash(Node n, uint32_t val)
-{
-  NodeManager* nm = NodeManager::currentNM();
-  return nm->mkNode(kind::SEXPR, n, nm->mkConst(Rational(val)));
-}
-
-Node TCtxNode::decomposeNodeHash(Node h, uint32_t& val)
-{
-  if (h.getKind() != kind::SEXPR || h.getNumChildren() != 2)
-  {
-    Assert(false) << "TermContext::decomposeNodeHash: unexpected node " << h;
-    return Node::null();
-  }
-  Node ival = h[1];
-  if (!ival.isConst() || !ival.getType().isInteger()
-      || !ival.getConst<Rational>().getNumerator().fitsUnsignedInt())
-  {
-    Assert(false) << "TermContext::decomposeNodeHash: unexpected term context "
-                     "integer in hash "
-                  << h;
-    return Node::null();
-  }
-  val = ival.getConst<Rational>().getNumerator().toUnsignedInt();
-  return h[0];
-}
-
-TCtxStack::TCtxStack(const TermContext* tctx) : d_tctx(tctx) {}
-
-void TCtxStack::pushInitial(Node t)
-{
-  Assert(d_stack.empty());
-  d_stack.push_back(std::pair<Node, uint32_t>(t, d_tctx->initialValue()));
-}
-
-void TCtxStack::pushChildren(Node t, uint32_t tval)
-{
-  for (size_t i = 0, nchild = t.getNumChildren(); i < nchild; i++)
-  {
-    pushChild(t, tval, i);
-  }
-}
-
-void TCtxStack::pushChild(Node t, uint32_t tval, size_t index)
-{
-  Assert(index < t.getNumChildren());
-  Trace("tctx-debug") << "TCtxStack::pushChild: computing " << t << "[" << index
-                      << "] / " << tval << std::endl;
-  uint32_t tcval = d_tctx->computeValue(t, tval, index);
-  Trace("tctx-debug") << "TCtxStack::pushChild: returned " << t << "[" << index
-                      << "] / " << tval << " ---> " << tcval << std::endl;
-  d_stack.push_back(std::pair<Node, uint32_t>(t[index], tcval));
-}
-
-void TCtxStack::push(Node t, uint32_t tval)
-{
-  d_stack.push_back(std::pair<Node, uint32_t>(t, tval));
-}
-
-void TCtxStack::pop() { d_stack.pop_back(); }
-
-void TCtxStack::clear() { d_stack.clear(); }
-size_t TCtxStack::size() const { return d_stack.size(); }
-
-bool TCtxStack::empty() const { return d_stack.empty(); }
-
-std::pair<Node, uint32_t> TCtxStack::getCurrent() const
-{
-  return d_stack.back();
 }
 
 }  // namespace CVC4
--- src/expr/term_context.h
+++ src/expr/term_context.h
@@ -9,7 +9,7 @@
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
- ** \brief Term context
+ ** \brief Term context utilities.
  **/
 
 #include "cvc4_private.h"
@@ -24,41 +24,53 @@ namespace CVC4 {
 /**
  * This is an abstract class for computing "term context identifiers". A term
  * context identifier is a hash value that identifies some property of the
- * context in which a term occurs.
+ * context in which a term occurs. Common examples of the implementation of
+ * such a mapping are implemented in the subclasses below.
+ * 
+ * In the below documentation, we write t @ [p] to refer to a
+ * term-context-sensitive term, where p is a list of indices corresponding to
+ * its position in another term. For example, the atomic subterms of:
+ *   (and P (not Q))
+ * are P @ [0] and Q @ [1,0].
  */
 class TermContext
 {
  public:
-  TermContext(uint32_t ivalue = 0);
+  TermContext() {}
   virtual ~TermContext() {}
   /** Initial value */
-  uint32_t initialValue() const;
+  virtual uint32_t initialValue() const = 0;
   /**
    * Compute the term context identifier of the index^th child of t, where tval
    * is the term context identifier of t.
    */
   virtual uint32_t computeValue(TNode t, uint32_t tval, size_t index) const = 0;
-
- private:
-  /** The initial value for terms in no context */
-  uint32_t d_initVal;
 };
 
 /**
  * Computes whether we are inside a term (as opposed to being part of Boolean
- * skeleton) and whether we are inside a quantifier.
+ * skeleton) and whether we are inside a quantifier. For example, for:
+ *   (and (= a b) (forall ((x Int)) (P x)))
+ * we have the following mappings (term -> inTerm,inQuant)
+ *   (= a b) @ [0] -> false, false
+ *   a @ [0,1] -> true, false
+ *   (P x) @ [1,1] -> false, true
+ *    x @ [1,1,0] -> true, true
+ * Notice that the hash of a child can be computed from the parent's hash only,
+ * and hence this can be implemented without considering paths.
  */
 class RtfTermContext : public TermContext
 {
  public:
-  RtfTermContext();
+  RtfTermContext(){}
+  /** The initial value */
+  uint32_t initialValue() const override;
   /** Compute the value */
   uint32_t computeValue(TNode t, uint32_t tval, size_t index) const override;
   /** get value */
   static uint32_t getValue(bool inQuant, bool inTerm);
   /** get flags */
   static void getFlags(uint32_t val, bool& inQuant, bool& inTerm);
-
  private:
   /**
    * Returns true if the children of t should be considered in a "term" context.
@@ -67,97 +79,47 @@ class RtfTermContext : public TermContext
 };
 
 /**
- * Computes the polarity of a term.
+ * Computes the polarity of a term-context-sensitive term, which is one of
+ * {true, false, none}. This corresponds to the value that can be assigned to
+ * that term while preservering satisfiability of the overall formula. This
+ * typically corresponds to the number of NOT the formula is beneath, although
+ * special cases exist (e.g. IMPLIES).
+ *
+ * For example, given the formula:
+ *   (and P (not (= (f x) 0)))
+ * assuming the root of this formula has positive polarity, we have that:
+ *   P @ [0] -> true
+ *   (not (= (f x) 0)) @ [1] -> true
+ *   (= (f x) 0) @ [1,0] -> false
+ *   (f x) @ [1,0,0]), x @ [1,0,0,0]), 0 @ [1,0,1] -> none
+ *
+ * Notice that a term-context-sensitive Node is not one-to-one with Node.
+ * In particular, given the formula:
+ *   (and P (not P))
+ * We have that the P at path [0] has polarity true and the P at path [1,0] has
+ * polarity false.
+ * 
+ * Finally, notice that polarity does not correspond to an entailed value. Thus,
+ * for the formula:
+ *   (or P Q)
+ * we have that
+ *   P @ [0] -> true
+ *   Q @ [1] -> true
+ * although neither is entailed.
+ * 
+ * Notice that the hash of a child can be computed from the parent's hash only,
+ * and hence this can be implemented without considering paths.
  */
 class PolarityTermContext : public TermContext
 {
  public:
-  PolarityTermContext();
+  PolarityTermContext(){}
   /** Compute the value */
   uint32_t computeValue(TNode t, uint32_t tval, size_t index) const override;
   /** get value */
   static uint32_t getValue(bool hasPol, bool pol);
   /** get flags */
   static void getFlags(uint32_t val, bool& hasPol, bool& pol);
-};
-
-/**
- * A (term-context) sensitive term. This is a wrapper around a Node that
- * additionally has a term context identifier, see getTermContext(). It depends
- * on a pointer to a TermContext callback class from above.
- */
-class TCtxNode
-{
- public:
-  TCtxNode(Node n, const TermContext* tctx);
-  /** get number of children */
-  size_t getNumChildren() const;
-  /** get child at index i */
-  TCtxNode getChild(size_t i) const;
-  /** get node */
-  Node getNode() const;
-  /** get term context */
-  uint32_t getTermContext() const;
-  //---------------------- utility methods
-  /**
-   * Get node hash, which is a unique node representation of this TCtxNode.
-   * This method calls the method below on the data members of this class.
-   */
-  Node getNodeHash() const;
-  /**
-   * Get node hash, which is a unique node representation of the pair (n, val).
-   * In particular, this returns (SEXPR n (CONST_RATIONAL val)).
-   */
-  static Node computeNodeHash(Node n, uint32_t val);
-  /**
-   * Decompose node hash, which is an inverse of the above operation.
-   */
-  static Node decomposeNodeHash(Node h, uint32_t& val);
-  //---------------------- end utility methods
- private:
-  /** private constructor */
-  TCtxNode(Node n, uint32_t val, const TermContext* tctx);
-  /** The node */
-  Node d_node;
-  /** The term context identifier */
-  uint32_t d_val;
-  /** The term context */
-  const TermContext* d_tctx;
-};
-
-/**
- * A stack for term-context-sensitive terms. Its main advantage is that
- * it does not rely on explicit construction of TCtxNode for efficiency.
- */
-class TCtxStack
-{
- public:
-  TCtxStack(const TermContext* tctx);
-  virtual ~TCtxStack() {}
-  /** Push t to the stack */
-  void pushInitial(Node t);
-  /** Push all children of t to the stack */
-  void pushChildren(Node t, uint32_t tval);
-  /** Push the child of t with the given index to the stack */
-  void pushChild(Node t, uint32_t tval, size_t index);
-  /** Push t to the stack */
-  void push(Node t, uint32_t tval);
-  /** Pop a term from the context */
-  void pop();
-  /** Clear the stack */
-  void clear();
-  /** Return the size of the stack */
-  size_t size() const;
-  /** Return true if the stack is empty */
-  bool empty() const;
-  /** Get the current stack element */
-  std::pair<Node, uint32_t> getCurrent() const;
-
- private:
-  /** The stack */
-  std::vector<std::pair<Node, uint32_t>> d_stack;
-  /** The term context */
-  const TermContext* d_tctx;
 };
 
 }  // namespace CVC4
