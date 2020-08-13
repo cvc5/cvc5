3e7cb40b124f474a51a9aa276f505596c2b5c767
ajreynol <andrew.j.reynolds@gmail.com>
2020-08-13 11:44:32 -0500

    Fix

--- src/theory/sets/theory_sets.h
+++ src/theory/sets/theory_sets.h
@@ -71,7 +71,6 @@ class TheorySets : public Theory
   PPAssertStatus ppAssert(TNode in, SubstitutionMap& outSubstitutions) override;
   void presolve() override;
   void propagate(Effort) override;
-  void setMasterEqualityEngine(eq::EqualityEngine* eq) override;
   bool isEntailed(Node n, bool pol);
  private:
   /** Functions to handle callbacks from equality engine */
