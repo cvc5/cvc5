8368596d5f3057257444db704aa0ddbc5202bef9
ajreynol <andrew.j.reynolds@gmail.com>
2020-08-14 17:11:09 -0500

    Comment

--- src/theory/bv/bv_subtheory_core.cpp
+++ src/theory/bv/bv_subtheory_core.cpp
@@ -102,8 +102,11 @@ void CoreSolver::preRegister(TNode node) {
       }
   } else {
     d_equalityEngine.addTerm(node);
-    // register with the extended theory
-    d_extTheory->registerTerm(t);
+    // Register with the extended theory, for context-dependent simplification.
+    // Notice we do this for registered terms not internally generated
+    // equivalence classes. The two should roughly cooincide. Since ExtTheory is
+    // being used as a heuristic, it is good enough to be registered here.
+    d_extTheory->registerTerm(node);
   }
 }
 
