715f84357dd49591c461d858e73c745eae78c386
ajreynol <andrew.j.reynolds@gmail.com>
2020-08-19 06:31:08 -0500

    Split QuantElimSolver from SmtEngine

---
+++ src/smt/quant_elim_solver.cpp
@@ -0,0 +1,100 @@
+/*********************                                                        */
+/*! \file abduction_solver.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief The solver for abduction queries
+ **/
+
+#include "smt/abduction_solver.h"
+
+#include "options/smt_options.h"
+#include "smt/smt_engine.h"
+#include "theory/quantifiers/quantifiers_attributes.h"
+#include "theory/quantifiers/sygus/sygus_abduct.h"
+#include "theory/quantifiers/sygus/sygus_grammar_cons.h"
+#include "theory/smt_engine_subsolver.h"
+
+using namespace CVC4::theory;
+using namespace CVC4::kind;
+
+namespace CVC4 {
+namespace smt {
+
+AbductionSolver::AbductionSolver(SmtEngine* parent) : d_parent(parent) {}
+
+AbductionSolver::~AbductionSolver() {}
+
+Node SmtEngine::doQuantifierElimination(Assertions& as, Node e, bool doFull)
+{
+  Trace("smt-qe") << "Do quantifier elimination " << e << std::endl;
+  if (e.getKind() != EXISTS && e.getKind() != FORALL)
+  {
+    throw ModalException(
+        "Expecting a quantified formula as argument to get-qe.");
+  }
+  NodeManager * nm = NodeManager::currentNM();
+  //tag the quantified formula with the quant-elim attribute
+  TypeNode t = nm->booleanType();
+  Node n_attr = nm->mkSkolem("qe", t, "Auxiliary variable for qe attr.");
+  std::vector< Node > node_values;
+  TheoryEngine* te = d_smtSolver.getTheoryEngine();
+  Assert(te != nullptr);
+  te->setUserAttribute(
+      doFull ? "quant-elim" : "quant-elim-partial", n_attr, node_values, "");
+  n_attr = nm->mkNode(INST_ATTRIBUTE, n_attr);
+  n_attr = nm->mkNode(INST_PATTERN_LIST, n_attr);
+  std::vector< Node > e_children;
+  e_children.push_back( e[0] );
+  e_children.push_back(e.getKind() == EXISTS ? e[1]
+                                                     : e[1].negate());
+  e_children.push_back( n_attr );
+  Node ne = nm->mkNode( EXISTS, e_children );
+  Trace("smt-qe-debug") << "Query for quantifier elimination : " << ne << std::endl;
+  Assert(ne.getNumChildren() == 3);
+  Result r = d_smtSolver.checkSatisfiability(as, std::vector<Node>{ne}, false, false);
+  Trace("smt-qe") << "Query returned " << r << std::endl;
+  if(r.asSatisfiabilityResult().isSat() != Result::UNSAT ) {
+    if( r.asSatisfiabilityResult().isSat() != Result::SAT && doFull ){
+      Notice()
+          << "While performing quantifier elimination, unexpected result : "
+          << r << " for query.";
+      // failed, return original
+      return e;
+    }
+    std::vector< Node > inst_qs;
+    te->getInstantiatedQuantifiedFormulas(inst_qs);
+    Assert(inst_qs.size() <= 1);
+    Node ret_n;
+    if( inst_qs.size()==1 ){
+      Node top_q = inst_qs[0];
+      //Node top_q = Rewriter::rewrite( ne ).negate();
+      Assert(top_q.getKind() == FORALL);
+      Trace("smt-qe") << "Get qe for " << top_q << std::endl;
+      ret_n = te->getInstantiatedConjunction(top_q);
+      Trace("smt-qe") << "Returned : " << ret_n << std::endl;
+      if (e.getKind() == EXISTS)
+      {
+        ret_n = Rewriter::rewrite(ret_n.negate());
+      }
+    }else{
+      ret_n = nm->mkConst(e.getKind() != EXISTS);
+    }
+    // do extended rewrite to minimize the size of the formula aggressively
+    theory::quantifiers::ExtendedRewriter extr(true);
+    ret_n = extr.extendedRewrite(ret_n);
+    return ret_n;
+  }
+  // otherwise, just true/false
+    return nm
+        ->mkConst(e.getKind() == EXISTS);
+}
+
+}  // namespace smt
+}  // namespace CVC4
---
+++ src/smt/quant_elim_solver.h
@@ -0,0 +1,94 @@
+/*********************                                                        */
+/*! \file quant_elim_solver.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief The solver for quantifier elimination queries
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef CVC4__SMT__QUANT_ELIM_SOLVER_H
+#define CVC4__SMT__QUANT_ELIM_SOLVER_H
+
+#include "expr/node.h"
+#include "smt/assertions.h"
+
+namespace CVC4 {
+namespace smt {
+  
+class SmtSolver;
+
+/**
+ * A solver for quantifier elimination queries.
+ *
+ * This class is responsible for responding to get-qe and get-qe-partial
+ * commands.
+ */
+class QuantElimSolver
+{
+ public:
+  QuantElimSolver(SmtSolver& sms);
+  ~QuantElimSolver();
+ 
+  /**
+   * Do quantifier elimination.
+   *
+   * This function takes as input a quantified formula e
+   * of the form:
+   *   Q x1...xn. P( x1...xn, y1...yn )
+   * where P( x1...xn, y1...yn ) is a quantifier-free
+   * formula in a logic that supports quantifier elimination.
+   * Currently, the only logics supported by quantifier
+   * elimination is LRA and LIA.
+   *
+   * This function returns a formula ret such that, given
+   * the current set of formulas A asserted to this SmtEngine :
+   *
+   * If doFull = true, then
+   *   - ( A ^ e ) and ( A ^ ret ) are equivalent
+   *   - ret is quantifier-free formula containing
+   *     only free variables in y1...yn.
+   *
+   * If doFull = false, then
+   *   - (A ^ e) => (A ^ ret) if Q is forall or
+   *     (A ^ ret) => (A ^ e) if Q is exists,
+   *   - ret is quantifier-free formula containing
+   *     only free variables in y1...yn,
+   *   - If Q is exists, let A^Q_n be the formula
+   *       A ^ ~ret^Q_1 ^ ... ^ ~ret^Q_n
+   *     where for each i=1,...n, formula ret^Q_i
+   *     is the result of calling doQuantifierElimination
+   *     for e with the set of assertions A^Q_{i-1}.
+   *     Similarly, if Q is forall, then let A^Q_n be
+   *       A ^ ret^Q_1 ^ ... ^ ret^Q_n
+   *     where ret^Q_i is the same as above.
+   *     In either case, we have that ret^Q_j will
+   *     eventually be true or false, for some finite j.
+   *
+   * The former feature is quantifier elimination, and
+   * is run on invocations of the smt2 extended command get-qe.
+   * The latter feature is referred to as partial quantifier
+   * elimination, and is run on invocations of the smt2
+   * extended command get-qe-disjunct, which can be used
+   * for incrementally computing the result of a
+   * quantifier elimination.
+   */
+  Node doQuantifierElimination(Assertions& as, Node e, bool doFull);
+
+ private:
+  /** The SMT solver, which is used during doQuantifierElimination. */
+  SmtSolver& d_smtSolver;
+   
+};
+
+}  // namespace smt
+}  // namespace CVC4
+
+#endif /* CVC4__SMT__QUANT_ELIM_SOLVER_H */
