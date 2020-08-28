52984842acbf233816d40b77101539b04c914e97
Haniel Barbosa <hanielbbarbosa@gmail.com>
2020-08-28 11:26:23 -0300

    fixing non-replacement of proof option test

--- src/smt/set_defaults.cpp
+++ src/smt/set_defaults.cpp
@@ -834,7 +834,8 @@ void setDefaults(LogicInfo& logic, bool isInternalSubsolver)
   }
   // Do we need to track instantiations?
   // Needed for sygus due to single invocation techniques.
-  if (options::cegqiNestedQE() || (!options::trackInstLemmas.wasSetByUser())
+  if (options::cegqiNestedQE()
+      || (options::unsatCores() && !options::trackInstLemmas.wasSetByUser())
       || is_sygus)
   {
     options::trackInstLemmas.set(true);
