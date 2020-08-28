e088bc589e6a2f7447b44cf111ca150f4e52b971
ajreynol <andrew.j.reynolds@gmail.com>
2020-08-28 13:34:04 -0500

    (new theory) Update TheoryDatatypes to new standard

---
+++ src/theory/datatypes/inference_manager.cpp
@@ -0,0 +1,190 @@
+/*********************                                                        */
+/*! \file datatypes_inference_manager.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Datatypes inference manager
+ **/
+
+#include "theory/datatypes/inference_manager.h"
+
+#include "expr/dtype.h"
+#include "options/datatypes_options.h"
+#include "theory/theory.h"
+
+using namespace CVC4::kind;
+
+namespace CVC4 {
+namespace theory {
+namespace datatypes {
+
+InferenceManager::InferenceManager(Theory& t,
+                                   TheoryState& state,
+                                   ProofNodeManager* pnm)
+    : InferenceManagerBuffered(t, state, pnm),
+      d_lemmasSent(t.getSatContext()),
+      d_addedLemma(false),
+      d_addedFact(false)
+{
+  d_true = NodeManager::currentNM()->mkConst(true);
+}
+
+void InferenceManager::reset()
+{
+  d_addedLemma = false;
+  d_addedFact = false;
+}
+
+bool InferenceManager::mustCommunicateFact(Node n, Node exp) const
+{
+  Trace("dt-lemma-debug") << "Compute for " << exp << " => " << n << std::endl;
+  bool addLemma = false;
+  if (options::dtInferAsLemmas() && exp != d_true)
+  {
+    addLemma = true;
+  }
+  else if (n.getKind() == EQUAL)
+  {
+    TypeNode tn = n[0].getType();
+    if (!tn.isDatatype())
+    {
+      addLemma = true;
+    }
+    else
+    {
+      const DType& dt = tn.getDType();
+      addLemma = dt.involvesExternalType();
+    }
+  }
+  else if (n.getKind() == LEQ || n.getKind() == OR)
+  {
+    addLemma = true;
+  }
+  if (addLemma)
+  {
+    Trace("dt-lemma-debug") << "Communicate " << n << std::endl;
+    return true;
+  }
+  Trace("dt-lemma-debug") << "Do not need to communicate " << n << std::endl;
+  return false;
+}
+
+void InferenceManager::process()
+{
+  // process pending lemmas, used infrequently, only for definitional lemmas
+  doPendingLemmas();
+  // now process the pending facts
+  size_t i = 0;
+  NodeManager* nm = NodeManager::currentNM();
+  while (!d_theoryState.isInConflict() && i < d_pendingFact.size())
+  {
+    std::pair<Node, Node>& pfact = d_pendingFact[i];
+    Node fact = pfact.first;
+    Node exp = pfact.second;
+    Trace("datatypes-debug")
+        << "Assert fact (#" << (i + 1) << "/" << d_pendingFact.size() << ") "
+        << fact << " with explanation " << exp << std::endl;
+    // check to see if we have to communicate it to the rest of the system
+    if (mustCommunicateFact(fact, exp))
+    {
+      Node lem = fact;
+      if (exp.isNull() || exp == d_true)
+      {
+        Trace("dt-lemma-debug") << "Trivial explanation." << std::endl;
+      }
+      else
+      {
+        Trace("dt-lemma-debug") << "Get explanation..." << std::endl;
+        std::vector<TNode> assumptions;
+        explain(exp, assumptions);
+        if (assumptions.empty())
+        {
+          lem = fact;
+        }
+        else
+        {
+          std::vector<Node> children;
+          for (const TNode& assumption : assumptions)
+          {
+            children.push_back(assumption.negate());
+          }
+          children.push_back(fact);
+          lem = nm->mkNode(OR, children);
+        }
+      }
+      Trace("dt-lemma") << "Datatypes lemma : " << lem << std::endl;
+      doSendLemma(lem);
+    }
+    else
+    {
+      // assert the internal fact
+      bool polarity = fact.getKind() != NOT;
+      TNode atom = polarity ? fact : fact[0];
+      assertInternalFact(atom, polarity, exp);
+      d_addedFact = true;
+    }
+    Trace("datatypes-debug") << "Finished fact " << fact
+                             << ", now = " << d_theoryState.isInConflict()
+                             << " " << d_pendingFact.size() << std::endl;
+    i++;
+  }
+  d_pendingFact.clear();
+}
+
+bool InferenceManager::hasAddedFact() const
+{
+  return d_addedFact;
+}
+bool InferenceManager::hasAddedLemma() const
+{
+  return d_addedLemma;
+}
+bool InferenceManager::doSendLemma(Node lem, LemmaProperty p, bool cached)
+{
+  // don't cache lemmas with non-standard properties
+  Assert (!cached || p==LemmaProperty::NONE);
+  bool doSend = false;
+  if (!cached)
+  {
+    // always send
+    doSend = true;
+  }
+  else if (d_lemmasSent.find(lem) == d_lemmasSent.end())
+  {
+    Trace("dt-lemma-send") << "TheoryDatatypes::doSendLemma : " << lem
+                           << std::endl;
+    d_lemmasSent.insert(lem);
+    doSend = true;
+  }
+  if (doSend)
+  {
+    // call the base class
+    lemma(lem);
+    d_addedLemma = true;
+    return true;
+  }
+  Trace("dt-lemma-send") << "TheoryDatatypes::doSendLemma : duplicate : " << lem
+                         << std::endl;
+  return false;
+}
+
+bool InferenceManager::doSendLemmas(const std::vector<Node>& lemmas)
+{
+  bool ret = false;
+  for (const Node& lem : lemmas)
+  {
+    bool cret = doSendLemma(lem);
+    ret = ret || cret;
+  }
+  return ret;
+}
+
+}  // namespace datatypes
+}  // namespace theory
+}  // namespace CVC4
---
+++ src/theory/datatypes/inference_manager.h
@@ -0,0 +1,107 @@
+/*********************                                                        */
+/*! \file inference_manager.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Datatypes inference manager
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef CVC4__THEORY__DATATYPES__INFERENCE_MANAGER_H
+#define CVC4__THEORY__DATATYPES__INFERENCE_MANAGER_H
+
+#include "context/cdhashmap.h"
+#include "expr/node.h"
+#include "theory/inference_manager_buffered.h"
+
+namespace CVC4 {
+namespace theory {
+namespace datatypes {
+
+/**
+ * The datatypes inference manager. The main unique features of this inference
+ * manager are:
+ * (1) Explicit caching of lemmas,
+ * (2) A custom process() method with relies on a policy determining which
+ * facts must be sent as lemmas (mustCommunicateFact).
+ * (3) Methods for tracking when lemmas and facts have been processed.
+ */
+class InferenceManager : public InferenceManagerBuffered
+{
+  typedef context::CDHashSet<Node, NodeHashFunction> NodeSet;
+
+ public:
+  InferenceManager(Theory& t, TheoryState& state, ProofNodeManager* pnm);
+  ~InferenceManager() {}
+  /**
+   * Reset, which resets flags regarding whether we have added lemmas or facts.
+   */
+  void reset();
+  /**
+   * Process the current lemmas and facts. This is a custom method that can
+   * be seen as overriding the behavior of calling both doPendingLemmas and
+   * doPendingFacts. It determines whether facts should be sent as lemmas
+   * or processed internally.
+   */
+  void process();
+  /** 
+   * Have we asserted an internal fact (via process) since the most recent call
+   * to reset? 
+   */
+  bool hasAddedFact() const;
+  /** 
+   * Have we sent a lemma (via process) since the most recent call to reset? 
+   */
+  bool hasAddedLemma() const;
+  /**
+   * If not cached, send lemma on lem the output channel and cache. Returns
+   * true if a lemma was sent.
+   */
+  bool doSendLemma(Node lem, LemmaProperty p = LemmaProperty::NONE, bool cached=true);
+  /** Multi-version of the above, returns true if any lemma was sent. */
+  bool doSendLemmas(const std::vector<Node>& lem);
+ protected:
+  /** must communicate fact
+     //the datatypes decision procedure makes "internal" inferences :
+  //  (1) Unification : C( t1...tn ) = C( s1...sn ) => ti = si
+  //  (2) Label : ~is_C1( t ) ... ~is_C{i-1}( t ) ~is_C{i+1}( t ) ... ~is_Cn( t
+  ) => is_Ci( t )
+  //  (3) Instantiate : is_C( t ) => t = C( sel_1( t ) ... sel_n( t ) )
+  //  (4) collapse selector : S( C( t1...tn ) ) = t'
+  //  (5) collapse term size : size( C( t1...tn ) ) = 1 + size( t1 ) + ... +
+  size( tn )
+  //  (6) non-negative size : 0 <= size( t )
+  This method returns true if the fact must be sent out as a lemma. If it
+  returns false, then we assert the fact internally.
+  //We may need to communicate outwards if the conclusions involve other
+  theories.  Also communicate (6) and OR conclusions.
+  */
+  bool mustCommunicateFact(Node n, Node exp) const;
+  /** A cache of all lemmas sent */
+  NodeSet d_lemmasSent;
+  /** Common nodes */
+  Node d_true;
+  /**
+   * This flag is set to true during a full effort check if this theory
+   * called d_out->lemma(...).
+   */
+  bool d_addedLemma;
+  /**
+   * This flag is set to true during a full effort check if this theory
+   * added an internal fact to its equality engine.
+   */
+  bool d_addedFact;
+};
+
+}  // namespace datatypes
+}  // namespace theory
+}  // namespace CVC4
+
+#endif
