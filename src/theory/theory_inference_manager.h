e397377b484cd29467e4ae6017999f3fb9cb807e
ajreynol <andrew.j.reynolds@gmail.com>
2020-09-04 15:36:33 -0500

    Add asLemma flag to theory inference process

--- src/theory/theory_inference_manager.h
+++ src/theory/theory_inference_manager.h
@@ -96,7 +96,9 @@ class TheoryInferenceManager
    * Returns true if we are in conflict, or if we have sent a lemma or fact
    * since the last call to reset.
    */
-  bool hasProcessed() const;
+  bool hasSent() const;
+  /** Get the underlying proof equality engine */
+  eq::ProofEqEngine* getProofEqEngine();
   //--------------------------------------- propagations
   /**
    * T-propagate literal lit, possibly encountered by equality engine,
