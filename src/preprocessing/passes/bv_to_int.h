a8939854b88a4d9ead58ef7191897f0e5f68cefb
yoni206 <yoni206@users.noreply.github.com>
2020-09-10 10:23:30 -0700

    bv2int: refactoring the main translation loop (#5051)
    This PR introduces a refactoring of the main translation loop in bv2int preprocessing pass.
    Many parts are wrapped by helper functions and so the main loop becomes smaller.
    
    remark: selecting "Hide whitespace changes" cuts the diff by ~50%.

--- src/preprocessing/passes/bv_to_int.h
+++ src/preprocessing/passes/bv_to_int.h
@@ -292,8 +292,33 @@ class BVToInt : public PreprocessingPass
    * This is useful, for example, when asking
    * for a model-value of a term that includes the
    * original UF f.
+   * @param bvUF the original function
+   * @param intUF the translated function
    */
-  void defineBVUFAsIntUF(Node bvUF);
+  void defineBVUFAsIntUF(Node bvUF, Node intUF);
+
+  /**
+   * @param bvUF is an uninterpreted function symbol from the original formula
+   * @return a fresh uninterpreted function symbol, obtained from bvUF
+     by replacing every argument of type BV to an argument of type Integer,
+     and the return type becomes integer in case it was BV.
+   */
+  Node translateFunctionSymbol(Node bvUF);
+
+  /**
+   * Performs the actual translation to integers for nodes
+   * that have children.
+   */
+  Node translateWithChildren(Node original,
+                             const vector<Node>& translated_children);
+
+  /**
+   * Performs the actual translation to integers for nodes
+   * that don't have children (variables, constants, uninterpreted function
+   * symbols).
+   */
+  Node translateNoChildren(Node original);
+
   /**
    * Caches for the different functions
    */
