e40ed205a53ee865dba6b7bb4ff74136d7cc28ad
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2020-09-11 16:01:47 -0500

    Update src/smt/proof_manager.h
    Co-authored-by: Haniel Barbosa <hanielbbarbosa@gmail.com>

--- src/smt/proof_manager.h
+++ src/smt/proof_manager.h
@@ -85,7 +85,7 @@ class PfManager
   std::unique_ptr<ProofChecker> d_pchecker;
   /** A proof node manager based on the above checker */
   std::unique_ptr<ProofNodeManager> d_pnm;
-  /** The preprocess proof manager. */
+  /** The preprocess proof generator. */
   std::unique_ptr<smt::PreprocessProofGenerator> d_pppg;
   /** The proof post-processor */
   std::unique_ptr<smt::ProofPostproccess> d_pfpp;
