e6e416bcd98c04ce99927946fa9e524a3b289de1
Haniel Barbosa <hanielbbarbosa@gmail.com>
2020-09-12 20:21:57 -0300

    [proof-new] Adds Lazy CDProof chain data-structure

---
+++ src/expr/lazy_proof_chain.cpp
@@ -0,0 +1,274 @@
+/*********************                                                        */
+/*! \file lazy_proof_chain.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Haniel Barbosa
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implementation of lazy proof utility
+ **/
+
+#include "expr/lazy_proof_chain.h"
+
+#include "expr/proof.h"
+#include "expr/proof_node_algorithm.h"
+#include "options/smt_options.h"
+
+namespace CVC4 {
+
+LazyCDProofChain::LazyCDProofChain(ProofNodeManager* pnm, context::Context* c)
+    : d_manager(pnm), d_context(), d_gens(c ? c : &d_context)
+{
+}
+
+LazyCDProofChain::~LazyCDProofChain() {}
+
+std::shared_ptr<ProofNode> LazyCDProofChain::getProofFor(Node fact)
+{
+  Trace("lazy-cdproofchain")
+      << "LazyCDProofChain::getProofFor " << fact << "\n";
+  // which facts have had proofs retrieved for. This is maintained to avoid
+  // cycles. It also saves the proof node of the fact
+  std::unordered_map<Node, std::shared_ptr<ProofNode>, NodeHashFunction>
+      expandedToConnect;
+  std::unordered_set<Node, NodeHashFunction> expanded;
+  std::vector<Node> visit{fact};
+  std::unordered_map<Node, Node, NodeHashFunction> visited;
+  Node cur;
+  do
+  {
+    cur = visit.back();
+    visit.pop_back();
+    const auto itVisited = visited.find(cur);
+    // pre-traversal
+    if (itVisited == visited.end())
+    {
+      Trace("lazy-cdproofchain")
+          << "LazyCDProofChain::getProofFor: check " << cur << "\n";
+      if (!hasGenerator(cur))
+      {
+        Trace("lazy-cdproofchain")
+            << "LazyCDProofChain::getProofFor: nothing to do\n";
+        // nothing to do for this fact, it'll be a leaf in the final proof node
+        visited[cur] = cur;
+        continue;
+      }
+      // retrive its proof node
+      bool isSym = false;
+      ProofGenerator* pg = getGeneratorFor(cur, isSym);
+      Assert(pg != nullptr);
+      Trace("lazy-cdproofchain")
+          << "LazyCDProofChain::getProofFor: Call generator " << pg->identify()
+          << " for chain link " << cur << "\n";
+      Node curFactGen = isSym ? CDProof::getSymmFact(cur) : cur;
+      std::shared_ptr<ProofNode> curPfn = pg->getProofFor(curFactGen);
+      if (isSym)
+      {
+        curPfn = d_manager->mkNode(PfRule::SYMM, {curPfn}, {});
+      }
+      Trace("lazy-cdproofchain-debug")
+          << "LazyCDProofChain::getProofFor: stored proof: " << *curPfn.get()
+          << "\n";
+      // retrieve free assumptions and their respective proof nodes
+      std::map<Node, std::vector<ProofNode*>> famap;
+      expr::getFreeAssumptionsMap(curPfn.get(), famap);
+      if (Trace.isOn("lazy-cdproofchain"))
+      {
+        Trace("lazy-cdproofchain")
+            << "LazyCDProofChain::getProofFor: free assumptions:\n";
+        for (auto fap : famap)
+        {
+          Trace("lazy-cdproofchain")
+              << "LazyCDProofChain::getProofFor:  - " << fap.first << "\n";
+        }
+      }
+      Trace("lazy-cdproofchain") << push;
+      Trace("lazy-cdproofchain-debug") << push;
+      // mark for future connection, when free assumptions that are chain links
+      // must have been expanded and connected
+      visited[cur] = Node::null();
+      visit.push_back(cur);
+      // enqueue free assumptions to process
+      for (const std::pair<const Node, std::vector<ProofNode*>>& fap : famap)
+      {
+        // check cycles, which are cases in which we have facts in
+        // expandedToConnect but not already expanded
+        auto it = expandedToConnect.find(fap.first);
+        if (it != expandedToConnect.end() && !expanded.count(fap.first))
+        {
+          // mark as a not to expand assumption. We do this rather than just
+          // assining visited[fap.first] to a non-null value so that we properly
+          // pop the traces previously pushed.
+          Trace("lazy-cdproofchain")
+              << "LazyCDProofChain::getProofFor: removing cyclic assumption "
+              << fap.first << " from expansion\n";
+          expandedToConnect.erase(it);
+          break;
+        }
+        // already expanded, skip
+        if (expanded.count(fap.first))
+        {
+          continue;
+        }
+        visit.push_back(fap.first);
+      }
+      // map node whose proof node must be expanded to the respective poof node
+      expandedToConnect[cur] = curPfn;
+    }
+    // post-traversal
+    else if (itVisited->second.isNull())
+    {
+      Assert(!expanded.count(cur));
+      Trace("lazy-cdproofchain") << pop;
+      Trace("lazy-cdproofchain-debug") << pop;
+      // mark final processing
+      visited[cur] = cur;
+      // get proof node to expand
+      auto it = expandedToConnect.find(cur);
+      // this was part of a cycle, ignore it
+      if (it == expandedToConnect.end())
+      {
+        continue;
+      }
+      Trace("lazy-cdproofchain") << "LazyCDProofChain::getProofFor: "
+                                    "connect proofs for assumptions of: "
+                                 << cur << "\n";
+
+      // get assumptions
+      std::map<Node, std::vector<ProofNode*>> famap;
+      expr::getFreeAssumptionsMap(it->second.get(), famap);
+      // the first element of the iterator pair is the proofNode of cur, the
+      // second is the map of the assumption node to all its proofnodes
+      for (const std::pair<const Node, std::vector<ProofNode*>>& fap : famap)
+      {
+        // see if assumption has been expanded and thus has a new proof node to
+        // connect here
+        if (!expanded.count(fap.first))
+        {
+          Trace("lazy-cdproofchain")
+              << "LazyCDProofChain::getProofFor: assumption " << fap.first
+              << " not expanded\n";
+          continue;
+        }
+        auto itt = expandedToConnect.find(fap.first);
+        Assert(itt != expandedToConnect.end());
+        Trace("lazy-cdproofchain")
+            << "LazyCDProofChain::getProofFor: assumption " << fap.first
+            << " expanded\n";
+        Trace("lazy-cdproofchain-debug")
+            << "LazyCDProofChain::getProofFor: expanded to: "
+            << *itt->second.get() << "\n";
+        // update each assumption proof node with the expanded proof node of
+        // that assumption
+        for (ProofNode* pfn : fap.second)
+        {
+          d_manager->updateNode(pfn, itt->second.get());
+        }
+      }
+      // mark the fact as expanded
+      expanded.insert(cur);
+      Trace("lazy-cdproofchain-debug")
+          << "LazyCDProofChain::getProofFor: expanded proof node: "
+          << *it->second.get() << "\n";
+    }
+  } while (!visit.empty());
+  if (!expanded.count(cur))
+  {
+    return nullptr;
+  }
+  // final proof of fact
+  Assert(expandedToConnect.find(cur) != expandedToConnect.end());
+  return expandedToConnect[cur];
+}
+
+void LazyCDProofChain::addLazyStep(Node expected, ProofGenerator* pg)
+{
+  Assert(pg != nullptr);
+  Trace("lazy-cdproofchain") << "LazyCDProofChain::addLazyStep: " << expected
+                             << " set to generator " << pg->identify() << "\n";
+  // note this will rewrite the generator for expected, if any
+  d_gens.insert(expected, pg);
+}
+
+void LazyCDProofChain::addLazyStep(Node expected,
+                                   ProofGenerator* pg,
+                                   const std::vector<Node>& assumptions,
+                                   const char* ctx)
+{
+  Assert(pg != nullptr);
+  Trace("lazy-cdproofchain") << "LazyCDProofChain::addLazyStep: " << expected
+                             << " set to generator " << pg->identify() << "\n";
+  // note this will rewrite the generator for expected, if any
+  d_gens.insert(expected, pg);
+  // check if chain is closed if options::proofNewEagerChecking() is on
+  if (options::proofNewEagerChecking())
+  {
+    Trace("lazy-cdproofchain")
+        << "LazyCDProofChain::addLazyStep: Checking closed proof...\n";
+    std::shared_ptr<ProofNode> pfn = pg->getProofFor(expected);
+    std::vector<Node> allowedLeaves{assumptions.begin(), assumptions.end()};
+    // add all current links in the chain
+    for (const std::pair<const Node, ProofGenerator*>& link : d_gens)
+    {
+      allowedLeaves.push_back(link.first);
+    }
+    if (Trace.isOn("lazy-cdproofchain"))
+    {
+      Trace("lazy-cdproofchain") << "Checking relative to leaves...\n";
+      for (const Node& n : allowedLeaves)
+      {
+        Trace("lazy-cdproofchain") << "  - " << n << "\n";
+      }
+      Trace("lazy-cdproofchain") << "\n";
+    }
+    pfnEnsureClosedWrt(pfn.get(), allowedLeaves, "lazy-cdproofchain", ctx);
+  }
+}
+
+bool LazyCDProofChain::hasGenerator(Node fact) const
+{
+  auto it = d_gens.find(fact);
+  if (it != d_gens.end())
+  {
+    return true;
+  }
+  // maybe there is a symmetric fact?
+  Node factSym = CDProof::getSymmFact(fact);
+  if (!factSym.isNull())
+  {
+    it = d_gens.find(factSym);
+  }
+  return it != d_gens.end();
+}
+
+ProofGenerator* LazyCDProofChain::getGeneratorFor(Node fact, bool& isSym)
+{
+  isSym = false;
+  auto it = d_gens.find(fact);
+  if (it != d_gens.end())
+  {
+    return (*it).second;
+  }
+  Node factSym = CDProof::getSymmFact(fact);
+  // could be symmetry
+  if (factSym.isNull())
+  {
+    // can't be symmetry, return the default generator
+    return nullptr;
+  }
+  it = d_gens.find(factSym);
+  if (it != d_gens.end())
+  {
+    isSym = true;
+    return (*it).second;
+  }
+  return nullptr;
+}
+
+std::string LazyCDProofChain::identify() const { return "LazyCDProofChain"; }
+
+}  // namespace CVC4
---
+++ src/expr/lazy_proof_chain.h
@@ -0,0 +1,125 @@
+/*********************                                                        */
+/*! \file lazy_proof_chain.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Haniel Barbosa
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Lazy proof chain utility
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef CVC4__EXPR__LAZY_PROOF_CHAIN_H
+#define CVC4__EXPR__LAZY_PROOF_CHAIN_H
+
+#include <unordered_map>
+#include <vector>
+
+#include "context/cdhashmap.h"
+#include "expr/proof_generator.h"
+#include "expr/proof_node_manager.h"
+
+namespace CVC4 {
+
+/**
+ * A (context-dependent) lazy generator for proof chains. This class is an
+ * extension of ProofGenerator that additionally that maps facts to proof
+ * generators in a context-dependent manner. The map is built with the addition
+ * of lazy steps mapping facts to proof generators. More importantly, its
+ * getProofFor method expands the proof generators registered to this class by
+ * connecting, for the proof generated to one fact, assumptions to the proofs
+ * generated for those assumptinos that are registered in the chain.
+ */
+class LazyCDProofChain : public ProofGenerator
+{
+ public:
+  /** Constructor
+   *
+   * @param pnm The proof node manager for constructing ProofNode objects.
+   * @param c The context that this class depends on. If none is provided,
+   * this class is context-independent.
+   */
+  LazyCDProofChain(ProofNodeManager* pnm, context::Context* c = nullptr);
+  ~LazyCDProofChain();
+  /**
+   * Get lazDyet have a concrete proof and that are registered in the chains
+   * (i.e., are in the domain of d_gens). Starting with the proof generated for
+   * fact, if any.
+   *
+   * For example, if d_gens consists of the following pairs
+   *
+   * --- (A, PG1)
+   * --- (B, PG2)
+   * --- (C, PG3)
+   *
+   * and getProofFor(A) is called, with PG1 generating a proof with assumptions
+   * B and D, then B is expanded, with its assumption proof node being updated
+   * to the expanded proof node, while D is not. Assuming PG2 provides a proof
+   * with assumptions C and E, then C is expanded and E is not. Suppose PG3
+   * gives a closed proof, thus the call getProofFor(A) produces a proof node
+   *
+   *  A : ( ... B : ( ... C : (...), ... ASSUME( E ) ), ... ASSUME( D ) )
+   *
+   * Note that the expansions are done directly on the proof nodes produced by
+   * the generators.
+   */
+  std::shared_ptr<ProofNode> getProofFor(Node fact) override;
+  /** Add step by generator
+   *
+   * This method stores that expected can be proven by proof generator pg if
+   * it is required to do so. This mapping is maintained in the remainder of
+   * the current context (according to the context c provided to this class).
+   *
+   * Moreover the lazy steps of this class are expected to fulfill the
+   * requirement that pg.getProofFor(expected) generates a proof node closed
+   * with relation to
+   *  (1) a fixed set F1, ..., Fn,
+   *  (2) formulas in the current domain of d_gens.
+   *
+   * This is so that we only add links to the chain that depend on a fixed set
+   * of assumptions or in other links.
+   *
+   * @param expected The fact that can be proven.
+   * @param pg The generator that can proof expected.
+   * @param assumptions The fixed set of assumptions with relation to which the
+   * chain, now augmented with expect, must be closed.
+   * @param ctx The context we are in (for debugging).
+   */
+  void addLazyStep(Node expected,
+                   ProofGenerator* pg,
+                   const std::vector<Node>& assumptions,
+                   const char* ctx = "LazyCDProofChain::addLazyStep");
+
+  /** As above but does not do the closedness check. */
+  void addLazyStep(Node expected, ProofGenerator* pg);
+
+  /** Does the given fact have an explicitly provided generator? */
+  bool hasGenerator(Node fact) const;
+
+  /**
+   * Get generator for fact, or nullptr if it doesnt exist. This method is
+   * robust to symmetry of (dis)equality. It updates isSym to true if a
+   * proof generator for the symmetric form of fact was provided.
+   */
+  ProofGenerator* getGeneratorFor(Node fact, bool& isSym);
+
+  /** identify */
+  std::string identify() const override;
+
+ private:
+  /** The proof manager, used for allocating new ProofNode objects */
+  ProofNodeManager* d_manager;
+  /** A dummy context used by this class if none is provided */
+  context::Context d_context;
+  /** Maps facts that can be proven to generators */
+  context::CDHashMap<Node, ProofGenerator*, NodeHashFunction> d_gens;
+};
+
+}  // namespace CVC4
+
+#endif /* CVC4__EXPR__LAZY_PROOF_CHAIN_H */
