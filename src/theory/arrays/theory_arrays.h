2f6a69a7e32be51f612ba9a34a27e7813b14eb9c
Mathias Preiner <mathias.preiner@gmail.com>
2020-09-14 14:29:42 -0700

    Update headers.

--- src/theory/arrays/theory_arrays.h
+++ src/theory/arrays/theory_arrays.h
@@ -2,10 +2,10 @@
 /*! \file theory_arrays.h
  ** \verbatim
  ** Top contributors (to current version):
- **   Morgan Deters, Clark Barrett, Andrew Reynolds
+ **   Morgan Deters, Andrew Reynolds, Clark Barrett
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
- ** in the top-level source directory) and their institutional affiliations.
+ ** in the top-level source directory and their institutional affiliations.
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
  **
