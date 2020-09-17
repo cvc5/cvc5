279f2cbdd00c61bda7f601bd421450eb60ed79ce
ajreynol <andrew.j.reynolds@gmail.com>
2020-09-16 19:59:05 -0500

    Update

--- test/api/issue4889.cpp
+++ test/api/issue4889.cpp
@@ -13,22 +13,25 @@
  **/
 
 #include "api/cvc4cpp.h"
-                                                                                   
-using namespace CVC4::api;                                                      
-                                                                                   
-int main()                                                                      
-{                                                                               
-  Solver slv;                                                                   
-  Sort sort_int = slv.getIntegerSort();                                         
-  Sort sort_array = slv.mkArraySort(sort_int, sort_int);                        
-  Sort sort_fp128 = slv.mkFloatingPointSort(15, 113);                           
-  Sort sort_fp32 = slv.mkFloatingPointSort(8, 24);                              
-  Sort sort_bool = slv.getBooleanSort();                                        
-  Term const0 = slv.mkConst(sort_fp32, "_c0");                                  
-  Term const1 = slv.mkConst(sort_fp32, "_c2");                                  
-  Term const2 = slv.mkConst(sort_bool, "_c4");                                                                                                                                                                           
-  Term ite = slv.mkTerm(ITE, const2, const1, const0);                           
-  Term rem = slv.mkTerm(FLOATINGPOINT_REM, ite, const1);                        
-  Term isnan = slv.mkTerm(FLOATINGPOINT_ISNAN, rem);                            
-  slv.checkSatAssuming(isnan);                                                  
+
+using namespace CVC4::api;
+
+int main()
+{
+#ifdef CVC4_USE_SYMFPU
+  Solver slv;
+  Sort sort_int = slv.getIntegerSort();
+  Sort sort_array = slv.mkArraySort(sort_int, sort_int);
+  Sort sort_fp128 = slv.mkFloatingPointSort(15, 113);
+  Sort sort_fp32 = slv.mkFloatingPointSort(8, 24);
+  Sort sort_bool = slv.getBooleanSort();
+  Term const0 = slv.mkConst(sort_fp32, "_c0");
+  Term const1 = slv.mkConst(sort_fp32, "_c2");
+  Term const2 = slv.mkConst(sort_bool, "_c4");
+  Term ite = slv.mkTerm(ITE, const2, const1, const0);
+  Term rem = slv.mkTerm(FLOATINGPOINT_REM, ite, const1);
+  Term isnan = slv.mkTerm(FLOATINGPOINT_ISNAN, rem);
+  slv.checkSatAssuming(isnan);
+#endif
+  return 0;
 }
