8886673d16749d1f35da9b39a7b1ec779eec10bd
mudathirmahgoub <mudathirmahgoub@gmail.com>
2020-09-21 02:49:09 -0500

    Address

--- src/theory/bags/term_registry.h
+++ src/theory/bags/term_registry.h
@@ -2,7 +2,7 @@
 /*! \file term_registry.h
  ** \verbatim
  ** Top contributors (to current version):
- **   Mudathir Mohamed, Andrew Reynolds
+ **   Mudathir Mohamed
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
--- src/theory/bags/theory_bags_type_rules.h
+++ src/theory/bags/theory_bags_type_rules.h
@@ -2,7 +2,7 @@
 /*! \file theory_bags_type_rules.h
  ** \verbatim
  ** Top contributors (to current version):
- **   Mudathir Mohamed, Andrew Reynolds
+ **   Mudathir Mohamed
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
