e2c4c4bdeaa5b49a293b23725802f688908c35ab
mudathirmahgoub <mudathirmahgoub@gmail.com>
2020-09-21 02:48:12 -0500

    Address

--- src/theory/bags/theory_bags_rewriter.h
+++ src/theory/bags/theory_bags_rewriter.h
@@ -26,38 +26,9 @@ namespace bags {
 class TheoryBagsRewriter : public TheoryRewriter
 {
  public:
-  /**
-   * Rewrite a node into the normal form for the theory of bags.
-   * Called in post-order (really reverse-topological order) when
-   * traversing the expression DAG during rewriting.  This is the
-   * main function of the rewriter, and because of the ordering,
-   * it can assume its children are all rewritten already.
-   *
-   * This function can return one of three rewrite response codes
-   * along with the rewritten node:
-   *
-   *   REWRITE_DONE indicates that no more rewriting is needed.
-   *   REWRITE_AGAIN means that the top-level expression should be
-   *     rewritten again, but that its children are in final form.
-   *   REWRITE_AGAIN_FULL means that the entire returned expression
-   *     should be rewritten again (top-down with preRewrite(), then
-   *     bottom-up with postRewrite()).
-   *
-   * Even if this function returns REWRITE_DONE, if the returned
-   * expression belongs to a different theory, it will be fully
-   * rewritten by that theory's rewriter.
-   */
+
   RewriteResponse postRewrite(TNode node) override;
 
-  /**
-   * Rewrite a node into the normal form for the theory of bags
-   * in pre-order (really topological order)---meaning that the
-   * children may not be in the normal form.  This is an optimization
-   * for theories with cancelling terms (e.g., 0 * (big-nasty-expression)
-   * in arithmetic rewrites to 0 without the need to look at the big
-   * nasty expression).  Since it's only an optimization, the
-   * implementation here can do nothing.
-   */
   RewriteResponse preRewrite(TNode node) override;
 }; /* class TheoryBagsRewriter */
 
