99e4b7521d5425c62b653e818ebda0a91ceb9975
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-10-01 14:52:19 +0200

    Replace static flag by option --nl-cad-initial

--- src/theory/arith/nl/cad/cdcac.h
+++ src/theory/arith/nl/cad/cdcac.h
@@ -183,12 +183,6 @@ class CDCAC
 
   /** The linear assignment used as an initial guess. */
   std::vector<poly::Value> d_initialAssignment;
-
-  /**
-   * Decides whether we use the model from the linear solver as an initial
-   * assignment to guide the sampling process.
-   */
-  static constexpr bool use_initial_assignment = false;
 };
 
 }  // namespace cad
