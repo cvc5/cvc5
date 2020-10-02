0221ae1928804c7bb8bee9e00769759c4322a9af
ajreynol <andrew.j.reynolds@gmail.com>
2020-10-02 09:43:01 -0500

    Format

--- src/theory/arith/arith_preprocess.cpp
+++ src/theory/arith/arith_preprocess.cpp
@@ -26,9 +26,11 @@ ArithPreprocess::ArithPreprocess(ArithState& state,
 {
 }
 TrustNode ArithPreprocess::eliminate(TNode n) { return d_opElim.eliminate(n); }
-bool ArithPreprocess::reduceAssertion(TNode atom) {
-  context::CDHashMap<Node, bool, NodeHashFunction>::const_iterator it = d_reduced.find(atom);
-  if (it!=d_reduced.end())
+bool ArithPreprocess::reduceAssertion(TNode atom)
+{
+  context::CDHashMap<Node, bool, NodeHashFunction>::const_iterator it =
+      d_reduced.find(atom);
+  if (it != d_reduced.end())
   {
     // already computed
     return (*it).second;
@@ -38,20 +40,22 @@ bool ArithPreprocess::reduceAssertion(TNode atom) {
   {
     // did not reduce
     d_reduced[atom] = false;
-    return false; 
+    return false;
   }
   // tn is of kind REWRITE, turn this into a LEMMA here
   TrustNode tlem = TrustNode::mkTrustLemma(tn.getProven(), tn.getGenerator());
   // must preprocess
   d_im.trustedLemma(tlem, LemmaProperty::PREPROCESS);
-  // mark the atom as reduced 
+  // mark the atom as reduced
   d_reduced[atom] = true;
   return true;
 }
 
-bool ArithPreprocess::isReduced(TNode atom) const { 
-  context::CDHashMap<Node, bool, NodeHashFunction>::const_iterator it = d_reduced.find(atom);
-  if (it==d_reduced.end())
+bool ArithPreprocess::isReduced(TNode atom) const
+{
+  context::CDHashMap<Node, bool, NodeHashFunction>::const_iterator it =
+      d_reduced.find(atom);
+  if (it == d_reduced.end())
   {
     return false;
   }
