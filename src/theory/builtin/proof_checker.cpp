c36a7ff186c7d98c29cb873ae767964e123928c9
ajreynol <andrew.j.reynolds@gmail.com>
2020-10-03 08:27:56 -0500

    Format

--- src/theory/builtin/proof_checker.cpp
+++ src/theory/builtin/proof_checker.cpp
@@ -352,8 +352,9 @@ Node BuiltinProofRuleChecker::checkInternal(PfRule id,
     return RemoveTermFormulas::getAxiomFor(args[0]);
   }
   else if (id == PfRule::PREPROCESS || id == PfRule::THEORY_PREPROCESS
-           || id == PfRule::WITNESS_AXIOM || id == PfRule::THEORY_LEMMA
-           || id == PfRule::PREPROCESS_LEMMA || id == PfRule::THEORY_REWRITE || id = PfRule::TRUST_REWRITE)
+               || id == PfRule::WITNESS_AXIOM || id == PfRule::THEORY_LEMMA
+               || id == PfRule::PREPROCESS_LEMMA || id == PfRule::THEORY_REWRITE
+               || id = PfRule::TRUST_REWRITE)
   {
     // "trusted" rules
     Assert(children.empty());
