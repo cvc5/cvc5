822a6d2e10c8eed5a3a9178d8c2eec3199ba429e
ajreynol <andrew.j.reynolds@gmail.com>
2020-10-03 08:41:52 -0500

    Fix

--- src/theory/builtin/proof_checker.cpp
+++ src/theory/builtin/proof_checker.cpp
@@ -355,7 +355,7 @@ Node BuiltinProofRuleChecker::checkInternal(PfRule id,
   else if (id == PfRule::PREPROCESS || id == PfRule::THEORY_PREPROCESS
                || id == PfRule::WITNESS_AXIOM || id == PfRule::THEORY_LEMMA
                || id == PfRule::PREPROCESS_LEMMA || id == PfRule::THEORY_REWRITE
-               || id = PfRule::TRUST_REWRITE || id == PfRule::TRUST_SUBS)
+               || id == PfRule::TRUST_REWRITE || id == PfRule::TRUST_SUBS)
   {
     // "trusted" rules
     Assert(children.empty());
