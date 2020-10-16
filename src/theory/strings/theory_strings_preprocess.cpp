efa9ce6741dafe4e5f1de6224b3dd7f3f773aa0a
yoni206 <yoni206@gmail.com>
2020-10-16 11:49:10 -0700

    description

--- src/theory/strings/theory_strings_preprocess.cpp
+++ src/theory/strings/theory_strings_preprocess.cpp
@@ -453,6 +453,13 @@ Node StringsPreprocess::reduce(Node t,
     Node b2 = nm->mkNode(EQUAL, skt, nm->mkNode(APPLY_UF, uf, s, n));
     Node lemma = nm->mkNode(ITE, cond, b1, b2);
 
+    // assert:
+    // IF    n >=0 AND n < len( s ) 
+    // THEN: s = sk1 ++ unit(skt) ++ sk2 AND
+    //       len( sk1 ) = n AND
+    //       ( len( sk2 ) = len( s )- (n+1)
+    // ELSE: skt = Uf(s, n), where Uf is a cached skolem function.
+    
     asserts.push_back(lemma);
     retNode = skt;
   }
