88c7c48ef27a4e28bdb18bfc336438669451efea
ajreynol <andrew.j.reynolds@gmail.com>
2020-10-19 17:40:19 -0500

    Format

--- src/theory/uf/cardinality_extension.cpp
+++ src/theory/uf/cardinality_extension.cpp
@@ -852,7 +852,8 @@ void SortModel::assertCardinality(uint32_t c, bool val)
       bool doCheckRegions = !d_hasCard;
       bool prevHasCard = d_hasCard;
       d_hasCard = true;
-      if( !prevHasCard || c<static_cast<uint32_t>(d_cardinality) ){
+      if (!prevHasCard || c < static_cast<uint32_t>(d_cardinality))
+      {
         d_cardinality = c;
         simpleCheckCardinality();
         if (d_state.isInConflict())
