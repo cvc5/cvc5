f9b85f16b19b67eb936f21f67a6ff660c92a0664
ajreynol <andrew.j.reynolds@gmail.com>
2020-10-20 17:13:53 -0500

    Format

--- src/theory/rewriter.cpp
+++ src/theory/rewriter.cpp
@@ -384,8 +384,11 @@ Node Rewriter::rewriteTo(theory::TheoryId theoryId,
                 << "with proofs: " << rewriteStackTop.d_node << std::endl;
             Trace("rewriter-proof") << " w/o proofs: " << cached << std::endl;
             Node eq = rewriteStackTop.d_node.eqNode(cached);
-            tcpg->addRewriteStep(
-                rewriteStackTop.d_node, cached, PfRule::TRUST_REWRITE, {}, {eq});
+            tcpg->addRewriteStep(rewriteStackTop.d_node,
+                                 cached,
+                                 PfRule::TRUST_REWRITE,
+                                 {},
+                                 {eq});
             // don't overwrite the cache, should be the same
             rewriteStackTop.d_node = cached;
           }
