34c9bdf869fcc87306732d64e64e1bb3e7f2fcd5
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-10-22 16:24:34 +0200

    Set default for QF_NRA to theoryof-mode=type

--- src/smt/set_defaults.cpp
+++ src/smt/set_defaults.cpp
@@ -604,7 +604,8 @@ void setDefaults(LogicInfo& logic, bool isInternalSubsolver)
     if (logic.isSharingEnabled() && !logic.isTheoryEnabled(THEORY_BV)
         && !logic.isTheoryEnabled(THEORY_STRINGS)
         && !logic.isTheoryEnabled(THEORY_SETS)
-        && !logic.isTheoryEnabled(THEORY_BAGS))
+        && !logic.isTheoryEnabled(THEORY_BAGS)
+        && !(logic.isTheoryEnabled(THEORY_ARITH) && !logic.isLinear()))
     {
       Trace("smt") << "setting theoryof-mode to term-based" << std::endl;
       options::theoryOfMode.set(options::TheoryOfMode::THEORY_OF_TERM_BASED);
