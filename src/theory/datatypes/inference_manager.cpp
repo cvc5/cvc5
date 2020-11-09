140ed5c368d3591d00e982f74a819853318f372e
ajreynol <andrew.j.reynolds@gmail.com>
2020-11-09 11:19:10 -0600

    Fix datatypes proofs based on new fact vs lemma policy from master.

--- src/theory/datatypes/inference_manager.cpp
+++ src/theory/datatypes/inference_manager.cpp
@@ -32,8 +32,10 @@ InferenceManager::InferenceManager(Theory& t,
       d_inferenceLemmas("theory::datatypes::inferenceLemmas"),
       d_inferenceFacts("theory::datatypes::inferenceFacts"),
       d_inferenceConflicts("theory::datatypes::inferenceConflicts"),
+      d_pnm(pnm),
       d_ipc(pnm == nullptr ? nullptr
-                           : new InferProofCons(state.getSatContext(), pnm))
+                           : new InferProofCons(state.getSatContext(), pnm)),
+      d_lemPg(pnm == nullptr ? nullptr : new EagerProofGenerator(pnm, state.getUserContext(), "datatypes::lemPg"))
 {
   d_false = NodeManager::currentNM()->mkConst(false);
   smtStatisticsRegistry()->registerStat(&d_inferenceLemmas);
@@ -93,7 +95,7 @@ void InferenceManager::sendDtConflict(const std::vector<Node>& conf, InferId id)
   if (isProofEnabled())
   {
     Node exp = NodeManager::currentNM()->mkAnd(conf);
-    prepareDtInference(d_false, exp, id);
+    prepareDtInference(d_false, exp, id, d_ipc.get());
   }
   conflictExp(conf, d_ipc.get());
   d_inferenceConflicts << id;
@@ -117,7 +119,48 @@ bool InferenceManager::isProofEnabled() const { return d_ipc != nullptr; }
 bool InferenceManager::processDtLemma(
     Node conc, Node exp, InferId id, LemmaProperty p, bool doCache)
 {
-  conc = prepareDtInference(conc, exp, id);
+  // set up a proof constructor
+  std::shared_ptr<InferProofCons> ipcl;
+  if (isProofEnabled())
+  {
+    ipcl = std::make_shared<InferProofCons>(nullptr, d_pnm);
+  }
+  conc = prepareDtInference(conc, exp, id, ipcl.get());
+  // send it as a lemma
+  Node lem;
+  if (!exp.isNull() && !exp.isConst())
+  {
+    lem = NodeManager::currentNM()->mkNode(kind::IMPLIES, exp, conc);
+  }
+  else
+  {
+    lem = conc;
+  }
+  if (isProofEnabled())
+  {
+    // store its proof
+    std::shared_ptr<ProofNode> pbody = ipcl->getProofFor(conc);
+    std::shared_ptr<ProofNode> pn = pbody;
+    if (!exp.isNull() && !exp.isConst())
+    {
+      std::vector<Node> expv;
+      expv.push_back(exp);
+      pn = d_pnm->mkScope(pbody, expv);
+    }
+    d_lemPg->setProofFor(lem, pn);
+  }
+  // use trusted lemma
+  TrustNode tlem = TrustNode::mkTrustLemma(lem, d_lemPg.get());
+  if (!trustedLemma(tlem))
+  {
+    Trace("dt-lemma-debug") << "...duplicate lemma" << std::endl;
+    return false;
+  }
+  d_inferenceLemmas << id;
+  return true;
+
+  
+/*
   if (conc.getKind() == EQUAL)
   {
     // Also process it as a fact first. Some lemmas concluding equalities
@@ -147,18 +190,19 @@ bool InferenceManager::processDtLemma(
   }
   d_inferenceLemmas << id;
   return true;
+  */
 }
 
 bool InferenceManager::processDtFact(Node conc, Node exp, InferId id)
 {
-  conc = prepareDtInference(conc, exp, id);
+  conc = prepareDtInference(conc, exp, id, d_ipc.get());
   // assert the internal fact, which has the same issue as above
   processDtFactInternal(conc, exp);
   d_inferenceFacts << id;
   return true;
 }
 
-Node InferenceManager::prepareDtInference(Node conc, Node exp, InferId id)
+Node InferenceManager::prepareDtInference(Node conc, Node exp, InferId id, InferProofCons * ipc)
 {
   Trace("dt-lemma-debug") << "prepareDtInference : " << conc << " via " << exp
                           << " by " << id << std::endl;
@@ -169,6 +213,7 @@ Node InferenceManager::prepareDtInference(Node conc, Node exp, InferId id)
   }
   if (isProofEnabled())
   {
+    Assert (ipc!=nullptr);
     // If proofs are enabled, notify the proof constructor.
     // Notice that we have to reconstruct a datatypes inference here. This is
     // because the inference in the pending vector may be destroyed as we are
@@ -176,7 +221,7 @@ Node InferenceManager::prepareDtInference(Node conc, Node exp, InferId id)
     // call below, since it is a unique pointer.
     std::shared_ptr<DatatypesInference> di =
         std::make_shared<DatatypesInference>(this, conc, exp, id);
-    d_ipc->notifyFact(di);
+    ipc->notifyFact(di);
   }
   return conc;
 }
@@ -190,7 +235,7 @@ void InferenceManager::processDtFactInternal(Node conc, Node exp)
     std::vector<Node> expv;
     if (!exp.isNull() && !exp.isConst())
     {
-      lem = NodeManager::currentNM()->mkNode(kind::IMPLIES, exp, conc);
+      expv.push_back(exp);
     }
     assertInternalFact(atom, polarity, expv, d_ipc.get());
   }
