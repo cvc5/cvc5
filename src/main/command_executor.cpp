43e6771025db50c44f431291d2d08e3aa25e3916
ajreynol <andrew.j.reynolds@gmail.com>
2020-11-10 07:56:58 -0600

    More

--- src/main/command_executor.cpp
+++ src/main/command_executor.cpp
@@ -119,9 +119,9 @@ bool CommandExecutor::doCommandSingleton(Command* cmd)
 {
   bool status = true;
   if(d_options.getVerbosity() >= -1) {
-    status = solverInvoke(d_solver.get(), cmd, d_options.getOut());
+    status = solverInvoke(d_solver.get(), d_symman.get(), cmd, d_options.getOut());
   } else {
-    status = solverInvoke(d_solver.get(), cmd, nullptr);
+    status = solverInvoke(d_solver.get(), d_symman.get(), cmd, nullptr);
   }
 
   api::Result res;
