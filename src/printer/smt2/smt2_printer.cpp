532ee814ada06d76287d18c8318c2d7d5d1e48fd
ajreynol <andrew.j.reynolds@gmail.com>
2020-11-13 18:29:48 -0600

    Use new letifier by default

--- src/printer/smt2/smt2_printer.cpp
+++ src/printer/smt2/smt2_printer.cpp
@@ -110,7 +110,7 @@ void Smt2Printer::toStream(std::ostream& out,
                            size_t dag) const
 {
   if(dag != 0) {
-#if 0
+#if 1
     LetBinding lbind(dag+1);
     toStreamWithLetify(out, n, toDepth, &lbind);
 #else
