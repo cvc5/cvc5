28aa000eccdf22363f88d8ca2d816ea47346a031
ajreynol <andrew.j.reynolds@gmail.com>
2020-11-18 16:32:31 -0600

    Rm

--- src/proof/unsat_core.h
+++ src/proof/unsat_core.h
@@ -40,8 +40,6 @@ class UnsatCore
 
   /** Whether we are using names for this unsat core */
   bool useNames() const { return d_useNames; }
-  /** Get the number of assertions in the unsat core */
-  size_t size() const { return d_core.size() + d_names.size(); }
   /** Get the assertions in the unsat core */
   const std::vector<Node>& getCore() const;
   /** Get their names */
