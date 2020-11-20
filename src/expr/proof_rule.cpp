c34ee2b2e95960c96ae8eb06651aaacb85cc44cc
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-11-20 13:07:53 +0100

    Add proof rules.

--- src/expr/proof_rule.cpp
+++ src/expr/proof_rule.cpp
@@ -163,6 +163,9 @@ const char* toString(PfRule id)
     case PfRule::INT_TIGHT_UB: return "INT_TIGHT_UB";
     case PfRule::ARITH_OP_ELIM_AXIOM: return "ARITH_OP_ELIM_AXIOM";
     case PfRule::INT_TRUST: return "INT_TRUST";
+    case PfRule::ARITH_MULT_SIGN: return "ARITH_MULT_SIGN";
+    case PfRule::ARITH_MULT_POS: return "ARITH_MULT_POS";
+    case PfRule::ARITH_MULT_NEG: return "ARITH_MULT_NEG";
     case PfRule::ARITH_NL_CAD_DIRECT: return "ARITH_NL_CAD_DIRECT";
     case PfRule::ARITH_NL_CAD_RECURSIVE:
       return "ARITH_NL_CAD_RECURSIVE";
