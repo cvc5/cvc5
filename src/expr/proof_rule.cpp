18be966d77664d35450e6f304d07ac96c8ca5d5c
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-11-20 14:05:25 +0100

    (proof-new) Add proofs for NL_TANGENT_PLANE (#5439)
    This PR adds proofs for the NL_TANGENT_PLANE lemmas.

--- src/expr/proof_rule.cpp
+++ src/expr/proof_rule.cpp
@@ -163,6 +163,7 @@ const char* toString(PfRule id)
     case PfRule::INT_TIGHT_UB: return "INT_TIGHT_UB";
     case PfRule::ARITH_OP_ELIM_AXIOM: return "ARITH_OP_ELIM_AXIOM";
     case PfRule::INT_TRUST: return "INT_TRUST";
+    case PfRule::ARITH_MULT_TANGENT: return "ARITH_MULT_TANGENT";
     case PfRule::ARITH_NL_CAD_DIRECT: return "ARITH_NL_CAD_DIRECT";
     case PfRule::ARITH_NL_CAD_RECURSIVE:
       return "ARITH_NL_CAD_RECURSIVE";
