afa05695f8c556f18e9fa4d3d95049b802ef2e98
Aina Niemetz <aina.niemetz@gmail.com>
2020-12-02 19:39:55 -0800

    google test: context: Migrate cdo_black.

--- test/unit/context/cdo_black.cpp
+++ test/unit/context/cdo_black.cpp
@@ -1,8 +1,8 @@
 /*********************                                                        */
-/*! \file cdo_black.h
+/*! \file cdo_black.cpp
  ** \verbatim
  ** Top contributors (to current version):
- **   Dejan Jovanovic, Morgan Deters, Andres Noetzli
+ **   Aina Niemetz, Morgan Deters, Dejan Jovanovic
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
  ** in the top-level source directory and their institutional affiliations.
@@ -14,8 +14,6 @@
  ** Black box testing of CVC4::context::CDO<>.
  **/
 
-#include <cxxtest/TestSuite.h>
-
 #include <iostream>
 #include <vector>
 
@@ -23,33 +21,32 @@
 #include "context/cdlist.h"
 #include "context/cdo.h"
 #include "context/context.h"
+#include "test.h"
 
-using namespace std;
 using namespace CVC4;
 using namespace CVC4::context;
 
-class CDOBlack : public CxxTest::TestSuite {
-private:
+class TestCDOBlack : public TestInternal
+{
+ protected:
+  void SetUp() override { d_context = new Context; }
+
+  void TearDown() override { delete d_context; }
 
   Context* d_context;
-
- public:
-  void setUp() override { d_context = new Context; }
-
-  void tearDown() override { delete d_context; }
-
-  void testIntCDO()
-  {
-    // Test that push/pop maintains the original value
-    CDO<int> a1(d_context);
-    a1 = 5;
-    TS_ASSERT(d_context->getLevel() == 0);
-    d_context->push();
-    a1 = 10;
-    TS_ASSERT(d_context->getLevel() == 1);
-    TS_ASSERT(a1 == 10);
-    d_context->pop();
-    TS_ASSERT(d_context->getLevel() == 0);
-    TS_ASSERT(a1 == 5);
-  }
 };
+
+TEST_F(TestCDOBlack, cdo)
+{
+  // Test that push/pop maintains the original value
+  CDO<int> a1(d_context);
+  a1 = 5;
+  EXPECT_EQ(d_context->getLevel(), 0);
+  d_context->push();
+  a1 = 10;
+  EXPECT_EQ(d_context->getLevel(), 1);
+  EXPECT_EQ(a1, 10);
+  d_context->pop();
+  EXPECT_EQ(d_context->getLevel(), 0);
+  EXPECT_EQ(a1, 5);
+}
