d255124a10d55efb85a07b803408b823831af235
Makai Mann <makaim@stanford.edu>
2020-12-03 10:58:15 -0800

    Add pyproject.toml and add license headers

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
+#####################
+## setup.py
+## Top contributors (to current version):
+##   Makai Mann
+## This file is part of the CVC4 project.
+## Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+## in the top-level source directory and their institutional affiliations.
+## All rights reserved.  See the file COPYING in the top-level source
+## directory for licensing information.
+##
+## This setup.py will build CVC4 using the CMake arguments in cmake_args below
+## This is primarily intended for building Python wheels to be uploaded to PyPi.
+## If you are installing from source, this is not the recommended way to build
+## CVC4 (it does not allow you to customize the options and it will hide the
+## build folder). Please instead use the main approach:
+##
+## ./configure.sh [your options] --python-bindings
+## (since you're looking at this setup.py, you probably want python bindings)
+## cd build
+## make
+##
+
 from skbuild import setup
 
 setup(
