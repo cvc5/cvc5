480cb51b0b2ed4f9148e18758e096b260246d45a
Makai Mann <makaim@stanford.edu>
2020-12-03 17:27:49 -0800

    Name cython module, pycvc4_cython and adjust imports

--- src/api/pycvc4/__init__.py
+++ src/api/pycvc4/__init__.py
@@ -1,4 +1,1 @@
-import sys
-from .pycvc4 import *
-# fake a submodule for dotted imports, e.g. from pycvc4.kinds import *
-sys.modules['%s.%s'%(__name__, kinds.__name__)] = kinds
+from pycvc4_cython import *
--- src/api/pycvc4/pycvc4.pyx
+++ src/api/pycvc4/pycvc4.pyx
@@ -1,2 +1,6 @@
 include "cvc4kinds.pxi"
 include "cvc4.pxi"
+
+import sys
+# fake a submodule for dotted imports, e.g. from pycvc4.kinds import *
+sys.modules['pycvc4.kinds'] = kinds
