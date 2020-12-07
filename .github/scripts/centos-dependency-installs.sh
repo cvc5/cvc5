90dd79f5cf99f89270657415d085cac1d4e9814c
Makai Mann <makaim@stanford.edu>
2020-12-07 15:40:27 -0800

    Add get-antlr-3.4 to centos install script

--- .github/scripts/centos-dependency-installs.sh
+++ .github/scripts/centos-dependency-installs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Install dependencies GitHub actions in a centos environment (e.g. for manylinux for Python wheels)
+# Install dependencies in a centos environment (e.g. for manylinux for Python wheels)
 
 # NOTE manylinux doesn't seem to have cln-devel -- leaving out, it's optional
 
@@ -16,3 +16,8 @@ yum install -y \
     flex-devel \
     java-1.8.0-openjdk
 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CVC4_DIR=$DIR/../../
+
+$CVC4_DIR/contrib/get-antlr-3.4
