9e7b52a2dd2a87f0c4cf687dc24141d1650ead92
Mathias Preiner <mathias.preiner@gmail.com>
2020-12-08 13:03:50 -0800

    Update headers.

--- src/theory/bv/bv_solver_bitblast.h
+++ src/theory/bv/bv_solver_bitblast.h
@@ -2,7 +2,7 @@
 /*! \file bv_solver_bitblast.h
  ** \verbatim
  ** Top contributors (to current version):
- **   Mathias Preiner, Andrew Reynolds
+ **   Mathias Preiner
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
  ** in the top-level source directory and their institutional affiliations.
