65a0c402c29c2b1dac5a486a448f6937f81e6e34
mudathirmahgoub <mudathirmahgoub@gmail.com>
2020-12-19 01:01:45 -0600

    Added infer info

---
+++ src/theory/bags/infer_info.cpp
@@ -0,0 +1,88 @@
+/*********************                                                        */
+/*! \file infer_info.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Mudathir Mohamed
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implementation of inference information utility.
+ **/
+
+#include "theory/bags/infer_info.h"
+
+#include "theory/bags/inference_manager.h"
+
+namespace CVC4 {
+namespace theory {
+namespace bags {
+
+const char* toString(Inference i)
+{
+  switch (i)
+  {
+    default: return "?";
+  }
+}
+
+std::ostream& operator<<(std::ostream& out, Inference i)
+{
+  out << toString(i);
+  return out;
+}
+
+InferInfo::InferInfo() : d_im(nullptr), d_id(Inference::NONE) {}
+
+bool InferInfo::process(TheoryInferenceManager* im, bool asLemma)
+{
+  //  if (asLemma)
+  //  {
+  //    return d_im->processLemma(*this);
+  //  }
+  //  return d_im->processFact(*this);
+}
+
+bool InferInfo::isTrivial() const
+{
+  Assert(!d_conc.isNull());
+  return d_conc.isConst() && d_conc.getConst<bool>();
+}
+
+bool InferInfo::isConflict() const
+{
+  Assert(!d_conc.isNull());
+  return d_conc.isConst() && !d_conc.getConst<bool>();
+}
+
+bool InferInfo::isFact() const
+{
+  Assert(!d_conc.isNull());
+  TNode atom = d_conc.getKind() == kind::NOT ? d_conc[0] : d_conc;
+  return !atom.isConst() && atom.getKind() != kind::OR;
+}
+
+Node InferInfo::getAntecedent() const
+{
+  // d_noExplain is a subset of d_ant
+  NodeManager* nm = NodeManager::currentNM();
+  return nm->mkAnd(d_ant);
+}
+
+std::ostream& operator<<(std::ostream& out, const InferInfo& ii)
+{
+  out << "(infer " << ii.d_id << " " << ii.d_conc;
+  if (!ii.d_ant.empty())
+  {
+    out << " :ant (" << ii.d_ant << ")";
+  }
+
+  out << ")";
+  return out;
+}
+
+}  // namespace bags
+}  // namespace theory
+}  // namespace CVC4
