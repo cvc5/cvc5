3a03d204519e754f05212b35be35c7721a215f8a
mudathirmahgoub <mudathirmahgoub@gmail.com>
2020-12-19 01:02:02 -0600

    Added inference_generator

---
+++ src/theory/bags/inference_generator.cpp
@@ -0,0 +1,218 @@
+/*********************                                                        */
+/*! \file inference_generator.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Mudathir Mohamed
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Inference generator utility
+ **/
+
+#include "inference_generator.h"
+
+#include "expr/skolem_manager.h"
+
+namespace CVC4 {
+namespace theory {
+namespace bags {
+
+InferenceGenerator::InferenceGenerator(NodeManager* nm) : d_nm(nm)
+{
+  d_sm = d_nm->getSkolemManager();
+  d_true = d_nm->mkConst(true);
+  d_zero = d_nm->mkConst(Rational(0));
+  d_one = d_nm->mkConst(Rational(1));
+}
+
+InferInfo InferenceGenerator::bagEquality(Node n, Node e)
+{
+  Assert(n.getKind() == kind::EQUAL && n[0].getType().isBag());
+  Assert(e.getType() == n[0].getType().getBagElementType());
+
+  Node A = n[0];
+  Node B = n[1];
+  InferInfo inferInfo;
+  inferInfo.d_id = Inference::BAG_EQUALITY;
+  inferInfo.d_ant.push_back(n);
+  Node mA = d_nm->mkNode(kind::BAG_COUNT, e, A);
+  Node mB = d_nm->mkNode(kind::BAG_COUNT, e, B);
+  Node equal = mA.eqNode(mB);
+  inferInfo.d_conc = equal;
+  return inferInfo;
+}
+//
+// struct BagsDeqIndexAttributeId
+//{
+//};
+// typedef expr::Attribute<BagsDeqIndexAttributeId, Node> BagsDeqIndexAttribute;
+
+InferInfo InferenceGenerator::bagDisequality(Node n)
+{
+  Assert(n.getKind() == kind::NOT && n[0].getKind() == kind::EQUAL);
+  Assert(n[0][0].getType().isBag());
+
+  Node A = n[0][0];
+  Node B = n[0][1];
+
+  TypeNode elementType = A.getType().getBagElementType();
+  SkolemManager* sm = d_nm->getSkolemManager();
+  Node pred;
+  // Bound exists x => m(x)
+  BoundVarManager* bvm = d_nm->getBoundVarManager();
+  Node v;  // =  bvm->mkBoundVar<BagsDeqIndexAttribute>(n, elementType);
+
+  InferInfo inferInfo;
+  inferInfo.d_id = Inference::BAG_DISEQUALITY;
+  // add e as a new skolem variable
+  inferInfo.d_new_skolem.push_back(v);
+  inferInfo.d_ant.push_back(n);
+  Node mA = d_nm->mkNode(kind::BAG_COUNT, v, A);
+  Node mB = d_nm->mkNode(kind::BAG_COUNT, v, B);
+  Node equal = mA.eqNode(mB);
+  Node notEqual = equal.notNode();
+  inferInfo.d_conc = notEqual;
+  //  Node e = sm->mkSkolem(v, pred, "disequalityWitness", elementType);
+  return inferInfo;
+}
+
+InferInfo InferenceGenerator::bagEmpty(Node e)
+{
+  EmptyBag emptyBag = EmptyBag(d_nm->mkBagType(e.getType()));
+  Node empty = d_nm->mkConst(emptyBag);
+  InferInfo inferInfo;
+  inferInfo.d_id = Inference::BAG_EMPTY;
+  Node multiplicity = d_nm->mkNode(kind::BAG_COUNT, e, empty);
+  Node equal = multiplicity.eqNode(d_zero);
+  inferInfo.d_conc = equal;
+  return inferInfo;
+}
+
+InferInfo InferenceGenerator::unionDisjoint(Node n, Node e)
+{
+  Assert(n.getKind() == kind::UNION_DISJOINT && n[0].getType().isBag());
+  Assert(e.getType() == n[0].getType().getBagElementType());
+
+  Node A = n[0];
+  Node B = n[1];
+  InferInfo inferInfo;
+  inferInfo.d_id = Inference::BAG_UNION_DISJOINT;
+  Node countA = d_nm->mkNode(kind::BAG_COUNT, e, A);
+  Node countB = d_nm->mkNode(kind::BAG_COUNT, e, B);
+  Node skolem = d_sm->mkPurifySkolem(n, "union_disjoint");
+  inferInfo.d_new_skolem.push_back(skolem);
+  Node countSkolem = d_nm->mkNode(kind::BAG_COUNT, e, skolem);
+  Node sum = d_nm->mkNode(kind::PLUS, countA, countB);
+  Node equal = countSkolem.eqNode(sum);
+  inferInfo.d_conc = equal;
+  return inferInfo;
+}
+
+InferInfo InferenceGenerator::unionMax(Node n, Node e)
+{
+  Assert(n.getKind() == kind::UNION_MAX && n[0].getType().isBag());
+  Assert(e.getType() == n[0].getType().getBagElementType());
+
+  Node A = n[0];
+  Node B = n[1];
+  InferInfo inferInfo;
+  inferInfo.d_id = Inference::BAG_UNION_MAX;
+  inferInfo.d_ant.push_back(d_true);
+  Node mA = d_nm->mkNode(kind::BAG_COUNT, e, A);
+  Node mB = d_nm->mkNode(kind::BAG_COUNT, e, B);
+  Node mUnionMax = d_nm->mkNode(kind::BAG_COUNT, e, n);
+  Node gt = d_nm->mkNode(kind::GT, mA, mB);
+  Node max = d_nm->mkNode(kind::ITE, gt, mA, mB);
+  Node equal = mUnionMax.eqNode(max);
+  inferInfo.d_conc = equal;
+  return inferInfo;
+}
+
+InferInfo InferenceGenerator::intersection(Node n, Node e)
+{
+  Assert(n.getKind() == kind::INTERSECTION_MIN && n[0].getType().isBag());
+  Assert(e.getType() == n[0].getType().getBagElementType());
+
+  Node A = n[0];
+  Node B = n[1];
+  InferInfo inferInfo;
+  inferInfo.d_id = Inference::BAG_INTERSECTION_MIN;
+  inferInfo.d_ant.push_back(d_true);
+  Node mA = d_nm->mkNode(kind::BAG_COUNT, e, A);
+  Node mB = d_nm->mkNode(kind::BAG_COUNT, e, B);
+  Node mIntersection = d_nm->mkNode(kind::BAG_COUNT, e, n);
+  Node lt = d_nm->mkNode(kind::LT, mA, mB);
+  Node min = d_nm->mkNode(kind::ITE, lt, mA, mB);
+  Node equal = mIntersection.eqNode(min);
+  inferInfo.d_conc = equal;
+  return inferInfo;
+}
+
+InferInfo InferenceGenerator::differenceSubtract(Node n, Node e)
+{
+  Assert(n.getKind() == kind::DIFFERENCE_SUBTRACT && n[0].getType().isBag());
+  Assert(e.getType() == n[0].getType().getBagElementType());
+
+  Node A = n[0];
+  Node B = n[1];
+  InferInfo inferInfo;
+  inferInfo.d_id = Inference::BAG_DIFFERENCE_SUBTRACT;
+  inferInfo.d_ant.push_back(d_true);
+  Node mA = d_nm->mkNode(kind::BAG_COUNT, e, A);
+  Node mB = d_nm->mkNode(kind::BAG_COUNT, e, B);
+  Node mDifference = d_nm->mkNode(kind::BAG_COUNT, e, n);
+  Node subtract = d_nm->mkNode(kind::MINUS, mA, mB);
+  Node gte = d_nm->mkNode(kind::GEQ, mA, mB);
+  Node difference = d_nm->mkNode(kind::ITE, gte, subtract, d_zero);
+  Node equal = mDifference.eqNode(difference);
+  inferInfo.d_conc = equal;
+  return inferInfo;
+}
+
+InferInfo InferenceGenerator::differenceRemove(Node n, Node e)
+{
+  Assert(n.getKind() == kind::DIFFERENCE_REMOVE && n[0].getType().isBag());
+  Assert(e.getType() == n[0].getType().getBagElementType());
+
+  Node A = n[0];
+  Node B = n[1];
+  InferInfo inferInfo;
+  inferInfo.d_id = Inference::BAG_DIFFERENCE_REMOVE;
+  inferInfo.d_ant.push_back(d_true);
+  Node mA = d_nm->mkNode(kind::BAG_COUNT, e, A);
+  Node mB = d_nm->mkNode(kind::BAG_COUNT, e, B);
+  Node mDifference = d_nm->mkNode(kind::BAG_COUNT, e, n);
+  Node notInB = d_nm->mkNode(kind::EQUAL, mB, d_zero);
+  Node difference = d_nm->mkNode(kind::ITE, notInB, mA, d_zero);
+  Node equal = mDifference.eqNode(difference);
+  inferInfo.d_conc = equal;
+  return inferInfo;
+}
+
+InferInfo InferenceGenerator::duplicateRemoval(Node n, Node e)
+{
+  Assert(n.getKind() == kind::DUPLICATE_REMOVAL && n[0].getType().isBag());
+  Assert(e.getType() == n[0].getType().getBagElementType());
+
+  Node A = n[0];
+
+  InferInfo inferInfo;
+  Node mA = d_nm->mkNode(kind::BAG_COUNT, e, A);
+  inferInfo.d_id = Inference::BAG_DUPLICATE_REMOVAL;
+  inferInfo.d_ant.push_back(d_true);
+  Node multiplicity = d_nm->mkNode(kind::BAG_COUNT, e, A);
+
+  Node mDistinct = d_nm->mkNode(kind::BAG_COUNT, e, n);
+  Node gte = d_nm->mkNode(kind::GEQ, mA, d_one);
+  Node ite = d_nm->mkNode(kind::ITE, gte, d_one, d_zero);
+  Node equal = multiplicity.eqNode(ite);
+  inferInfo.d_conc = equal;
+  return inferInfo;
+}
+
+}  // namespace bags
+}  // namespace theory
+}  // namespace CVC4
---
+++ src/theory/bags/inference_generator.h
@@ -0,0 +1,149 @@
+/*********************                                                        */
+/*! \file inference_generator.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Mudathir Mohamed
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Inference generator utility
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef CVC4__THEORY__BAGS__INFERENCE_GENERATOR_H
+#define CVC4__THEORY__BAGS__INFERENCE_GENERATOR_H
+
+#include <map>
+#include <vector>
+
+#include "expr/node.h"
+#include "infer_info.h"
+
+namespace CVC4 {
+namespace theory {
+namespace bags {
+
+/**
+ * An inference generator class. This class is used by the core solver to
+ * generate lemmas
+ */
+class InferenceGenerator
+{
+ public:
+  InferenceGenerator(NodeManager* nm);
+  /**
+   * @param n is (= A B) where A, B are bags of type (Bag E)
+   * @param e is a node of Type E
+   * @return an inference that represents the following implication
+   * (=>
+   *   (= A B)
+   *   (= (count e A) (count e B)))
+   */
+  InferInfo bagEquality(Node n, Node e);
+  /**
+   * @param n is (not (= A B)) where A, B are bags of type (Bag E)
+   * @return an inference that represents the following implication
+   * (=>
+   *   (not (= A B))
+   *   (not (= (count e A) (count e B))))
+   *   where e is a fresh skolem of type E
+   */
+  InferInfo bagDisequality(Node n);
+  /**
+   * @param e is a node of Type E
+   * @return an inference that represents the following implication
+   * (=>
+   *   true
+   *   (= 0 (count e (as emptybag (Bag E)))))
+   */
+  InferInfo bagEmpty(Node e);
+  /**
+   * @param n is (union_disjoint A B) where A, B are bags of type (Bag E)
+   * @param e is a node of Type E
+   * @return an inference that represents the following implication
+   * (=>
+   *   true
+   *   (= (count e k_{(union_disjoint A B)})
+   *      (+ (count e A) (count e B))))
+   */
+  InferInfo unionDisjoint(Node n, Node e);
+  /**
+   * @param n is (union_disjoint A B) where A, B are bags of type (Bag E)
+   * @param e is a node of Type E
+   * @return an inference that represents the following implication
+   * (=>
+   *   true
+   *   (= (count e (union_disjoint A B))
+   *     (ite
+   *     (> (count e A) (count e B))
+   *     (count e A)
+   *     (count e B)))))
+   */
+  InferInfo unionMax(Node n, Node e);
+  /**
+   * @param n is (intersection_min A B) where A, B are bags of type (Bag E)
+   * @param e is a node of Type E
+   * @return an inference that represents the following implication
+   * (=>
+   *   true
+   *   (= (count e (union_disjoint A B))
+   *     (ite(
+   *     (< (count e A) (count e B))
+   *     (count e A)
+   *     (count e B)))))
+   */
+  InferInfo intersection(Node n, Node e);
+  /**
+   * @param n is (difference_subtract A B) where A, B are bags of type (Bag E)
+   * @param e is a node of Type E
+   * @return an inference that represents the following implication
+   * (=>
+   *   true
+   *   (= (count e (difference_subtract A B))
+   *     (ite
+   *        (>= (count e A) (count e B))
+   *        (- (count e A) (count e B))
+   *        0))))
+   */
+  InferInfo differenceSubtract(Node n, Node e);
+  /**
+   * @param n is (difference_remove A B) where A, B are bags of type (Bag E)
+   * @param e is a node of Type E
+   * @return an inference that represents the following implication
+   * (=>
+   *   true
+   *   (= (count e (difference_remove A B))
+   *     (ite
+   *        (= (count e B) 0)
+   *        (count e A)
+   *        0))))
+   */
+  InferInfo differenceRemove(Node n, Node e);
+  /**
+   * @param n is (duplicate_removal A) where A is a bag of type (Bag E)
+   * @param e is a node of Type E
+   * @return an inference that represents the following implication
+   * (=>
+   *   true
+   *   (= (count e (duplicate_removal A))
+   *     (ite (>= (count e A) 1) 1 0))))
+   */
+  InferInfo duplicateRemoval(Node n, Node e);
+
+ private:
+  NodeManager* d_nm;
+  SkolemManager* d_sm;
+  Node d_true;
+  Node d_zero;
+  Node d_one;
+};
+
+}  // namespace bags
+}  // namespace theory
+}  // namespace CVC4
+
+#endif /* CVC4__THEORY__BAGS__INFERENCE_GENERATOR_H */
