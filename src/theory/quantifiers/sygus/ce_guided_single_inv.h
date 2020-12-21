4ae8ad58b2e1aa4da4424a7af94ba1723ebfef06
ajreynol <andrew.j.reynolds@gmail.com>
2020-12-21 09:59:11 -0600

    Improve doc

--- src/theory/quantifiers/sygus/ce_guided_single_inv.h
+++ src/theory/quantifiers/sygus/ce_guided_single_inv.h
@@ -96,8 +96,7 @@ class CegSingleInv
 
   // get simplified conjecture
   Node getSimplifiedConjecture() { return d_simp_quant; }
- public:
-  // initialize this class for synthesis conjecture q
+  /** initialize this class for synthesis conjecture q */
   void initialize( Node q );
   /** finish initialize
    *
@@ -117,8 +116,17 @@ class CegSingleInv
    */
   bool solve();
   /**
-   * Get solution for the index^th function to synthesize of the conjecture
-   * this class was initialized with.
+   * Get solution for the sol_index^th function to synthesize of the conjecture
+   * this class was assigned.
+   *
+   * @param sol_index The index of the function-to-synthesize
+   * @param stn The sygus type of the solution, which corresponds to syntactic
+   * restrictions
+   * @param reconstructed Set to the status of reconstructing the solution,
+   * where 1 = success, 0 = no reconstruction specified, -1 = faled
+   * @param rconsSygus Whether to apply sygus reconstruction techniques
+   * @return the solution for the sol_index^th function to synthesize of the
+   * conjecture assigned to this class.
    */
   Node getSolution(size_t sol_index,
                    TypeNode stn,
