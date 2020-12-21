f0152a2360298854ef6720997c55b5f8abb705b7
ajreynol <andrew.j.reynolds@gmail.com>
2020-12-21 10:54:39 -0600

    Fix

--- src/preprocessing/preprocessing_pass_registry.cpp
+++ src/preprocessing/preprocessing_pass_registry.cpp
@@ -25,6 +25,7 @@
 #include "base/output.h"
 #include "preprocessing/passes/ackermann.h"
 #include "preprocessing/passes/apply_substs.h"
+#include "preprocessing/passes/arith_rewrite_eq.h"
 #include "preprocessing/passes/bool_to_bv.h"
 #include "preprocessing/passes/bv_abstraction.h"
 #include "preprocessing/passes/bv_eager_atoms.h"
@@ -149,6 +150,7 @@ PreprocessingPassRegistry::PreprocessingPassRegistry()
   registerPassInfo("bool-to-bv", callCtor<BoolToBV>);
   registerPassInfo("ho-elim", callCtor<HoElim>);
   registerPassInfo("fun-def-fmf", callCtor<FunDefFmf>);
+  registerPassInfo("arith-rewrite-eq", callCtor<ArithRewriteEq>);
 }
 
 }  // namespace preprocessing
