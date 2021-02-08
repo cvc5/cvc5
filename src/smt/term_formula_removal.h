1189212fab5dd932de50aa6e1aed7c488a7c3cc1
ajreynol <andrew.j.reynolds@gmail.com>
2021-02-08 12:02:55 -0600

    Fix

--- src/smt/term_formula_removal.h
+++ src/smt/term_formula_removal.h
@@ -33,7 +33,7 @@
 #include "util/hash.h"
 
 namespace CVC4 {
-
+  
 typedef std::unordered_map<Node, unsigned, NodeHashFunction> IteSkolemMap;
 
 class RemoveTermFormulas {
@@ -135,6 +135,10 @@ class RemoveTermFormulas {
    */
   void getSkolems(TNode n,
                   std::unordered_set<Node, NodeHashFunction>& skolems) const;
+  /**
+   * Does n have skolems introduced by this class?
+   */
+  bool hasSkolems(TNode n) const;
 
   /**
    * Get the lemma for the skolem, or the null node if k is not a skolem this
