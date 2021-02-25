a51813c8c2e1bda81515f9c4faf2ccf97a3fbe4b
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-02-25 17:53:07 +0100

    Add comments.

--- src/base/collect_tags.py
+++ src/base/collect_tags.py
@@ -9,13 +9,17 @@ import subprocess
 
 
 def parse_args():
+    """Parse command line arguments and return them."""
     ap = argparse.ArgumentParser(description='collect debug and trace tags')
     ap.add_argument('destdir', help='where to put the results')
     ap.add_argument('type', choices=['Debug', 'Trace'], help='which tags to collect')
     ap.add_argument('basedir', help='where to look for source file')
     return ap.parse_args()
 
-def collect_tags(basedir):
+
+def collect_tags(basedir: str):
+    """Collect all tags used in filed within the given base directory.
+    Return them sorted lexicographically."""
     tags = set()
     for ext in ['.cc', '.cpp', '.g', '.h']:
         for filename in glob.iglob(f'{basedir}/**/*{ext}', recursive=True):
@@ -25,7 +29,8 @@ def collect_tags(basedir):
     return sorted(tags)
 
 
-def write_tmp_file(filename, type, tags):
+def write_file(filename: str, type: str, tags):
+    """Render the header file to the given filename."""
     with open(filename, 'w') as out:
         out.write(f'static char const* const {type}_tags[] = {{\n')
         for t in tags:
@@ -35,15 +40,23 @@ def write_tmp_file(filename, type, tags):
 
 
 if __name__ == '__main__':
+    # setup
     opts = parse_args()
     RE_PAT = re.compile(f'{opts.type}(?:\\.isOn)?\\("([^"]+)"\\)')
     FILENAME_TMP = f'{opts.destdir}/{opts.type}_tags.tmp'
     FILENAME_DEST = f'{opts.destdir}/{opts.type}_tags.h'
 
-
+    # collect tags
     tags = collect_tags(opts.basedir)
-    write_tmp_file(FILENAME_TMP, opts.type, tags)
+    # write header file
+    write_file(FILENAME_TMP, opts.type, tags)
+
     if not os.path.isfile(FILENAME_DEST):
+        # file does not exist yet
         shutil.copy(FILENAME_TMP, FILENAME_DEST)
-    elif subprocess.run(['diff', FILENAME_TMP, FILENAME_DEST], stdout=subprocess.PIPE).returncode == 1:
+    elif subprocess.run(
+        ['diff', FILENAME_TMP, FILENAME_DEST],
+        stdout=subprocess.PIPE
+    ).returncode == 1:
+        # file needs to change
         shutil.copy(FILENAME_TMP, FILENAME_DEST)
