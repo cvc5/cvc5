bfa85c8d9c309defd536d12eb5004dc4d811b584
Aina Niemetz <aina.niemetz@gmail.com>
2021-02-25 16:54:46 -0800

    google test: theory: Migrate theory_white.
    This moves test utils for theory tests to test_smt.h and consolidates
    two implementations of dummy theories into one.

--- test/unit/theory/evaluator_white.cpp
+++ test/unit/theory/evaluator_white.cpp
@@ -22,7 +22,6 @@
 #include "theory/bv/theory_bv_utils.h"
 #include "theory/evaluator.h"
 #include "theory/rewriter.h"
-#include "theory/theory_test_utils.h"
 #include "util/rational.h"
 
 namespace CVC4 {
