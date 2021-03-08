afd56bfae0bf4d28c83b41b0e176c62b469d5bac
Aina Niemetz <aina.niemetz@gmail.com>
2021-03-08 13:49:58 -0800

    Headers

--- test/api/ouroborous.cpp
+++ test/api/ouroborous.cpp
@@ -2,9 +2,9 @@
 /*! \file ouroborous.cpp
  ** \verbatim
  ** Top contributors (to current version):
- **   Morgan Deters, Andrew Reynolds, Tim King
+ **   Aina Niemetz, Morgan Deters, Christopher L. Conway
  ** This file is part of the CVC4 project.
- ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
  ** in the top-level source directory and their institutional affiliations.
  ** All rights reserved.  See the file COPYING in the top-level source
  ** directory for licensing information.\endverbatim
