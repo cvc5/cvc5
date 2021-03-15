f7d17c096a8f6f1d4188e1a3330b937d896bd3a3
lachnitt <lachnitt@stanford.edu>
2021-03-14 17:16:14 -0700

    Rewritten printer

---
+++ src/proof/verit/verit_printer.cpp
@@ -0,0 +1,173 @@
+/*********************                                                        */
+/*! \file verit_printer.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Hanna Lachnitt
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief The module for printing veriT proof nodes
+ **/
+
+#include "proof/verit/verit_printer.h"
+
+namespace CVC4 {
+
+namespace proof {
+
+VeritProofPrinter::VeritProofPrinter(bool extended) : d_extended(extended){
+  assumption_level=0;
+  step_id = 1;
+  prefix = "";
+  assumptions.push_back({});
+}
+
+void VeritProofPrinter::veritPrinter(std::ostream& out, std::shared_ptr<ProofNode> pfn){
+
+  Trace("verit-printer") << "- Print proof in veriT format. " << std::endl;
+
+  //Print assumptions and add them to the list
+  for(int i = 0; i < pfn->getArguments().size(); i++){
+    Trace("verit-printer") << "... print assumption " << pfn->getArguments()[i] << std::endl;
+    out << "(assume a" << std::to_string(i) << " " <<  pfn->getArguments()[i] << ")\n";
+    assumptions[0].push_back(pfn->getArguments()[i]);
+  }
+
+  veritPrinterInternal(out,pfn->getChildren()[0]);
+}
+
+std::string VeritProofPrinter::veritPrinterInternal(std::ostream& out, std::shared_ptr<ProofNode> pfn){
+  int current_step_id;
+
+  if (pfn->getArguments().size() < 3 || pfn->getRule() != PfRule::VERIT_RULE)
+  {
+     Trace("verit-printer") << "... printing failed! Encountered untranslated Node. " << pfn->getResult() << " " << pfn->getRule()
+                          << " " << pfn->getChildren() << " / " << pfn->getArguments() << std::endl;
+     return "";
+  }
+
+  //Get the verit proof rule
+  VeritRule vrule = static_cast<VeritRule>(std::stoul(pfn->getArguments()[0].toString()));
+
+  // In case the rule is an anchor it is printed before its children. The arguments of the anchor are printed as assumptions
+  if (vrule == VeritRule::ANCHOR_SUBPROOF)
+  {
+    //Start a new list of assumptions in the new local scope
+    assumption_level++;
+    assumptions.resize(assumption_level+1);
+
+    //Print anchor
+    Trace("verit-printer") << "... print anchor " << pfn->getResult() << " " << vrule
+                           << " " << pfn->getChildren() << " / " << pfn->getArguments() << std::endl;
+    out << "(anchor :step " << prefix << "t" <<  step_id << " :args (";
+    for (unsigned long int j = 3; j < pfn->getArguments().size(); j++)
+    {
+      out << pfn->getArguments()[j].toString();
+      if(j != pfn->getArguments().size()-1){out << " ";}
+    }
+    out << "))\n";
+    prefix.append("t"+std::to_string(step_id)+".");
+    step_id++;
+
+    //Print assumptions
+    for(int i = 3; i < pfn->getArguments().size(); i++){
+      Trace("verit-printer") << "... print assumption " << pfn->getArguments()[i] << std::endl;
+      out << "(assume " << prefix << "a" <<  std::to_string(i-3) << " " <<  pfn->getArguments()[i] << ")\n";
+      assumptions[assumption_level].push_back(pfn->getArguments()[i]);
+    }
+
+    //Store step_id until children are printed to resume counter at current position
+    current_step_id = step_id;
+    step_id = 1;
+  }
+
+  //Assumptions are printed at the anchor and therefore have to be in the list of assumptions when an assume is reached
+  //The id of an assume is its position in the list.
+  if (vrule == VeritRule::ASSUME){
+    Trace("verit-printer") << "... reached assumption " << pfn->getResult() << " " << vrule
+                           << " " << pfn->getChildren() << " / " << pfn->getArguments() << std::endl;
+    Trace("verit-printer") << "... search assumption in list " << pfn->getResult() << "/" << assumptions[assumption_level] << std::endl;
+    return prefix + "a" + std::to_string(std::find(assumptions[assumption_level].begin(),assumptions[assumption_level].end(),pfn->getResult()) - assumptions[assumption_level].begin());
+  }
+
+  std::vector<std::string> child_prefixes;
+  //First print children
+  for(auto child : pfn->getChildren()){
+     child_prefixes.push_back(veritPrinterInternal(out,child));
+  }
+
+  //In this cases the rule should not be printed.
+  if(!d_extended && (vrule == VeritRule::REORDER || vrule == VeritRule::SYMM)){
+    Trace("verit-printer") << "... non-extended mode skip child " << pfn->getResult() << " " << vrule
+                           << " " << pfn->getChildren() << " / " << pfn->getArguments() << std::endl;
+    return child_prefixes[0];
+  }
+
+
+  if (vrule == VeritRule::ANCHOR_SUBPROOF)
+  {
+    Trace("verit-printer") << "... print node " << pfn->getResult() << " " << vrule
+                           << " " << pfn->getChildren() << " / " << pfn->getArguments() << std::endl;
+    // Remove last .
+    prefix.pop_back();
+    out << "(step " << prefix << " "
+        << pfn->getArguments()[2] << " :rule "
+        << veritRuletoString(vrule)
+        << " :discharge (";
+    for (unsigned long int j = 0; j < assumptions[assumption_level].size(); j++) //TODO: I am not sure what should be printed here.
+    {
+      out << prefix << ".a" + std::to_string(j);
+      if (j != assumptions[assumption_level].size() - 1)
+      {
+        out << " ";
+      }
+    }
+    out << "))\n";
+    std::string current_t = prefix;
+    prefix = prefix.substr(0,prefix.find_last_of("t"));
+
+    step_id = current_step_id;
+    assumptions[assumption_level].clear();
+    assumption_level--;
+    return current_t;
+  }
+
+  //Print current step
+  std::string current_t;
+  current_t = "t" + std::to_string(step_id);
+  out << "(step " << prefix << current_t + " ";
+  out << pfn->getArguments()[2].toString() + " :rule " +  veritRuletoString(vrule);
+  if (pfn->getArguments().size() > 3)
+  {
+    out << " :args ";
+    for (int i = 3; i < pfn->getArguments().size(); i++)
+    {
+      out << " " << pfn->getArguments()[i].toString();
+      if(i!= pfn->getArguments().size()-1){ out << " ";}
+    }
+  }
+  if (pfn->getChildren().size() >= 1)
+  {
+    out << " :premises (";
+    for(unsigned long int i = 0; i < child_prefixes.size(); i++){
+      out << child_prefixes[i];
+      if(i!= child_prefixes.size()-1){ out << " ";}
+    }
+    out << "))\n";
+  }
+  else{
+    out << ")\n";
+  }
+  ++step_id;
+  return prefix + current_t;
+
+}
+
+
+}  // namespace proof
+
+}  // namespace CVC4
+
--- src/proof/verit/verit_printer.h
+++ src/proof/verit/verit_printer.h
@@ -12,16 +12,17 @@
  ** \brief The module for printing veriT proof nodes
  **/
 
-#include <memory>
-#include <string>
-
-#include "cvc4_private.h"
-#include "expr/proof_node_updater.h"
-//#include "proof/verit/verit_proof_rule.cpp"
-#include "proof/verit/verit_proof_checker.h"
-
 #ifndef CVC4__PROOF__VERIT_PROOF_PRINTER_H
 #define CVC4__PROOF__VERIT_PROOF_PRINTER_H
+
+#include <memory>
+#include <string>
+#include <vector>
+#include "cvc4_private.h"
+#include "expr/proof_node.h"
+#include "proof/verit/verit_proof_rule.h"
+#include "proof/verit/verit_proof_checker.h"
+
 
 #include <iostream>
 
@@ -29,226 +30,56 @@ namespace CVC4 {
 
 namespace proof {
 
-/**
- * Prints an index for a step or an assume. E.g. if the current id is 1 in a
- * subproof with prefix t3.t4 it returns the string t13.t4.t1. If the current
- * step is an assumption it prints t3.t4.h1
- *
- * @param ids Nested ids of the current subproof,
- * @param i The id of the current step,
- * @param isAssump True, if the current step is an assumption,
- * @return A string consisting of the ids of the subproof followed by the
- * current step printed either as assumption (a) or step (t).
- */
-static std::string veritPrintName(std::vector<int> ids, int i, bool isAssump)
+class VeritProofPrinter
 {
-  std::string name;
-  for(int id:ids){
-    name.push_back('t');
-    name.append(std::to_string(id));
-    name.push_back('.');
-  }
-  if (isAssump)
-  {
-    name.push_back('a');
-  }
-  else
-  {
-    name.push_back('t');
-  }
-  name.append(std::to_string(i));
-  return name;
-}
+ public:
+   VeritProofPrinter(bool extended);
+   ~VeritProofPrinter() {}
+   /**
+   * This method prints a proof node that has been transformed into the veriT proof format
+   *
+   * @param out The stream to write to
+   * @param pfn The proof node to be printed
+   */
+   void veritPrinter(std::ostream& out, std::shared_ptr<ProofNode> pfn);
+ private:
+   /** Used for printing after the initial anchor */
+   std::string veritPrinterInternal(std::ostream& out, std::shared_ptr<ProofNode> pfn);
+   /** Flag to indicate whether the veriT proof format should be extended */
+   bool d_extended;
+   /** The current level of nesting, which increases when in a subproof */
+   int assumption_level;
+   /** Current step id */
+   int step_id;
+   /** The current prefix which is updated whenever a subproof is encountered
+    * E.g. prefix = "t19.t2." */
+   std::string prefix;
+   /** A list of assumption lists for every level of the nested proof node */
+   //Note: This could easily be replaced by a map, e.g. to deal with named assumptions
+   std::vector<std::vector<Node>> assumptions;
+};
+
+
 
 /**
+ * This method prints a proof node that has been transformed into the veriT proof format
  *
- * @param out,
- * @param pfn,
- * @param ids,
- * @param i,
- * @param h,
- * @return
- */
-static std::vector<int> veritPrintInternal(std::ostream& out,
-                                           std::shared_ptr<ProofNode> pfn,
-                                           std::vector<int>& ids,
-                                           int i,
-                                           int h)
-{
-  if (pfn->getRule() == PfRule::SYMM)
-  {
-    return {i, h};
-  }
-  else if (pfn->getRule() != PfRule::VERIT_RULE)
-  {
-    std::cout << "Not translated yet, rule is: " << pfn->getRule() << std::endl;
-    return {i, h};
-  }
-  // std::cout << " verit rule " <<
-  // veritRuletoString(static_cast<VeritRule>(
-  // std::stoul(pfn->getArguments()[0].toString())));
-  // std::cout << " res " << pfn->getResult() << std::endl;
-  // The id of the current proof node
-  int current_i = i;
-  // The id of the latest assumption
-  int current_h = h;
-  // Ids of the childrens of this proof node that are used to print the premises
-  std::vector<int> childIds;
-
-
-  if (pfn->getArguments().size() >= 3 && pfn->getRule() == PfRule::VERIT_RULE)
-  {
-  // In case the rule is an anchor add anchor step before children are printed
-  if (static_cast<VeritRule>(std::stoul(pfn->getArguments()[0].toString()))
-      == VeritRule::ANCHOR_SUBPROOF)
-  {
-    out << "(anchor :step " << veritPrintName(ids, i, false) << " :args (";
-    for (int j = 3; j < pfn->getArguments().size(); j++)
-    {
-      out << pfn->getArguments()[j];
-      if(j != pfn->getArguments().size()-1){out << " ";}
-    }
-    out << "))\n";
-    ids.push_back(i);
-    i = 1;
-    h = 1;
-  }
-
-  // In case the rule is an assume add assume
-  if (static_cast<VeritRule>(std::stoul(pfn->getArguments()[0].toString()))
-      == VeritRule::ASSUME)
-  {
-    out << "(assume " << veritPrintName(ids, h, true) << " "
-        << pfn->getArguments()[2] << ")" << std::endl;
-    return {i, h + 1};
-  }
-
-  //Print Children
-  for (int j = 0; j < pfn->getChildren().size(); j++)
-  {
-    std::shared_ptr<ProofNode> child = pfn->getChildren()[j];
-    std::vector<int> res = veritPrintInternal(out, child, ids, i, h);
-    i = res[0];
-    h = res[1];
-    if (static_cast<VeritRule>(std::stoul(child->getArguments()[0].toString()))
-        == VeritRule::ASSUME)
-    {
-      childIds.push_back(h);
-    }
-    else
-    {
-      childIds.push_back(i);
-    }
-  }
-
-  // In case the rule is an anchor add subproof step after children are printed
-  if (static_cast<VeritRule>(std::stoul(pfn->getArguments()[0].toString()))
-      == VeritRule::ANCHOR_SUBPROOF)
-  {
-    auto last = ids.back();
-    ids.pop_back();
-    out << "(step " << veritPrintName(ids, last, false) << " "
-        << pfn->getArguments()[2] << " :rule "
-        << veritRuletoString(static_cast<VeritRule>(
-               std::stoul(pfn->getArguments()[0].toString())))
-        << " :discharge (";
-    ids.push_back(last);
-    for (int j = 1; j < current_h; j++)
-    {  // TODO: Does this also work with nested anchors?
-      out << veritPrintName(ids, j, true);
-      if (j != current_h - 1)
-      {
-        out << " ";
-      }
-    }
-    ids.pop_back();
-    // TODO: Print premises
-    out << "))\n";
-    i = current_i;
-    h = current_h;
-    return {i + 1, h};
-  }
-
-
-  //Print current step
-
-    out << "(step " << veritPrintName(ids, i, false) << " ";
-
-    if (pfn->getArguments()[2].end() - pfn->getArguments()[2].begin() >= 2)
-    {
-        out << pfn->getArguments()[2];
-    }
-    else
-    {
-      out << pfn->getArguments()[2];
-    }
-
-    out << " :rule " << veritRuletoString(static_cast<VeritRule>(std::stoul(pfn->getArguments()[0].toString())));
-    if (pfn->getArguments().size() > 3)
-    {
-      out << " :args";
-      for (int i = 3; i < pfn->getArguments().size(); i++)
-      {
-        out << " " << pfn->getArguments()[i];
-      }
-    }
-    if (childIds.size() >= 1)
-    {
-      out << " :premises (";
-      for (int j = 0; j < childIds.size(); j++)
-      {
-        if (static_cast<VeritRule>(
-                std::stoul(pfn->getChildren()[j]->getArguments()[0].toString()))
-            == VeritRule::ASSUME)
-        {
-          out << veritPrintName(ids, childIds[j] - 1, true);
-        }
-        else
-        {
-          out << veritPrintName(ids, childIds[j] - 1, false);
-        }
-        if (j != childIds.size() - 1)
-        {
-          out << " ";
-        }
-      }
-      out << ")";
-    }
-    out << ")\n";
-  }
-  else
-  {
-    out << "Not translated yet\n";  // TODO: Give out undefined
-  }
-
-  return {i + 1, h};
-}
-
-/**
- * This method updates the proof rule application by splitting on the given
- * rule and translating it into a proof node in terms of the veriT rules.
- *
- * @param res The expected result of the application,
- * @param rule The id of the veriT rule,
- * @param children The children of the application,
- * @param args The arguments of the application,
- * @param cdp The proof to add to,
- * @return True if the step could be added, or null if not.
+ * @param out The stream to write to
+ * @param pfn The proof node to be printed
  */
 static void veritPrinter(std::ostream& out, std::shared_ptr<ProofNode> pfn)
 {
-  //out << "\n";
-  //out << "\n";
-  std::vector<int> ids;
-  veritPrintInternal(out, pfn, ids, 1, 0);
-  //out << "\n";
-  //out << "Check proof? (0/1)" << "\n";
+  VeritProofPrinter vpp(true);
+  vpp.veritPrinter(out,pfn);
+
+  /*out << "\n";
+  out << "Check proof? (0/1)" << "\n";
   bool check;
   std::cin >> check;
   if (check)
   {
-    veritProofChecker(pfn, out);
-  }
+    veritProofChecker(pfn->getChildren()[0], out);
+  }*/
 }
 
 }  // namespace proof
