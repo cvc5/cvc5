5b852d4a987b9167b423cb16fa5d48e5250abe37
ajreynol <andrew.j.reynolds@gmail.com>
2021-03-16 10:23:12 -0500

    Fixes

--- src/smt/proof_manager.cpp
+++ src/smt/proof_manager.cpp
@@ -42,7 +42,6 @@ PfManager::PfManager(context::UserContext* u, SmtEngine* smte)
       d_pppg(new PreprocessProofGenerator(
           d_pnm.get(), u, "smt::PreprocessProofGenerator")),
       d_pfpp(new ProofPostproccess(d_pnm.get(), smte, d_pppg.get())),
-      d_vpfpp(nullptr),
       d_finalProof(nullptr)
 {
   // add rules to eliminate here
@@ -143,14 +142,12 @@ void PfManager::printProof(std::ostream& out,
     proof::LeanProofPostprocess lpfpp(d_pnm.get());
     std::vector<Node> assertions;
     getAssertions(as, df, assertions);
-    lpfpp.reset(new proof::LeanProofPostprocess(d_pnm.get()));
-    lpfpp->process(fp);
+    lpfpp.process(fp);
     proof::LeanPrinter::print(out, assertions, fp);
   }
   else if (options::proofFormatMode() == options::ProofFormatMode::VERIT)
   {
     proof::VeritProofPostprocess vpfpp(d_pnm.get());
-    vpfpp.reset(new proof::VeritProofPostprocess(d_pnm.get()));
     vpfpp.process(fp);
     proof::veritPrinter(out, fp);
   }
