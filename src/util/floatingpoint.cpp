c522469f3c2083b90850d60a0c8668a7c799e030
Aina Niemetz <aina.niemetz@gmail.com>
2021-03-18 14:31:52 -0700

    BitVector: Change setBit to set the bit in place.
    Creating BitVectors (and deleting them) is in general expensive because
    of the underlying multi-precision Integer. If possible, unnecessary
    constructions and desctructions of BitVectors should be avoided.
    The most common use case for `setBit` is that for an existing BitVector,
    a given bit should be set to a certain value. Not doing this in place
    generates unnecessary constructions and destructions of BitVectors.

--- src/util/floatingpoint.cpp
+++ src/util/floatingpoint.cpp
@@ -361,8 +361,8 @@ FloatingPoint FloatingPoint::makeMaxNormal(const FloatingPointSize& size,
                                            bool sign)
 {
   BitVector bvsign = sign ? BitVector::mkOne(1) : BitVector::mkZero(1);
-  BitVector bvexp =
-      BitVector::mkOnes(size.packedExponentWidth()).setBit(0, false);
+  BitVector bvexp = BitVector::mkOnes(size.packedExponentWidth());
+  bvexp.setBit(0, false);
   BitVector bvsig = BitVector::mkOnes(size.packedSignificandWidth());
   return FloatingPoint(size, bvsign.concat(bvexp).concat(bvsig));
 }
