5d965434abbb625d7922d729fa6f220814410282
ajreynol <andrew.j.reynolds@gmail.com>
2021-03-25 07:28:07 -0500

    Add

---
+++ src/theory/quantifiers/term_pools.cpp
@@ -0,0 +1,44 @@
+/*********************                                                        */
+/*! \file term_pools.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ ** in the top-level source directory and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief utilities for term enumeration
+ **/
+
+#include "theory/quantifiers/term_pools.h"
+
+namespace CVC4 {
+namespace theory {
+namespace quantifiers {
+
+
+void TermPoolDomain::initialize() {
+  d_terms.clear();
+}
+void TermPoolDomain::add(Node n){
+  d_terms.insert(n);
+}
+
+void TermPools::registerPool(Node p) { d_pools[p].initialize(); }
+
+void TermPools::addToPool(Node n, Node p)
+{
+  TermPoolDomain& dom = getDomain(p);
+  dom.addTo(n);
+}
+
+TermPoolDomain& TermPools::getDomain(Node p)
+{
+  return d_pools[p];
+}
+
+}
+}
+}
---
+++ src/theory/quantifiers/term_pools.h
@@ -0,0 +1,66 @@
+/*********************                                                        */
+/*! \file term_pools.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ ** in the top-level source directory and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief utilities for term enumeration
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef CVC4__THEORY__QUANTIFIERS__TERM_POOLS_H
+#define CVC4__THEORY__QUANTIFIERS__TERM_POOLS_H
+
+#include <unordered_set>
+
+#include "expr/node.h"
+
+namespace CVC4 {
+namespace theory {
+namespace quantifiers {
+
+
+class TermPoolDomain
+{
+public:
+  /** initialize */
+  void initialize();
+  /** add node to pool */
+  void add(Node n);
+  /** The list in this pool */
+  std::unordered_set<Node, NodeHashFunction> d_terms;
+};
+  
+/** Term pools
+ */
+class TermPools
+{
+ public:
+  TermPools();
+  ~TermPools() {}
+
+  /** register pool */
+  void registerPool(Node p);
+
+  /** add to pool */
+  void addToPool(Node n, Node p);
+  
+  /** get domain */
+  TermPoolDomain& getDomain(Node p);
+  
+ private:
+  /** Maps nodes to a domain */
+  std::map<Node, TermPoolDomain> d_pools;
+};
+
+} /* CVC4::theory::quantifiers namespace */
+} /* CVC4::theory namespace */
+} /* CVC4 namespace */
+
+#endif /* CVC4__THEORY__QUANTIFIERS__TERM_POOLS_H */
