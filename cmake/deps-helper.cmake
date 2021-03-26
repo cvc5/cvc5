d8e9e8e5a1d5e1b2af0d984aa7cf7fb09d79c4fe
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-03-26 18:42:57 +0100

    Address review.

--- cmake/deps-helper.cmake
+++ cmake/deps-helper.cmake
@@ -2,13 +2,13 @@
 set(DEPS_PREFIX "${CMAKE_BINARY_DIR}/deps")
 # base path to installed dependencies
 set(DEPS_BASE "${CMAKE_BINARY_DIR}/deps")
-# search path hint for find_X family
-list(APPEND CMAKE_PREFIX_PATH "${DEPS_BASE}/deps/")
-list(REMOVE_DUPLICATES CMAKE_PREFIX_PATH)
 # cmake wants include dirs to exist when a target is created
 file(MAKE_DIRECTORY "${DEPS_BASE}/include/")
 
 macro(check_system_version name)
+    # find_package sets this variable when called with a version
+    # https://cmake.org/cmake/help/latest/command/find_package.html#version-selection
+    # we ignore the EXACT option and version ranges here
     if (${name}_FIND_VERSION)
         if(${name}_VERSION VERSION_LESS ${name}_FIND_VERSION)
             message(VERBOSE "System version for ${name} has incompatible version: required ${${name}_FIND_VERSION} but found ${${name}_VERSION}")
