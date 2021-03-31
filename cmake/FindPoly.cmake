bb4d6a4d4d9b4ec5e830da86699caae791a4443c
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-03-31 17:13:52 +0200

    Fix indent

--- cmake/FindPoly.cmake
+++ cmake/FindPoly.cmake
@@ -1,13 +1,13 @@
 #####################
-# FindPoly.cmake
-# Top contributors (to current version):
-#   Gereon Kremer
-# This file is part of the CVC4 project.
-# Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
-# in the top-level source directory and their institutional affiliations.
-# All rights reserved.  See the file COPYING in the top-level source
-# directory for licensing information.
-#
+## FindPoly.cmake
+## Top contributors (to current version):
+##   Gereon Kremer
+## This file is part of the CVC4 project.
+## Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+## in the top-level source directory and their institutional affiliations.
+## All rights reserved.  See the file COPYING in the top-level source
+## directory for licensing information.
+##
 # Find LibPoly
 # Poly_FOUND - system has LibPoly
 # Poly_INCLUDE_DIR - the LibPoly include directory
