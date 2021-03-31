21c79e1807c4072b1159283ae5a3e70164e0cc4a
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-03-31 18:46:26 -0500

    small changes

--- cmake/FindJUnit.cmake
+++ cmake/FindJUnit.cmake
@@ -1,7 +1,7 @@
 #####################
-## FindJUnit5.cmake
+## FindJUnit.cmake
 ## Top contributors (to current version):
-##   Mathias Preiner, Mudathir
+##   Mathias Preiner, Mudathir Mohamed
 ## This file is part of the CVC4 project.
 ## Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
 ## in the top-level source directory and their institutional affiliations.
