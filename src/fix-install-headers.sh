d72adfafbf3ef3aa9aad2810b44d7c966883b3b5
Aina Niemetz <aina.niemetz@gmail.com>
2021-04-05 18:04:11 -0700

    try

--- src/fix-install-headers.sh
+++ src/fix-install-headers.sh
@@ -5,5 +5,7 @@ set -e -o pipefail
 dir="$DESTDIR$1"
 
 find "$dir/include/cvc5/" -type f \
-  -exec sed -i'' -e 's/include.*"api\/cpp\/\(.*\)"/include <cvc5\/\1>/' -e 's/"cvc4_export.h"/<cvc5\/cvc4_export.h>/' {} +
+  -exec sed -i'' -e 's/include.*"api\/cpp\/\(.*\)"/include <cvc5\/\1>/' {} +
 
+find "$dir/include/cvc5/" -type f \
+  -exec sed -i'' -e 's/"cvc4_export.h"/<cvc5\/cvc4_export.h>/' {} +
