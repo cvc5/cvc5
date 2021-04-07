5e6ef00208247e93ca68416a81c9cc3609cbe2bc
ajreynol <andrew.j.reynolds@gmail.com>
2021-04-07 17:35:55 -0500

    More

---
+++ src/theory/incomplete_id.cpp
@@ -0,0 +1,38 @@
+/*********************                                                        */
+/*! \file incomplete_id.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ ** in the top-level source directory and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implementation of incompleteness enumeration.
+ **/
+
+#include "theory/inference_id.h"
+
+#include <iostream>
+
+namespace cvc5 {
+namespace theory {
+
+const char* toString(IncompleteId i)
+{
+  switch (i)
+  {
+    case IncompleteId::UNKNOWN: return "UNKNOWN";
+    default: return "?IncompleteId?";
+  }
+}
+
+std::ostream& operator<<(std::ostream& out, IncompleteId i)
+{
+  out << toString(i);
+  return out;
+}
+
+}  // namespace theory
+}  // namespace cvc5
---
+++ src/theory/incomplete_id.h
@@ -0,0 +1,55 @@
+/*********************                                                        */
+/*! \file incomplete_id.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ ** in the top-level source directory and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Incompleteness enumeration.
+ **/
+
+#include "cvc4_private.h"
+
+#include <iosfwd>
+
+#ifndef CVC4__THEORY__INCOMPLETE_ID_H
+#define CVC4__THEORY__INCOMPLETE_ID_H
+
+namespace cvc5 {
+namespace theory {
+
+/** 
+ * Reasons for incompleteness in CVC4.
+ */
+enum class IncompleteId
+{
+  
+  //-------------------------------------- unknown
+  UNKNOWN
+};
+
+/**
+ * Converts an incompleteness id to a string.
+ *
+ * @param i The incompleteness identifier
+ * @return The name of the incompleteness identifier
+ */
+const char* toString(IncompleteId i);
+
+/**
+ * Writes an incompleteness identifier to a stream.
+ *
+ * @param out The stream to write to
+ * @param i The incompleteness identifier to write to the stream
+ * @return The stream
+ */
+std::ostream& operator<<(std::ostream& out, IncompleteId i);
+
+}  // namespace theory
+}  // namespace cvc5
+
+#endif /* CVC4__THEORY__INCOMPLETE_ID_H */
