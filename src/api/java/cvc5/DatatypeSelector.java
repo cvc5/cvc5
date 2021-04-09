31700bf825854270834d8e693a2283111cdbc618
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-04-09 17:03:24 -0500

    Datatype selector

--- src/api/java/cvc5/DatatypeSelector.java
+++ src/api/java/cvc5/DatatypeSelector.java
@@ -17,6 +17,45 @@ public class DatatypeSelector extends AbstractPointer
 
   // endregion
 
+  /** @return the name of this Datatype selector. */
+  String getName()
+  {
+    return getName(pointer);
+  }
+
+  private native String getName(long pointer);
+
+  /**
+   * Get the selector operator of this datatype selector.
+   * @return the selector term
+   */
+  public Term getSelectorTerm()
+  {
+    long termPointer = getSelectorTerm(pointer);
+    return new Term(solver, termPointer);
+  }
+
+  private native long getSelectorTerm(long pointer);
+
+  /** @return the range sort of this argument. */
+  Sort getRangeSort()
+  {
+    long sortPointer = getRangeSort(pointer);
+    return new Sort(solver, sortPointer);
+  }
+
+  private native long getRangeSort(long pointer);
+
+  /**
+   * @return true if this DatatypeSelector is a null object
+   */
+  public boolean isNull()
+  {
+    return isNull(pointer);
+  }
+
+  private native boolean isNull(long pointer);
+
   /**
    * @return a string representation of this datatype selector
    */
