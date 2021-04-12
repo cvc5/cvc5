a41bce1ae03217541180fec4ece8ba840f30772a
ajreynol <andrew.j.reynolds@gmail.com>
2021-04-12 16:03:17 -0500

    Update headers

--- src/decision/justify_stack.h
+++ src/decision/justify_stack.h
@@ -1,16 +1,17 @@
-/*********************                                                        */
-/*! \file justify_stack.h
- ** \verbatim
- ** Top contributors (to current version):
- **   Andrew Reynolds
- ** This file is part of the CVC4 project.
- ** Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
- ** in the top-level source directory and their institutional affiliations.
- ** All rights reserved.  See the file COPYING in the top-level source
- ** directory for licensing information.\endverbatim
- **
- ** \brief Justification stack
- **/
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Justification stack.
+ */
 
 #include "cvc4_private.h"
 
