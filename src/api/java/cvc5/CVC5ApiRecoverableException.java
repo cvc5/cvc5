2808808de61b35a415e7043a33d0e423df74d2d1
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-04-12 14:33:59 -0500

    small changes

--- src/api/java/cvc5/CVC5ApiRecoverableException.java
+++ src/api/java/cvc5/CVC5ApiRecoverableException.java
@@ -1,3 +1,18 @@
+/*********************                                                        */
+/*! \file CVC5ApiRecoverableException.java
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Mudathir Mohamed
+ ** This file is part of the CVC5 project.
+ ** Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ ** in the top-level source directory and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief class CVC5ApiRecoverableException for cvc5 recoverable API exceptions
+ **/
+
+
 package cvc5;
 
 public class CVC5ApiRecoverableException extends CVC5ApiException
