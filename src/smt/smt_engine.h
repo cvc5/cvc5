e0fe6ab245e8e603ffd0024076fd0ff2ba36ae94
ajreynol <andrew.j.reynolds@gmail.com>
2021-04-13 16:41:47 -0500

    Doc

--- src/smt/smt_engine.h
+++ src/smt/smt_engine.h
@@ -497,7 +497,14 @@ class CVC4_EXPORT SmtEngine
   /*------------------------- end of sygus commands ------------------------*/
 
   /**
-   * Declare pool.
+   * Declare pool whose initial value is the terms in initValue. A pool is
+   * a variable of type (Set T) that is used in quantifier annotations and does
+   * not occur in constraints.
+   *
+   * @param p The pool to declare, which should be a variable of type (Set T)
+   * for some type T.
+   * @param initValue The initial value of p, which should be a vector of terms
+   * of type T.
    */
   void declarePool(const Node& p, const std::vector<Node>& initValue);
   /**
