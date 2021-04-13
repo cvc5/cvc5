bf29a47d2f7f835c6d591a772924a686ad714109
Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
2021-04-13 03:56:03 -0500

    Pass iterators instead of making a copy.
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>

--- src/theory/quantifiers/sygus/sygus_reconstruct.cpp
+++ src/theory/quantifiers/sygus/sygus_reconstruct.cpp
@@ -283,9 +283,8 @@ TypeObligationSetMap SygusReconstruct::matchNewObs(Node k, Node sz)
       // As it traverses sz, substitute populates its input cache with TNodes
       // that are not preserved by this module and maybe destroyed after the
       // method call. To avoid referencing those unsafe TNodes throughout this
-      // module, we pass a temporary copy of d_sol instead.
-      std::unordered_map<TNode, TNode, TNodeHashFunction> sol = d_sol;
-      Node s = sz.substitute(sol);
+      // module, we pass a iterators of d_sol instead.
+      Node s = sz.substitute(d_sol.cbegin(), d_sol.cend());
       markSolved(k, s);
     }
     else
@@ -341,10 +340,8 @@ void SygusReconstruct::markSolved(Node k, Node s)
       {
         // then it is completely solved and can be used as a solution of its
         // corresponding obligation
-        // pass a temporary copy of d_sol to avoid populating it with unsafe
-        // TNodes
-        std::unordered_map<TNode, TNode, TNodeHashFunction> sol = d_sol;
-        Node parentSol = parent.substitute(sol);
+        // pass iterators of d_sol to avoid populating it with unsafe TNodes
+        Node parentSol = parent.substitute(d_sol.cbegin(), d_sol.cend());
         Node parentOb = d_parentOb[parent];
         // proceed only if parent obligation is not already solved
         if (d_sol[parentOb].isNull())
