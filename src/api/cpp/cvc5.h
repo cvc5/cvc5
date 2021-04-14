523b5e0f64bbe416e6de6d1188d978ed3cc03529
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-04-14 20:52:23 +0200

    Document params and return values

--- src/api/cpp/cvc5.h
+++ src/api/cpp/cvc5.h
@@ -64,17 +64,29 @@ struct Statistics;
 class CVC4_EXPORT CVC4ApiException : public std::exception
 {
  public:
-  /** Construct with message from a string. */
+  /**
+   * Construct with message from a string.
+   * @param str The error message.
+   */
   CVC4ApiException(const std::string& str) : d_msg(str) {}
-  /** Construct with message from a string stream. */
+  /**
+   * Construct with message from a string stream.
+   * @param stream The error message.
+   */
   CVC4ApiException(const std::stringstream& stream) : d_msg(stream.str()) {}
-  /** Retrieve the message from this exception. */
+  /**
+   * Retrieve the message from this exception.
+   * @return The error message.
+   */
   const std::string& getMessage() const { return d_msg; }
-  /** Retrieve the message as a C-style array. */
+  /**
+   * Retrieve the message as a C-style array.
+   * @return The error message.
+   */
   const char* what() const noexcept override { return d_msg.c_str(); }
 
  private:
-  /** The stored message. */
+  /** The stored error message. */
   std::string d_msg;
 };
 
@@ -85,9 +97,15 @@ class CVC4_EXPORT CVC4ApiException : public std::exception
 class CVC4_EXPORT CVC4ApiRecoverableException : public CVC4ApiException
 {
  public:
-  /** Construct with message from a string. */
+  /**
+   * Construct with message from a string.
+   * @param str The error message.
+   */
   CVC4ApiRecoverableException(const std::string& str) : CVC4ApiException(str) {}
-  /** Construct with message from a string stream. */
+  /**
+   * Construct with message from a string stream.
+   * @param stream The error message.
+   */
   CVC4ApiRecoverableException(const std::stringstream& stream)
       : CVC4ApiException(stream.str())
   {
