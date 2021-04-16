c775c6e8b5360da119a1851d81f93879cdb87d85
ajreynol <andrew.j.reynolds@gmail.com>
2021-04-16 13:54:20 -0500

    Format

--- src/decision/justify_stats.cpp
+++ src/decision/justify_stats.cpp
@@ -19,17 +19,21 @@
 namespace cvc5 {
 
 JustifyStatistics::JustifyStatistics()
-    : d_numStatusNoDecision(smtStatisticsRegistry().registerInt("JustifyStrategy::StatusNoDecision", 0)),
-      d_numStatusDecision(smtStatisticsRegistry().registerInt("JustifyStrategy::StatusDecision", 0)),
-      d_numStatusBacktrack(smtStatisticsRegistry().registerInt("JustifyStrategy::StatusBacktrack", 0)),
-      d_maxStackSize(smtStatisticsRegistry().registerInt("JustifyStrategy::MaxStackSize", 0)),
-      d_maxAssertionsSize(smtStatisticsRegistry().registerInt("JustifyStrategy::MaxAssertionsSize", 0)),
-      d_maxSkolemDefsSize(smtStatisticsRegistry().registerInt("JustifyStrategy::MaxSkolemDefsSize", 0))
+    : d_numStatusNoDecision(smtStatisticsRegistry().registerInt(
+          "JustifyStrategy::StatusNoDecision", 0)),
+      d_numStatusDecision(smtStatisticsRegistry().registerInt(
+          "JustifyStrategy::StatusDecision", 0)),
+      d_numStatusBacktrack(smtStatisticsRegistry().registerInt(
+          "JustifyStrategy::StatusBacktrack", 0)),
+      d_maxStackSize(smtStatisticsRegistry().registerInt(
+          "JustifyStrategy::MaxStackSize", 0)),
+      d_maxAssertionsSize(smtStatisticsRegistry().registerInt(
+          "JustifyStrategy::MaxAssertionsSize", 0)),
+      d_maxSkolemDefsSize(smtStatisticsRegistry().registerInt(
+          "JustifyStrategy::MaxSkolemDefsSize", 0))
 {
 }
 
-JustifyStatistics::~JustifyStatistics()
-{
-}
+JustifyStatistics::~JustifyStatistics() {}
 
 }  // namespace cvc5
