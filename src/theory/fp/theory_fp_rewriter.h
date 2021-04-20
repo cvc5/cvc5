a1c8ee2a3fdc017e097fdbb3268079c042f7ccc4
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2021-04-20 12:38:51 -0500

    Apply suggestions from code review
    Co-authored-by: Aina Niemetz <aina.niemetz@gmail.com>

--- src/theory/fp/theory_fp_rewriter.h
+++ src/theory/fp/theory_fp_rewriter.h
@@ -46,14 +46,14 @@ class TheoryFpRewriter : public TheoryRewriter
     // often this will suffice
     return postRewrite(equality).d_node;
   }
-  /** expand definitions in node */
+  /** Expand definitions in node. */
   TrustNode expandDefinition(Node node);
 
  protected:
   RewriteFunction d_preRewriteTable[kind::LAST_KIND];
   RewriteFunction d_postRewriteTable[kind::LAST_KIND];
   RewriteFunction d_constantFoldTable[kind::LAST_KIND];
-  /** the expand definitions module */
+  /** The expand definitions module. */
   FpExpandDefs d_fpExpDef;
 }; /* class TheoryFpRewriter */
 
