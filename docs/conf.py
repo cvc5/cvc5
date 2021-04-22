66ca3bf7f64802c7d17c490046827bf48a6c5210
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-04-22 08:20:24 +0200

    Add custom css to change code style

--- docs/conf.py
+++ docs/conf.py
@@ -54,11 +54,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 # -- Breathe configuration ---------------------------------------------------
 breathe_default_project = "cvc5"
