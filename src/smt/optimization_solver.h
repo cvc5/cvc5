8b049575e3459634ff4813bfa88e0b5759a6b314
Ouyancheng <1024842937@qq.com>
2021-04-30 12:49:49 -0700

    c

--- src/smt/optimization_solver.h
+++ src/smt/optimization_solver.h
@@ -174,7 +174,10 @@ class OptimizationSolver
   }
   ~OptimizationSolver() = default;
 
-  /** Runs the optimization loop for the pushed objective **/
+  /** Runs the optimization loop for the pushed objective 
+   * FIXME: this function currently supports only single objective 
+   * for multiple pushed objectives it always optimizes the first one. 
+   * Add support for multi-obj later **/
   OptimizationResult::ResultType checkOpt();
 
   /**
