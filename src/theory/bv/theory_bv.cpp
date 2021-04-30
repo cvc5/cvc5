8b31afef2d0a5efdae8927ce5a6d703e340219e3
Mathias Preiner <mathias.preiner@gmail.com>
2021-04-30 13:25:00 -0700

    Minor.

--- src/theory/bv/theory_bv.cpp
+++ src/theory/bv/theory_bv.cpp
@@ -186,85 +186,74 @@ Theory::PPAssertStatus TheoryBV::ppAssert(
     TrustNode tin, TrustSubstitutionMap& outSubstitutions)
 {
   TNode in = tin.getNode();
-  switch (in.getKind())
+  Kind k = in.getKind();
+  if (k == kind::EQUAL)
   {
-    case kind::EQUAL:
+    // Substitute variables
+    if (in[0].isVar() && isLegalElimination(in[0], in[1]))
     {
-      // Substitute variables
-      if (in[0].isVar() && isLegalElimination(in[0], in[1]))
+      ++d_stats.d_solveSubstitutions;
+      outSubstitutions.addSubstitutionSolved(in[0], in[1], tin);
+      return Theory::PP_ASSERT_STATUS_SOLVED;
+    }
+    if (in[1].isVar() && isLegalElimination(in[1], in[0]))
+    {
+      ++d_stats.d_solveSubstitutions;
+      outSubstitutions.addSubstitutionSolved(in[1], in[0], tin);
+      return Theory::PP_ASSERT_STATUS_SOLVED;
+    }
+    /**
+     * Eliminate extract over bit-vector variables.
+     *
+     * x[j:i] = c, where c is a constant and x is a variable.
+     *
+     * i == 0:          x = sk1::c with bw(sk1) = bw(x) - 1 - j
+     * j == bw(x) - 1:  x = c::sk2 with bw(sk2) = i
+     * else:            x = sk1::c::sk2 with bw(sk1) = bw(x) - 1 - j
+     *                                  and  bw(sk2) = i
+     */
+    Node node = Rewriter::rewrite(in);
+    if ((node[0].getKind() == kind::BITVECTOR_EXTRACT && node[1].isConst())
+        || (node[1].getKind() == kind::BITVECTOR_EXTRACT
+            && node[0].isConst()))
+    {
+      Node extract = node[0].isConst() ? node[1] : node[0];
+      if (extract[0].isVar())
       {
-        ++d_stats.d_solveSubstitutions;
-        outSubstitutions.addSubstitutionSolved(in[0], in[1], tin);
-        return Theory::PP_ASSERT_STATUS_SOLVED;
-      }
-      if (in[1].isVar() && isLegalElimination(in[1], in[0]))
-      {
-        ++d_stats.d_solveSubstitutions;
-        outSubstitutions.addSubstitutionSolved(in[1], in[0], tin);
-        return Theory::PP_ASSERT_STATUS_SOLVED;
-      }
-      /**
-       * Eliminate extract over bit-vector variables.
-       *
-       * x[j:i] = c, where c is a constant and x is a variable.
-       *
-       * i == 0:          x = sk1::c with bw(sk1) = bw(x) - 1 - j
-       * j == bw(x) - 1:  x = c::sk2 with bw(sk2) = i
-       * else:            x = sk1::c::sk2 with bw(sk1) = bw(x) - 1 - j
-       *                                  and  bw(sk2) = i
-       */
-      Node node = Rewriter::rewrite(in);
-      if ((node[0].getKind() == kind::BITVECTOR_EXTRACT && node[1].isConst())
-          || (node[1].getKind() == kind::BITVECTOR_EXTRACT
-              && node[0].isConst()))
-      {
-        Node extract = node[0].isConst() ? node[1] : node[0];
-        if (extract[0].isVar())
+        Node c = node[0].isConst() ? node[0] : node[1];
+
+        uint32_t high = utils::getExtractHigh(extract);
+        uint32_t low = utils::getExtractLow(extract);
+        uint32_t var_bw = utils::getSize(extract[0]);
+        std::vector<Node> children;
+
+        if (low == 0 || high != var_bw - 1)
         {
-          Node c = node[0].isConst() ? node[0] : node[1];
+          Assert(high != var_bw - 1);
+          uint32_t skolem_size = var_bw - high - 1;
+          Node skolem = utils::mkVar(skolem_size);
+          children.push_back(skolem);
+        }
 
-          uint32_t high = utils::getExtractHigh(extract);
-          uint32_t low = utils::getExtractLow(extract);
-          uint32_t var_bw = utils::getSize(extract[0]);
-          std::vector<Node> children;
+        children.push_back(c);
 
-          if (low == 0 || high != var_bw - 1)
-          {
-            Assert(high != var_bw - 1);
-            uint32_t skolem_size = var_bw - high - 1;
-            Node skolem = utils::mkVar(skolem_size);
-            children.push_back(skolem);
-          }
+        if (high == var_bw - 1 || low != 0)
+        {
+          Assert(low != 0);
+          uint32_t skolem_size = low;
+          Node skolem = utils::mkVar(skolem_size);
+          children.push_back(skolem);
+        }
 
-          children.push_back(c);
-
-          if (high == var_bw - 1 || low != 0)
-          {
-            Assert(low != 0);
-            uint32_t skolem_size = low;
-            Node skolem = utils::mkVar(skolem_size);
-            children.push_back(skolem);
-          }
-
-          Node concat = utils::mkConcat(children);
-          Assert(utils::getSize(concat) == utils::getSize(extract[0]));
-          if (isLegalElimination(extract[0], concat))
-          {
-            outSubstitutions.addSubstitutionSolved(extract[0], concat, tin);
-            return Theory::PP_ASSERT_STATUS_SOLVED;
-          }
+        Node concat = utils::mkConcat(children);
+        Assert(utils::getSize(concat) == utils::getSize(extract[0]));
+        if (isLegalElimination(extract[0], concat))
+        {
+          outSubstitutions.addSubstitutionSolved(extract[0], concat, tin);
+          return Theory::PP_ASSERT_STATUS_SOLVED;
         }
       }
     }
-    break;
-    case kind::BITVECTOR_ULT:
-    case kind::BITVECTOR_SLT:
-    case kind::BITVECTOR_ULE:
-    case kind::BITVECTOR_SLE:
-
-    default:
-      // TODO other predicates
-      break;
   }
   return Theory::PP_ASSERT_STATUS_UNSOLVED;
 }
