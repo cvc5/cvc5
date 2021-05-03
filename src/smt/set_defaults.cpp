00fc62f70356de09a0e587f56f1fd7c1ef67d5b1
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-05-03 18:14:30 -0300

    hardcoding safety for testing

--- src/smt/set_defaults.cpp
+++ src/smt/set_defaults.cpp
@@ -421,7 +421,7 @@ void setDefaults(LogicInfo& logic, bool isInternalSubsolver)
   // whether we want to force safe unsat cores, i.e., if we are in the OLD_PROOF
   // unsat core mode, since new ones are experimental
   bool safeUnsatCores =
-      options::unsatCoresMode() == options::UnsatCoresMode::OLD_PROOF;
+      options::unsatCoresMode() != options::UnsatCoresMode::OFF;
 
   // Disable options incompatible with incremental solving, unsat cores or
   // output an error if enabled explicitly. It is also currently incompatible
