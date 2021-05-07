8aac080b672f6d4242cb393cfb71fba2505bc0a8
Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
2021-05-06 19:30:09 -0500

    Change Obligation Info to Obligation.
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>

--- src/theory/quantifiers/sygus/rcons_type_info.cpp
+++ src/theory/quantifiers/sygus/rcons_type_info.cpp
@@ -17,6 +17,7 @@
 
 #include "expr/skolem_manager.h"
 #include "theory/datatypes/sygus_datatype_utils.h"
+#include "theory/quantifiers/sygus/rcons_obligation_info.h"
 
 namespace cvc5 {
 namespace theory {
@@ -63,12 +64,20 @@ Node RConsTypeInfo::addTerm(Node n)
   return d_crd->addTerm(n, false, out);
 }
 
-void RConsTypeInfo::setBuiltinToOb(Node builtin, Node ob)
+void RConsTypeInfo::setBuiltinToOb(Node t, Obligation* ob)
 {
-  d_ob[builtin] = ob;
+  d_ob.emplace(t, ob);
 }
 
-Node RConsTypeInfo::builtinToOb(Node builtin) { return d_ob[builtin]; }
+Obligation* RConsTypeInfo::builtinToOb(Node t)
+{
+  auto it = d_ob.find(t);
+  if (it != d_ob.cend())
+  {
+    return it->second;
+  }
+  return nullptr;
+}
 
 }  // namespace quantifiers
 }  // namespace theory
