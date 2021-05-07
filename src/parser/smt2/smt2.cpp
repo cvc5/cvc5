057a61d77b74e8fba080a76cf72016cf5e23b78b
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-05-07 00:07:01 -0500

    bagmap

--- src/parser/smt2/smt2.cpp
+++ src/parser/smt2/smt2.cpp
@@ -634,6 +634,7 @@ Command* Smt2::setLogic(std::string name, bool fromCommand)
     addOperator(api::BAG_IS_SINGLETON, "bag.is_singleton");
     addOperator(api::BAG_FROM_SET, "bag.from_set");
     addOperator(api::BAG_TO_SET, "bag.to_set");
+    addOperator(api::BAG_MAP, "bag.map");
   }
   if(d_logic.isTheoryEnabled(theory::THEORY_STRINGS)) {
     defineType("String", d_solver->getStringSort(), true, true);
