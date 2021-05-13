f4b0ce18845667bcafe9ca41c14cf6bcf8e7f0c8
yoni206 <yoni206@gmail.com>
2021-05-13 14:42:52 -0700

    yapf

--- test/python/unit/api/test_grammar.py
+++ test/python/unit/api/test_grammar.py
@@ -18,113 +18,115 @@ import pytest
 import pycvc5
 from pycvc5 import kinds
 
+
 def test_add_rule():
-  solver = pycvc5.Solver()
-  boolean = solver.getBooleanSort()
-  integer = solver.getIntegerSort()
+    solver = pycvc5.Solver()
+    boolean = solver.getBooleanSort()
+    integer = solver.getIntegerSort()
 
-  nullTerm = pycvc5.Term(solver)
-  start = solver.mkVar(boolean)
-  nts = solver.mkVar(boolean)
+    nullTerm = pycvc5.Term(solver)
+    start = solver.mkVar(boolean)
+    nts = solver.mkVar(boolean)
 
-  # expecting no error
-  g = solver.mkSygusGrammar([], [start])
+    # expecting no error
+    g = solver.mkSygusGrammar([], [start])
 
-  g.addRule(start, solver.mkBoolean(False))
+    g.addRule(start, solver.mkBoolean(False))
 
-  # expecting errors
-  with pytest.raises(Exception):
-    g.addRule(nullTerm, solver.mkBoolean(false))
-  with pytest.raises(Exception):
-    g.addRule(start, nullTerm)
-  with pytest.raises(Exception):
-    g.addRule(nts, solver.mkBoolean(false))
-  with pytest.raises(Exception):
-    g.addRule(start, solver.mkInteger(0))
+    # expecting errors
+    with pytest.raises(Exception):
+        g.addRule(nullTerm, solver.mkBoolean(false))
+    with pytest.raises(Exception):
+        g.addRule(start, nullTerm)
+    with pytest.raises(Exception):
+        g.addRule(nts, solver.mkBoolean(false))
+    with pytest.raises(Exception):
+        g.addRule(start, solver.mkInteger(0))
 
-  # expecting no errors
-  solver.synthFun("f", {}, boolean, g)
+    # expecting no errors
+    solver.synthFun("f", {}, boolean, g)
 
-  # expecting an error
-  with pytest.raises(Exception):
-    g.addRule(start, solver.mkBoolean(false))
+    # expecting an error
+    with pytest.raises(Exception):
+        g.addRule(start, solver.mkBoolean(false))
+
 
 def test_add_rules():
-  solver = pycvc5.Solver()
-  boolean = solver.getBooleanSort()
-  integer = solver.getIntegerSort()
+    solver = pycvc5.Solver()
+    boolean = solver.getBooleanSort()
+    integer = solver.getIntegerSort()
 
-  nullTerm = pycvc5.Term(solver)
-  start = solver.mkVar(boolean)
-  nts = solver.mkVar(boolean)
+    nullTerm = pycvc5.Term(solver)
+    start = solver.mkVar(boolean)
+    nts = solver.mkVar(boolean)
 
-  g = solver.mkSygusGrammar([], [start])
+    g = solver.mkSygusGrammar([], [start])
 
-  g.addRules(start, {solver.mkBoolean(False)})
+    g.addRules(start, {solver.mkBoolean(False)})
 
-  #Expecting errors
-  with pytest.raises(Exception):
-    g.addRules(nullTerm, solver.mkBoolean(False))
-  with pytest.raises(Exception):
-    g.addRules(start, {nullTerm})
-  with pytest.raises(Exception):
-    g.addRules(nts, {solver.mkBoolean(False)})
-  with pytest.raises(Exception):
-    g.addRules(start, {solver.mkInteger(0)})
-  #Expecting no errors
-  solver.synthFun("f", {}, boolean, g)
+    #Expecting errors
+    with pytest.raises(Exception):
+        g.addRules(nullTerm, solver.mkBoolean(False))
+    with pytest.raises(Exception):
+        g.addRules(start, {nullTerm})
+    with pytest.raises(Exception):
+        g.addRules(nts, {solver.mkBoolean(False)})
+    with pytest.raises(Exception):
+        g.addRules(start, {solver.mkInteger(0)})
+    #Expecting no errors
+    solver.synthFun("f", {}, boolean, g)
 
-  #Expecting an error
-  with pytest.raises(Exception):
-    g.addRules(start, solver.mkBoolean(False))
+    #Expecting an error
+    with pytest.raises(Exception):
+        g.addRules(start, solver.mkBoolean(False))
+
 
 def testAddAnyConstant():
-  solver = pycvc5.Solver()
-  boolean = solver.getBooleanSort()
+    solver = pycvc5.Solver()
+    boolean = solver.getBooleanSort()
 
-  nullTerm = pycvc5.Term(solver)
-  start = solver.mkVar(boolean)
-  nts = solver.mkVar(boolean)
+    nullTerm = pycvc5.Term(solver)
+    start = solver.mkVar(boolean)
+    nts = solver.mkVar(boolean)
 
-  g = solver.mkSygusGrammar({}, {start})
+    g = solver.mkSygusGrammar({}, {start})
 
-  g.addAnyConstant(start)
-  g.addAnyConstant(start)
-
-  with pytest.raises(Exception):
-    g.addAnyConstant(nullTerm)
-  with pytest.raises(Exception):
-    g.addAnyConstant(nts)
-
-  solver.synthFun("f", {}, boolean, g)
-
-  with pytest.raises(Exception):
     g.addAnyConstant(start)
+    g.addAnyConstant(start)
+
+    with pytest.raises(Exception):
+        g.addAnyConstant(nullTerm)
+    with pytest.raises(Exception):
+        g.addAnyConstant(nts)
+
+    solver.synthFun("f", {}, boolean, g)
+
+    with pytest.raises(Exception):
+        g.addAnyConstant(start)
 
 
 def testAddAnyVariable():
-  solver = pycvc5.Solver()
-  boolean = solver.getBooleanSort()
+    solver = pycvc5.Solver()
+    boolean = solver.getBooleanSort()
 
-  nullTerm = pycvc5.Term(solver)
-  x = solver.mkVar(boolean)
-  start = solver.mkVar(boolean)
-  nts = solver.mkVar(boolean)
+    nullTerm = pycvc5.Term(solver)
+    x = solver.mkVar(boolean)
+    start = solver.mkVar(boolean)
+    nts = solver.mkVar(boolean)
 
-  g1 = solver.mkSygusGrammar({x}, {start})
-  g2 = solver.mkSygusGrammar({}, {start})
+    g1 = solver.mkSygusGrammar({x}, {start})
+    g2 = solver.mkSygusGrammar({}, {start})
 
-  g1.addAnyVariable(start)
-  g1.addAnyVariable(start)
-  g2.addAnyVariable(start)
-
-  with pytest.raises(Exception):
-    g1.addAnyVariable(nullTerm)
-  with pytest.raises(Exception):
-    g1.addAnyVariable(nts)
-
-  solver.synthFun("f", {}, boolean, g1)
-
-  with pytest.raises(Exception):
     g1.addAnyVariable(start)
+    g1.addAnyVariable(start)
+    g2.addAnyVariable(start)
 
+    with pytest.raises(Exception):
+        g1.addAnyVariable(nullTerm)
+    with pytest.raises(Exception):
+        g1.addAnyVariable(nts)
+
+    solver.synthFun("f", {}, boolean, g1)
+
+    with pytest.raises(Exception):
+        g1.addAnyVariable(start)
