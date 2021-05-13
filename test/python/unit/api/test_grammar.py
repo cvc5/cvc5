1396b82085bc030697f12aeb8f709fe490e62d48
yoni206 <yoni206@gmail.com>
2021-05-13 14:54:26 -0700

    yapf

--- test/python/unit/api/test_grammar.py
+++ test/python/unit/api/test_grammar.py
@@ -16,15 +16,17 @@
 import pytest
 
 import pycvc5
-from pycvc5 import kinds
+from pycvc5 import kinds, Term
 
+@pytest.fixture
+def solver():
+    return pycvc5.Solver()
 
-def test_add_rule():
-    solver = pycvc5.Solver()
+def test_add_rule(solver):
     boolean = solver.getBooleanSort()
     integer = solver.getIntegerSort()
 
-    nullTerm = pycvc5.Term(solver)
+    null_term = Term(solver)
     start = solver.mkVar(boolean)
     nts = solver.mkVar(boolean)
 
@@ -34,29 +36,30 @@ def test_add_rule():
     g.addRule(start, solver.mkBoolean(False))
 
     # expecting errors
-    with pytest.raises(Exception):
-        g.addRule(nullTerm, solver.mkBoolean(false))
-    with pytest.raises(Exception):
-        g.addRule(start, nullTerm)
-    with pytest.raises(Exception):
-        g.addRule(nts, solver.mkBoolean(false))
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
+        g.addRule(null_term, solver.mkBoolean(False))
+    with pytest.raises(RuntimeError):
+        g.addRule(start, null_term)
+    with pytest.raises(RuntimeError):
+        g.addRule(nts, solver.mkBoolean(False))
+    with pytest.raises(RuntimeError):
         g.addRule(start, solver.mkInteger(0))
+    with pytest.raises(RuntimeError):
+        g.addRule(start, nts)
 
     # expecting no errors
     solver.synthFun("f", {}, boolean, g)
 
     # expecting an error
-    with pytest.raises(Exception):
-        g.addRule(start, solver.mkBoolean(false))
+    with pytest.raises(RuntimeError):
+        g.addRule(start, solver.mkBoolean(False))
 
 
-def test_add_rules():
-    solver = pycvc5.Solver()
+def test_add_rules(solver):
     boolean = solver.getBooleanSort()
     integer = solver.getIntegerSort()
 
-    nullTerm = pycvc5.Term(solver)
+    null_term = Term(solver)
     start = solver.mkVar(boolean)
     nts = solver.mkVar(boolean)
 
@@ -65,27 +68,28 @@ def test_add_rules():
     g.addRules(start, {solver.mkBoolean(False)})
 
     #Expecting errors
-    with pytest.raises(Exception):
-        g.addRules(nullTerm, solver.mkBoolean(False))
-    with pytest.raises(Exception):
-        g.addRules(start, {nullTerm})
-    with pytest.raises(Exception):
-        g.addRules(nts, {solver.mkBoolean(False)})
-    with pytest.raises(Exception):
-        g.addRules(start, {solver.mkInteger(0)})
+    with pytest.raises(RuntimeError):
+        g.addRules(null_term, [solver.mkBoolean(False)])
+    with pytest.raises(RuntimeError):
+        g.addRules(start, [null_term])
+    with pytest.raises(RuntimeError):
+        g.addRules(nts, [solver.mkBoolean(False)])
+    with pytest.raises(RuntimeError):
+        g.addRules(start, [solver.mkInteger(0)])
+    with pytest.raises(RuntimeError):
+        g.addRules(start, [nts])
     #Expecting no errors
     solver.synthFun("f", {}, boolean, g)
 
     #Expecting an error
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
         g.addRules(start, solver.mkBoolean(False))
 
 
-def testAddAnyConstant():
-    solver = pycvc5.Solver()
+def testAdd_any_constant(solver):
     boolean = solver.getBooleanSort()
 
-    nullTerm = pycvc5.Term(solver)
+    null_term = Term(solver)
     start = solver.mkVar(boolean)
     nts = solver.mkVar(boolean)
 
@@ -94,22 +98,21 @@ def testAddAnyConstant():
     g.addAnyConstant(start)
     g.addAnyConstant(start)
 
-    with pytest.raises(Exception):
-        g.addAnyConstant(nullTerm)
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
+        g.addAnyConstant(null_term)
+    with pytest.raises(RuntimeError):
         g.addAnyConstant(nts)
 
     solver.synthFun("f", {}, boolean, g)
 
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
         g.addAnyConstant(start)
 
 
-def testAddAnyVariable():
-    solver = pycvc5.Solver()
+def test_add_any_variable(solver):
     boolean = solver.getBooleanSort()
 
-    nullTerm = pycvc5.Term(solver)
+    null_term = Term(solver)
     x = solver.mkVar(boolean)
     start = solver.mkVar(boolean)
     nts = solver.mkVar(boolean)
@@ -121,12 +124,12 @@ def testAddAnyVariable():
     g1.addAnyVariable(start)
     g2.addAnyVariable(start)
 
-    with pytest.raises(Exception):
-        g1.addAnyVariable(nullTerm)
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
+        g1.addAnyVariable(null_term)
+    with pytest.raises(RuntimeError):
         g1.addAnyVariable(nts)
 
     solver.synthFun("f", {}, boolean, g1)
 
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
         g1.addAnyVariable(start)
