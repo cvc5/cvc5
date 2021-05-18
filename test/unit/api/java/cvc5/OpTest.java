ac8d5ed959f163e24d367926ac2ac1a21bb12c39
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-05-18 07:02:13 -0500

    small change

--- test/unit/api/java/cvc5/OpTest.java
+++ test/unit/api/java/cvc5/OpTest.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class GrammarTest
+class OpTest
 {
   private Solver d_solver;
 
