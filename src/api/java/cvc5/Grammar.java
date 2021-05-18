c3b073f80f3ff530d49c01a85cba4a68dc19bc79
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-05-18 07:21:50 -0500

    Update Grammar.java

--- src/api/java/cvc5/Grammar.java
+++ src/api/java/cvc5/Grammar.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Aina Niemetz, Andrew Reynolds, Abdalrhman Mohamed, Mudathir Mohamed
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * The cvc5 java API.
+ */
+
 package cvc5;
 
 public class Grammar extends AbstractPointer
@@ -13,6 +28,12 @@ public class Grammar extends AbstractPointer
   public long getPointer()
   {
     return pointer;
+  }
+
+  @Override
+  public void finalize()
+  {
+    deletePointer(pointer);
   }
 
   // endregion
