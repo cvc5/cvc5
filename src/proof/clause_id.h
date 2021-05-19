debd7b6633a55b2dcb5bddea5619cde6b317c7ce
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-05-19 18:32:05 -0300

    reverting

---
+++ src/proof/clause_id.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Paul Meng, Mathias Preiner, Liana Hadarean
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Definition of ClauseId.
+ *
+ * A ClauseId is a shared identifier between the proofs module and the sat
+ * solver for a clause.
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__PROOF__CLAUSE_ID_H
+#define CVC5__PROOF__CLAUSE_ID_H
+
+namespace cvc5 {
+
+/**
+ * A ClauseId is a shared identifier between the proofs module and the sat
+ * solver for a clause.
+ */
+typedef unsigned ClauseId;
+
+/** Reserved clauseId values used in the resolution proof. The represent,
+ * respectively, the empty clause, that adding the clause to the SAT solver was
+ * a no-op, and that an error occurred when trying to add. */
+const ClauseId ClauseIdEmpty(-1);
+const ClauseId ClauseIdUndef(-2);
+const ClauseId ClauseIdError(-3);
+
+}  // namespace cvc5
+
+#endif /* CVC5__PROOF__CLAUSE_ID_H */
