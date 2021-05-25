3e7752278da865f07ebf0f79cc0f0bf9ae4d29b4
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-05-25 06:34:26 -0500

    Update Op.java

--- src/api/java/cvc5/Op.java
+++ src/api/java/cvc5/Op.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Aina Niemetz, Andrew Reynolds, Abdalrhman Mohamed, Mudathir Mohamed
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * The cvc5 java API.
+ */
+
 package cvc5;
 
 public class Op extends AbstractPointer
@@ -13,6 +28,12 @@ public class Op extends AbstractPointer
   public long getPointer()
   {
     return pointer;
+  }
+
+  @Override
+  public void finalize()
+  {
+    deletePointer(pointer);
   }
 
   // endregion
@@ -73,6 +94,16 @@ public class Op extends AbstractPointer
   }
 
   private native boolean isIndexed(long pointer);
+
+  /**
+   * @return the number of indices of this op
+   */
+  public int getNumIndices()
+  {
+    return getNumIndices(pointer);
+  }
+
+  private native int getNumIndices(long pointer);
 
   /**
    * Get the indices used to create this Op.
