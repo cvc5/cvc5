e3eefb2ab8b291e4c213314e4cfd711788228c89
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-05-25 06:43:34 -0500

    Update DatatypeTest.java

--- test/unit/api/java/cvc5/DatatypeTest.java
+++ test/unit/api/java/cvc5/DatatypeTest.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds, Aina Niemetz, Andres Noetzli, Mudathir Mohamed
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Black box testing of the datatype classes of the Java API.
+ */
+
 package cvc5;
 
 import static cvc5.Kind.*;
@@ -16,11 +31,6 @@ class DatatypeTest
   @BeforeEach void setUp()
   {
     d_solver = new Solver();
-  }
-
-  @AfterEach void tearDown()
-  {
-    d_solver.deletePointer();
   }
 
   @Test void mkDatatypeSort() throws CVC5ApiException
