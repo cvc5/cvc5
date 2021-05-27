e538ee780a51c1f8ad606721cbbf7e15a7abb7ce
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-05-27 09:33:39 +0200

    fix

--- src/options/options_public.cpp
+++ src/options/options_public.cpp
@@ -41,116 +41,116 @@ namespace cvc5::options {
 
 InputLanguage getInputLanguage(const Options& opts)
 {
-  return opts.base().inputLanguage;
+  return opts.base.inputLanguage;
 }
 InstFormatMode getInstFormatMode(const Options& opts)
 {
-  return opts.printer().instFormatMode;
+  return opts.printer.instFormatMode;
 }
 OutputLanguage getOutputLanguage(const Options& opts)
 {
-  return opts.base().outputLanguage;
+  return opts.base.outputLanguage;
 }
-bool getUfHo(const Options& opts) { return opts.uf().ufHo; }
+bool getUfHo(const Options& opts) { return opts.uf.ufHo; }
 bool getDumpInstantiations(const Options& opts)
 {
-  return opts.smt().dumpInstantiations;
+  return opts.smt.dumpInstantiations;
 }
-bool getDumpModels(const Options& opts) { return opts.smt().dumpModels; }
-bool getDumpProofs(const Options& opts) { return opts.smt().dumpProofs; }
+bool getDumpModels(const Options& opts) { return opts.smt.dumpModels; }
+bool getDumpProofs(const Options& opts) { return opts.smt.dumpProofs; }
 bool getDumpUnsatCores(const Options& opts)
 {
-  return opts.smt().dumpUnsatCores || opts.smt().dumpUnsatCoresFull;
+  return opts.smt.dumpUnsatCores || opts.smt.dumpUnsatCoresFull;
 }
-bool getEarlyExit(const Options& opts) { return opts.driver().earlyExit; }
+bool getEarlyExit(const Options& opts) { return opts.driver.earlyExit; }
 bool getFilesystemAccess(const Options& opts)
 {
-  return opts.parser().filesystemAccess;
+  return opts.parser.filesystemAccess;
 }
 bool getForceNoLimitCpuWhileDump(const Options& opts)
 {
-  return opts.smt().forceNoLimitCpuWhileDump;
+  return opts.smt.forceNoLimitCpuWhileDump;
 }
-bool getHelp(const Options& opts) { return opts.driver().help; }
+bool getHelp(const Options& opts) { return opts.driver.help; }
 bool getIncrementalSolving(const Options& opts)
 {
-  return opts.smt().incrementalSolving;
+  return opts.smt.incrementalSolving;
 }
-bool getInteractive(const Options& opts) { return opts.driver().interactive; }
+bool getInteractive(const Options& opts) { return opts.driver.interactive; }
 bool getInteractivePrompt(const Options& opts)
 {
-  return opts.driver().interactivePrompt;
+  return opts.driver.interactivePrompt;
 }
-bool getLanguageHelp(const Options& opts) { return opts.base().languageHelp; }
-bool getMemoryMap(const Options& opts) { return opts.parser().memoryMap; }
-bool getParseOnly(const Options& opts) { return opts.base().parseOnly; }
-bool getProduceModels(const Options& opts) { return opts.smt().produceModels; }
-bool getSegvSpin(const Options& opts) { return opts.driver().segvSpin; }
+bool getLanguageHelp(const Options& opts) { return opts.base.languageHelp; }
+bool getMemoryMap(const Options& opts) { return opts.parser.memoryMap; }
+bool getParseOnly(const Options& opts) { return opts.base.parseOnly; }
+bool getProduceModels(const Options& opts) { return opts.smt.produceModels; }
+bool getSegvSpin(const Options& opts) { return opts.driver.segvSpin; }
 bool getSemanticChecks(const Options& opts)
 {
-  return opts.parser().semanticChecks;
+  return opts.parser.semanticChecks;
 }
-bool getStatistics(const Options& opts) { return opts.base().statistics; }
+bool getStatistics(const Options& opts) { return opts.base.statistics; }
 bool getStatsEveryQuery(const Options& opts)
 {
-  return opts.base().statisticsEveryQuery;
+  return opts.base.statisticsEveryQuery;
 }
 bool getStrictParsing(const Options& opts)
 {
-  return opts.parser().strictParsing;
+  return opts.parser.strictParsing;
 }
 int getTearDownIncremental(const Options& opts)
 {
-  return opts.driver().tearDownIncremental;
+  return opts.driver.tearDownIncremental;
 }
 unsigned long getCumulativeTimeLimit(const Options& opts)
 {
-  return opts.resman().cumulativeMillisecondLimit;
+  return opts.resman.cumulativeMillisecondLimit;
 }
-bool getVersion(const Options& opts) { return opts.driver().version; }
+bool getVersion(const Options& opts) { return opts.driver.version; }
 const std::string& getForceLogicString(const Options& opts)
 {
-  return opts.parser().forceLogicString;
+  return opts.parser.forceLogicString;
 }
-int getVerbosity(const Options& opts) { return opts.base().verbosity; }
+int getVerbosity(const Options& opts) { return opts.base.verbosity; }
 
-std::istream* getIn(const Options& opts) { return opts.base().in; }
-std::ostream* getErr(const Options& opts) { return opts.base().err; }
-std::ostream* getOut(const Options& opts) { return opts.base().out; }
+std::istream* getIn(const Options& opts) { return opts.base.in; }
+std::ostream* getErr(const Options& opts) { return opts.base.err; }
+std::ostream* getOut(const Options& opts) { return opts.base.out; }
 const std::string& getBinaryName(const Options& opts)
 {
-  return opts.base().binary_name;
+  return opts.base.binary_name;
 }
 
 void setInputLanguage(InputLanguage val, Options& opts)
 {
-  opts.base().inputLanguage = val;
+  opts.base.inputLanguage = val;
 }
 void setInteractive(bool val, Options& opts)
 {
-  opts.driver().interactive = val;
+  opts.driver.interactive = val;
 }
-void setOut(std::ostream* val, Options& opts) { opts.base().out = val; }
+void setOut(std::ostream* val, Options& opts) { opts.base.out = val; }
 void setOutputLanguage(OutputLanguage val, Options& opts)
 {
-  opts.base().outputLanguage = val;
+  opts.base.outputLanguage = val;
 }
 
 bool wasSetByUserEarlyExit(const Options& opts)
 {
-  return opts.driver().earlyExit__setByUser__;
+  return opts.driver.earlyExit__setByUser__;
 }
 bool wasSetByUserForceLogicString(const Options& opts)
 {
-  return opts.parser().forceLogicString__setByUser__;
+  return opts.parser.forceLogicString__setByUser__;
 }
 bool wasSetByUserIncrementalSolving(const Options& opts)
 {
-  return opts.smt().incrementalSolving__setByUser__;
+  return opts.smt.incrementalSolving__setByUser__;
 }
 bool wasSetByUserInteractive(const Options& opts)
 {
-  return opts.driver().interactive__setByUser__;
+  return opts.driver.interactive__setByUser__;
 }
 
 }  // namespace cvc5::options
