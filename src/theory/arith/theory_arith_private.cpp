a505418166c0eaf2e8ceddb6c89867b137b5c1bf
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-06-09 20:36:25 +0200

    Remove include

--- src/theory/arith/theory_arith_private.cpp
+++ src/theory/arith/theory_arith_private.cpp
@@ -36,7 +36,6 @@
 #include "expr/skolem_manager.h"
 #include "options/arith_options.h"
 #include "options/base_options.h"
-#include "options/smt_options.h"  // for incrementalSolving()
 #include "preprocessing/util/ite_utilities.h"
 #include "proof/proof_generator.h"
 #include "proof/proof_node_manager.h"
