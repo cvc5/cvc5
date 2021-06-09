9c4e4f6b69dd08575fd762ed927fe436dc557f3e
ajreynol <andrew.j.reynolds@gmail.com>
2021-06-09 18:50:33 -0500

    Fix pass

--- src/preprocessing/passes/bv_to_int.cpp
+++ src/preprocessing/passes/bv_to_int.cpp
@@ -408,6 +408,13 @@ Node BVToInt::translateWithChildren(Node original,
       returnNode = translated_children[0];
       break;
     }
+    case kind::INT_TO_BITVECTOR:
+    {
+      // ((_ int2bv n) t) ---> (mod t n)
+      size_t sz = original.getOperator().getConst<IntToBitVector>().d_size;
+      returnNode = d_nm->mkNode(kind::INTS_MODULUS_TOTAL, translated_children[0], d_nm->mkConst(Rational(sz)));
+    }
+      break;
     case kind::BITVECTOR_AND:
     {
       // We support three configurations:
