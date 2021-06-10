632ff9a108189489e6c73d76fd75e889be58b1a1
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-06-10 16:34:17 -0300

    printing as Diego requested

--- src/proof/dot/dot_printer.cpp
+++ src/proof/dot/dot_printer.cpp
@@ -137,6 +137,9 @@ void DotPrinter::printInternal(
   }
   classes << " \"";
   out << classes.str() << colors.str();
+  // add number of subchildren
+  std::map<const ProofNode*, size_t>::const_iterator it = subpfCounter.find(pn);
+  out << ", comment = \"\{\'subProofQty\':" << it->second  << "}\"";
   out << " ];\n";
 
   for (const std::shared_ptr<ProofNode>& c : children)
