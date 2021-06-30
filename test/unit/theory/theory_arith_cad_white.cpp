4beb4674729f028977110912cad01dfec715bba1
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-06-30 08:36:16 +0200

    Use new header

--- test/unit/theory/theory_arith_cad_white.cpp
+++ test/unit/theory/theory_arith_cad_white.cpp
@@ -1,18 +1,17 @@
-/*********************                                                        */
-/*! \file theory_nl_white.h
- ** \verbatim
- ** Top contributors (to current version):
- **   Gereon Kremer
- ** This file is part of the CVC4 project.
- ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
- ** in the top-level source directory) and their institutional affiliations.
- ** All rights reserved.  See the file COPYING in the top-level source
- ** directory for licensing information.\endverbatim
- **
- ** \brief Unit tests for the CAD module for nonlinear arithmetic
- **
- ** Unit tests for the CAD module for nonlinear arithmetic.
- **/
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Gereon Kremer
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Unit tests for the CAD module for nonlinear arithmetic.
+ */
 
 #include <poly/polyxx.h>
 
