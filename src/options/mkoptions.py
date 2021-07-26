c71d1a473f4861e30a3df1afd29f79ee813199ef
Gereon Kremer <gkremer@cs.stanford.edu>
2021-07-26 15:53:27 -0700

    format

--- src/options/mkoptions.py
+++ src/options/mkoptions.py
@@ -724,7 +724,8 @@ def add_getopt_long(long_name, argument_req, getopt_long):
             'required' if argument_req else 'no', value))
 
 
-def codegen_all_modules(modules, build_dir, dst_dir, tpl_options_h, tpl_options_cpp, tpl_options_public):
+def codegen_all_modules(modules, build_dir, dst_dir, tpl_options_h, tpl_options_cpp,
+                        tpl_options_public):
     """
     Generate code for all option modules (options.cpp).
     """
