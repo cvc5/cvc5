defaeb95d1b56d18a0459356f247539861eb9ef4
Gereon Kremer <gkremer@cs.stanford.edu>
2021-07-30 17:24:55 -0700

    AlwaysAssert -> Assert

--- src/theory/arith/theory_arith.cpp
+++ src/theory/arith/theory_arith.cpp
@@ -324,7 +324,7 @@ bool TheoryArith::collectModelValues(TheoryModel* m,
     {
       continue;
     }
-    AlwaysAssert(false) << "A model equality could not be asserted: " << p.first
+    Assert(false) << "A model equality could not be asserted: " << p.first
                         << " == " << p.second << std::endl;
     // If we failed to assert an equality, it is likely due to theory
     // combination, namely the repaired model for non-linear changed
