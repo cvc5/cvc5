216b8690707bc826a38d3f770f2a4e08176630c0
Gereon Kremer <gkremer@cs.stanford.edu>
2021-08-04 16:18:15 -0700

    Use resetSolver in ResetCommand

--- src/smt/command.cpp
+++ src/smt/command.cpp
@@ -930,15 +930,7 @@ void ResetCommand::invoke(api::Solver* solver, SymbolManager* sm)
   try
   {
     sm->reset();
-    Options opts;
-    opts.copyValues(getOriginalOptionsFrom(solver));
-    // This reconstructs a new solver object at the same memory location as the
-    // current one. Note that this command does not own the solver object!
-    // It may be safer to instead make the ResetCommand a special case in the
-    // CommandExecutor such that this reconstruction can be done within the
-    // CommandExecutor, who actually owns the solver.
-    solver->~Solver();
-    new (solver) api::Solver(&opts);
+    resetSolver(solver);
     d_commandStatus = CommandSuccess::instance();
   }
   catch (exception& e)
