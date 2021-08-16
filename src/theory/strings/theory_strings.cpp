edbd5055006d3565cf658e7333ee23001360c53f
Gereon Kremer <gkremer@cs.stanford.edu>
2021-08-16 08:30:04 -0700

    Fix

--- src/theory/strings/theory_strings.cpp
+++ src/theory/strings/theory_strings.cpp
@@ -58,7 +58,7 @@ TheoryStrings::TheoryStrings(Env& env, OutputChannel& out, Valuation valuation)
       d_eagerSolver(d_state),
       d_termReg(d_state, d_statistics, d_pnm),
       d_extTheoryCb(),
-      d_im(*this, d_state, d_termReg, d_extTheory, d_statistics, pnm),
+      d_im(*this, d_state, d_termReg, d_extTheory, d_statistics, d_pnm),
       d_extTheory(d_extTheoryCb, getSatContext(), getUserContext(), d_im),
       d_rewriter(&d_statistics.d_rewrites),
       d_bsolver(d_state, d_im),
