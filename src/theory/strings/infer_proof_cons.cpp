3ea76867206c44531606efabea57e8f9db474b7f
ajreynol <andrew.j.reynolds@gmail.com>
2021-08-19 10:27:37 -0500

    Fix comment

--- src/theory/strings/infer_proof_cons.cpp
+++ src/theory/strings/infer_proof_cons.cpp
@@ -128,10 +128,8 @@ void InferProofCons::convert(InferenceId infer,
       if (!ps.d_children.empty())
       {
         std::vector<Node> exps(ps.d_children.begin(), ps.d_children.end() - 1);
-        Node psrc = ps.d_children[ps.d_children.size() - 1];
-        // we apply the substitution on the purified form to get the
-        // original conclusion
-        if (psb.applyPredTransform(psrc, conc, exps))
+        Node src = ps.d_children[ps.d_children.size() - 1];
+        if (psb.applyPredTransform(src, conc, exps))
         {
           useBuffer = true;
         }
