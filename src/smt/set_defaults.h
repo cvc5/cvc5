1ed3d2c92dde0a64242fe3aa22f6db4da70aaf06
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2021-08-20 14:19:30 -0500

    More refactoring of set defaults (#7043)
    A few minor changes to which options are enabled for sygus, otherwise no intended changes.

--- src/smt/set_defaults.h
+++ src/smt/set_defaults.h
@@ -47,15 +47,37 @@ class SetDefaults
 
  private:
   /**
+   * Determine whether we will be solving a SyGuS problem.
+   */
+  bool isSygus(const Options& opts) const;
+  /**
+   * Determine whether we will be using SyGuS.
+   */
+  bool usesSygus(const Options& opts) const;
+  /**
+   * Return true if proofs must be disabled. This is the case for any technique
+   * that answers "unsat" without showing a proof of unsatisfiabilty.
+   */
+  bool mustDisableProofs(const Options& opts) const;
+  /**
    * Widen logic to theories that are required, since some theories imply the
    * use of other theories to handle certain operators, e.g. UF to handle
    * partial functions.
    */
-  void widenLogic(LogicInfo& logic, Options& opts);
+  void widenLogic(LogicInfo& logic, Options& opts) const;
+  /**
+   * Set defaults related to quantifiers, called when quantifiers are enabled.
+   * This method modifies opt.quantifiers only.
+   */
+  void setDefaultsQuantifiers(const LogicInfo& logic, Options& opts) const;
   /**
    * Set defaults related to SyGuS, called when SyGuS is enabled.
    */
-  void setDefaultsSygus(Options& opts);
+  void setDefaultsSygus(Options& opts) const;
+  /**
+   * Set default decision mode
+   */
+  void setDefaultDecisionMode(const LogicInfo& logic, Options& opts) const;
   /** Are we an internal subsolver? */
   bool d_isInternalSubsolver;
 };
