1f402481fd9db0ee7333c350d7c8532763e17919
Yoni Zohar <yoni206@gmail.com>
2021-08-22 13:43:43 -0700

    trying to add a test

--- test/unit/theory/theory_bv_int_blaster_white.cpp
+++ test/unit/theory/theory_bv_int_blaster_white.cpp
@@ -21,6 +21,7 @@
 #include "theory/bv/int_blaster.h"
 #include "util/bitvector.h"
 #include "util/rational.h"
+#include "context/context.h"
 
 namespace cvc5 {
 
@@ -39,9 +40,23 @@ class TestTheoryWhiteBvIntblaster : public TestSmtNoFinishInit
     d_smtEngine->finishInit();
     d_true = d_nodeManager->mkConst<bool>(true);
     d_one = d_nodeManager->mkConst<Rational>(Rational(1));
+    d_intBlaster(d_smtEngine->getContext(), options::SolveBVAsIntMode::SUM, 1, false);
   }
   Node d_true;
   Node d_one;
+  IntBlaster d_intBlaster;
 };
+
+TEST_F(TestTheoryWhiteBvIntblaster, bitblaster_constants)
+{
+	std::vector<Node> lemmas;
+	std::map<Node, Node> skolems;
+	BitVector c1(4, 7);
+	Node bv0_4 = d_nodeManager->mkConst<BitVector>(c1);
+	Node result = d_intBlaster->translateNoChildren(c1, lemmas, skolems);
+	Node expected = d_nodeManager->mkConst(Rational(7));
+	ASSERT_EQ(bv0_4, result);
+}
+
 }  // namespace test
 }  // namespace cvc5
