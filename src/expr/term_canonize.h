1b5d3e8fed135469b528f190f1a27ce998109356
ajreynol <andrew.j.reynolds@gmail.com>
2021-08-24 20:01:44 -0500

    Format

--- src/expr/term_canonize.h
+++ src/expr/term_canonize.h
@@ -87,9 +87,9 @@ class TermCanonize
   Node getCanonicalTerm(TNode n,
                         bool apply_torder = false,
                         bool doHoVar = true);
-  /** 
+  /**
    * Same as above but tracks visited map, mapping subterms of n to their
-   * canonical forms. 
+   * canonical forms.
    */
   Node getCanonicalTerm(TNode n,
                         std::map<TNode, Node>& visited,
