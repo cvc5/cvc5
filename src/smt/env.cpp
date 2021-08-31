0932db8230e4ff72d4c894e004a81fecb441ddbe
Gereon Kremer <gkremer@cs.stanford.edu>
2021-08-31 09:41:44 -0700

    Rename

--- src/smt/env.cpp
+++ src/smt/env.cpp
@@ -51,7 +51,7 @@ Env::Env(NodeManager* nm, const Options* opts)
   {
     d_options.copyValues(*opts);
   }
-  d_statisticsRegistry->registerTimer("driver::totalTime").start();
+  d_statisticsRegistry->registerTimer("global::totalTime").start();
   d_resourceManager = std::make_unique<ResourceManager>(*d_statisticsRegistry, d_options);
 }
 
