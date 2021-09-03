c332ef15bb94c3ab2a5d13dcc841e96c0ccc26c6
ajreynol <andrew.j.reynolds@gmail.com>
2021-09-03 09:12:21 -0500

    Split rewriter

--- src/proof/lfsc/lfsc_print_channel.cpp
+++ src/proof/lfsc/lfsc_print_channel.cpp
@@ -18,7 +18,6 @@
 #include <sstream>
 
 #include "proof/lfsc/lfsc_util.h"
-#include "rewriter/rewrite_proof_rule.h"
 
 using namespace cvc5::rewriter;
 
@@ -106,21 +105,6 @@ void LfscPrintChannelOut::printRule(std::ostream& out, const ProofNode* pn)
     out << getLfscRule(args[0]);
     return;
   }
-  else if (pn->getRule() == PfRule::DSL_REWRITE)
-  {
-    const std::vector<Node>& args = pn->getArguments();
-    DslPfRule di;
-    if (rewriter::getDslPfRule(args[0], di))
-    {
-      printDslProofRuleId(out, di);
-    }
-    else
-    {
-      out << "?";
-      Assert(false);
-    }
-    return;
-  }
   // Otherwise, convert to lower case
   std::stringstream ss;
   ss << pn->getRule();
@@ -176,20 +160,7 @@ void LfscPrintChannelPre::printTrust(TNode res, PfRule src)
 
 void LfscPrintChannelPre::printOpenRule(const ProofNode* pn)
 {
-  // if its a DSL rule, remember it
-  if (pn->getRule() == PfRule::DSL_REWRITE)
-  {
-    Node idn = pn->getArguments()[0];
-    DslPfRule di;
-    if (rewriter::getDslPfRule(idn, di))
-    {
-      d_dprs.insert(di);
-    }
-    else
-    {
-      Assert(false);
-    }
-  }
+
 }
 
 const std::unordered_set<DslPfRule>& LfscPrintChannelPre::getDslRewrites() const
