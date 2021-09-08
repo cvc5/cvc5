4b0650bfe0c1df81ad3236def912543510932320
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2021-09-08 15:54:22 -0500

    Improve pre-skolemization, move quantifiers preprocess to own file (#7153)
    This also heavily refactors the preskolemization method (now in QuantifiersPreprocess), in preparation for it being enabled by default. This method previously was doing a tree traversal, it now maintains a visited cache.
    
    It makes minor cleanup to the dependencies of this method.

--- src/smt/env_obj.h
+++ src/smt/env_obj.h
@@ -48,12 +48,12 @@ class EnvObj
    * Rewrite a node.
    * This is a wrapper around theory::Rewriter::rewrite via Env.
    */
-  Node rewrite(TNode node);
+  Node rewrite(TNode node) const;
   /**
    * Extended rewrite a node.
    * This is a wrapper around theory::Rewriter::extendedRewrite via Env.
    */
-  Node extendedRewrite(TNode node, bool aggr = true);
+  Node extendedRewrite(TNode node, bool aggr = true) const;
 
   /** Get the current logic information. */
   const LogicInfo& logicInfo() const;
