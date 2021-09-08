15541c99d182d1fa6f701949ccfdc835b5278d1b
ajreynol <andrew.j.reynolds@gmail.com>
2021-09-08 15:26:36 -0500

    Disable shared selectors when quantifiers are present

--- src/smt/set_defaults.cpp
+++ src/smt/set_defaults.cpp
@@ -701,6 +701,16 @@ void SetDefaults::setDefaultsPost(const LogicInfo& logic, Options& opts) const
 
   // set all defaults in the quantifiers theory, which includes sygus
   setDefaultsQuantifiers(logic, opts);
+  
+  // shared selectors are generally not good to combine with standard
+  // quantifier techniques e.g. E-matching
+  if (options.datatypes.dtSharedSelectorsWasSetByUser)
+  {
+    if (logic.isQuantified() && !usesSygus(opts)
+    {
+      options.datatypes.dtSharedSelectors = false;
+    }
+  }
 
   // until bugs 371,431 are fixed
   if (!opts.prop.minisatUseElimWasSetByUser)
