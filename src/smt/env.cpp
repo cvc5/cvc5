8f885bd555fe3e5ad540c646d0c55550f1b96b44
ajreynol <andrew.j.reynolds@gmail.com>
2021-09-09 10:39:06 -0500

    Env

--- src/smt/env.cpp
+++ src/smt/env.cpp
@@ -86,8 +86,10 @@ bool Env::isSatProofProducing() const
 {
   return d_proofNodeManager != nullptr
          && (!d_options.smt.unsatCores
-             || d_options.smt.unsatCoresMode
-                    != options::UnsatCoresMode::ASSUMPTIONS);
+             || (d_options.smt.unsatCoresMode
+                     != options::UnsatCoresMode::ASSUMPTIONS
+                 && d_options.smt.unsatCoresMode
+                        != options::UnsatCoresMode::PP_ONLY));
 }
 
 bool Env::isTheoryProofProducing() const
