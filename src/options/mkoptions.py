02088737d2845ea00714b6ba6fed8c20485a9645
Gereon Kremer <nafur42@gmail.com>
2021-09-13 17:54:55 +0200

    Refactor options parsing (#7143)
    This PR refactors the code for options parsing.

--- src/options/mkoptions.py
+++ src/options/mkoptions.py
@@ -422,6 +422,82 @@ def generate_holder_mem_copy(modules):
 # stuff for main/options.cpp
 
 
+def _add_cmdoption(option, name, opts, next_id):
+    fmt = {
+        'name': name,
+        'arg': 'no' if option.type in ['bool', 'void'] else 'required',
+        'next_id': next_id
+    }
+    opts.append(
+        '{{ "{name}", {arg}_argument, nullptr, {next_id} }},'.format(**fmt))
+
+
+def generate_parsing(modules):
+    """Generates the implementation for main::parseInternal() and matching
+    options definitions suitable for getopt_long(). Returns a tuple with:
+    - short options description (passed as third argument to getopt_long)
+    - long options description (passed as fourth argument to getopt_long)
+    - handler code that turns getopt_long return value to a setOption call
+    """
+    short = ""
+    opts = []
+    code = []
+    next_id = 256
+    for _, option in all_options(modules, False):
+        needs_impl = False
+        if option.short:  # short option
+            needs_impl = True
+            code.append("case '{0}': // -{0}".format(option.short))
+            short += option.short
+            if option.type not in ['bool', 'void']:
+                short += ':'
+        if option.long:  # long option
+            needs_impl = True
+            _add_cmdoption(option, option.long_name, opts, next_id)
+            code.append('case {}: // --{}'.format(next_id, option.long_name))
+            next_id += 1
+        if option.alias:  # long option aliases
+            needs_impl = True
+            for alias in option.alias:
+                _add_cmdoption(option, alias, opts, next_id)
+                code.append('case {}: // --{}'.format(next_id, alias))
+                next_id += 1
+
+        if needs_impl:
+            # there is some way to call it, add call to solver.setOption()
+            if option.type == 'bool':
+                code.append('  solver.setOption("{}", "true"); break;'.format(
+                    option.long_name))
+            elif option.type == 'void':
+                code.append('  solver.setOption("{}", ""); break;'.format(
+                    option.long_name))
+            else:
+                code.append(
+                    '  solver.setOption("{}", optionarg); break;'.format(
+                        option.long_name))
+
+        if option.alternate:
+            assert option.type == 'bool'
+            # bool option that wants a --no-*
+            needs_impl = False
+            if option.long:  # long option
+                needs_impl = True
+                _add_cmdoption(option, 'no-' + option.long_name, opts, next_id)
+                code.append('case {}: // --no-{}'.format(
+                    next_id, option.long_name))
+                next_id += 1
+            if option.alias:  # long option aliases
+                needs_impl = True
+                for alias in option.alias:
+                    _add_cmdoption(option, 'no-' + alias, opts, next_id)
+                    code.append('case {}: // --no-{}'.format(next_id, alias))
+                    next_id += 1
+            code.append('  solver.setOption("{}", "false"); break;'.format(
+                option.long_name))
+
+    return short, '\n  '.join(opts), '\n    '.join(code)
+
+
 def _cli_help_format_options(option):
     """
     Format short and long options for the cmdline documentation
@@ -809,28 +885,13 @@ def codegen_module(module, dst_dir, tpls):
         write_file(dst_dir, filename, tpl['content'].format(**data))
 
 
-def add_getopt_long(long_name, argument_req, getopt_long):
-    """
-    For each long option we need to add an instance of the option struct in
-    order to parse long options (command-line) with getopt_long. Each long
-    option is associated with a number that gets incremented by one each time
-    we add a new long option.
-    """
-    value = g_getopt_long_start + len(getopt_long)
-    getopt_long.append(
-        TPL_GETOPT_LONG.format(
-            long_get_option(long_name),
-            'required' if argument_req else 'no', value))
-
-
 def codegen_all_modules(modules, build_dir, dst_dir, tpls):
     """Generate code for all option modules."""
 
     headers_module = []      # generated *_options.h header includes
-    getopt_short = []        # short options for getopt_long
-    getopt_long = []         # long options for getopt_long
     options_get_info = []    # code for getOptionInfo()
-    options_handler = []     # option handler calls
+    help_common = []         # help text for all common options
+    help_others = []         # help text for all non-common options
 
     sphinxgen = SphinxGenerator()
 
@@ -845,46 +906,6 @@ def codegen_all_modules(modules, build_dir, dst_dir, tpls):
             argument_req = option.type not in ['bool', 'void']
 
             sphinxgen.add(module, option)
-
-            # Generate options_handler and getopt_long
-            cases = []
-            if option.short:
-                cases.append("case '{0}': // -{0}".format(option.short))
-
-                getopt_short.append(option.short)
-                if argument_req:
-                    getopt_short.append(':')
-
-            if option.long:
-                cases.append(
-                    'case {}: // --{}'.format(
-                        g_getopt_long_start + len(getopt_long),
-                        option.long))
-                add_getopt_long(option.long, argument_req, getopt_long)
-                if option.alias:
-                    for alias in option.alias:
-                        cases.append(
-                            'case {}: // --{}'.format(
-                                g_getopt_long_start + len(getopt_long),
-                                alias))
-                        add_getopt_long(alias, argument_req, getopt_long)
-
-            if cases:
-                if option.type == 'bool':
-                    cases.append(
-                        '  solver.setOption("{}", "true");'.format(option.long_name)
-                        )
-                elif option.type == 'void':
-                    cases.append(
-                        '  solver.setOption("{}", "");'.format(option.long_name))
-                else:
-                    cases.append(
-                        '  solver.setOption("{}", optionarg);'.format(option.long_name))
-
-                cases.append('  break;')
-
-                options_handler.extend(cases)
-
 
             # Generate handlers for setOption/getOption
             if option.long:
@@ -927,30 +948,7 @@ def codegen_all_modules(modules, build_dir, dst_dir, tpls):
                 else:
                     options_get_info.append('if ({}) return OptionInfo{{"{}", {{{alias}}}, false, OptionInfo::VoidInfo{{}}}};'.format(cond, long_get_option(option.long), alias=alias))
 
-            # Add --no- alternative options for boolean options
-            if option.long and option.alternate:
-                cases = []
-                cases.append(
-                    'case {}: // --no-{}'.format(
-                        g_getopt_long_start + len(getopt_long),
-                        option.long))
-
-                add_getopt_long('no-{}'.format(option.long), argument_req,
-                                getopt_long)
-                if option.alias:
-                    for alias in option.alias:
-                        cases.append(
-                            'case {}: // --no-{}'.format(
-                                g_getopt_long_start + len(getopt_long),
-                                alias))
-                        add_getopt_long('no-{}'.format(alias), argument_req,
-                                getopt_long)
-
-                cases.append(
-                        '  solver.setOption("{}", "false");'.format(option.long_name)
-                        )
-                cases.append('  break;')
-                options_handler.extend(cases)
+    short, cmdline_opts, parseinternal = generate_parsing(modules)
 
     help_common, help_others = generate_cli_help(modules)
 
@@ -969,12 +967,13 @@ def codegen_all_modules(modules, build_dir, dst_dir, tpls):
         'getnames_impl': generate_getnames_impl(modules),
         'get_impl': generate_get_impl(modules),
         'set_impl': generate_set_impl(modules),
-        'cmdline_options': '\n  '.join(getopt_long),
         'help_common': help_common,
         'help_others': help_others,
-        'options_handler': '\n    '.join(options_handler),
-        'options_short': ''.join(getopt_short),
         'options_get_info': '\n  '.join(sorted(options_get_info)),
+        # main/options.cpp
+        'cmdoptions_long': cmdline_opts,
+        'cmdoptions_short': short,
+        'parseinternal_impl': parseinternal,
     }
     for tpl in tpls:
         write_file(dst_dir, tpl['output'], tpl['content'].format(**data))
