86460c41713243e0018e3038bcba5d053156b8b6
Andres Noetzli <andres.noetzli@gmail.com>
2021-09-14 14:17:06 -0700

    fix

--- test/regress/run_regression.py
+++ test/regress/run_regression.py
@@ -141,7 +141,7 @@ def run_benchmark(dump, wrapper, scrubber, error_scrubber, cvc5_binary,
     exit_status = None
     if dump:
         dump_args = [
-            '--parse-only', '--output-lang=smt2'
+            '--parse-only', '-o', 'raw-benchmark', '--output-lang=smt2'
         ]
         dump_output, _, _ = run_process(
             bin_args + command_line + dump_args + [benchmark_filename],
