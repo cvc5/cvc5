fe66fad293f940c1f6b69b9b5f2a18785ee1ba2f
ajreynol <andrew.j.reynolds@gmail.com>
2021-09-14 15:11:29 -0500

    Unit tests

--- test/unit/api/solver_black.cpp
+++ test/unit/api/solver_black.cpp
@@ -1467,6 +1467,35 @@ TEST_F(TestApiBlackSolver, getUnsatCoreAndProof)
   ASSERT_NO_THROW(d_solver.getProof());
 }
 
+TEST_F(TestApiBlackSolver, getDifficulty)
+{
+  ASSERT_THROW(d_solver.getDifficulty(), CVC5ApiException);
+  d_solver.setOption("produce-difficulty", "true");
+  ASSERT_THROW(d_solver.getDifficulty(), CVC5ApiException);
+  d_solver.checkSat();
+  ASSERT_NO_THROW(d_solver.getDifficulty());
+}
+
+TEST_F(TestApiBlackSolver, getDifficulty2)
+{
+  d_solver.setOption("produce-difficulty", "true");
+  Sort intSort = d_solver.getIntegerSort();
+  Term x = d_solver.mkConst(intSort, "x");
+  Term zero = d_solver.mkInteger(0);
+  Term ten = d_solver.mkInteger(10);
+  Term f0 = d_solver.mkTerm(GEQ, x, ten);
+  Term f1 = d_solver.mkTerm(GEQ, zero, x);
+  d_solver.checkSat();
+  std::map<Term, Term> dmap
+  ASSERT_NO_THROW(dmap = d_solver.getDifficulty());
+  // difficulty should map assertions to integer values
+  for (const std::pair<Term, Term>& t : dmap)
+  {
+    ASSERT_TRUE(t.first==f0 || t.first==f1);
+    ASSERT_TRUE(t.getKind()==CONST_RATIONAL);
+  }
+}
+
 TEST_F(TestApiBlackSolver, getValue1)
 {
   d_solver.setOption("produce-models", "false");
