e886f56c42f388ad7eb30bc637ccc1c46dc874f2
ajreynol <andrew.j.reynolds@gmail.com>
2021-09-16 16:00:47 -0500

    Fix

--- test/unit/theory/sequences_rewriter_white.cpp
+++ test/unit/theory/sequences_rewriter_white.cpp
@@ -43,7 +43,7 @@ class TestTheoryWhiteSequencesRewriter : public TestSmt
     TestSmt::SetUp();
     Options opts;
     d_rewriter = d_smtEngine->getRewriter();
-    d_seqRewriter.reset(new SequencesRewriter(d_rewriter, nullptr);
+    d_seqRewriter.reset(new SequencesRewriter(d_rewriter, nullptr));
   }
 
   Rewriter* d_rewriter;
