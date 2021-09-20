c8655bf6cf0f4ef02d46b2c35ec1c3fcfa1d555d
Aina Niemetz <aina.niemetz@gmail.com>
2021-09-20 10:42:49 -0700

    Update copyright.

--- src/theory/arrays/type_enumerator.cpp
+++ src/theory/arrays/type_enumerator.cpp
@@ -1,3 +1,17 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Aina Niemetz, Clark Barrett, Morgan Deters
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * An enumerator for arrays.
+ */
 #include "theory/arrays/type_enumerator.h"
 
 #include "expr/array_store_all.h"
