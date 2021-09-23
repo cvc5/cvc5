24676d8051fe3703501ca49d5ce43a9c41f7def4
lachnitt <lachnitt@stanford.edu>
2021-09-22 19:39:24 -0700

    Added Resolution rule to updater

--- src/proof/alethe/alethe_post_processor.cpp
+++ src/proof/alethe/alethe_post_processor.cpp
@@ -389,6 +389,175 @@ bool AletheProofPostprocessCallback::update(Node res,
       return addAletheStep(
           vrule, res, nm->mkNode(kind::SEXPR, d_cl, res), children, {}, *cdp);
     }
+    //================================================= Boolean rules
+    // ======== Resolution
+    // See proof_rule.h for documentation on the RESOLUTION rule. This comment
+    // uses variable names as introduced there.
+    //
+    // In the case that C = (or G1 ... Gn) the result is ambigous. E.g.,
+    //
+    // (cl F1 (or F2 F3))    (cl (not F1))
+    // -------------------------------------- RESOLUTION
+    // (cl (or F2 F3))
+    //
+    // (cl F1 F2 F3)        (cl (not F1))
+    // -------------------------------------- RESOLUTION
+    // (cl F2 F3)
+    //
+    // both (cl (or F2 F3)) and (cl F2 F3) correspond to the same proof node (or
+    // F2 F3). Therefore, the translation has to keep track of the current
+    // resolvent that is then compared to the result. E.g. in the first case
+    // current_resolvent = {(or F2 F3)} indicates that the result is a singleton
+    // clause, in the second current_resolvent = {F2,F3} that it is an or node.
+    //
+    // It is always clear what clauses to add to the current_resolvent, except
+    // for when a child is an assumption or the result of an equality resolution
+    // step. In these cases it might be necessary to add an additional or step.
+    //
+    //
+    // If rule(C1) = ASSUME or rule(C1) = EQ_RESOLVE:
+    //
+    //  If C1 = (or F1 ... Fn) and C2 != (not (or F1 ... Fn)):
+    //
+    //            P1
+    //  ---------------------
+    //   VP1: (cl F1 ... Fn)
+    //
+    //  Otherwise, VP1 = P1
+    //
+    // If rule(C2) = ASSUME or rule(C2) = EQ_RESOLVE:
+    //
+    //  If C2 = (or F1 ... Fn) and C1 != (not (or F1 ... Fn)):
+    //
+    //            P2
+    //  ---------------------
+    //   VP2: (cl F1 ... Fn)
+    //
+    //  Otherwise, VP2 = P2
+    //
+    //
+    // If C = (or G1 ... Gn) and C is not a singleton:
+    //
+    //    VP1           VP2
+    //  ---------------------
+    //     (cl G1 ... Gn)*
+    //
+    // Otherwise, if C = false
+    //
+    //  VP1           VP2
+    // --------------------
+    //    (cl)**
+    //
+    // Otherwise,
+    //
+    //  VP1           VP2
+    // --------------------
+    //    (cl C)***
+    //
+    //  *   the corresponding proof node is (not (and F1 ... Fn))
+    //  **  the corresponding proof node is False
+    //  *** the corresponding proof node is C
+    case PfRule::RESOLUTION:
+    {  // This can be simplified
+      // The only way that or steps can be added is if one child is the negation
+      // of the other, i.e. they resolve to (cl)
+      bool success = true;
+      std::vector<Node> vps = children;
+
+      // Needed to determine if (cl C) or (cl G1 ... Gn) should be added in the
+      // end.
+      std::vector<Node> current_resolvent;
+
+      // If the rule of a child is ASSUME or EQ_RESOLUTION an application of the
+      // or rule might be needed.
+      for (unsigned long int i = 0; i < 2; i++)
+      {
+        if (cdp->getProofFor(children[i])->getRule() == PfRule::ASSUME
+            || cdp->getProofFor(children[i])->getRule() == PfRule::EQ_RESOLVE)
+        {
+          // The current child is not a singleton if its the negation of the
+          // other child.
+          Node child2;
+          if (i == 0)
+          {
+            child2 = children[1];
+          }
+          else
+          {
+            child2 = children[0];
+          }
+          if (children[i].getKind() == kind::OR
+              && children[i] != child2.notNode())
+          {
+            std::vector<Node> clauses;
+            clauses.push_back(d_cl);
+            clauses.insert(
+                clauses.end(), children[i].begin(), children[i].end());
+            vps[i] = d_nm->mkNode(kind::SEXPR, clauses);
+            success &=
+                addAletheStep(vps[i], AletheRule::OR, {children[i]}, {}, *cdp);
+            // If this is the case the literals in C1 are added to the
+            // current_resolvent.
+            current_resolvent.insert(current_resolvent.end(),
+                                     children[i].begin(),
+                                     children[i].end());
+          }
+          else
+          {
+            // Otherwise, the whole clause is added.
+            current_resolvent.push_back(children[i]);
+          }
+        }
+        // For all other rules it is easy to determine if the whole clause or
+        // the literals in the clause should be added. If the node is an or node
+        // add literals otherwise the whole clause.
+        else
+        {
+          if (children[i].getKind() == kind::OR)
+          {
+            current_resolvent.insert(current_resolvent.end(),
+                                     children[i].begin(),
+                                     children[i].end());
+          }
+          else
+          {
+            current_resolvent.push_back(children[i]);
+          }
+        }
+      }
+
+      // The pivot and its negation are deleted from the current_resolvent
+      current_resolvent.erase(std::find(
+          current_resolvent.begin(), current_resolvent.end(), args[1]));
+      current_resolvent.erase(std::find(current_resolvent.begin(),
+                                        current_resolvent.end(),
+                                        args[1].notNode()));
+      // If there is only one element left in current_resolvent C should be
+      // printed as (cl C), otherwise as (cl G1 ... Gn)
+      if (res.getKind() == kind::OR && current_resolvent.size() != 1)
+      {
+        return success &= addAletheStepFromOr(res,
+                                              AletheRule::RESOLUTION,
+                                              vps,
+                                              {},
+                                              *cdp);  //(cl G1 ... Gn)
+      }
+      else if (res == d_nm->mkConst(false))
+      {
+        return success &= addAletheStep(res,
+                                        AletheRule::RESOLUTION,
+                                        d_nm->mkNode(kind::SEXPR, d_cl),
+                                        vps,
+                                        {},
+                                        *cdp);  //(cl)
+      }
+      return success &= addAletheStep(res,
+                                      AletheRule::RESOLUTION,
+                                      d_nm->mkNode(kind::SEXPR, d_cl, res),
+                                      vps,
+                                      {},
+                                      *cdp);  //(cl C)
+    }
     default:
     {
       return addAletheStep(AletheRule::UNDEFINED,
