6b56848d3b09894aa4d987ca2e91a87ff1d022ab
Lachnitt <lachnitt@stanford.edu>
2021-09-23 15:35:32 -0700

    [proofs[ Alethe: Fix Order of Arguments of addAletheStepFromOr (#7237)
    Changes the order of the arguments of addAletheStepFromOr to be consistent with addAletheStep.

--- src/proof/alethe/alethe_post_processor.cpp
+++ src/proof/alethe/alethe_post_processor.cpp
@@ -444,8 +444,8 @@ bool AletheProofPostprocessCallback::addAletheStep(
 }
 
 bool AletheProofPostprocessCallback::addAletheStepFromOr(
-    Node res,
     AletheRule rule,
+    Node res,
     const std::vector<Node>& children,
     const std::vector<Node>& args,
     CDProof& cdp)
