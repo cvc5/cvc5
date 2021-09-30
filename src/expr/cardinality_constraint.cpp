48bbae84334768a2b09b434dd3ae020d99ce5637
ajreynol <andrew.j.reynolds@gmail.com>
2021-09-30 13:30:04 -0500

    Make cardinality constraint a nullary operator

---
+++ src/expr/cardinality_constraint.cpp
@@ -0,0 +1,68 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * The cardinality constraint operator
+ */
+
+#include "expr/cardinality_constraint.h"
+
+#include <iostream>
+
+#include "expr/type_node.h"
+
+namespace cvc5 {
+
+std::ostream& operator<<(std::ostream& out, const CardinalityConstraint& cc) {
+  return out << "fmf.card(" << cc.getType() << ", " << cc.getUpperBound() << ')';
+}
+
+/**
+ * Constructs an emptyset of the specified type. Note that the argument
+ * is the type of the set itself, NOT the type of the elements.
+ */
+CardinalityConstraint::CardinalityConstraint(const TypeNode& setType, const Integer& ub) : d_type(new TypeNode(setType)), d_ubound(ub) {}
+
+CardinalityConstraint::CardinalityConstraint(const CardinalityConstraint& cc) : d_type(new TypeNode(cc.getType())), d_ubound(cc.getUpperBound()) {}
+
+CardinalityConstraint& CardinalityConstraint::operator=(const CardinalityConstraint& cc) {
+  (*d_type) = cc.getType();
+  d_ubound = cc.getUpperBound();
+  return *this;
+}
+
+CardinalityConstraint::~CardinalityConstraint() {}
+
+const TypeNode& CardinalityConstraint::getType() const { return *d_type; }
+
+const TypeNode& CardinalityConstraint::getType() const { return *d_type; }
+
+bool CardinalityConstraint::operator==(const CardinalityConstraint& cc) const
+{
+  return getType() == cc.getType() && getUpperBound()==cc.getUpperBound();
+}
+
+bool CardinalityConstraint::operator!=(const CardinalityConstraint& cc) const { return !(*this == cc); }
+bool CardinalityConstraint::operator<(const CardinalityConstraint& cc) const
+{
+  return getType() < cc.getType();
+}
+
+bool CardinalityConstraint::operator<=(const CardinalityConstraint& cc) const
+{
+  return getType() <= cc.getType();
+}
+
+bool CardinalityConstraint::operator>(const CardinalityConstraint& cc) const { return !(*this <= cc); }
+
+bool CardinalityConstraint::operator>=(const CardinalityConstraint& cc) const { return !(*this < cc); }
+
+}  // namespace cvc5
---
+++ src/expr/cardinality_constraint.h
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * The cardinality constraint operator
+ */
+
+#include "cvc5_public.h"
+
+#ifndef CVC5__EXPR__CARDINALITY_CONSTRAINT_H
+#define CVC5__EXPR__CARDINALITY_CONSTRAINT_H
+
+#include <iosfwd>
+#include <memory>
+
+#include "util/integer.h"
+
+namespace cvc5 {
+
+class TypeNode;
+
+/**
+ * A cardinality constraint, handled in the cardinality extension of the UF
+ * solver, used for finite model finding.
+ */
+class CardinalityConstraint
+{
+ public:
+  /**
+   * Constructs a cardinality constraint of the specified type, which should
+   * be an uninterpreted sort.
+   */
+  CardinalityConstraint(const TypeNode& setType, const Integer& ub);
+  ~CardinalityConstraint();
+  CardinalityConstraint(const CardinalityConstraint& other);
+  CardinalityConstraint& operator=(const CardinalityConstraint& other);
+
+  /** Get the type of the cardinality constraint */
+  const TypeNode& getType() const;
+  /** Get the upper bound value of the cardinality constraint */
+  const Integer& getUpperBound() const;
+  
+  bool operator==(const CardinalityConstraint& cc) const;
+  bool operator!=(const CardinalityConstraint& cc) const;
+  bool operator<(const CardinalityConstraint& cc) const;
+  bool operator<=(const CardinalityConstraint& cc) const;
+  bool operator>(const CardinalityConstraint& cc) const;
+  bool operator>=(const CardinalityConstraint& cc) const;
+
+ private:
+  CardinalityConstraint();
+
+  std::unique_ptr<TypeNode> d_type;
+  const Integer d_ubound;
+};
+
+std::ostream& operator<<(std::ostream& out, const CardinalityConstraint& cc);
+
+}
+
+#endif /* CVC5__EXPR__CARDINALITY_CONSTRAINT_H */
