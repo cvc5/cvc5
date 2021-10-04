b36750e4cddab13fcd6d6e4ba99bfd9cb15244a9
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-10-04 08:50:39 -0500

    refactoring

--- src/api/java/jni/cvc5_OptionInfo.cpp
+++ src/api/java/jni/cvc5_OptionInfo.cpp
@@ -124,36 +124,34 @@ JNIEXPORT jobject JNICALL Java_cvc5_OptionInfo_getValueInfo(JNIEnv* env,
     return ret;
   }
 
-  if (std::holds_alternative<OptionInfo::ValueInfo<bool>>(v))
+  if (std::holds_alternative<OptionInfo::ValueInfo<bool>>(v)
+      || std::holds_alternative<OptionInfo::ValueInfo<std::string>>(v))
   {
-    auto info = std::get<OptionInfo::ValueInfo<bool>>(v);
     jclass valueInfoClass = env->FindClass("cvc5/OptionInfo$ValueInfo");
     jmethodID methodId = env->GetMethodID(
         valueInfoClass,
         "<init>",
         "(Lcvc5/OptionInfo;Ljava/lang/Object;Ljava/lang/Object;)V");
 
-    jobject currentValue = getBooleanObject(env, info.currentValue);
-    jobject defaultValue = getBooleanObject(env, info.defaultValue);
-    jobject ret = env->NewObject(
-        valueInfoClass, methodId, optionInfo, defaultValue, currentValue);
-    return ret;
-  }
+    if (std::holds_alternative<OptionInfo::ValueInfo<bool>>(v))
+    {
+      auto info = std::get<OptionInfo::ValueInfo<bool>>(v);
+      jobject currentValue = getBooleanObject(env, info.currentValue);
+      jobject defaultValue = getBooleanObject(env, info.defaultValue);
+      jobject ret = env->NewObject(
+          valueInfoClass, methodId, optionInfo, defaultValue, currentValue);
+      return ret;
+    }
 
-  if (std::holds_alternative<OptionInfo::ValueInfo<std::string>>(v))
-  {
-    auto info = std::get<OptionInfo::ValueInfo<std::string>>(v);
-    jclass valueInfoClass = env->FindClass("cvc5/OptionInfo$ValueInfo");
-    jmethodID methodId = env->GetMethodID(
-        valueInfoClass,
-        "<init>",
-        "(Lcvc5/OptionInfo;Ljava/lang/Object;Ljava/lang/Object;)V");
-
-    jstring defaultValue = env->NewStringUTF(info.defaultValue.c_str());
-    jstring currentValue = env->NewStringUTF(info.currentValue.c_str());
-    jobject ret = env->NewObject(
-        valueInfoClass, methodId, optionInfo, defaultValue, currentValue);
-    return ret;
+    if (std::holds_alternative<OptionInfo::ValueInfo<std::string>>(v))
+    {
+      auto info = std::get<OptionInfo::ValueInfo<std::string>>(v);
+      jstring defaultValue = env->NewStringUTF(info.defaultValue.c_str());
+      jstring currentValue = env->NewStringUTF(info.currentValue.c_str());
+      jobject ret = env->NewObject(
+          valueInfoClass, methodId, optionInfo, defaultValue, currentValue);
+      return ret;
+    }
   }
 
   if (std::holds_alternative<OptionInfo::NumberInfo<int64_t>>(v)
