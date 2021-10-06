1f0d8fbc54fa3138fae1a74ec0671b932f6e7ef2
lachnitt <lachnitt@stanford.edu>
2021-10-05 18:57:56 -0700

    Format

--- src/proof/alethe/alethe_printer.cpp
+++ src/proof/alethe/alethe_printer.cpp
@@ -34,16 +34,17 @@ AletheProofPrinter::AletheProofPrinter(bool extended) : d_extended(extended)
 }
 
 void AletheProofPrinter::alethePrinter(std::ostream& out,
-                                     std::shared_ptr<ProofNode> pfn)
+                                       std::shared_ptr<ProofNode> pfn)
 {
   Trace("alethe-printer") << "- Print proof in Alethe format. " << std::endl;
 
   // Special handling for the first scope
   // Print assumptions and add them to the list but do not print anchor.
-  for (unsigned long int i = 3, size = pfn->getArguments().size(); i < size;  i++)
+  for (unsigned long int i = 3, size = pfn->getArguments().size(); i < size;
+       i++)
   {
-    Trace("alethe-printer") << "... print assumption " << pfn->getArguments()[i]
-                           << std::endl;
+    Trace("alethe-printer")
+        << "... print assumption " << pfn->getArguments()[i] << std::endl;
     out << "(assume a" << std::to_string(i - 3) << " " << pfn->getArguments()[i]
         << ")\n";
     assumptions[0][pfn->getArguments()[i]] = i - 3;
