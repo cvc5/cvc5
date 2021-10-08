2b03dd747cee899c93979cc6866176f1694d2da4
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-08 15:56:54 -0500

    Fix

--- src/theory/quantifiers/sygus/synth_conjecture.cpp
+++ src/theory/quantifiers/sygus/synth_conjecture.cpp
@@ -628,11 +628,23 @@ bool SynthConjecture::doRefine()
   {
     Trace("cegqi-refine") << "Get model values for skolems..." << std::endl;
     Assert(d_inner_vars.size() == d_ce_sk_vars.size());
-    Assert(!d_ce_sk_var_mvs.empty());
-    Assert(d_ce_sk_var_mvs.size() == d_ce_sk_vars.size());
-    sk_subs.insert(
-        sk_subs.end(), d_ce_sk_var_mvs.begin(), d_ce_sk_var_mvs.end());
-    sk_vars.insert(sk_vars.end(), d_inner_vars.begin(), d_inner_vars.end());
+    if (d_ce_sk_var_mvs.empty())
+    {
+      std::vector<Node> model_values;
+      for (const Node& v : d_ce_sk_vars)
+      {
+        Node mv = getModelValue(v);
+        Trace("cegqi-refine") << "  " << v << " -> " << mv << std::endl;
+        model_values.push_back(mv);
+      }
+      sk_subs.insert(sk_subs.end(), model_values.begin(), model_values.end());
+    }
+    else
+    {
+      Assert(d_ce_sk_var_mvs.size() == d_ce_sk_vars.size());
+      sk_subs.insert(
+          sk_subs.end(), d_ce_sk_var_mvs.begin(), d_ce_sk_var_mvs.end());
+    }
   }
   else
   {
