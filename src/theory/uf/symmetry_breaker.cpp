7d4d0e173e139faacdc8011296fef2766e080119
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-11 09:36:59 -0500

    Format

--- src/theory/uf/symmetry_breaker.cpp
+++ src/theory/uf/symmetry_breaker.cpp
@@ -79,7 +79,8 @@ bool SymmetryBreaker::Template::matchRecursive(TNode t, TNode n) {
   }
 
   if(t.getNumChildren() == 0) {
-    if(!t.isVar()) {
+    if (!t.isVar())
+    {
       Assert(n.isVar());
       Debug("ufsymm:match") << "UFSYMM non-variables, failing match" << endl;
       return false;
