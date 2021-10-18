f4aa711cf0c2930da5ee3ad1924dab807d1f5c7f
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-18 18:51:17 -0500

    Format

--- test/api/issue7000.cpp
+++ test/api/issue7000.cpp
@@ -19,16 +19,16 @@ using namespace cvc5::api;
 
 int main()
 {
-  Solver slv;                                                                   
-  Sort s1 = slv.getIntegerSort();                                               
-  Sort s2 = slv.mkFunctionSort(s1, s1);                                         
-  Sort s3 = slv.getRealSort();                                                  
-  Term t4 = slv.mkPi();                                                         
-  Term t7 = slv.mkConst(s3, "_x5");                                             
-  Term t37 = slv.mkConst(s2, "_x32");                                           
-  Term t59 = slv.mkConst(s2, "_x51");                                           
-  Term t72 = slv.mkTerm(EQUAL, t37, t59);                                       
-  Term t74 = slv.mkTerm(GT, t4, t7);                                            
-  slv.checkEntailed({t72, t74, t72, t72});  
+  Solver slv;
+  Sort s1 = slv.getIntegerSort();
+  Sort s2 = slv.mkFunctionSort(s1, s1);
+  Sort s3 = slv.getRealSort();
+  Term t4 = slv.mkPi();
+  Term t7 = slv.mkConst(s3, "_x5");
+  Term t37 = slv.mkConst(s2, "_x32");
+  Term t59 = slv.mkConst(s2, "_x51");
+  Term t72 = slv.mkTerm(EQUAL, t37, t59);
+  Term t74 = slv.mkTerm(GT, t4, t7);
+  slv.checkEntailed({t72, t74, t72, t72});
   return 0;
 }
