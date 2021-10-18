49e133d0d09fd51296d7f6c7aadc032ed3efa0fc
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-10-18 08:59:11 -0700

    address review

--- cmake/target-graphs.cmake
+++ cmake/target-graphs.cmake
@@ -1,3 +1,18 @@
+###############################################################################
+# Top contributors (to current version):
+#   Gereon Kremer
+#
+# This file is part of the cvc5 project.
+#
+# Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+# in the top-level source directory and their institutional affiliations.
+# All rights reserved.  See the file COPYING in the top-level source
+# directory for licensing information.
+# #############################################################################
+# Provides cmake target target-graphs with generates (png) dependency graphs
+# to visualize the interdependencies of all cmake targets.
+##
+
 get_target_property(APITESTS build-apitests MANUALLY_ADDED_DEPENDENCIES)
 string(REPLACE ";" " " APITESTS "${APITESTS}")
 
