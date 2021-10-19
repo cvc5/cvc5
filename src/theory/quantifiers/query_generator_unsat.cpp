7762e280dce29bc91543266cbd42646c4f9a44c1
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-19 09:37:56 -0500

    More

--- src/theory/quantifiers/query_generator_unsat.cpp
+++ src/theory/quantifiers/query_generator_unsat.cpp
@@ -16,8 +16,9 @@
 
 #include "theory/quantifiers/query_generator_unsat.h"
 
-#include "options/smt_options.h"
 #include "util/random.h"
+#include "options/smt_options.h"
+#include "smt/env.h"
 
 namespace cvc5 {
 namespace theory {
@@ -29,7 +30,7 @@ QueryGeneratorUnsat::QueryGeneratorUnsat(Env& env)
   d_false = NodeManager::currentNM()->mkConst(false);
   // determine the options to use for the verification subsolvers we spawn
   // we start with the provided options
-  d_subOptions.copyValues(options());
+  d_subOptions.copyValues(d_env.getOriginalOptions());
   d_subOptions.smt.produceProofs = true;
 }
 
@@ -49,12 +50,12 @@ bool QueryGeneratorUnsat::addTerm(Node n, std::ostream& out)
   std::unordered_set<size_t> processed;
   std::vector<Node> activeTerms;
   // always start with the new term
-  processed.insert(d_terms.size() - 1);
+  processed.insert(d_terms.size()-1);
   activeTerms.push_back(n);
   bool addSuccess = true;
-  while (true)
+  while(true)
   {
-    Assert(!activeTerms.empty());
+    Assert (!activeTerms.empty());
     // if we just successfully added a term, do a satisfiability check
     if (addSuccess)
     {
@@ -66,13 +67,13 @@ bool QueryGeneratorUnsat::addTerm(Node n, std::ostream& out)
         activeTerms.pop_back();
       }
     }
-    if (processed.size() == d_terms.size())
+    if (processed.size()==d_terms.size())
     {
       break;
     }
     // activeTerms is satisfiable, add a new term
     size_t rindex = getNextRandomIndex(processed);
-    Assert(rindex < d_terms.size());
+    Assert (rindex<d_terms.size());
     activeTerms.push_back(d_terms[rindex]);
     processed.insert(rindex);
     addSuccess = !d_cores.hasSubset(activeTerms);
@@ -85,8 +86,7 @@ bool QueryGeneratorUnsat::addTerm(Node n, std::ostream& out)
   return true;
 }
 
-Result QueryGeneratorUnsat::checkCurrent(const std::vector<Node>& activeTerms,
-                                         std::ostream& out)
+Result QueryGeneratorUnsat::checkCurrent(const std::vector<Node>& activeTerms, std::ostream& out)
 {
   NodeManager* nm = NodeManager::currentNM();
   Node qy = nm->mkAnd(activeTerms);
@@ -94,31 +94,49 @@ Result QueryGeneratorUnsat::checkCurrent(const std::vector<Node>& activeTerms,
   out << "(query " << qy << ")" << std::endl;
   std::unique_ptr<SolverEngine> queryChecker;
   initializeChecker(queryChecker, qy, d_subOptions, logicInfo());
-  Result r = queryChecker->checkSat();
+  Result r;
+  try                            
+  {
+    r = queryChecker->checkSat();
+  }                                                                        
+  catch (const cvc5::Exception& e) {  
+    Trace("sygus-qgen-check") << "EXCEPTION: cvc5::Exception" << std::endl;
+  }
+  catch (const std::invalid_argument& e) { 
+    Trace("sygus-qgen-check") << "EXCEPTION: std::invalid_argument" << std::endl; }
+  /*
+  catch (const OptionException& e)                                             
+  {           
+    Trace("sygus-qgen-check") << "EXCEPTION: OptionException" << std::endl;                                                                 
+  } 
+  catch (const cvc5::RecoverableModalException& e)                             
+  {                           
+    Trace("sygus-qgen-check") << "EXCEPTION: RecoverableModalException" << std::endl;
+  }   
+  */    
   if (r.asSatisfiabilityResult().isSat() == Result::UNSAT)
   {
     // if unsat, get the unsat core
     std::vector<Node> unsatCore;
     getUnsatCoreFromSubsolver(*queryChecker.get(), unsatCore);
-    Assert(!unsatCore.empty());
+    Assert (!unsatCore.empty());
     Trace("sygus-qgen-check") << "...unsat core: " << unsatCore << std::endl;
     d_cores.add(d_false, unsatCore);
   }
   return r;
 }
 
-size_t QueryGeneratorUnsat::getNextRandomIndex(
-    const std::unordered_set<size_t>& processed) const
+size_t QueryGeneratorUnsat::getNextRandomIndex(const std::unordered_set<size_t>& processed) const
 {
-  Assert(!d_terms.empty());
-  Assert(processed.size() < d_terms.size());
-  size_t rindex = Random::getRandom().pick(0, d_terms.size() - 1);
-  while (processed.find(rindex) != processed.end())
+  Assert (!d_terms.empty());
+  Assert (processed.size()<d_terms.size());
+  size_t rindex = Random::getRandom().pick(0, d_terms.size()-1);
+  while (processed.find(rindex)!=processed.end())
   {
     rindex++;
-    if (rindex == d_terms.size())
+    if (rindex==d_terms.size())
     {
-      rindex = 0;
+      rindex=0;
     }
   }
   return rindex;
