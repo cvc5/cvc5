72c96c246a3d8f8d8fb02ab8b617bc91d0ede169
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-19 15:36:06 -0500

    Fix

--- src/theory/arrays/theory_arrays_type_rules.cpp
+++ src/theory/arrays/theory_arrays_type_rules.cpp
@@ -18,9 +18,9 @@
 // for array-constant attributes
 #include "expr/array_store_all.h"
 #include "theory/arrays/theory_arrays_rewriter.h"
-#include "theory/builtin/theory_builtin_type_rules.h"
 #include "theory/type_enumerator.h"
 #include "util/cardinality.h"
+#include "theory/builtin/theory_builtin_type_rules.h"
 
 namespace cvc5 {
 namespace theory {
@@ -250,19 +250,21 @@ bool ArraysProperties::isWellFounded(TypeNode type)
 
 Node ArraysProperties::mkGroundTerm(TypeNode type)
 {
-  if (type.isClosedEnumerable())
+  Assert(type.getKind() == kind::ARRAY_TYPE);
+  TypeNode elemType = type.getArrayConstituentType();
+  Node elem = elemType.mkGroundTerm();
+  if (elem.isConst())
   {
-    return *TypeEnumerator(type);
+    return NodeManager::currentNM()->mkConst(ArrayStoreAll(type, elem));
   }
   // Note the distinction between mkGroundTerm and mkGroundValue. While
-  // an arbitrary value can be obtained by calling the type enumerator here,
+  // an arbitrary value can be obtained by calling the type enumerator here, 
   // that is wrong for types that are not closed enumerable since it may
   // return a term containing values that should not appear in e.g. assertions.
   // For example, arrays whose element type is an uninterpreted sort will
   // incorrectly introduce uninterpreted sort values if this is done.
   // It is current infeasible to construct an ArrayStoreAll with the element
-  // type's mkGroundTerm as an argument, since mkGroundTerm is not necessarily
-  // constant, and array constants require constant arguments currently.
+  // type's mkGroundTerm as an argument when that term is not constant.
   // Thus, we must simply return a fresh Skolem here, using the same utility
   // as that of uninterpreted sorts.
   return builtin::SortProperties::mkGroundTerm(type);
