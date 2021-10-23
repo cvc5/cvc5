91516d11639b9771a702d49e1d5f1a40f4bfcfd0
Gereon Kremer <gkremer@cs.stanford.edu>
2021-10-22 17:22:58 -0700

    Add target for fuzzing with murxla

---
+++ cmake/fuzzing-murxla.cmake
@@ -0,0 +1,53 @@
+###############################################################################
+# Top contributors (to current version):
+#   Gereon Kremer
+#
+# This file is part of the cvc5 project.
+#
+# Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+# in the top-level source directory and their institutional affiliations.
+# All rights reserved.  See the file COPYING in the top-level source
+# directory for licensing information.
+# #############################################################################
+#
+# Find Murxla
+# Murxla_FOUND - system has Murxla
+##
+
+if(NOT IS_DIRECTORY "${CMAKE_BINARY_DIR}/murxla")
+  # this is only necessary while the murxla repository is private
+  add_custom_target(fuzz-murxla
+    COMMAND echo "To enable make fuzz-murxla run"
+    COMMAND echo "git clone git@github.com:murxla/murxla.git"
+  )
+  return()
+endif()
+
+include(ExternalProject)
+
+set(Murxla_COMMIT "afc5744766d6aa61ad5b7ea27007666ac7a5aec2")
+
+add_custom_target(install-for-murxla
+  COMMAND make install DESTDIR=murxla-install
+  DEPENDS cvc5-bin
+)
+
+ExternalProject_Add(
+  Murxla-EP
+  EXCLUDE_FROM_ALL ON
+  ${COMMON_EP_CONFIG}
+  #URL https://github.com/murxla/murxla/archive/${Murxla_COMMIT}.tar.gz
+  #URL_HASH SHA1=da39a3ee5e6b4b0d3255bfef95601890afd80709
+  SOURCE_DIR ${CMAKE_BINARY_DIR}/murxla
+  CMAKE_ARGS
+    -DCMAKE_PREFIX_PATH=${CMAKE_BINARY_DIR}/murxla-install/usr/local/lib/cmake/cvc5/
+    -DENABLE_BITWUZLA=OFF
+    -DENABLE_BOOLECTOR=OFF
+    -DENABLE_YICES=OFF
+  INSTALL_COMMAND ""
+  DEPENDS install-for-murxla
+)
+
+add_custom_target(fuzz-murxla
+  DEPENDS Murxla-EP
+)
