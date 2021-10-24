d9fd500378d4fc2e1f260018c3908f5ae258788b
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-24 18:00:31 -0500

    Format

--- src/smt/assertions.h
+++ src/smt/assertions.h
@@ -58,11 +58,11 @@ class Assertions : protected EnvObj
    */
   void clearCurrent();
   /** refresh
-   * 
+   *
    * Ensures that all global declarations have been processed in the current
    * context. This may trigger substitutions to be added to the top-level
    * substitution and/or formulas added to the current set of assertions.
-   * 
+   *
    * If global declarations are true, this method must be called before
    * processing assertions.
    */
