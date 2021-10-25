d0dc24cd5b4032fbcaa14c011e63a25b57609752
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-10-25 12:57:21 -0500

    Update test/unit/api/java/SolverTest.java

--- test/unit/api/java/SolverTest.java
+++ test/unit/api/java/SolverTest.java
@@ -434,7 +434,8 @@ class SolverTest
     assertThrows(CVC5ApiException.class, () -> slv.mkFloatingPoint(3, 5, t1));
   }
 
-  @Test void mkCardinalityConstraint() throws mkCardinalityConstraint {
+  @Test void mkCardinalityConstraint() throws CVC5ApiException
+  {
     Sort su = d_solver.mkUninterpretedSort("u");
     Sort si = d_solver.getIntegerSort();
     assertDoesNotThrow(() -> d_solver.mkCardinalityConstraint(su, 3));
