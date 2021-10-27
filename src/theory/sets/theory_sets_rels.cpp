afa3ff9b8f53a8a77d4ccc489f7a876bad40f1ed
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-27 10:25:32 -0500

    Fix

--- src/theory/sets/theory_sets_rels.cpp
+++ src/theory/sets/theory_sets_rels.cpp
@@ -1058,21 +1058,20 @@ void TheorySetsRels::check(Theory::Effort level)
 
     std::vector<Node> r1_rep_exps = d_rReps_memberReps_exp_cache[r1_rep];
     std::vector<Node> r2_rep_exps = d_rReps_memberReps_exp_cache[r2_rep];
-    unsigned int r1_tuple_len = r1.getType().getSetElementType().getTupleLength();
-    unsigned int r2_tuple_len = r2.getType().getSetElementType().getTupleLength();
+    size_t r1_tuple_len = r1.getType().getSetElementType().getTupleLength();
+    size_t r2_tuple_len = r2.getType().getSetElementType().getTupleLength();
 
     Kind rk = rel.getKind();
     TypeNode tn = rel.getType().getSetElementType();
     for( unsigned int i = 0; i < r1_rep_exps.size(); i++ ) {
       for( unsigned int j = 0; j < r2_rep_exps.size(); j++ ) {
         std::vector<Node> tuple_elements;
+        tuple_elements.push_back(tn.getDType()[0].getConstructor());
         std::vector<Node> reasons;
         if (rk == kind::JOIN)
         {
-          Node r1_rmost =
-              RelsUtils::nthElementOfTuple(r1_rep_exps[i][0], r1_tuple_len - 1);
-          Node r2_lmost = RelsUtils::nthElementOfTuple(r2_rep_exps[j][0], 0);
-          tuple_elements.push_back(tn.getDType()[0].getConstructor());
+          Node r1_rmost = RelsUtils::nthElementOfTuple( r1_rep_exps[i][0], r1_tuple_len-1 );
+          Node r2_lmost = RelsUtils::nthElementOfTuple( r2_rep_exps[j][0], 0 );
 
           Trace("rels-debug") << "[Theory::Rels] r1_rmost: " << r1_rmost
                               << " of type " << r1_rmost.getType() << std::endl;
@@ -1087,14 +1086,11 @@ void TheorySetsRels::check(Theory::Effort level)
               Node lem = nm->mkNode(kind::OR, eq, eq.negate());
               d_im.addPendingLemma(lem, InferenceId::SETS_RELS_JOIN_ELEM_SPLIT);
             }
-            else
-            {
-              continue;
-            }
+            continue;
           }
           else if (r1_rmost != r2_lmost)
           {
-            reasons.push_back(nm->mkNode(kind::EQUAL, r1_rmost, r2_lmost));
+            reasons.push_back( nm->mkNode(kind::EQUAL, r1_rmost, r2_lmost) );
           }
         }
 
@@ -1115,17 +1111,16 @@ void TheorySetsRels::check(Theory::Effort level)
             tuple_elements.push_back( RelsUtils::nthElementOfTuple( r2_rep_exps[j][0], l ) );
           }
 
-          Node composed_tuple =
-              nm->mkNode(kind::APPLY_CONSTRUCTOR, tuple_elements);
+          Node composed_tuple = nm->mkNode(kind::APPLY_CONSTRUCTOR, tuple_elements);
           Node fact = nm->mkNode(kind::MEMBER, composed_tuple, rel);
           reasons.push_back( r1_rep_exps[i] );
           reasons.push_back( r2_rep_exps[j] );
 
           if( r1 != r1_rep_exps[i][1] ) {
-            reasons.push_back(nm->mkNode(kind::EQUAL, r1, r1_rep_exps[i][1]));
+            reasons.push_back( nm->mkNode(kind::EQUAL, r1, r1_rep_exps[i][1]) );
           }
           if( r2 != r2_rep_exps[j][1] ) {
-            reasons.push_back(nm->mkNode(kind::EQUAL, r2, r2_rep_exps[j][1]));
+            reasons.push_back( nm->mkNode(kind::EQUAL, r2, r2_rep_exps[j][1]) );
           }
           if( isProduct ) {
             sendInfer(
