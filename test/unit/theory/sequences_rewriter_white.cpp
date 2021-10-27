a1bed20250d9129fb0d8c7ce2ab7d5716d9ed6c6
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-27 08:17:32 -0500

    Address, fix

--- test/unit/theory/sequences_rewriter_white.cpp
+++ test/unit/theory/sequences_rewriter_white.cpp
@@ -242,6 +242,15 @@ TEST_F(TestTheoryWhiteSequencesRewriter, rewrite_substr)
   Node res = sr.rewriteSubstr(n);
   ASSERT_EQ(res, empty);
 
+  // (str.substr "A" (+ x 1) x) -> ""
+  n = d_nodeManager->mkNode(
+      kind::STRING_SUBSTR,
+      a,
+      d_nodeManager->mkNode(kind::PLUS, x, d_nodeManager->mkConst(Rational(1))),
+      x);
+  res = sr.rewriteSubstr(n);
+  sameNormalForm(res, empty);
+
   // (str.substr "A" (+ x (str.len s2)) x) -> ""
   n = d_nodeManager->mkNode(
       kind::STRING_SUBSTR,
@@ -250,7 +259,7 @@ TEST_F(TestTheoryWhiteSequencesRewriter, rewrite_substr)
           kind::PLUS, x, d_nodeManager->mkNode(kind::STRING_LENGTH, s)),
       x);
   res = sr.rewriteSubstr(n);
-  ASSERT_EQ(res, empty);
+  sameNormalForm(res, empty);
 
   // (str.substr "A" x y) -> (str.substr "A" x y)
   n = d_nodeManager->mkNode(kind::STRING_SUBSTR, a, x, y);
