6a4cb080354b85705576fb68808df72160a51d38
ajreynol <andrew.j.reynolds@gmail.com>
2021-10-28 16:36:29 -0500

    Fix

--- src/proof/lfsc/lfsc_node_converter.cpp
+++ src/proof/lfsc/lfsc_node_converter.cpp
@@ -226,7 +226,7 @@ Node LfscNodeConverter::postConvert(Node n)
   {
     TypeNode btn = nm->booleanType();
     TypeNode tnv = nm->mkFunctionType(btn, tn);
-    TypeNode btnv = nm->mkFunctionType(btn, btn);
+    TypeNode btnv = nm->mkFunctionType({btn, btn}, btn);
     BitVector bv = n.getConst<BitVector>();
     size_t w = bv.getSize();
     Node ret = getSymbolInternal(k, btn, "bvn");
@@ -352,6 +352,21 @@ Node LfscNodeConverter::postConvert(Node n)
     // currently unsupported
     return n;
   }
+  else if (k == BITVECTOR_BB_TERM)
+  {
+    TypeNode btn = nm->booleanType();
+    // (bbT t1 ... tn) is (bbT t1 (bbT t2 ... (bbT tn emptybv)))
+    // where notice that each bbT has a different type
+    Node curr = getNullTerminator(BITVECTOR_CONCAT, tn);
+    for (size_t i = 0, nchild = n.getNumChildren(); i < nchild; ++i)
+    {
+      TypeNode bvt = nm->mkBitVectorType(i + 1);
+      TypeNode ftype = nm->mkFunctionType({btn, curr.getType()}, bvt);
+      Node bbt = getSymbolInternal(k, ftype, "bbT");
+      curr = nm->mkNode(APPLY_UF, bbt, n[nchild - (i + 1)], curr);
+    }
+    return curr;
+  }
   else if (k == SEP_NIL)
   {
     Node tnn = typeAsNode(convertType(tn));
@@ -415,6 +430,16 @@ Node LfscNodeConverter::postConvert(Node n)
       std::stringstream opName;
       // currently allow subtyping
       opName << "a.";
+      /*
+      if (n.getType().isInteger())
+      {
+        opName << "int.";
+      }
+      else
+      {
+        opName << "real.";
+      }
+      */
       opName << printer::smt2::Smt2Printer::smtKindString(k);
       TypeNode ftype = nm->mkFunctionType({tn, tn}, tn);
       opc = getSymbolInternal(k, ftype, opName.str());
@@ -591,8 +616,8 @@ std::string LfscNodeConverter::getNameForUserName(const std::string& name)
 bool LfscNodeConverter::shouldTraverse(Node n)
 {
   Kind k = n.getKind();
-  // don't convert bound variable list directly
-  if (k == BOUND_VAR_LIST)
+  // don't convert bound variable or instantiation pattern list directly
+  if (k == BOUND_VAR_LIST || k == INST_PATTERN_LIST)
   {
     return false;
   }
@@ -924,6 +949,7 @@ Node LfscNodeConverter::getOperatorOfTerm(Node n, bool macroApply)
     {
       ret = getSymbolInternal(k, ftype, opName.str());
     }
+    // TODO: if parametric, instantiate the parameters?
     // if indexed, apply to index
     if (!indices.empty())
     {
@@ -962,6 +988,16 @@ Node LfscNodeConverter::getOperatorOfTerm(Node n, bool macroApply)
   {
     // currently allow subtyping
     opName << "a.";
+    /*
+    if (n[0].getType().isInteger())
+    {
+      opName << "int.";
+    }
+    else
+    {
+      opName << "real.";
+    }
+    */
   }
   if (k == UMINUS)
   {
