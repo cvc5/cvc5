5f80fc8eb80a91320eb3885c74a4cac5a50243b8
lachnitt <lachnitt@stanford.edu>
2021-10-28 18:44:10 -0700

    Added check if singleton or clause to REORDERING rule

--- src/proof/alethe/alethe_post_processor.cpp
+++ src/proof/alethe/alethe_post_processor.cpp
@@ -1644,7 +1644,26 @@ bool AletheProofPostprocessCallback::update(Node res,
     // This rule is translated according to the clauses pattern.
     case PfRule::REORDERING:
     {
-      return addAletheStepFromOr(AletheRule::REORDER, res, children, {}, *cdp);
+      Node trueNode = nm->mkConst(true);
+      std::vector<Node> new_children = children;
+      if (children[0].getKind() == kind::OR
+          && (args[0] != trueNode || children[0] != args[1]))
+      {
+        std::shared_ptr<ProofNode> childPf = cdp->getProofFor(children[0]);
+        if (childPf->getRule() == PfRule::ASSUME
+            || childPf->getRule() == PfRule::EQ_RESOLVE)
+        {
+          // Add or step
+          std::vector<Node> subterms{d_cl};
+          subterms.insert(
+              subterms.end(), children[0].begin(), children[0].end());
+          Node conclusion = nm->mkNode(kind::SEXPR, subterms);
+          addAletheStep(
+              AletheRule::OR, conclusion, conclusion, {children[0]}, {}, *cdp);
+          new_children[0] = conclusion;
+        }
+      }
+      return addAletheStepFromOr(AletheRule::REORDER, res, new_children, {}, *cdp);
     }
     //================================================= Arithmetic rules
     default:
