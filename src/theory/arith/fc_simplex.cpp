c0d589702608d8dd78b017df208dee76d2f90afa
Gereon Kremer <gkremer@cs.stanford.edu>
2021-11-04 16:46:26 -0700

    Fix

--- src/theory/arith/fc_simplex.cpp
+++ src/theory/arith/fc_simplex.cpp
@@ -737,6 +737,7 @@ Result::Sat FCSimplexDecisionProcedure::dualLike(){
 
     Assert(debugDualLike(
         w, Debug("dualLike"), instance, prevFocusSize, prevErrorSize));
+    Debug("dualLike") << "Error size " << d_errorSize << " (was " << prevErrorSize << ")" << endl;
   }
 
 
