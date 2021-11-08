e398f79c61304378cc736828632a462c74ce39d2
mudathirmahgoub <mudathirmahgoub@gmail.com>
2021-11-08 17:13:27 -0600

    expand bag.choose operator (#7481)
    This PR expands bag.choose operator as a preprocessing step.
    It also refactors the implementation of choose operator for sets

--- src/theory/bags/theory_bags.h
+++ src/theory/bags/theory_bags.h
@@ -52,6 +52,8 @@ class TheoryBags : public Theory
   bool needsEqualityEngine(EeSetupInfo& esi) override;
   /** finish initialization */
   void finishInit() override;
+  /** preprocess rewrite */
+  TrustNode ppRewrite(TNode atom, std::vector<SkolemLemma>& lems) override;
   //--------------------------------- end initialization
 
   //--------------------------------- standard check
@@ -86,6 +88,10 @@ class TheoryBags : public Theory
    private:
     TheoryBags& d_theory;
   };
+
+  /** expand the definition of the bag.choose operator */
+  TrustNode expandChooseOperator(const Node& node,
+                                 std::vector<SkolemLemma>& lems);
 
   /** The state of the bags solver at full effort */
   SolverState d_state;
