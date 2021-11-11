7867526e7070de52db36b1a2988d31ebadecf8b0
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2021-11-11 15:39:37 -0600

    Generalize front-end checks to check for shadowed variables (#7634)
    When using the API, we currently check for free variables in assertions and in terms passed to get-value.
    
    We also require checking for variable shadowing.
    
    This generalizes the utility method so that both free variables and shadowed variables are checked and reported to the user.

--- src/smt/assertions.cpp
+++ src/smt/assertions.cpp
@@ -190,16 +190,19 @@ void Assertions::addFormula(TNode n,
   // Ensure that it does not contain free variables
   if (maybeHasFv)
   {
-    if (expr::hasFreeVar(n))
+    bool wasShadow = false;
+    if (expr::hasFreeOrShadowedVar(n, wasShadow))
     {
+      std::string varType(wasShadow ? "shadowed" : "free");
       std::stringstream se;
       if (isFunDef)
       {
-        se << "Cannot process function definition with free variable.";
+        se << "Cannot process function definition with " << varType
+           << " variable.";
       }
       else
       {
-        se << "Cannot process assertion with free variable.";
+        se << "Cannot process assertion with " << varType << " variable.";
         if (language::isLangSygus(options().base.inputLanguage))
         {
           // Common misuse of SyGuS is to use top-level assert instead of
