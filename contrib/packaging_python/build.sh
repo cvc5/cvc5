a90287acdc740819d854afc446c84ceaf4057573
Gereon Kremer <gkremer@cs.stanford.edu>
2021-11-19 12:07:36 -0800

    Pass proper arguments

--- contrib/packaging_python/build.sh
+++ contrib/packaging_python/build.sh
@@ -4,10 +4,12 @@ set -xe
 
 echo VERSION_SUFFIX: "$1"
 
-VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp36-cp36m/bin/python
-VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp37-cp37m/bin/python
-VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp38-cp38/bin/python
-VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp39-cp39/bin/python
-VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp310-cp310/bin/python
+OPTS="production --auto-download"
+
+VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp36-cp36m/bin/python "$OPTS"
+VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp37-cp37m/bin/python "$OPTS"
+VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp38-cp38/bin/python "$OPTS"
+VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp39-cp39/bin/python "$OPTS"
+VERSION_SUFFIX="$1" ./contrib/packaging_python/create_wheel.sh /opt/python/cp310-cp310/bin/python "$OPTS"
 
 cp dist*/wheelhouse/*.whl ../build
