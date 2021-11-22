c8078965a8691d000f5152838bffdfdb200b2dd8
Lachnitt <lachnitt@stanford.edu>
2021-11-22 13:57:52 -0800

    Update src/proof/alethe/alethe_printer.h
    Co-authored-by: Haniel Barbosa <hanielbbarbosa@gmail.com>

--- src/proof/alethe/alethe_printer.h
+++ src/proof/alethe/alethe_printer.h
@@ -52,7 +52,7 @@ class AletheProofPrinter
    * The initial anchor introduces the initial assumptions of the problem, which
    * correspond to the problem assertions.
    */
-  std::string alethePrinterInternal(std::ostream& out,
+  std::string printIntenal(std::ostream& out,
                                     std::shared_ptr<ProofNode> pfn);
   /** The current level of nesting, which increases if a subproof is entered */
   int nested_level;
