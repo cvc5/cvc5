391f5e5a63474100e0178150d51e7ac1b1855b16
lachnitt <lachnitt@stanford.edu>
2021-11-29 17:44:14 -0800

    Improved comments

--- src/proof/alethe/alethe_post_processor.h
+++ src/proof/alethe/alethe_post_processor.h
@@ -51,13 +51,30 @@ class AletheProofPostprocessCallback : public ProofNodeUpdaterCallback
               const std::vector<Node>& args,
               CDProof* cdp,
               bool& continueUpdate) override;
-
+  /**
+   * This method is used to add an additional application of the or-rule between
+   * a conclusion (cl (or F1 ... Fn)) and a rule that uses this conclusion as
+   * premise and treats it as a clause, i.e. assumes that it has to be printed
+   * as (cl F1 ... Fn).
+   */
   bool finalize(Node res,
                 PfRule id,
                 const std::vector<Node>& children,
                 const std::vector<Node>& args,
                 CDProof* cdp) override;
-
+  /**
+   * This method is used to add the last step to a
+   * proof when this is necessary. The last step should always be printed as
+   * (cl).
+   *
+   * 1. If the last step of a proof which is false is reached it is printed as
+   * (cl false).
+   * 2. If one of the assumptions is false it is printed as false.
+   *
+   * Thus, an additional resolution step with (cl (not true)) has to be added to
+   * transfer (cl false) into (cl).
+   *
+   */
   bool finalStep(Node res,
                  PfRule id,
                  const std::vector<Node>& children,
