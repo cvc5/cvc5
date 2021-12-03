040fb1ebb534a89fa3ccb4a31f519a2c4a184e87
ajreynol <andrew.j.reynolds@gmail.com>
2021-12-03 11:44:50 -0600

    Format

--- test/unit/api/cpp/solver_black.cpp
+++ test/unit/api/cpp/solver_black.cpp
@@ -2642,21 +2642,23 @@ TEST_F(TestApiBlackSolver, issue5893)
   ASSERT_NO_FATAL_FAILURE(distinct.getOp());
 }
 
-TEST_F(TestApiBlackSolver, proj-issue373)                                                 
-{                                                                               
-  Sort s1 = d_solver.getRealSort();                                             
-                                                                                                                                                                                        
-  DatatypeConstructorDecl ctor13 = d_solver.mkDatatypeConstructorDecl("_x115"); 
-  ctor13.addSelector("_x109", s1);                                              
-  Sort s4 = d_solver.declareDatatype("_x86", {ctor13});                                                                
-                                                                                
-  Term t452 = d_solver.mkVar(s1, "_x281");                                      
-  Term bvl = d_solver.mkTerm(d_solver.mkOp(VARIABLE_LIST), {t452});             
-  Term acons = d_solver.mkTerm(d_solver.mkOp(APPLY_CONSTRUCTOR),                
-                               {s4.getDatatype().getConstructorTerm("_x115"),   
-                               t452});                                          
+TEST_F(TestApiBlackSolver, proj - issue373)
+{
+  Sort s1 = d_solver.getRealSort();
+
+  DatatypeConstructorDecl ctor13 = d_solver.mkDatatypeConstructorDecl("_x115");
+  ctor13.addSelector("_x109", s1);
+  Sort s4 = d_solver.declareDatatype("_x86", {ctor13});
+
+  Term t452 = d_solver.mkVar(s1, "_x281");
+  Term bvl = d_solver.mkTerm(d_solver.mkOp(VARIABLE_LIST), {t452});
+  Term acons =
+      d_solver.mkTerm(d_solver.mkOp(APPLY_CONSTRUCTOR),
+                      {s4.getDatatype().getConstructorTerm("_x115"), t452});
   // type exception
-  ASSERT_THROW(d_solver.mkTerm(d_solver.mkOp(APPLY_CONSTRUCTOR), {bvl, acons, t452}), CVC5ApiException);
+  ASSERT_THROW(
+      d_solver.mkTerm(d_solver.mkOp(APPLY_CONSTRUCTOR), {bvl, acons, t452}),
+      CVC5ApiException);
 }
 
 }  // namespace test
