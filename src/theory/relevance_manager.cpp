7e32b9f79cd3a84eb7e25cd90f19846e53a20665
ajreynol <andrew.j.reynolds@gmail.com>
2021-12-08 09:14:25 -0600

    Format

--- src/theory/relevance_manager.cpp
+++ src/theory/relevance_manager.cpp
@@ -335,7 +335,8 @@ int RelevanceManager::justify(TNode n)
             {
               d_rsetExp[cur.first] = n;
               Trace("rel-manager-exp")
-                  << "Reason for " << cur.first << " is " << n << ", polarity is " << hasPol << "/" << pol << std::endl;
+                  << "Reason for " << cur.first << " is " << n
+                  << ", polarity is " << hasPol << "/" << pol << std::endl;
             }
           }
         }
