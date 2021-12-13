054eaf2753e3cdd61f2ca9dc81a98320cd4b4451
ajreynol <andrew.j.reynolds@gmail.com>
2021-12-13 12:08:41 -0600

    Format

--- src/theory/strings/strategy.cpp
+++ src/theory/strings/strategy.cpp
@@ -105,7 +105,8 @@ void Strategy::initializeStrategy()
     addStrategyStep(CHECK_INIT);
     addStrategyStep(CHECK_CONST_EQC);
     addStrategyStep(CHECK_EXTF_EVAL, 0);
-    if (options().strings.stringSeqUpdate == options::StringSeqUpdateMode::EAGER)
+    if (options().strings.stringSeqUpdate
+        == options::StringSeqUpdateMode::EAGER)
     {
       addStrategyStep(CHECK_SEQUENCES_ARRAY_EAGER);
     }
@@ -138,8 +139,7 @@ void Strategy::initializeStrategy()
     {
       addStrategyStep(CHECK_LENGTH_EQC);
     }
-    if (options().strings.stringSeqUpdate
-        != options::StringSeqUpdateMode::NONE)
+    if (options().strings.stringSeqUpdate != options::StringSeqUpdateMode::NONE)
     {
       addStrategyStep(CHECK_SEQUENCES_ARRAY_CONCAT);
       addStrategyStep(CHECK_SEQUENCES_ARRAY);
