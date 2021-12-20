38917a5e65ac61eb2ab3523de35e5c5265f9e6f0
Gereon Kremer <gkremer@stanford.edu>
2021-12-20 14:51:06 -0800

    Apply suggestions from code review
    Co-authored-by: Andres Noetzli <andres.noetzli@gmail.com>

--- test/unit/api/cpp/theory_arith_nl_black.cpp
+++ test/unit/api/cpp/theory_arith_nl_black.cpp
@@ -28,7 +28,10 @@ class TestTheoryBlackArithNl : public TestApi
 
 TEST_F(TestTheoryBlackArithNl, cvc5Projects388)
 {
-  if (!Configuration::isBuiltWithPoly()) return;
+  if (!Configuration::isBuiltWithPoly())
+  {
+    return;
+  }
   Solver slv;
   slv.setLogic("QF_NRA");
   Sort s = slv.getRealSort();
@@ -47,7 +50,10 @@ TEST_F(TestTheoryBlackArithNl, cvc5Projects388)
 
 TEST_F(TestTheoryBlackArithNl, cvc5Projects388Min)
 {
-  if (!Configuration::isBuiltWithPoly()) return;
+  if (!Configuration::isBuiltWithPoly())
+  {
+    return;
+  }
   Solver slv;
   slv.setOption("nl-cad", "true");
   slv.setOption("nl-cad-var-elim", "true");
