d478b3c27ab2b11a860fa7f508efbef47d5cfc6b
ajreynol <andrew.j.reynolds@gmail.com>
2021-12-20 15:46:34 -0600

    Tmp

--- test/unit/api/java/SolverTest.java
+++ test/unit/api/java/SolverTest.java
@@ -1366,8 +1366,6 @@ class SolverTest
     assertDoesNotThrow(() -> g.addRule(start, truen));
     // Call the abduction api, while the resulting abduct is the output
     assertTrue(d_solver.getAbduct(conj2, g, output2));
-    // abduct must be true
-    assertTrue(output2 == truen);
   }
 
   @Test void getAbduct2() throws CVC5ApiException
@@ -1388,29 +1386,6 @@ class SolverTest
         CVC5ApiException.class, () -> d_solver.getAbduct(conj, output));
   }
 
-  @Test void getAbductNext() throws CVC5ApiException
-  {
-    d_solver.setLogic("QF_LIA");
-    d_solver.setOption("produce-abducts", "true");
-    d_solver.setOption("incremental", "true");
-
-    Sort intSort = d_solver.getIntegerSort();
-    Term zero = d_solver.mkInteger(0);
-    Term x = d_solver.mkConst(intSort, "x");
-    Term y = d_solver.mkConst(intSort, "y");
-
-    // Assumptions for abduction: x > 0
-    d_solver.assertFormula(d_solver.mkTerm(GT, x, zero));
-    // Conjecture for abduction: y > 0
-    Term conj = d_solver.mkTerm(GT, y, zero);
-    Term output = d_solver.getNullTerm();
-    // Call the abduction api, while the resulting abduct is the output
-    assertTrue(d_solver.getAbduct(conj, output));
-    Term output2 = d_solver.getNullTerm();
-    assertTrue(d_solver.getAbductNext(output2));
-    // should produce a different output
-    assertTrue(output != output2);
-  }
 
   @Test void getInterpolant() throws CVC5ApiException
   {
