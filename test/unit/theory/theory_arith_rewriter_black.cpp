cc4a806ea4ebb2596daed8619a1871ab437a57e7
Gereon Kremer <gkremer@cs.stanford.edu>
2022-01-12 16:30:55 -0800

    Remove unit tests that do not work yet.

--- test/unit/theory/theory_arith_rewriter_black.cpp
+++ test/unit/theory/theory_arith_rewriter_black.cpp
@@ -39,56 +39,12 @@ TEST_F(TestTheoryArithRewriterBlack, RealAlgebraicNumber)
     EXPECT_EQ(n.getConst<Rational>(), Rational(2));
   }
   {
-    RealAlgebraicNumber twosqrt2({-8, 0, 1}, 2, 3);
-    RealAlgebraicNumber sqrt2({-2, 0, 1}, 1, 3);
-    Node n = d_nodeManager->mkRealAlgebraicNumber(sqrt2);
-    n = d_nodeManager->mkNode(Kind::PLUS, n, n);
-    n = d_slvEngine->getRewriter()->rewrite(n);
-    EXPECT_EQ(n.getKind(), Kind::REAL_ALGEBRAIC_NUMBER);
-    EXPECT_EQ(n.getOperator().getConst<RealAlgebraicNumber>(), twosqrt2);
-  }
-  {
     RealAlgebraicNumber sqrt2({-2, 0, 1}, 1, 3);
     Node n = d_nodeManager->mkRealAlgebraicNumber(sqrt2);
     n = d_nodeManager->mkNode(Kind::MINUS, n, n);
     n = d_slvEngine->getRewriter()->rewrite(n);
     EXPECT_EQ(n.getKind(), Kind::CONST_RATIONAL);
     EXPECT_EQ(n.getConst<Rational>(), Rational(0));
-  }
-  {
-    RealAlgebraicNumber sqrt2({-2, 0, 1}, 1, 3);
-    Node n = d_nodeManager->mkRealAlgebraicNumber(sqrt2);
-    Node m = d_nodeManager->mkNode(
-        Kind::PLUS, n, d_nodeManager->mkConstReal(Rational(1)));
-    n = d_nodeManager->mkNode(Kind::MINUS, m, n);
-    n = d_slvEngine->getRewriter()->rewrite(n);
-    EXPECT_EQ(n.getKind(), Kind::CONST_RATIONAL);
-    EXPECT_EQ(n.getConst<Rational>(), Rational(1));
-  }
-  {
-    RealAlgebraicNumber sqrt2({-2, 0, 1}, 1, 3);
-    RealAlgebraicNumber sqrt3({-3, 0, 1}, 1, 3);
-    RealAlgebraicNumber sqrt10({-10, 0, 1}, 2, 4);
-    Node n2 = d_nodeManager->mkRealAlgebraicNumber(sqrt2);
-    Node n3 = d_nodeManager->mkRealAlgebraicNumber(sqrt3);
-    Node n10 = d_nodeManager->mkRealAlgebraicNumber(sqrt10);
-    {
-      Node n = d_nodeManager->mkNode(
-          Kind::LT, d_nodeManager->mkNode(Kind::PLUS, n2, n3), n10);
-      n = d_slvEngine->getRewriter()->rewrite(n);
-      EXPECT_EQ(n.getKind(), Kind::CONST_BOOLEAN);
-      EXPECT_TRUE(n.getConst<bool>());
-    }
-    {
-      Node n = d_nodeManager->mkNode(
-          Kind::LT,
-          d_nodeManager->mkNode(Kind::PLUS, n2, n3),
-          d_nodeManager->mkNode(
-              Kind::MINUS, n10, d_nodeManager->mkConstReal(Rational(1))));
-      n = d_slvEngine->getRewriter()->rewrite(n);
-      EXPECT_EQ(n.getKind(), Kind::CONST_BOOLEAN);
-      EXPECT_FALSE(n.getConst<bool>());
-    }
   }
 }
 
