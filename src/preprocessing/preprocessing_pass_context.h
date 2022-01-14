8ce7de266e731558fca38409ab025548867960c4
ajreynol <andrew.j.reynolds@gmail.com>
2022-01-14 09:10:49 -0600

    More

--- src/preprocessing/preprocessing_pass_context.h
+++ src/preprocessing/preprocessing_pass_context.h
@@ -79,7 +79,11 @@ class PreprocessingPassContext : protected EnvObj
   /** Spend resource in the resource manager of the associated Env. */
   void spendResource(Resource r);
 
-  /** Get a reference to the top-level substitution map */
+  /**
+   * Get a reference to the top-level substitution map. Note that all
+   * substitutions added to this map should use the addSubstitution methods
+   * below for the purposes of proper debugging information.
+   */
   theory::TrustSubstitutionMap& getTopLevelSubstitutions() const;
 
   /** Record symbols in assertions
