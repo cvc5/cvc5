0b628b74ec0006fbc904813b89d7c3fbf78ee07f
ajreynol <andrew.j.reynolds@gmail.com>
2022-01-14 11:53:34 -0600

    More

--- src/theory/datatypes/sygus_extension.cpp
+++ src/theory/datatypes/sygus_extension.cpp
@@ -691,7 +691,7 @@ Node SygusExtension::getSimpleSymBreakPred(Node e,
 
   // if we are the "any constant" constructor, we do no symmetry breaking
   // only do simple symmetry breaking up to depth 2
-  bool doSymBreak = options().datatypes.sygusSymBreak;
+  bool doSymBreak = options().datatypes.sygusSimpleSymBreak;
   if (isAnyConstant || depth > 2)
   {
     doSymBreak = false;
