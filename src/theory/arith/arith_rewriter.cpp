64dcc5540516d5749336f671f07c1c240ee17b8d
Gereon Kremer <gkremer@cs.stanford.edu>
2022-01-19 10:05:21 -0800

    Add helper functions

--- src/theory/arith/arith_rewriter.cpp
+++ src/theory/arith/arith_rewriter.cpp
@@ -42,6 +42,33 @@ namespace arith {
 
 namespace {
 
+/** Make a nonlinear multiplication from the given factors */
+template<typename T>
+Node mkMult(T&& factors)
+{
+  auto* nm = NodeManager::currentNM();
+  switch (factors.size())
+  {
+    case 0: return nm->mkConstInt(Rational(1));
+    case 1: return factors[0];
+    default: return nm->mkNode(Kind::NONLINEAR_MULT, std::forward<T>(factors));
+  }
+}
+
+/** Make a sum from the given summands */
+template<typename T>
+Node mkSum(T&& summands)
+{
+  auto* nm = NodeManager::currentNM();
+  switch (summands.size())
+  {
+    case 0: return nm->mkConstInt(Rational(0));
+    case 1: return summands[0];
+    default: return nm->mkNode(Kind::PLUS, std::forward<T>(summands));
+  }
+}
+
+/** Evaluate the given relation based on values l and r */
 template <typename L, typename R>
 bool evaluateRelation(Kind rel, const L& l, const R& r)
 {
