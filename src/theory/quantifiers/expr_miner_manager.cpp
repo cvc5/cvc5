dd9f57ea7072656a597a895f9957bc5d48fe1d41
ajreynol <andrew.j.reynolds@gmail.com>
2022-01-21 14:16:38 -0600

    Improvements to query generation

--- src/theory/quantifiers/expr_miner_manager.cpp
+++ src/theory/quantifiers/expr_miner_manager.cpp
@@ -177,7 +177,7 @@ bool ExpressionMinerManager::addTerm(Node sol,
   Node solb = sol;
   if (d_use_sygus_type)
   {
-    solb = d_tds->sygusToBuiltin(sol);
+    solb = datatypes::utils::sygusToBuiltin(sol, true);//d_tds->sygusToBuiltin(sol);
   }
 
   // add to the candidate rewrite rule database
