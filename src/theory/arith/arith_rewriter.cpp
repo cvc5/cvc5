dd38c894ad653aab595f999a4e8cbbd16c429605
ajreynol <andrew.j.reynolds@gmail.com>
2022-02-02 09:12:47 -0600

    Format

--- src/theory/arith/arith_rewriter.cpp
+++ src/theory/arith/arith_rewriter.cpp
@@ -747,7 +747,8 @@ RewriteResponse ArithRewriter::postRewriteIAnd(TNode t)
       // ((_ iand k) 0 y) ---> 0
       return RewriteResponse(REWRITE_DONE, t[i]);
     }
-    // Note that ((_ iand k) 111...1 y) ---> y is not a valid rewrite since it assumes that y is in the bounds [0, 2^k-1]
+    // Note that ((_ iand k) 111...1 y) ---> y is not a valid rewrite since it
+    // assumes that y is in the bounds [0, 2^k-1]
   }
   return RewriteResponse(REWRITE_DONE, t);
 }
