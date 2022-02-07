010f93252d50248a9dba208d02c49a214d6bb440
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2022-02-07 14:57:26 -0800

    build all the versions

--- contrib/packaging_python/build.sh
+++ contrib/packaging_python/build.sh
@@ -2,15 +2,13 @@
 
 set -xe
 
-echo VERSION_SUFFIX: "$2"
-
 OPTS="production --auto-download"
 
 pwd
 
 echo $PATH
 
-for version in "36"
+for version in $1
 do
     VERSION_SUFFIX="$2" ./contrib/packaging_python/create_wheel.sh /opt/python/cp${version}-cp${version}m/bin/python "$OPTS"
 done
