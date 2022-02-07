70b7e72f92467fa79ad52733cfdd3625e1ee10b6
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2022-02-07 15:07:59 -0800

    inline build.sh

--- contrib/packaging_python/build.sh
+++ contrib/packaging_python/build.sh
@@ -4,10 +4,6 @@ set -xe
 
 OPTS="production --auto-download"
 
-pwd
-
-echo $PATH
-
 for version in $1
 do
     VERSION_SUFFIX="$2" ./contrib/packaging_python/create_wheel.sh /opt/python/cp${version}-cp${version}m/bin/python "$OPTS"
