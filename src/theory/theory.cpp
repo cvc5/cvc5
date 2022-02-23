693b6af13428de01bb23717bdafe49319b7c2ba9
ajreynol <andrew.j.reynolds@gmail.com>
2022-02-23 10:14:42 -0600

    Revert

--- src/theory/theory.cpp
+++ src/theory/theory.cpp
@@ -339,11 +339,21 @@ bool Theory::isLegalElimination(TNode x, TNode val)
   {
     return false;
   }
-  // Notice that val may contain unevaluatable kinds, but we return true
-  // regardless. Notice that means that e.g. a Boolean variable may be
-  // eliminated based on an equality (= b (forall ((x)) (P x))), where its
-  // model value is (forall ((x)) (P x)).
-  return true;
+  if (!options().smt.produceModels || options().smt.modelVarElimUneval)
+  {
+    // Don't care about the model, or we allow variables to be eliminated by unevaluatable terms, we can eliminate.
+    return true;
+  }
+  // If models are enabled, then it depends on whether the term contains any
+  // unevaluable operators like FORALL, SINE, etc. Having such operators makes
+  // model construction contain non-constant values for variables, which is
+  // not ideal from a user perspective.
+  // We also insist on this check since the term to eliminate should never
+  // contain quantifiers, or else variable shadowing issues may arise.
+  // there should be a model object
+  TheoryModel* tm = d_valuation.getModel();
+  Assert(tm != nullptr);
+  return tm->isLegalElimination(x, val);
 }
 
 std::unordered_set<TNode> Theory::currentlySharedTerms() const
