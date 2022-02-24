839867458ace1977f5c6fea9422bf66955862433
Gereon Kremer <gkremer@cs.stanford.edu>
2022-02-24 09:28:49 -0800

    Add approximation scheme for pi

---
+++ src/theory/arith/nl/transcendental/approximate_pi.cpp
@@ -0,0 +1,56 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Gereon Kremer
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Incremental approximation scheme for pi.
+ */
+
+#include "theory/arith/nl/transcendental/approximate_pi.h"
+
+#include "base/check.h"
+
+namespace cvc5 {
+namespace theory {
+namespace arith {
+namespace nl {
+namespace transcendental {
+
+ApproximatePi::ApproximatePi()
+{
+    for (uint64_t i = 0; i < s_initial_refinement; ++i)
+    {
+        refine();
+    }
+}
+
+void ApproximatePi::refine()
+{
+    Assert(d_next_k < 1<<28);
+
+    Integer k(d_next_k);
+
+    Integer num = Integer(120) * k.pow(2) + Integer(151) * k + Integer(47);
+    Integer den = Integer(512) * k.pow(4) + Integer(1024) * k.pow(3) + Integer(712) * k.pow(2) + Integer(194) * k + Integer(15);
+    den = den.multiplyByPow2(d_next_k * 4);
+
+    Rational summand(num, den);
+
+    d_lower += summand;
+    d_upper = d_lower + Rational(Integer(1), Integer(1).multiplyByPow2(d_next_k * 4));
+
+    ++d_next_k;
+}
+
+}  // namespace transcendental
+}  // namespace nl
+}  // namespace arith
+}  // namespace theory
+}  // namespace cvc5
---
+++ src/theory/arith/nl/transcendental/approximate_pi.h
@@ -0,0 +1,65 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Gereon Kremer
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Incremental approximation scheme for pi.
+ */
+
+#ifndef CVC5__THEORY__ARITH__NL__TRANSCENDENTAL__APPROXIMATE_PI_H
+#define CVC5__THEORY__ARITH__NL__TRANSCENDENTAL__APPROXIMATE_PI_H
+
+#include "util/integer.h"
+#include "util/rational.h"
+
+namespace cvc5 {
+namespace theory {
+namespace arith {
+namespace nl {
+namespace transcendental {
+
+/**
+ * Utility to incrementally approximate pi and provide safe lower and upper
+ * bounds. The implementation is based on the Bailey-Borwein-Plouffe formula
+ * in the following formulation:
+ *             (  1            120k^2 + 151k + 47             )
+ * pi = \sum_k ( ---- * ------------------------------------- )
+ *             ( 16^k   512k^4 + 1025k^3 + 712k^2 + 194k + 15 )
+ *
+ * Every summand (except for k=0) is between zero and 1/16^k. Thus:
+ * - adding one summand improves the approximation by 4 correct bits
+ * - every partial sum is a proper lower bound
+ * - every partial sum (k>0) plus 1/16^k is a proper upper bound
+ *
+ * Furthermore, the rate of convergenve is comparably slow, making sure that we
+ * do not end up with huge constants / coefficients immediately.
+ */
+class ApproximatePi
+{
+ public:
+  ApproximatePi();
+  const Rational& getLowerBound() const { return d_lower; }
+  const Rational& getUpperBound() const { return d_upper; }
+  void refine();
+
+ private:
+  Rational d_lower;
+  Rational d_upper;
+  uint32_t d_next_k = 0;
+  static constexpr uint32_t s_initial_refinement = 1;
+}; /* class ApproximatePi */
+
+}  // namespace transcendental
+}  // namespace nl
+}  // namespace arith
+}  // namespace theory
+}  // namespace cvc5
+
+#endif /* CVC5__THEORY__ARITH__TRANSCENDENTAL_SOLVER_H */
