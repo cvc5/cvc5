fa6847b975375bd3732d4cf12adcf83783b2132c
ajreynol <andrew.j.reynolds@gmail.com>
2022-02-24 15:35:57 -0600

    Fix

--- src/smt/process_assertions.cpp
+++ src/smt/process_assertions.cpp
@@ -170,7 +170,7 @@ bool ProcessAssertions::apply(Assertions& as)
 
   bool noConflict = true;
 
-  if (options().smt.extRewPrep!=options::ExtRewPrepMode::NONE)
+  if (options().smt.extRewPrep!=options::ExtRewPrepMode::OFF)
   {
     applyPass("ext-rew-pre", as);
   }
