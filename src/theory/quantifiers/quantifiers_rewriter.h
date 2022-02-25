ffafefee0a66bc2eff46fcb0d994872d894c7ed1
ajreynol <andrew.j.reynolds@gmail.com>
2022-02-24 23:25:19 -0600

    Fix non-termination in quantifiers rewriter

--- src/theory/quantifiers/quantifiers_rewriter.h
+++ src/theory/quantifiers/quantifiers_rewriter.h
@@ -45,8 +45,6 @@ enum RewriteStep
   COMPUTE_MINISCOPING,
   /** Aggressive miniscoping */
   COMPUTE_AGGRESSIVE_MINISCOPING,
-  /** Apply the extended rewriter to quantified formula bodies */
-  COMPUTE_EXT_REWRITE,
   /**
    * Term processing (e.g. simplifying terms based on ITE lifting,
    * eliminating extended arithmetic symbols).
@@ -58,6 +56,11 @@ enum RewriteStep
   COMPUTE_VAR_ELIMINATION,
   /** Conditional splitting */
   COMPUTE_COND_SPLIT,
+  /**
+   * Apply the extended rewriter to quantified formula bodies. This step
+   * must come last.
+   */
+  COMPUTE_EXT_REWRITE,
   /** Placeholder for end of steps */
   COMPUTE_LAST
 };
