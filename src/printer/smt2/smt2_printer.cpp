21df02ecc5eb4d5f294b351bc3bccd2b4f7bade8
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-02 08:53:31 -0600

    Fix

--- src/printer/smt2/smt2_printer.cpp
+++ src/printer/smt2/smt2_printer.cpp
@@ -1277,18 +1277,18 @@ std::string smtKindStringOf(const Node& n, Variant v = smt2_6_variant)
     // this method parallels api::Term::getKind
     switch (k)
     {
-      case STRING_CONCAT: return "seq.concat";
-      case STRING_LENGTH: return "seq.len";
-      case STRING_SUBSTR: return "seq.extract";
-      case STRING_UPDATE: return "seq.update";
-      case STRING_CHARAT: return "seq.at";
-      case STRING_CONTAINS: return "seq.contains";
-      case STRING_INDEXOF: return "seq.indexof";
-      case STRING_REPLACE: return "seq.replace";
-      case STRING_REPLACE_ALL: return "seq.replace_all";
-      case STRING_REV: return "seq.rev";
-      case STRING_PREFIX: return "seq.prefixof";
-      case STRING_SUFFIX: return "seq.suffixof";
+      case kind::STRING_CONCAT: return "seq.concat";
+      case kind::STRING_LENGTH: return "seq.len";
+      case kind::STRING_SUBSTR: return "seq.extract";
+      case kind::STRING_UPDATE: return "seq.update";
+      case kind::STRING_CHARAT: return "seq.at";
+      case kind::STRING_CONTAINS: return "seq.contains";
+      case kind::STRING_INDEXOF: return "seq.indexof";
+      case kind::STRING_REPLACE: return "seq.replace";
+      case kind::STRING_REPLACE_ALL: return "seq.replace_all";
+      case kind::STRING_REV: return "seq.rev";
+      case kind::STRING_PREFIX: return "seq.prefixof";
+      case kind::STRING_SUFFIX: return "seq.suffixof";
       default:
         // fall through to conversion below
         break;
