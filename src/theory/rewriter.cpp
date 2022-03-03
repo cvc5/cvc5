1f54ac23862eac6c6a8940b9ea00743e5991f42e
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-03 11:29:32 -0600

    Improve

--- src/theory/rewriter.cpp
+++ src/theory/rewriter.cpp
@@ -331,7 +331,7 @@ Node Rewriter::rewriteTo(theory::TheoryId theoryId,
 #ifdef CVC5_ASSERTIONS
           RewriteResponse r2 =
               d_theoryRewriters[newTheoryId]->postRewrite(newNode);
-          Assert(r2.d_node == newNode) << r2.d_node << " != " << newNode;
+          Assert(r2.d_node == newNode) << "Non-idempotent rewriting: " <<< r2.d_node << " != " << newNode;
 #endif
           rewriteStackTop.d_node = newNode;
           break;
@@ -395,7 +395,7 @@ Node Rewriter::rewriteTo(theory::TheoryId theoryId,
     if (rewriteStack.size() == 1) {
       Assert(!isEquality || rewriteStackTop.d_node.getKind() == kind::EQUAL
              || rewriteStackTop.d_node.isConst());
-      Assert (rewriteStackTop.d_node.getType().isSubtypeOf(node.getType()));
+      Assert (rewriteStackTop.d_node.getType().isSubtypeOf(node.getType())) << "Rewriting " << node << " does not preserve type";
       return rewriteStackTop.d_node;
     }
 
