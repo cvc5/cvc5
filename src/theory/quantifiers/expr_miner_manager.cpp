94d4dfe37c05449b601c7c239096d9365a04c7b9
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-03 14:03:47 -0600

    Fix

--- src/theory/quantifiers/expr_miner_manager.cpp
+++ src/theory/quantifiers/expr_miner_manager.cpp
@@ -179,8 +179,7 @@ bool ExpressionMinerManager::addTerm(Node sol,
   Node solb = sol;
   if (d_use_sygus_type)
   {
-    solb = datatypes::utils::sygusToBuiltin(
-        sol, true);  // d_tds->sygusToBuiltin(sol);
+    solb = datatypes::utils::sygusToBuiltin(sol, true);
   }
 
   // add to the candidate rewrite rule database
