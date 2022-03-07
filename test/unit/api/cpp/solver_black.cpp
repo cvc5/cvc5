33e8124c753bc5cc3d6ac44a5d3c4a9e5c9960d1
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-07 10:35:54 -0600

    Move

--- test/unit/api/cpp/solver_black.cpp
+++ test/unit/api/cpp/solver_black.cpp
@@ -3148,22 +3148,5 @@ TEST_F(TestApiBlackSolver, proj_issue429)
   slv.checkEntailed({t126});
 }
 
-TEST_F(TestApiBlackSolver, proj_issue387)
-{
-  Sort s1 = d_solver.getBooleanSort();
-
-  Sort u1 = d_solver.mkSortConstructorSort("_x0", 1);
-  Sort u2 = d_solver.mkSortConstructorSort("_x1", 1);
-  Sort p1 = d_solver.mkParamSort("_x4");
-  Sort p2 = d_solver.mkParamSort("_x27");
-  Sort p3 = d_solver.mkParamSort("_x3");
-
-  DatatypeDecl dtdecl1 = d_solver.mkDatatypeDecl("_x0", p1);
-  DatatypeConstructorDecl ctordecl1 =
-      d_solver.mkDatatypeConstructorDecl("_x18");
-  ASSERT_THROW(ctordecl1.addSelector("_x17", u2.instantiate({p1, p1})),
-               CVC5ApiException);
-}
-
 }  // namespace test
 }  // namespace cvc5
