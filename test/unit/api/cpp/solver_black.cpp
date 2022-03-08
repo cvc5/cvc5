447347fd3c692677675800b40171bd8b56242c19
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-07 18:13:30 -0600

    Fix

--- test/unit/api/cpp/solver_black.cpp
+++ test/unit/api/cpp/solver_black.cpp
@@ -3225,14 +3225,9 @@ TEST_F(TestApiBlackSolver, proj_issue423)
     t2 = slv.mkEmptySequence(s3);
   }
   Term t22 = slv.mkReal("119605652059157009");
-  ASSERT_TRUE(t22.getSort().isReal() && !t22.getSort().isInteger());
   Term t32 = slv.mkTerm(Kind::SEQ_UNIT, {t22});
-  std::cout << "Sort1: " << t2.getSort() << std::endl;
-  std::cout << "Sort2: " << t32.getSort() << std::endl;
-  ASSERT_TRUE(t2.getSort()==t32.getSort());
-  Term t43 = slv.mkTerm(Kind::SEQ_CONCAT, {t2, t32});
-  //Term t51 = slv.mkTerm(Kind::DISTINCT, {t32, t32});
-  //slv.checkSat();
+  // incorrect types: t2 is a sequence of sequence of reals
+  ASSERT_THROW(slv.mkTerm(Kind::SEQ_CONCAT, {t2, t32}), CVC5ApiException);
 }
 
 }  // namespace test
