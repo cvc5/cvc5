1a1fcd85a09787147fc0975b89a411eb493b7cb4
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-10 15:53:15 -0600

    More

--- src/theory/theory_model.cpp
+++ src/theory/theory_model.cpp
@@ -849,10 +849,11 @@ bool TheoryModel::isValue(TNode n) const
     }
     if (!finishedComputing)
     {
-      // The only non-constant operator is for APPLY_UF. All other operators
-      // are either builtin, or should be considered constants, e.g.
-      // constructors.
-      bool hasOperator = cur.getKind() == kind::APPLY_UF;
+      // The only non-constant operators we consider are APPLY_UF and
+      // APPLY_SELECTOR. All other operators are either builtin, or should be
+      // considered constants, e.g. constructors.
+      Kind k = cur.getKind();
+      bool hasOperator = k == kind::APPLY_UF || k == kind::APPLY_SELECTOR;
       size_t nextChildIndex = v.second;
       if (hasOperator && nextChildIndex > 0)
       {
