12b078db61dba5901994a3d3373dc9a3f48284b4
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-11 15:09:26 -0600

    Fix

--- test/api/cpp/proj-issue484.cpp
+++ test/api/cpp/proj-issue484.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Top contributors (to current version):
- *   Andres Noetzli
+ *   Andrew Reynolds
  *
  * This file is part of the cvc5 project.
  *
