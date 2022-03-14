b7d9301cdf3fdf0f3e2bebb2e46a37041e3e6c1c
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-14 14:45:40 -0500

    Fixing skolem definition management

--- src/prop/theory_proxy.cpp
+++ src/prop/theory_proxy.cpp
@@ -131,6 +131,7 @@ void TheoryProxy::theoryCheck(theory::Theory::Effort effort) {
       // check if this corresponds to a zero-level asserted literal
       d_zll->notifyAsserted(assertion);
     }
+    Trace("ajr-temp") << "assertFact " << assertion << " " << d_dmNeedsActiveDefs << std::endl;
     // now, assert to theory engine
     d_theoryEngine->assertFact(assertion);
     if (d_dmNeedsActiveDefs)
