9e13edf05de5046a7b9607e94b339be3bbe6ab7e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2022-03-14 14:11:08 -0700

    first two rules

--- src/proof/proof_rule.h
+++ src/proof/proof_rule.h
@@ -48,33 +48,44 @@ namespace cvc5 {
  */
 enum class PfRule : uint32_t
 {
-  //================================================= Core rules
-  //======================== Assume and Scope
-  // ======== Assumption (a leaf)
-  // Children: none
-  // Arguments: (F)
-  // --------------
-  // Conclusion: F
-  //
-  // This rule has special status, in that an application of assume is an
-  // open leaf in a proof that is not (yet) justified. An assume leaf is
-  // analogous to a free variable in a term, where we say "F is a free
-  // assumption in proof P" if it contains an application of F that is not
-  // bound by SCOPE (see below).
+  /*
+   * ================================================= Core rules
+   * ======================== Assume and Scope
+   * ======== Assumption (a leaf)
+   */
+  /** 
+   * \verbatim embed:rst:leading-asterisk
+   * 
+   * .. math::
+   *   
+   *   \texttt{ASSUME}(F): \inferrule{\top}{F}
+   *
+   * \endverbatim
+   *
+   * This rule has special status, in that an application of assume is an
+   * open leaf in a proof that is not (yet) justified. An assume leaf is
+   * analogous to a free variable in a term, where we say "F is a free
+   * assumption in proof P" if it contains an application of F that is not
+   * bound by SCOPE (see below).
+   */
   ASSUME,
-  // ======== Scope (a binder for assumptions)
-  // Children: (P:F)
-  // Arguments: (F1, ..., Fn)
-  // --------------
-  // Conclusion: (=> (and F1 ... Fn) F) or (not (and F1 ... Fn)) if F is false
-  //
-  // This rule has a dual purpose with ASSUME. It is a way to close
-  // assumptions in a proof. We require that F1 ... Fn are free assumptions in
-  // P and say that F1, ..., Fn are not free in (SCOPE P). In other words, they
-  // are bound by this application. For example, the proof node:
-  //   (SCOPE (ASSUME F) :args F)
-  // has the conclusion (=> F F) and has no free assumptions. More generally, a
-  // proof with no free assumptions always concludes a valid formula.
+  /**
+   * \verbatim embed:rst:leading-asterisk
+   * 
+   * .. math::
+   *   
+   *   \texttt{SCOPE}(F_1 \dots F_n): \inferrule{F \neq \bot}{(F_1 \land \dots \land F_n) \Rightarrow F} \textrm{ or } \inferrule{\bot}{\neg (F_1 \land \dots \land F_n)}
+   *
+   * This rule has a dual purpose with ASSUME. It is a way to close
+   * assumptions in a proof. We require that F1 ... Fn are free assumptions in
+   * P and say that F1, ..., Fn are not free in (SCOPE P). In other words, they
+   * are bound by this application. For example, the proof node:
+   * ``(SCOPE (ASSUME F) :args F)``
+   * has the conclusion (=> F F) and has no free assumptions. More generally, a
+   * proof with no free assumptions always concludes a valid formula.
+   * 
+   * \endverbatim
+   */
   SCOPE,
 
   //======================== Builtin theory (common node operations)
