677d24c2c47b1495917c488f8802a47c6c95ca54
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-22 16:50:04 -0500

    More

--- src/api/java/io/github/cvc5/api/SynthResult.java
+++ src/api/java/io/github/cvc5/api/SynthResult.java
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Top contributors (to current version):
- *   Aina Niemetz, Andrew Reynolds, Abdalrhman Mohamed, Mudathir Mohamed
+ *   Andrew Reynolds
  *
  * This file is part of the cvc5 project.
  *
