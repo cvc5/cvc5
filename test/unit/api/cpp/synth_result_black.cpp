3421d31d7b62ba6c6d2c21af765d54f432a5e950
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-22 17:03:08 -0500

    Split for now

--- test/unit/api/cpp/synth_result_black.cpp
+++ test/unit/api/cpp/synth_result_black.cpp
@@ -32,44 +32,6 @@ TEST_F(TestApiBlackSynthResult, isNull)
   ASSERT_FALSE(res_null.hasSolution());
   ASSERT_FALSE(res_null.hasNoSolution());
   ASSERT_FALSE(res_null.isUnknown());
-  Sort u_sort = d_solver.mkUninterpretedSort("u");
-  Term x = d_solver.mkConst(u_sort, "x");
-  d_solver.assertFormula(x.eqTerm(x));
-  cvc5::api::SynthResult res = d_solver.checkSynth();
-  ASSERT_FALSE(res.isNull());
-}
-
-TEST_F(TestApiBlackSynthResult, hasSolution)
-{
-  Sort u_sort = d_solver.mkUninterpretedSort("u");
-  Term x = d_solver.mkConst(u_sort, "x");
-  d_solver.assertFormula(x.eqTerm(x));
-  cvc5::api::SynthResult res = d_solver.checkSynth();
-  ASSERT_TRUE(res.hasSolution());
-  ASSERT_FALSE(res.isUnknown());
-}
-
-TEST_F(TestApiBlackSynthResult, hasNoSolution)
-{
-  Sort u_sort = d_solver.mkUninterpretedSort("u");
-  Term x = d_solver.mkConst(u_sort, "x");
-  d_solver.assertFormula(x.eqTerm(x).notTerm());
-  cvc5::api::SynthResult res = d_solver.checkSynth();
-  ASSERT_TRUE(res.hasNoSolution());
-  ASSERT_FALSE(res.isUnknown());
-}
-
-TEST_F(TestApiBlackSynthResult, isUnknown)
-{
-  d_solver.setLogic("QF_NIA");
-  d_solver.setOption("incremental", "false");
-  d_solver.setOption("solve-int-as-bv", "32");
-  Sort int_sort = d_solver.getIntegerSort();
-  Term x = d_solver.mkConst(int_sort, "x");
-  d_solver.assertFormula(x.eqTerm(x).notTerm());
-  cvc5::api::SynthResult res = d_solver.checkSynth();
-  ASSERT_FALSE(res.hasSolution());
-  ASSERT_TRUE(res.isUnknown());
 }
 
 }  // namespace test
