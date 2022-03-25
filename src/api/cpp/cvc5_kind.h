0abc7f7faa1fd5b69a6bbc68eda2a360d2d1c797
Aina Niemetz <aina.niemetz@gmail.com>
2022-03-25 12:42:31 -0700

    api: More comprehensive documentation of INTERNAL_KIND.

--- src/api/cpp/cvc5_kind.h
+++ src/api/cpp/cvc5_kind.h
@@ -46,6 +46,10 @@ enum Kind : int32_t
   /**
    * Internal kind.
    *
+   * This kind serves as an abstraction for internal kinds that are not exposed
+   * via the API but may appear in terms returned by API functions, e.g.,
+   * when querying model values.
+   *
    * @note Should never be created via the API.
    */
   INTERNAL_KIND = -2,
