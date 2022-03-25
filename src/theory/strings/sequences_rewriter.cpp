e3e931de9002d03eac899e29b45aec53b0873dae
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-25 09:47:39 -0500

    Minor

--- src/theory/strings/sequences_rewriter.cpp
+++ src/theory/strings/sequences_rewriter.cpp
@@ -634,7 +634,7 @@ Node SequencesRewriter::rewriteLength(Node node)
   else if (nk0 == STRING_TO_LOWER || nk0 == STRING_TO_UPPER || nk0 == STRING_REV
            || nk0 == STRING_UPDATE)
   {
-    // len( f( x ) ) == len( x ) where f is to_lower, toupper, or rev.
+    // len( f( x ) ) == len( x ) where f is to_lower, to_upper, or rev.
     // len( update( x, n, y ) ) = len( x )
     Node retNode = nm->mkNode(STRING_LENGTH, node[0][0]);
     return returnRewrite(node, retNode, Rewrite::LEN_CONV_INV);
