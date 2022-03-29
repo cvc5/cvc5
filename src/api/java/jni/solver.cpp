cb49e0143ac7b71a671b238cae6d0e63c0e90d2e
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2022-03-29 14:37:22 -0500

    Make ensureTermSort private (#8436)
    This method was used to correct a limitation in the old API regarding parsing decimals. It should not be a public API method.

--- src/api/java/jni/solver.cpp
+++ src/api/java/jni/solver.cpp
@@ -2387,23 +2387,6 @@ JNIEXPORT void JNICALL Java_io_github_cvc5_api_Solver_setOption(
 
 /*
  * Class:     io_github_cvc5_api_Solver
- * Method:    ensureTermSort
- * Signature: (JJJ)J
- */
-JNIEXPORT jlong JNICALL Java_io_github_cvc5_api_Solver_ensureTermSort(
-    JNIEnv* env, jobject, jlong pointer, jlong termPointer, jlong sortPointer)
-{
-  CVC5_JAVA_API_TRY_CATCH_BEGIN;
-  Solver* solver = reinterpret_cast<Solver*>(pointer);
-  Term* term = reinterpret_cast<Term*>(termPointer);
-  Sort* sort = reinterpret_cast<Sort*>(sortPointer);
-  Term* retPointer = new Term(solver->ensureTermSort(*term, *sort));
-  return reinterpret_cast<jlong>(retPointer);
-  CVC5_JAVA_API_TRY_CATCH_END_RETURN(env, 0);
-}
-
-/*
- * Class:     io_github_cvc5_api_Solver
  * Method:    declareSygusVar
  * Signature: (JJLjava/lang/String;)J
  */
