b9c80bcf25c3730a6028baeb61216a770881e964
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-28 21:53:02 -0500

    Format

--- src/api/cpp/cvc5.cpp
+++ src/api/cpp/cvc5.cpp
@@ -3203,13 +3203,15 @@ std::pair<Sort, uint32_t> Term::getCardinalityConstraint() const
       << "Term to be a cardinality constraint when calling "
          "getCardinalityConstraint()";
   // this should never happen since we restrict what the user can create
-  CVC5_API_ARG_CHECK_EXPECTED(
-      detail::checkIntegerBounds<std::uint32_t>(
-          d_node->getOperator().getConst<CardinalityConstraint>().getUpperBound()),
-      *d_node)
+  CVC5_API_ARG_CHECK_EXPECTED(detail::checkIntegerBounds<std::uint32_t>(
+                                  d_node->getOperator()
+                                      .getConst<CardinalityConstraint>()
+                                      .getUpperBound()),
+                              *d_node)
       << "Upper bound for cardinality constraint does not fit uint32_t";
   //////// all checks before this line
-  const CardinalityConstraint& cc = d_node->getOperator().getConst<CardinalityConstraint>();
+  const CardinalityConstraint& cc =
+      d_node->getOperator().getConst<CardinalityConstraint>();
   return std::make_pair(Sort(d_solver, cc.getType()),
                         cc.getUpperBound().getUnsignedInt());
   ////////
