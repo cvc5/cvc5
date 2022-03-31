29539b8a9e273396bb40b14c366a6dc73593a311
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-31 10:04:36 -0500

    Disable minisat variable elimination when a parametric theory is present

--- src/smt/set_defaults.cpp
+++ src/smt/set_defaults.cpp
@@ -695,11 +695,12 @@ void SetDefaults::setDefaultsPost(const LogicInfo& logic, Options& opts) const
       && opts.prop.minisatSimpMode == options::MinisatSimpMode::ALL)
   {
     // cannot use minisat variable elimination for logics where a theory solver
-    // introduces new literals into the search. This includes quantifiers
+    // introduces new literals into the search, or for parametric theories
+    // which may require non-trivial preprocessing. This includes quantifiers
     // (quantifier instantiation), and the lemma schemas used in non-linear
     // and sets. We also can't use it if models are enabled.
     if (logic.isTheoryEnabled(THEORY_SETS) || logic.isTheoryEnabled(THEORY_BAGS)
-        || logic.isQuantified() || opts.smt.produceModels
+        || logic.isTheoryEnabled(THEORY_ARRAYS) ||  logic.isTheoryEnabled(THEORY_STRINGS) || logic.isTheoryEnabled(THEORY_DATATYPES) || logic.isQuantified() || opts.smt.produceModels
         || opts.smt.produceAssignments || opts.smt.checkModels
         || (logic.isTheoryEnabled(THEORY_ARITH) && !logic.isLinear()))
     {
