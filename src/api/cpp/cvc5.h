e8cbd2580d441876f4d148c3841aec117e544a23
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-31 12:48:11 -0500

    Explaining things in the API

--- src/api/cpp/cvc5.h
+++ src/api/cpp/cvc5.h
@@ -265,7 +265,7 @@ class CVC5_EXPORT Result
 std::ostream& operator<<(std::ostream& out, const Result& r) CVC5_EXPORT;
 
 /* -------------------------------------------------------------------------- */
-/* Result                                                                     */
+/* SynthResult                                                                */
 /* -------------------------------------------------------------------------- */
 
 /**
@@ -415,12 +415,16 @@ class CVC5_EXPORT Sort
   bool operator>=(const Sort& s) const;
 
   /**
+   * Does the sort have a symbol, i.e. a name? For example, uninterpreted sorts
+   * and uninterpreted sort constructors have symbols.
    * @return true if the sort has a symbol.
    */
   bool hasSymbol() const;
 
   /**
-   * Asserts hasSymbol().
+   * Asserts hasSymbol(). The symbol of the sort is the string that was
+   * provided when constructing it via mkUninterpretedSort, mkUnresolvedSort or
+   * mkUninterpretedSortConstructorSort.
    * @return the raw symbol of the sort.
    */
   std::string getSymbol() const;
@@ -788,6 +792,8 @@ class CVC5_EXPORT Sort
   /* Datatype sort ------------------------------------------------------- */
 
   /**
+   * Get the arity of a datatype sort, which is the number of type parameters
+   * if the datatype is parametric, or 0 otherwise.
    * @return the arity of a datatype sort
    */
   size_t getDatatypeArity() const;
@@ -1169,12 +1175,15 @@ class CVC5_EXPORT Term
   Op getOp() const;
 
   /**
+   * Does the term have a symbol, i.e. a name? For example, free constants
+   * and variables have symbols.
    * @return true if the term has a symbol.
    */
   bool hasSymbol() const;
 
   /**
-   * Asserts hasSymbol().
+   * Asserts hasSymbol(). The symbol of the term is the string that was
+   * provided when constructing it via mkConst or mkVar.
    * @return the raw symbol of the term.
    */
   std::string getSymbol() const;
@@ -1568,13 +1577,46 @@ class CVC5_EXPORT Term
 
   /**
    * @return true if the term is a sequence value.
+   * 
+   * A term is a sequence value if it has kind CONST_SEQUENCE. In contrast to
+   * values for the set sort (as described in isSetValue()), sequence values
+   * are represented as a Term with no children.
+   *
+   * Semantically, a sequence value is a concatentation of unit sequences
+   * whose elements are themselves values. For example:
+   * 
+   * \verbatim embed:rst:leading-asterisk
+   * .. code:: smtlib
+   *
+   *     (seq.++ (seq.unit 0) (seq.unit 1))
+   * \endverbatim
+   * 
+   * The above term has two representations in Term, either as the sequence
+   * concatenation term:
+   * 
+   * \rst
+   * .. code:: lisp
+   *
+   *     (SEQ_CONCAT (SEQ_UNIT CONST_RATIONAL_0) (SEQ_UNIT CONST_RATIONAL_1))
+   * \endrst
+   * 
+   * or as the constant sequence value:
+   * 
+   * \rst
+   * .. code:: lisp
+   *
+   *     CONST_SEQUENCE_{CONST_RATIONAL_0,1}
+   * \endrst
+   *
+   * where calling getSequenceValue() on the latter returns the vector
+   * {CONST_RATIONAL_0, CONST_RATIONAL_1}. 
+   * 
+   * The former term is not a sequence value, but the latter term is. Calling
+   * e.g. Solver::simplify() on the first term will result in the second term.
    */
   bool isSequenceValue() const;
   /**
    * Asserts isSequenceValue().
-   * @note It is usually necessary for sequences to call Solver::simplify()
-   *       to turn a sequence that is constructed by, e.g., concatenation of
-   *       unit sequences, into a sequence value.
    * @return the representation of a sequence value as a vector of terms.
    */
   std::vector<Term> getSequenceValue() const;
@@ -3230,7 +3272,8 @@ class CVC5_EXPORT Solver
   Sort mkParamSort(const std::string& symbol) const;
 
   /**
-   * Create a predicate sort.
+   * Create a predicate sort. This is equivalent to calling mkFunctionSort with
+   * the Boolean sort as the codomain.
    * @param sorts the list of sorts of the predicate
    * @return the predicate sort
    */
@@ -3637,7 +3680,7 @@ class CVC5_EXPORT Solver
   /* .................................................................... */
 
   /**
-   * Create (first-order) constant (0-arity function symbol).
+   * Create a free constant.
    *
    * SMT-LIB:
    *
@@ -3650,15 +3693,14 @@ class CVC5_EXPORT Solver
    *
    * @param sort the sort of the constant
    * @param symbol the name of the constant
-   * @return the first-order constant
+   * @return the constant
    */
   Term mkConst(const Sort& sort, const std::string& symbol) const;
   /**
-   * Create (first-order) constant (0-arity function symbol), with a default
-   * symbol name.
+   * Create a free constant with a default symbol name.
    *
    * @param sort the sort of the constant
-   * @return the first-order constant
+   * @return the constant
    */
   Term mkConst(const Sort& sort) const;
 
@@ -3726,8 +3768,7 @@ class CVC5_EXPORT Solver
   /**
    * Simplify a formula without doing "much" work.  Does not involve
    * the SAT Engine in the simplification, but uses the current
-   * definitions, assertions, and the current partial model, if one
-   * has been constructed.  It also involves theory normalization.
+   * definitions, and assertions.  It also involves theory normalization.
    *
    * @warning This method is experimental and may change in future versions.
    *
