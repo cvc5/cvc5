f3925bc00382d91bee9aa8032d5e0f1a2355e86b
ajreynol <andrew.j.reynolds@gmail.com>
2022-04-04 09:36:34 -0500

    More

--- src/proof/proof_node_to_sexpr.cpp
+++ src/proof/proof_node_to_sexpr.cpp
@@ -30,8 +30,8 @@ namespace cvc5::internal {
 ProofNodeToSExpr::ProofNodeToSExpr()
 {
   NodeManager* nm = NodeManager::currentNM();
-  d_conclusionMarker = nm->mkBoundVar(":conclusion", nm->sExprType());
-  d_argsMarker = nm->mkBoundVar(":args", nm->sExprType());
+  d_conclusionMarker = nm->mkRawSymbol(":conclusion", nm->sExprType());
+  d_argsMarker = nm->mkRawSymbol(":args", nm->sExprType());
 }
 
 Node ProofNodeToSExpr::convertToSExpr(const ProofNode* pn)
