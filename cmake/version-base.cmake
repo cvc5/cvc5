8ed45006996dae8777e553ad6ce906fb7074cc6f
Mathias Preiner <mathias.preiner@gmail.com>
2022-04-04 18:44:23 -0700

    Update headers.

--- cmake/version-base.cmake
+++ cmake/version-base.cmake
@@ -1,3 +1,20 @@
+###############################################################################
+# Top contributors (to current version):
+#   Gereon Kremer, Mathias Preiner
+#
+# This file is part of the cvc5 project.
+#
+# Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
+# in the top-level source directory and their institutional affiliations.
+# All rights reserved.  See the file COPYING in the top-level source
+# directory for licensing information.
+# #############################################################################
+#
+# [[ Add one-line brief description here ]]
+#
+# [[ Add lengthier description here ]]
+# \todo document this file
+##
 # These are updated when making a release
 set(CVC5_LAST_RELEASE "0.0.12")
 set(CVC5_IS_RELEASE "false")
--- src/theory/bv/theory_bv_rewrite_rules_operator_elimination.h
+++ src/theory/bv/theory_bv_rewrite_rules_operator_elimination.h
@@ -1,19 +1,16 @@
 /******************************************************************************
  * Top contributors (to current version):
- *   Yoni Zohar, Liana Hadarean, Aina Niemetz
+ *   Liana Hadarean, Yoni Zohar, Aina Niemetz
  *
  * This file is part of the cvc5 project.
  *
- * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
  * in the top-level source directory and their institutional affiliations.
  * All rights reserved.  See the file COPYING in the top-level source
  * directory for licensing information.
  * ****************************************************************************
  *
- * [[ Add one-line brief description here ]]
- *
- * [[ Add lengthier description here ]]
- * \todo document this file
+ * Theory BV Operator elimination rewrites.
  */
 
 #include "cvc5_private.h"
