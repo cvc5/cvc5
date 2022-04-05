8883f0e8e5dc4ed10872bf87a76cd87e35b68457
Mathias Preiner <mathias.preiner@gmail.com>
2022-04-04 19:03:41 -0700

    Fix.

--- test/api/python/two_solvers.py
+++ test/api/python/two_solvers.py
@@ -10,23 +10,6 @@
 # directory for licensing information.
 # #############################################################################
 #
-# [[ Add one-line brief description here ]]
-#
-# [[ Add lengthier description here ]]
-# \todo document this file
-##
-##############################################################################
-# Top contributors (to current version):
-#   Yoni Zohar
-#
-# This file is part of the cvc5 project.
-#
-# Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
-# in the top-level source directory and their institutional affiliations.
-# All rights reserved.  See the file COPYING in the top-level source
-# directory for licensing information.
-# ############################################################################
-#
 # A simple test of multiple SmtEngines.
 ##
 
