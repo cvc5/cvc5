424f802e37c1d250e80df043510a452510f713e8
mudathirmahgoub <mudathirmahgoub@gmail.com>
2022-04-18 19:36:23 -0500

    fix compile

---
+++ src/theory/bags/table_project_op.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Mudathir Mohamed
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * A class for TableProjectOp operator.
+ */
+
+#include "table_project_op.h"
+
+namespace cvc5::internal {
+
+}  // namespace cvc5::internal
