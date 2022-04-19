09a14944dd68943f6a21c6377c0aaf77ac861812
mudathirmahgoub <mudathirmahgoub@gmail.com>
2022-04-19 18:40:18 -0500

    refactoring rewriter

--- src/theory/bags/theory_bags.cpp
+++ src/theory/bags/theory_bags.cpp
@@ -38,7 +38,7 @@ TheoryBags::TheoryBags(Env& env, OutputChannel& out, Valuation valuation)
       d_ig(&d_state, &d_im),
       d_notify(*this, d_im),
       d_statistics(),
-      d_rewriter(&d_statistics.d_rewrites),
+      d_rewriter(env.getRewriter(), &d_statistics.d_rewrites),
       d_termReg(env, d_state, d_im),
       d_solver(env, d_state, d_im, d_termReg),
       d_cardSolver(env, d_state, d_im),
