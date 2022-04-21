fe8e3d8b5e8ac194c53c1a52531cadf2cd260b1c
ajreynol <andrew.j.reynolds@gmail.com>
2022-04-21 11:55:05 -0500

    Sketching inst eval class

---
+++ src/theory/quantifiers/inst_evaluator.h
@@ -0,0 +1,77 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Inst evaluator class.
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__THEORY__QUANTIFIERS__INST_EVALUATOR_H
+#define CVC5__THEORY__QUANTIFIERS__INST_EVALUATOR_H
+
+#include <vector>
+
+#include "expr/node.h"
+#include "context/context.h"
+
+namespace cvc5::internal {
+namespace theory {
+namespace quantifiers {
+
+class TermRegistry;
+
+/**
+ * Inst evaluator
+ *
+ * Incrementally maintains the state of the rewritten form of the quantified
+ * formula.
+ */
+class InstEvaluator
+{
+public:
+  InstEvaluator(TermRegistry& tr, bool doCanonize = true);
+  /** 
+   * Set that we are watching quantified formula q.
+   */
+  void watch(Node q);
+  /** Same as above, with possibly preprocessed body. */
+  void watch(Node q, Node body);
+  /**
+   * Set that we are considering instantiations v -> s.
+   *
+   * Return false if all quantified formulas watched by this class are
+   * infeasible.
+   * 
+   * If this returns true, this adds quantified formulas that are fully
+   * instantiated.
+   */
+  bool push(TNode v, TNode s, std::vector<Node>& assignedQuants);
+  /** pop the last (successful) push */
+  void pop();
+  /**
+   * Get instantiation for quantified formula q.
+   */
+  std::vector<Node> getInstantiationFor(Node q);
+private:
+  /** Reference to term registry */
+  TermRegistry& d_treg;
+  /** do canonize */
+  bool d_doCanonize;
+  /** A context object */
+  context::Context d_context;
+};
+
+}  // namespace quantifiers
+}  // namespace theory
+}  // namespace cvc5::internal
+
+#endif /* CVC5__THEORY__QUANTIFIERS__INST_EVALUATOR_H */
