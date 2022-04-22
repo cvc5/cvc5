346271f6024fbdb7aa79c02264ed67e3434b7d09
ajreynol <andrew.j.reynolds@gmail.com>
2022-04-22 15:33:33 -0500

    Add back

--- src/expr/node_manager_template.cpp
+++ src/expr/node_manager_template.cpp
@@ -1171,6 +1171,24 @@ TNode NodeManager::operatorOf(Kind k)
   return d_operators[k];
 }
 
+/**
+  * This template gives a mechanism to stack-allocate a NodeValue
+  * with enough space for N children (where N is a compile-time
+  * constant).  You use it like this:
+  *
+  *   NVStorage<4> nvStorage;
+  *   NodeValue& nvStack = reinterpret_cast<NodeValue&>(nvStorage);
+  *
+  * ...and then you can use nvStack as a NodeValue that you know has
+  * room for 4 children.
+  */
+template <size_t N>
+struct NVStorage
+{
+  expr::NodeValue nv;
+  expr::NodeValue* child[N];
+};
+  
 template <class NodeClass, class T>
 NodeClass NodeManager::mkConstInternal(Kind k, const T& val)
 {
