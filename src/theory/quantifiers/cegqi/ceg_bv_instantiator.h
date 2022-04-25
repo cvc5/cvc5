eb6c4c958b8c363c603510672fd8a9e2dc4d832e
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2022-04-25 13:21:57 -0500

    Make BVInstantiatorUtil an EnvObj (#8633)
    Eliminates 5 of the remaining 8 static calls to Rewriter::rewrite.

--- src/theory/quantifiers/cegqi/ceg_bv_instantiator.h
+++ src/theory/quantifiers/cegqi/ceg_bv_instantiator.h
@@ -19,7 +19,9 @@
 #define CVC5__THEORY__QUANTIFIERS__CEG_BV_INSTANTIATOR_H
 
 #include <unordered_map>
+
 #include "theory/quantifiers/bv_inverter.h"
+#include "theory/quantifiers/cegqi/ceg_bv_instantiator_utils.h"
 #include "theory/quantifiers/cegqi/ceg_instantiator.h"
 
 namespace cvc5::internal {
@@ -104,6 +106,8 @@ class BvInstantiator : public Instantiator
  private:
   /** pointer to the bv inverter class */
   BvInverter* d_inverter;
+  /** Utility class */
+  BvInstantiatorUtil d_util;
   //--------------------------------solved forms
   /** identifier counter, used to allocate ids to each solve form */
   unsigned d_inst_id_counter;
