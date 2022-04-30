f7c70a5a7a0f9764cc58427447db642fe3bf6610
ajreynol <andrew.j.reynolds@gmail.com>
2022-04-30 09:33:52 -0500

    Make msum agnostic

--- src/theory/arith/arith_msum.h
+++ src/theory/arith/arith_msum.h
@@ -104,12 +104,13 @@ class ArithMSum
    * Make the Node corresponding to the interpretation of msum, [msum], where:
    *   [msum] = sum_{( v, c ) \in msum } [c]*[v]
    *
-   * @param tn The type of the node to return, which is used only if msum is
-   * empty
    * @param msum The monomial sum
    * @return The node corresponding to the monomial sum
+   *
+   * Note this utility is agnostic to types, it will return the integer 0 if
+   * msum is empty.
    */
-  static Node mkNode(TypeNode tn, const std::map<Node, Node>& msum);
+  static Node mkNode(const std::map<Node, Node>& msum);
 
   /** make coefficent term
    *
