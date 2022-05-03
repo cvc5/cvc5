6d4d3e533ce0a7b330dd3f939d2f75ae9e2f856d
ajreynol <andrew.j.reynolds@gmail.com>
2022-05-02 20:21:44 -0500

    Format

--- src/api/cpp/cvc5.cpp
+++ src/api/cpp/cvc5.cpp
@@ -2655,7 +2655,7 @@ bool isReal64(const internal::Node& node)
 bool isInteger(const internal::Node& node)
 {
   return (node.getKind() == internal::Kind::CONST_RATIONAL
-         || node.getKind() == internal::Kind::CONST_INTEGER)
+          || node.getKind() == internal::Kind::CONST_INTEGER)
          && node.getConst<internal::Rational>().isIntegral();
 }
 bool isInt32(const internal::Node& node)
