26c356eafde30e8784fbffd2f1be62786ab61eb2
mudathirmahgoub <mudathirmahgoub@gmail.com>
2022-05-04 16:48:58 -0500

    Address

--- src/theory/bags/bag_reduction.h
+++ src/theory/bags/bag_reduction.h
@@ -96,7 +96,19 @@ class BagReduction
    *   unionDisjoint: Int -> (Bag T1) is an uninterpreted function
    */
   static Node reduceCardOperator(Node node, std::vector<Node>& asserts);
-
+  /**
+   * @param node of the form ((_ table.aggr n1 ... nk) f initial A))
+   * @return reduction term that uses map, fold, and partition using
+   * tuple projection as the equivalence relation as follows:
+   * (bag.map
+   *   (lambda ((B Table)) (bag.fold f initial B))
+   *   (bag.partition
+   *     (lambda ((t1 Tuple) (t2 Tuple)) ; equivalence relation
+   *             (=
+   *               ((_ tuple.project n1 ... nk) t1)
+   *               ((_ tuple.project n1 ... nk) t2)))
+   *     A))
+   */
   static Node reduceAggregateOperator(Node node);
 };
 
