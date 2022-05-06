79cd3544f0068d84baacecb8e233a90dd78f75a4
ajreynol <andrew.j.reynolds@gmail.com>
2022-05-06 14:01:05 -0500

    More preparation for making equality strictly typed

--- src/theory/arith/arith_msum.cpp
+++ src/theory/arith/arith_msum.cpp
@@ -219,12 +219,13 @@ int ArithMSum::isolate(
   int ires = isolate(v, msum, veq_c, val, k);
   if (ires != 0)
   {
+    NodeManager* nm = NodeManager::currentNM();
     Node vc = v;
     if (!veq_c.isNull())
     {
       if (doCoeff)
       {
-        vc = NodeManager::currentNM()->mkNode(MULT, veq_c, vc);
+        vc = nm->mkNode(MULT, veq_c, vc);
       }
       else
       {
@@ -232,8 +233,12 @@ int ArithMSum::isolate(
       }
     }
     bool inOrder = ires == 1;
-    veq = NodeManager::currentNM()->mkNode(
-        k, inOrder ? vc : val, inOrder ? val : vc);
+    // ensure type is correct for equality
+    if (k == EQUAL && !vc.getType().isInteger() && val.getType().isInteger())
+    {
+      val = nm->mkNode(TO_REAL, val);
+    }
+    veq = nm->mkNode(k, inOrder ? vc : val, inOrder ? val : vc);
   }
   return ires;
 }
