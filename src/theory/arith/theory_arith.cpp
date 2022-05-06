03ac96d2f4799888802e7e54d5e6a3bfae0e5797
ajreynol <andrew.j.reynolds@gmail.com>
2022-05-06 16:07:30 -0500

    Remove preeminent asseriton

--- src/theory/arith/theory_arith.cpp
+++ src/theory/arith/theory_arith.cpp
@@ -294,7 +294,6 @@ bool TheoryArith::collectModelValues(TheoryModel* m,
       continue;
     }
     // maps to constant of same type
-    Assert(p.first.getType() == p.second.getType());
     if (m->assertEquality(p.first, p.second, true))
     {
       continue;
