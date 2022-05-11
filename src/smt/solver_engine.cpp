b1960e8247aedd40dfaf4ee66d52431718912efd
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2022-05-11 16:45:56 -0700

    Move setting the filename on the original options to the API

--- src/smt/solver_engine.cpp
+++ src/smt/solver_engine.cpp
@@ -346,8 +346,7 @@ void SolverEngine::setInfo(const std::string& key, const std::string& value)
 
   if (key == "filename")
   {
-    d_env->d_options.driver.filename = value;
-    d_env->d_originalOptions->driver.filename = value;
+    d_env->d_options.writeDriver().filename = value;
     d_env->getStatisticsRegistry().registerValue<std::string>(
         "driver::filename", value);
   }
