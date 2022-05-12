901e8648200f73cf1bb8c53c544346565d647c8b
ajreynol <andrew.j.reynolds@gmail.com>
2022-05-12 18:09:37 -0500

    Towards

--- src/expr/subs.h
+++ src/expr/subs.h
@@ -50,7 +50,7 @@ class Subs
   /** Add v -> k for fresh skolem of the same type as v for each v in vs */
   void add(const std::vector<Node>& vs);
   /** Add v -> s to the substitution */
-  void add(Node v, Node s);
+  void add(const Node& v, const Node& s);
   /** Add vs -> ss to the substitution */
   void add(const std::vector<Node>& vs, const std::vector<Node>& ss);
   /** Add eq[0] -> eq[1] to the substitution */
@@ -58,7 +58,7 @@ class Subs
   /** Append the substitution s to this */
   void append(Subs& s);
   /** Return the result of this substitution on n */
-  Node apply(Node n) const;
+  virtual Node apply(const Node& n) const;
   /** Return the result of the reverse of this substitution on n */
   Node rapply(Node n) const;
   /** Apply this substitution to all nodes in the range of s */
@@ -76,6 +76,9 @@ class Subs
   /** The data */
   std::vector<Node> d_vars;
   std::vector<Node> d_subs;
+protected:
+  /** check if the substitution v -> s is permitted by this class */
+  virtual void checkSubs(const Node& v, const Node& s);
 };
 
 /**
