cf8fa6e2cd89790e69f92af7e4f594d94227576c
mudathirmahgoub <mudathirmahgoub@gmail.com>
2022-05-23 07:19:09 -0500

    format

--- src/api/java/io/github/cvc5/IOracle.java
+++ src/api/java/io/github/cvc5/IOracle.java
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Top contributors (to current version):
- *   Mudathir Mohamed, Andres Noetzli
+ *   Mudathir Mohamed
  *
  * This file is part of the cvc5 project.
  *
