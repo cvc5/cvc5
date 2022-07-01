62a6110f58706033b5e682b3ad0fdc6e5ad20f22
ajreynol <andrew.j.reynolds@gmail.com>
2022-07-01 11:56:04 -0500

    Fixes

--- src/smt/env.cpp
+++ src/smt/env.cpp
@@ -76,9 +76,9 @@ void Env::finishInit(ProofNodeManager* pnm)
     Assert(d_proofNodeManager == nullptr);
     d_proofNodeManager = pnm;
     d_rewriter->finishInit(*this);
-    d_topLevelSubs.reset(
-        new theory::TrustSubstitutionMap(*this, d_userContext.get()));
   }
+  d_topLevelSubs.reset(
+      new theory::TrustSubstitutionMap(*this, d_userContext.get()));
 }
 
 void Env::shutdown()
