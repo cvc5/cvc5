e310cacec7fba0980f7d2b238ae5af204da3acaf
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-07-22 18:02:55 -0300

    [cnf] Delete unused parameter (#8991)
    This parameter has been made obsolete by the new ways in which we compute unsat cores.

--- src/prop/prop_engine.cpp
+++ src/prop/prop_engine.cpp
@@ -269,7 +269,7 @@ void PropEngine::assertInternal(
     }
     else
     {
-      d_cnfStream->convertAndAssert(node, removable, negated, input);
+      d_cnfStream->convertAndAssert(node, removable, negated);
     }
   }
   else if (isProofEnabled())
@@ -283,7 +283,7 @@ void PropEngine::assertInternal(
   }
   else
   {
-    d_cnfStream->convertAndAssert(node, removable, negated, input);
+    d_cnfStream->convertAndAssert(node, removable, negated);
   }
 }
 
