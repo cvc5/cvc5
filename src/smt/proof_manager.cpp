25fc18b94315678c5ff01ea159e2e195201e23fa
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-07-22 10:17:24 -0300

    [proof] Reintroduce option to print conclusions in internal proof format (#8985)
    Does so via a dedicated printing method in proof nodes. For now it is only used for the final proof node.

--- src/smt/proof_manager.cpp
+++ src/smt/proof_manager.cpp
@@ -211,7 +211,9 @@ void PfManager::printProof(std::ostream& out, std::shared_ptr<ProofNode> fp)
   {
     // otherwise, print using default printer
     out << "(proof\n";
-    out << *fp;
+    // we call the printing method explicitly because we may want to print the
+    // final proof node with conclusions
+    fp->printDebug(out, options().proof.proofPrintConclusion);
     out << "\n)\n";
   }
 }
