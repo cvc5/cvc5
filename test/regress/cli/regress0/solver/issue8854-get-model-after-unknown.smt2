cvc5/cvc5

commit a407bd70c09724dc20af3241775abedd3a73ec67

Connect combination engine to theory engine (#4940)
This connects the implementation of CombinationEngine into TheoryEngine. By default, the combination engine of theory engine is CombinationCareGraph.

This PR also consolidates and simplifies how models are built, note that:

The TheoryModel object no longer tracks whether it is built, instead that is the job of ModelManager,
The TheoryModelBuilder object is no longer responsible for calling TheoryEngine's collect model info method.
Quantifiers engine uses a simpler interface for ensuring that TheoryEngine's model is built.
This PR also makes some minor simplifications to TheoryEngine from the centralEe branch.

Note that no significant behavior changes are intended in this PR.

src/theory/theory_engine.h
@@ -38,7 +38,6 @@
 #include "theory/engine_output_channel.h"
 #include "theory/interrupted.h"
 #include "theory/rewriter.h"
-#include "theory/shared_terms_database.h"
 #include "theory/sort_inference.h"
 #include "theory/substitutions.h"
 #include "theory/term_registration_visitor.h"
@@ -87,11 +86,8 @@ struct NodeTheoryPairHashFunction {
 
 /* Forward declarations */
 namespace theory {
-class CombinationEngine;
 class TheoryModel;
-class TheoryEngineModelBuilder;
-class EqEngineManagerDistributed;
-
+class CombinationEngine;
 class DecisionManager;
 class RelevanceManager;
 
@@ -122,6 +118,7 @@ class TheoryEngine {
   friend class theory::CombinationEngine;
   friend class theory::quantifiers::TermDb;
   friend class theory::EngineOutputChannel;
+  friend class theory::CombinationEngine;
 
   /** Associated PropEngine engine */
   prop::PropEngine* d_propEngine;
@@ -152,15 +149,8 @@ class TheoryEngine {
    */
   SharedTermsDatabase d_sharedTerms;
 
-  /**
-   * The distributed equality manager. This class is responsible for
-   * configuring the theories of this class for handling equalties
-   * in a "distributed" fashion, i.e. each theory maintains a unique
-   * instance of an equality engine. These equality engines are memory
-   * managed by this class.
-   */
-  std::unique_ptr<theory::EqEngineManagerDistributed> d_eeDistributed;
-
+  /** The combination manager we are using */
+  std::unique_ptr<theory::CombinationEngine> d_tc;
   /**
    * The quantifiers engine
    */
@@ -172,16 +162,12 @@ class TheoryEngine {
   /** The relevance manager */
   std::unique_ptr<theory::RelevanceManager> d_relManager;
 
-  /**
-   * Default model object
-   */
-  theory::TheoryModel* d_curr_model;
-  bool d_aloc_curr_model;
-  /**
-   * Model builder object
-   */
-  theory::TheoryEngineModelBuilder* d_curr_model_builder;
-  bool d_aloc_curr_model_builder;
+  /** Default visitor for pre-registration */
+  PreRegisterVisitor d_preRegistrationVisitor;
+
+  /** Visitor for collecting shared terms */
+  SharedTermsVisitor d_sharedTermsVisitor;
+
   /** are we in eager model building mode? (see setEagerModelBuilding). */
   bool d_eager_model_building;
 
@@ -241,7 +227,6 @@ class TheoryEngine {
   void setIncomplete(theory::TheoryId theory) {
     d_incomplete = true;
   }
-
 
   /**
    * Mapping of propagations from recievers to senders.
@@ -428,11 +413,6 @@ class TheoryEngine {
   context::CDO<bool> d_factsAsserted;
 
   /**
-   * Map from equality atoms to theories that would like to be notified about them.
-   */
-
-
-  /**
    * Assert the formula to the given theory.
    * @param assertion the assertion to send (not necesserily normalized)
    * @param original the assertion as it was sent in from the propagating theory
@@ -466,8 +446,7 @@ class TheoryEngine {
    */
   void getExplanation(std::vector<NodeTheoryPair>& explanationVector, LemmaProofRecipe* lemmaProofRecipe);
 
-public:
-
+ public:
   /**
    * Signal the start of a new round of assertion preprocessing
    */
@@ -510,7 +489,6 @@ public:
    * or during LAST_CALL effort.
    */
   bool isRelevant(Node lit) const;
-
   /**
    * This is called at shutdown time by the SmtEngine, just before
    * destruction.  It is important because there are destruction
@@ -541,11 +519,6 @@ public:
    * @param effort the effort level to use
    */
   void check(theory::Theory::Effort effort);
-
-  /**
-   * Run the combination framework.
-   */
-  void combineTheories();
 
   /**
    * Calls ppStaticLearn() on all theories, accumulating their
@@ -585,8 +558,6 @@ public:
   Node getNextDecisionRequest();
 
   bool properConflict(TNode conflict) const;
-  bool properPropagation(TNode lit) const;
-  bool properExplanation(TNode node, TNode expl) const;
 
   /**
    * Returns an explanation of the node propagated to the SAT solver.
@@ -598,13 +569,6 @@ public:
    * that propagated it.
    */
   Node getExplanationAndRecipe(TNode node, LemmaProofRecipe* proofRecipe);
-
-  /**
-   * collect model info
-   */
-  bool collectModelInfo(theory::TheoryModel* m);
-  /** post process model */
-  void postProcessModel( theory::TheoryModel* m );
 
   /**
    * Get the pointer to the model object used by this theory engine.
@@ -622,6 +586,15 @@ public:
    * was interrupted), then this returns the null pointer.
    */
   theory::TheoryModel* getBuiltModel();
+  /**
+   * This forces the model maintained by the combination engine to be built
+   * if it has not been done so already. This should be called only during a
+   * last call effort check after theory combination is run.
+   *
+   * @return true if the model was successfully built (possibly prior to this
+   * call).
+   */
+  bool buildModel();
   /** set eager model building
    *
    * If this method is called, then this TheoryEngine will henceforth build
@@ -648,11 +621,6 @@ public:
    * SynthConjecture::getSynthSolutions.
    */
   bool getSynthSolutions(std::map<Node, std::map<Node, Node> >& sol_map);
-
-  /**
-   * Get the model builder
-   */
-  theory::TheoryEngineModelBuilder* getModelBuilder() { return d_curr_model_builder; }
 
   /**
    * Get the theory associated to a given Node.
@@ -735,19 +703,12 @@ public:
   std::pair<bool, Node> entailmentCheck(options::TheoryOfMode mode, TNode lit);
 
  private:
-  /** Default visitor for pre-registration */
-  PreRegisterVisitor d_preRegistrationVisitor;
-
-  /** Visitor for collecting shared terms */
-  SharedTermsVisitor d_sharedTermsVisitor;
 
   /** Dump the assertions to the dump */
   void dumpAssertions(const char* tag);
 
   /** For preprocessing pass lifting bit-vectors of size 1 to booleans */
 public:
-
-  SharedTermsDatabase* getSharedTermsDatabase();
 
   SortInference* getSortInference() { return &d_sortInfer; }
 

src/theory/quantifiers_engine.cpp
@@ -670,23 +670,15 @@ void QuantifiersEngine::check( Theory::Effort e ){
       QuantifiersModule::QEffort quant_e =
           static_cast<QuantifiersModule::QEffort>(qef);
       d_curr_effort_level = quant_e;
-      //build the model if any module requested it
+      // Force the theory engine to build the model if any module requested it.
       if (needsModelE == quant_e)
       {
-        if (!d_model->isBuilt())
+        Trace("quant-engine-debug") << "Build model..." << std::endl;
+        if (!d_te->buildModel())
         {
-          // theory engine's model builder is quantifier engine's builder if it
-          // has one
-          Assert(!getModelBuilder()
-                 || getModelBuilder() == d_te->getModelBuilder());
-          Trace("quant-engine-debug") << "Build model..." << std::endl;
-          if (!d_te->getModelBuilder()->buildModel(d_model.get()))
-          {
-            flushLemmas();
-          }
-        }
-        if (!d_model->isBuiltSuccess())
-        {
+          // If we failed to build the model, flush all pending lemmas and
+          // finish.
+          flushLemmas();
           break;
         }
       }

src/theory/theory_model.cpp
@@ -75,8 +75,6 @@ void TheoryModel::finishInit()
 }
 
 void TheoryModel::reset(){
-  d_modelBuilt = false;
-  d_modelBuiltSuccess = false;
   d_modelCache.clear();
   d_comment_str.clear();
   d_sep_heap = Node::null();

src/theory/theory_model.h
@@ -95,21 +95,6 @@ public:
 
   /** reset the model */
   virtual void reset();
-  /** is built
-   *
-   * Have we attempted to build this model since the last
-   * call to reset? Notice for model building techniques
-   * that are not guaranteed to succeed (such as
-   * when quantified formulas are enabled), a true return
-   * value does not imply that this is a model of the
-   * current assertions.
-   */
-  bool isBuilt() { return d_modelBuilt; }
-  /** is built success
-   *
-   * Was this model successfully built since the last call to reset?
-   */
-  bool isBuiltSuccess() { return d_modelBuiltSuccess; }
   //---------------------------- for building the model
   /** Adds a substitution from x to t. */
   void addSubstitution(TNode x, TNode t, bool invalidateCache = true);
@@ -363,10 +348,6 @@ public:
   std::string d_name;
   /** substitution map for this model */
   SubstitutionMap d_substitutions;
-  /** whether we have tried to build this model in the current context */
-  bool d_modelBuilt;
-  /** whether this model has been built successfully */
-  bool d_modelBuiltSuccess;
   /** equality engine containing all known equalities/disequalities */
   eq::EqualityEngine* d_equalityEngine;
   /** approximations (see recordApproximation) */

src/theory/theory_model_builder.cpp
@@ -372,31 +372,10 @@ void TheoryEngineModelBuilder::addToTypeList(
   }
 }
 
-bool TheoryEngineModelBuilder::buildModel(Model* m)
+bool TheoryEngineModelBuilder::buildModel(TheoryModel* tm)
 {
   Trace("model-builder") << "TheoryEngineModelBuilder: buildModel" << std::endl;
-  TheoryModel* tm = (TheoryModel*)m;
   eq::EqualityEngine* ee = tm->d_equalityEngine;
-
-  // buildModel should only be called once per check
-  Assert(!tm->isBuilt());
-
-  // Reset model
-  tm->reset();
-
-  // mark as built
-  tm->d_modelBuilt = true;
-  tm->d_modelBuiltSuccess = false;
-
-  // Collect model info from the theories
-  Trace("model-builder") << "TheoryEngineModelBuilder: Collect model info..."
-                         << std::endl;
-  if (!d_te->collectModelInfo(tm))
-  {
-    Trace("model-builder")
-        << "TheoryEngineModelBuilder: fail collect model info" << std::endl;
-    return false;
-  }
 
   Trace("model-builder")
       << "TheoryEngineModelBuilder: Preprocess build model..." << std::endl;
@@ -943,7 +922,6 @@ bool TheoryEngineModelBuilder::buildModel(Model* m)
     return false;
   }
   Trace("model-builder") << "TheoryEngineModelBuilder: success" << std::endl;
-  tm->d_modelBuiltSuccess = true;
   return true;
 }
 void TheoryEngineModelBuilder::computeAssignableInfo(
@@ -1124,7 +1102,6 @@ void TheoryEngineModelBuilder::postProcessModel(bool incomplete, Model* m)
 void TheoryEngineModelBuilder::debugCheckModel(TheoryModel* tm)
 {
 #ifdef CVC4_ASSERTIONS
-  Assert(tm->isBuilt());
   if (tm->hasApproximations())
   {
     // models with approximations may fail the assertions below

src/theory/theory_model_builder.h
@@ -46,19 +46,22 @@ class TheoryEngineModelBuilder
  public:
   TheoryEngineModelBuilder(TheoryEngine* te);
   virtual ~TheoryEngineModelBuilder() {}
-  /** Build model function.
-   *
-   * Should be called only on TheoryModels m.
+  /**
+   * Should be called only on models m after they have been prepared
+   * (e.g. using ModelManager). In other words, the equality engine of model
+   * m contains all relevant information from each theory that is needed
+   * for building a model. This class is responsible simply for ensuring
+   * that all equivalence classes of the equality engine of m are assigned
+   * constants.
    *
    * This constructs the model m, via the following steps:
-   * (1) call TheoryEngine::collectModelInfo,
-   * (2) builder-specified pre-processing,
-   * (3) find the equivalence classes of m's
+   * (1) builder-specified pre-processing,
+   * (2) find the equivalence classes of m's
    *     equality engine that initially contain constants,
-   * (4) assign constants to all equivalence classes
+   * (3) assign constants to all equivalence classes
    *     of m's equality engine, through alternating
    *     iterations of evaluation and enumeration,
-   * (5) builder-specific processing, which includes assigning total
+   * (4) builder-specific processing, which includes assigning total
    *     interpretations to uninterpreted functions.
    *
    * This function returns false if any of the above
@@ -67,7 +70,7 @@ class TheoryEngineModelBuilder
    * builder in steps (2) or (5), for instance, if the model we
    * are building fails to satisfy a quantified formula.
    */
-  bool buildModel(Model* m);
+  bool buildModel(TheoryModel* m);
 
   /** postprocess model
    *

commit ca5c012126fe9fe56e1ed679cc8700a19f27cc03

Allow `(get-model)` after answering `unknown`
Fixes #8854. Commit a407bd70c09724dc20af3241775abedd3a73ec67
accidentally removed our ability to print models after answering
`unknown`. Before that commit, we were able to print the models when we
had a theory that required a last effort check. In that case, we were
generating models periodically that would then get printed. This commit
restores this ability, simplifies `TheoryEngine`, and attempts to build
a model if none exists. I have tested multiple theories and examples and
it seems to work fairly well.

src/theory/theory_engine.cpp
@@ -225,7 +225,6 @@ TheoryEngine::TheoryEngine(Env& env)
       d_decManager(new DecisionManager(userContext())),
       d_relManager(nullptr),
       d_inConflict(context(), false),
-      d_inSatMode(false),
       d_incomplete(context(), false),
       d_incompleteTheory(context(), THEORY_BUILTIN),
       d_incompleteId(context(), IncompleteId::UNKNOWN),
@@ -494,12 +493,6 @@ void TheoryEngine::check(Theory::Effort effort) {
       {
         d_relManager->notifyCandidateModel(getModel());
       }
-      if (!d_inConflict && !needCheck())
-      {
-        // We only mark that we are in "SAT mode". We build the model later only
-        // if the user asks for it via getBuiltModel.
-        d_inSatMode = true;
-      }
     }
 
     Trace("theory") << "TheoryEngine::check(" << effort << "): done, we are " << (d_inConflict ? "unsat" : "sat") << (d_lemmasAdded ? " with new lemmas" : " with no new lemmas");
@@ -609,11 +602,6 @@ TheoryModel* TheoryEngine::getBuiltModel()
   // If this method was called, we should be in SAT mode, and produceModels
   // should be true.
   AlwaysAssert(options().smt.produceModels);
-  if (!d_inSatMode)
-  {
-    // not available, perhaps due to interuption.
-    return nullptr;
-  }
   // must build model at this point
   if (!d_tc->buildModel())
   {
@@ -679,8 +667,6 @@ bool TheoryEngine::presolve() {
 }/* TheoryEngine::presolve() */
 
 void TheoryEngine::postsolve() {
-  // no longer in SAT mode
-  d_inSatMode = false;
   // Reset the interrupt flag
   d_interrupted = false;
 }
@@ -1084,18 +1070,6 @@ theory::EqualityStatus TheoryEngine::getEqualityStatus(TNode a, TNode b)
 {
   Assert(a.getType() == b.getType());
   return d_sharedSolver->getEqualityStatus(a, b);
-}
-
-std::unordered_set<TNode> TheoryEngine::getRelevantAssertions(bool& success)
-{
-  // if we are not in SAT mode, or there is no relevance manager, we fail
-  if (!d_inSatMode || d_relManager == nullptr)
-  {
-    success = false;
-    // return empty set
-    return std::unordered_set<TNode>();
-  }
-  return d_relManager->getRelevantAssertions(success);
 }
 
 void TheoryEngine::getDifficultyMap(std::map<Node, Node>& dmap)

src/theory/theory_engine.h
@@ -350,20 +350,6 @@ class TheoryEngine : protected EnvObj
   Node getModelValue(TNode var);
 
   /**
-   * Get relevant assertions. This returns a set of assertions that are
-   * currently asserted to this TheoryEngine that propositionally entail the
-   * (preprocessed) input formula and all theory lemmas that have been marked
-   * NEEDS_JUSTIFY. For more details on this, see relevance_manager.h.
-   *
-   * This method updates success to false if the set of relevant assertions
-   * is not available. This may occur if we are not in SAT mode, if the
-   * relevance manager is disabled (see option::relevanceFilter) or if the
-   * relevance manager failed to compute relevant assertions due to an internal
-   * error.
-   */
-  std::unordered_set<TNode> getRelevantAssertions(bool& success);
-
-  /**
    * Get difficulty map, which populates dmap, mapping preprocessed assertions
    * to a value that estimates their difficulty for solving the current problem.
    *
@@ -520,9 +506,6 @@ class TheoryEngine : protected EnvObj
   /** The relevance manager */
   std::unique_ptr<theory::RelevanceManager> d_relManager;
 
-  /** are we in eager model building mode? (see setEagerModelBuilding). */
-  bool d_eager_model_building;
-
   /**
    * Output channels for individual theories.
    */
@@ -532,13 +515,6 @@ class TheoryEngine : protected EnvObj
    * Are we in conflict.
    */
   context::CDO<bool> d_inConflict;
-
-  /**
-   * Are we in "SAT mode"? In this state, the user can query for the model.
-   * This corresponds to the state in Figure 4.1, page 52 of the SMT-LIB
-   * standard, version 2.6.
-   */
-  bool d_inSatMode;
 
   /**
    * True if a theory has notified us of incompleteness (at this

test/regress/cli/CMakeLists.txt
@@ -1379,6 +1379,7 @@ set(regress_0_tests
   regress0/smtlib/reset-set-logic.smt2
   regress0/smtlib/reset.smt2
   regress0/smtlib/set-info-status.smt2
+  regress0/solver/issue8854-get-model-after-unknown.smt2
   regress0/strings/bug001.smt2
   regress0/strings/bug002.smt2
   regress0/strings/bug612.smt2

test/regress/cli/regress0/solver/issue8854-get-model-after-unknown.smt2
@@ -1,0 +1,31 @@
+; COMMAND-LINE: --rlimit-per=100
+; EXPECT: unknown
+; EXPECT: (:reason-unknown resourceout)
+; EXPECT: (
+; EXPECT: define-fun
+; EXPECT: define-fun
+; EXPECT: define-fun
+; EXPECT: )
+(set-logic ALL)
+(set-info :smt-lib-version 2.6)
+(set-option :produce-models true)
+(set-option :incremental true)
+
+(declare-fun x () Int)
+(declare-fun y () Int)
+(declare-fun z () Int)
+
+(assert (<= 1 x))
+(assert (<= 1 y))
+(assert (<= 1 z))
+
+(assert
+  (not (not (= (+ (* (* x x) x) (* (* y y) y)) (* (* z z) z)))))
+
+(assert
+  (forall ((x1 Int) (y1 Int) (z1 Int))
+    (=> (<= x1 y1) (=> (<= 0 z1) (<= (* x1 z1) (* y1 z1))))))
+
+(check-sat)
+(get-info :reason-unknown)
+(get-model)