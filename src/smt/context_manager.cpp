0d31ea5d4a3d164c7e6958f33714ff6b4f7c2eac
ajreynol <andrew.j.reynolds@gmail.com>
2022-08-05 15:14:56 -0500

    Decouple context management from solver engine state

---
+++ src/smt/context_manager.cpp
@@ -0,0 +1,197 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds, Aina Niemetz, Morgan Deters
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Utility for maintaining the state of the SMT engine.
+ */
+
+#include "smt/solver_engine_state.h"
+
+#include "base/modal_exception.h"
+#include "options/base_options.h"
+#include "options/main_options.h"
+#include "options/option_exception.h"
+#include "options/smt_options.h"
+#include "smt/env.h"
+#include "smt/solver_engine.h"
+
+namespace cvc5::internal {
+namespace smt {
+
+ContextManager::ContextManager(Env& env, SolverEngine& slv)
+    : EnvObj(env),
+      d_slv(slv),
+      d_pendingPops(0),
+      d_fullyInited(false),
+      d_queryMade(false),
+      d_needPostsolve(false),
+      d_status(),
+      d_expectedStatus(),
+      d_smtMode(SmtMode::START)
+{
+}
+void ContextManager::notifyResetAssertions()
+{
+  doPendingPops();
+  while (!d_userLevels.empty())
+  {
+    userPop();
+  }
+  // Remember the global push/pop around everything when beyond Start mode
+  // (see solver execution modes in the SMT-LIB standard)
+  Assert(d_userLevels.size() == 0 && userContext()->getLevel() == 1);
+  popto(0);
+}
+
+void ContextManager::setup()
+{
+  // push a context
+  push();
+}
+
+void ContextManager::finishInit()
+{
+  // set the flag to remember that we are fully initialized
+  d_fullyInited = true;
+}
+
+void ContextManager::shutdown()
+{
+  doPendingPops();
+
+  while (options().base.incrementalSolving && userContext()->getLevel() > 1)
+  {
+    internalPop(true);
+  }
+}
+
+void ContextManager::cleanup()
+{
+  // pop to level zero
+  popto(0);
+}
+
+void ContextManager::userPush()
+{
+  if (!options().base.incrementalSolving)
+  {
+    throw ModalException(
+        "Cannot push when not solving incrementally (use --incremental)");
+  }
+
+  d_userLevels.push_back(userContext()->getLevel());
+  internalPush();
+  Trace("userpushpop") << "ContextManager: pushed to level "
+                       << userContext()->getLevel() << std::endl;
+}
+
+void ContextManager::userPop()
+{
+  if (!options().base.incrementalSolving)
+  {
+    throw ModalException(
+        "Cannot pop when not solving incrementally (use --incremental)");
+  }
+  if (d_userLevels.size() == 0)
+  {
+    throw ModalException("Cannot pop beyond the first user frame");
+  }
+
+  AlwaysAssert(userContext()->getLevel() > 0);
+  AlwaysAssert(d_userLevels.back() < userContext()->getLevel());
+  while (d_userLevels.back() < userContext()->getLevel())
+  {
+    internalPop(true);
+  }
+  d_userLevels.pop_back();
+}
+void ContextManager::push()
+{
+  userContext()->push();
+  context()->push();
+}
+
+void ContextManager::pop()
+{
+  userContext()->pop();
+  context()->pop();
+}
+
+void ContextManager::popto(int toLevel)
+{
+  context()->popto(toLevel);
+  userContext()->popto(toLevel);
+}
+
+Result ContextManager::getStatus() const { return d_status; }
+
+bool ContextManager::isFullyInited() const { return d_fullyInited; }
+bool ContextManager::isFullyReady() const
+{
+  return d_fullyInited && d_pendingPops == 0;
+}
+bool ContextManager::isQueryMade() const { return d_queryMade; }
+size_t ContextManager::getNumUserLevels() const
+{
+  return d_userLevels.size();
+}
+void ContextManager::internalPush()
+{
+  Assert(d_fullyInited);
+  Trace("smt") << "ContextManager::internalPush()" << std::endl;
+  doPendingPops();
+  if (options().base.incrementalSolving)
+  {
+    // notifies the SolverEngine to process the assertions immediately
+    d_slv.notifyPushPre();
+    userContext()->push();
+    // the context push is done inside of the SAT solver
+    d_slv.notifyPushPost();
+  }
+}
+
+void ContextManager::internalPop(bool immediate)
+{
+  Assert(d_fullyInited);
+  Trace("smt") << "ContextManager::internalPop()" << std::endl;
+  if (options().base.incrementalSolving)
+  {
+    ++d_pendingPops;
+  }
+  if (immediate)
+  {
+    doPendingPops();
+  }
+}
+
+void ContextManager::doPendingPops()
+{
+  Trace("smt") << "ContextManager::doPendingPops()" << std::endl;
+  Assert(d_pendingPops == 0 || options().base.incrementalSolving);
+  // check to see if a postsolve() is pending
+  if (d_needPostsolve)
+  {
+    d_slv.notifyPostSolve();
+    d_needPostsolve = false;
+  }
+  while (d_pendingPops > 0)
+  {
+    // the context pop is done inside of the SAT solver
+    d_slv.notifyPopPre();
+    // pop the context
+    userContext()->pop();
+    --d_pendingPops;
+    // no need for pop post (for now)
+  }
+}
+
+}  // namespace smt
+}  // namespace cvc5::internal
---
+++ src/smt/context_manager.h
@@ -0,0 +1,165 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds, Aina Niemetz, Ying Sheng
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Utility for maintaining the state of the SMT engine.
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__SMT__CONTEXT_MANAGER_H
+#define CVC5__SMT__CONTEXT_MANAGER_H
+
+#include <string>
+
+#include "context/context.h"
+#include "smt/env_obj.h"
+
+namespace cvc5::internal {
+
+class SolverEngine;
+
+namespace smt {
+
+/**
+ * This utility is responsible for maintaining the basic state of the
+ * SolverEngine.
+ *
+ * It has no concept of anything related to the assertions of the SolverEngine,
+ * or more generally it does not depend on Node.
+ *
+ * This class has three sets of interfaces:
+ * (1) notification methods that are used by SolverEngine to notify when an
+ * event occurs (e.g. the beginning of a check-sat call), (2) maintaining the
+ * SAT and user contexts to be used by the SolverEngine, (3) general information
+ * queries, including the mode that the SolverEngine is in, based on the
+ * notifications it has received.
+ *
+ * It maintains a reference to the SolverEngine for the sake of making
+ * callbacks.
+ */
+class ContextManager : protected EnvObj
+{
+ public:
+  ContextManager(Env& env, SolverEngine& smt);
+  ~ContextManager() {}
+  /**
+   * Notify that the expected status of the next check-sat is given by the
+   * string status, which should be one of "sat", "unsat" or "unknown".
+   */
+  void notifyExpectedStatus(const std::string& status);
+  /**
+   * Notify that the SolverEngine is fully initialized, which is called when
+   * options are finalized.
+   */
+  void notifyFullyInited();
+  /**
+   * Notify that we are resetting the assertions, called when a reset-assertions
+   * command is issued by the user.
+   */
+  void notifyResetAssertions();
+  /**
+   * Setup the context, which makes a single push to maintain a global
+   * context around everything.
+   */
+  void setup();
+  /**
+   * Set that we are in a fully initialized state.
+   */
+  void finishInit();
+  /**
+   * Prepare for a shutdown of the SolverEngine, which does pending pops and
+   * pops the user context to zero.
+   */
+  void shutdown();
+  /**
+   * Cleanup, which pops all contexts to level zero.
+   */
+  void cleanup();
+
+  //---------------------------- context management
+  /**
+   * Do all pending pops, which ensures that the context levels are up-to-date.
+   * This method should be called by the SolverEngine before using any of its
+   * members that rely on the context (e.g. PropEngine or TheoryEngine).
+   */
+  void doPendingPops();
+  /**
+   * Called when the user of SolverEngine issues a push. This corresponds to
+   * the SMT-LIB command push.
+   */
+  void userPush();
+  /**
+   * Called when the user of SolverEngine issues a pop. This corresponds to
+   * the SMT-LIB command pop.
+   */
+  void userPop();
+  //---------------------------- end context management
+
+  //---------------------------- queries
+  /**
+   * Return true if the SolverEngine is fully initialized (post-construction).
+   * This post-construction initialization is automatically triggered by the
+   * use of the SolverEngine; e.g. when the first formula is asserted, a call
+   * to simplify() is issued, a scope is pushed, etc.
+   */
+  bool isFullyInited() const;
+  /**
+   * Return true if the SolverEngine is fully initialized and there are no
+   * pending pops.
+   */
+  bool isFullyReady() const;
+  /**
+   * Return true if a notifyCheckSat call has been made, e.g. a query has been
+   * issued to the SolverEngine.
+   */
+  bool isQueryMade() const;
+  /** Return the user context level.  */
+  size_t getNumUserLevels() const;
+  /** Get the status of the last check-sat */
+  Result getStatus() const;
+  /** Get the SMT mode we are in */
+  SmtMode getMode() const;
+  //---------------------------- end queries
+
+ private:
+  /** Pushes the user and SAT contexts */
+  void push();
+  /** Pops the user and SAT contexts */
+  void pop();
+  /** Pops the user and SAT contexts to the given level */
+  void popto(int toLevel);
+  /**
+   * Internal push, which processes any pending pops, and pushes (if in
+   * incremental mode).
+   */
+  void internalPush();
+  /**
+   * Internal pop. If immediate is true, this processes any pending pops, and
+   * pops (if in incremental mode). Otherwise, it increments the pending pop
+   * counter and does nothing.
+   */
+  void internalPop(bool immediate = false);
+  /** Reference to the SolverEngine */
+  SolverEngine& d_slv;
+  /** The context levels of user pushes */
+  std::vector<int> d_userLevels;
+
+  /**
+   * Number of internal pops that have been deferred.
+   */
+  unsigned d_pendingPops;
+};
+
+}  // namespace smt
+}  // namespace cvc5::internal
+
+#endif
