1537efd16e1350b7ba7f125ca6d4de9c3f55286f
ajreynol <andrew.j.reynolds@gmail.com>
2022-08-08 14:33:20 -0500

    Proper restriction

--- src/smt/solver_engine_state.h
+++ src/smt/solver_engine_state.h
@@ -71,20 +71,24 @@ class SolverEngineState : protected EnvObj
   void notifyResetAssertions();
   /**
    * Notify that we are about to call check-sat. This call is made prior to
-   * initializing the assertions. It processes pending pops and pushes a
-   * (user) context if necessary.
-   *
-   * @param hasAssumptions Whether the call to check-sat has assumptions. If
-   * so, we push a context.
+   * initializing the assertions.
    */
-  void notifyCheckSat(bool hasAssumptions);
+  void notifyCheckSat();
+  /**
+   * Called when the user of SolverEngine issues a push. This corresponds to
+   * the SMT-LIB command push.
+   */
+  void notifyUserPush();
+  /**
+   * Called when the user of SolverEngine issues a pop. This corresponds to
+   * the SMT-LIB command pop.
+   */
+  void notifyUserPop();
   /**
    * Notify that the result of the last check-sat was r. This should be called
    * once immediately following notifyCheckSat() if the check-sat call
    * returned normal (i.e. it was not interupted).
    *
-   * @param hasAssumptions Whether the prior call to check-sat had assumptions.
-   * If so, we pop a context.
    * @param r The result of the check-sat call.
    */
   void notifyCheckSatResult(bool hasAssumptions, const Result& r);
@@ -118,42 +122,9 @@ class SolverEngineState : protected EnvObj
    */
   void notifyGetInterpol(bool success);
   /**
-   * Setup the context, which makes a single push to maintain a global
-   * context around everything.
-   */
-  void setup();
-  /**
    * Set that we are in a fully initialized state.
    */
   void finishInit();
-  /**
-   * Prepare for a shutdown of the SolverEngine, which does pending pops and
-   * pops the user context to zero.
-   */
-  void shutdown();
-  /**
-   * Cleanup, which pops all contexts to level zero.
-   */
-  void cleanup();
-
-  //---------------------------- context management
-  /**
-   * Do all pending pops, which ensures that the context levels are up-to-date.
-   * This method should be called by the SolverEngine before using any of its
-   * members that rely on the context (e.g. PropEngine or TheoryEngine).
-   */
-  void doPendingPops();
-  /**
-   * Called when the user of SolverEngine issues a push. This corresponds to
-   * the SMT-LIB command push.
-   */
-  void userPush();
-  /**
-   * Called when the user of SolverEngine issues a pop. This corresponds to
-   * the SMT-LIB command pop.
-   */
-  void userPop();
-  //---------------------------- end context management
 
   //---------------------------- queries
   /**
@@ -164,17 +135,10 @@ class SolverEngineState : protected EnvObj
    */
   bool isFullyInited() const;
   /**
-   * Return true if the SolverEngine is fully initialized and there are no
-   * pending pops.
-   */
-  bool isFullyReady() const;
-  /**
    * Return true if a notifyCheckSat call has been made, e.g. a query has been
    * issued to the SolverEngine.
    */
   bool isQueryMade() const;
-  /** Return the user context level.  */
-  size_t getNumUserLevels() const;
   /** Get the status of the last check-sat */
   Result getStatus() const;
   /** Get the SMT mode we are in */
@@ -182,33 +146,6 @@ class SolverEngineState : protected EnvObj
   //---------------------------- end queries
 
  private:
-  /** Pushes the user and SAT contexts */
-  void push();
-  /** Pops the user and SAT contexts */
-  void pop();
-  /** Pops the user and SAT contexts to the given level */
-  void popto(int toLevel);
-  /**
-   * Internal push, which processes any pending pops, and pushes (if in
-   * incremental mode).
-   */
-  void internalPush();
-  /**
-   * Internal pop. If immediate is true, this processes any pending pops, and
-   * pops (if in incremental mode). Otherwise, it increments the pending pop
-   * counter and does nothing.
-   */
-  void internalPop(bool immediate = false);
-  /** Reference to the SolverEngine */
-  SolverEngine& d_slv;
-  /** The context levels of user pushes */
-  std::vector<int> d_userLevels;
-
-  /**
-   * Number of internal pops that have been deferred.
-   */
-  unsigned d_pendingPops;
-
   /**
    * Whether or not the SolverEngine is fully initialized (post-construction).
    * This post-construction initialization is automatically triggered by the
@@ -226,14 +163,6 @@ class SolverEngineState : protected EnvObj
    * a ModalException during notifyCheckSat.
    */
   bool d_queryMade;
-
-  /**
-   * Internal status flag to indicate whether we have been issued a
-   * notifyCheckSat call and have yet to process the "postsolve" methods of
-   * SolverEngine via SolverEngine::notifyPostSolve().
-   */
-  bool d_needPostsolve;
-
   /**
    * Most recent result of last checkSatisfiability in the
    * SolverEngine.
