d5c4f84d3fcad9574dd134f61ace3a795e1cef2b
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2022-08-22 13:32:41 -0500

    Update src/expr/node_manager_template.cpp
    Co-authored-by: Andres Noetzli <andres.noetzli@gmail.com>

--- src/expr/node_manager_template.cpp
+++ src/expr/node_manager_template.cpp
@@ -615,7 +615,7 @@ std::vector<TypeNode> NodeManager::mkMutualDatatypeTypesInternal(
   DatatypeIndexAttr dia;
   for (const DType& dt : datatypes)
   {
-    Assert (!dt.isResolved()) << "datatype is already resolved";
+    Assert(!dt.isResolved()) << "datatype is already resolved";
     uint32_t index = d_dtypes.size();
     d_dtypes.push_back(std::unique_ptr<DType>(new DType(dt)));
     DType* dtp = d_dtypes.back().get();
