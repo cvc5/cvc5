adeb458c5d86c8681e5247785fad9b93e17fc019
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-09-27 16:18:19 -0300

    allrun changes

--- allrun.sh
+++ allrun.sh
@@ -1,18 +1,16 @@
-# solver=cvc4
-solver=./alethe-check.sh
-options="--tlimit=1500 --simplification=none --full-saturate-quant --no-stats"
+solver="./prod/bin/cvc5"
+options="--lang smt2 --dump-proofs --proof-format=alethe --proof-granularity=theory-rewrite "
 traces=""
 time=""
-ulimit="ulimit -S -t 10"
-file="isabelle-mirabelle.txt"
-path="/home/hbarbosa/benchmarks/isabelle-mirabelle/"
+ulimit="ulimit -S -t 5"
+file="qf-liageneric.txt"
 
 echo "Options: $options"
 echo "Traces: $traces"
 echo
 
 while read name; do
-    echo "$solver on $path$name";
-    $ulimit; $time $solver "$path$name" $options $traces;
+    echo "$solver on $name";
+    $ulimit; $time $solver $options $traces $name > $name.cvc5proof;
     echo "=====================================";
 done < $file
