8484ae3b69c8c13a205f380fcaaae07b5baad90c
Alex Ozdemir <aozdemir@hmc.edu>
2022-10-07 10:04:51 -0700

    doc

--- src/theory/ff/groebner.h
+++ src/theory/ff/groebner.h
@@ -39,6 +39,21 @@ namespace ff {
 
 // Class for constructing a polynomial ideal for an incremental source of
 // generators.
+//
+// Essentially, you can pass a sequence of generators to this class:
+//
+//     p1, p2, p3, p4, p5, p6, p7
+//
+// in chunks. E.g., p1, p2 and then p3, p4, p5, p6, p7. After each chunk, you
+// can:
+//
+// * check if the whole sequence generates a trivial ideal
+//   * if so, get a subset of the generators that cause triviality
+// * if not, check if the variety (common zero set) for the sequence is empty
+// * if not, get a common zero
+//
+// The class support backtracking/popping: you can remove a chunk of generators
+// at any time.
 class IncrementalIdeal : EnvObj
 {
  public:
@@ -59,17 +74,34 @@ class IncrementalIdeal : EnvObj
   void pop();
 
  private:
+  // Run common-root extraction, ensuring a common root is found if one exists.
   void ensureSolution();
 
+  // Used to manage the sequence of generators. A new context is pushed for
+  // each new chunk of generators, and popped when they are removed.
+  //
+  // With this context, other data structures are automatically updated as
+  // generators are added/removed.
   std::unique_ptr<context::Context> d_context;
 
+  // The ring that generators/the ideal live in
   CoCoA::ring d_polyRing;
+  // Used to record steps in the ideal membership calculus.
   IncrementalTracer d_tracer{};
+  // The sequence of generators
   context::CDList<CoCoA::RingElem> d_gens;
+  // A GB for the current generators.
   context::CDO<std::vector<CoCoA::RingElem>> d_basis;
+  // Whether we've already computed a core for ideal triviality.
   context::CDO<bool> d_hasCore;
+  // What that core is.
   context::CDO<std::vector<size_t>> d_core;
+  // Whether we've search for a common root, and whether we found one.
+  //
+  // Empty if we haven't searched. True if we found one, False if not.
   context::CDO<std::optional<bool>> d_hasSolution;
+
+  // What the common root is.
   context::CDO<std::vector<CoCoA::RingElem>> d_solution;
 };
 
