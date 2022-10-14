9fe4c4ae6b0f5492567717a2c3f40b5665a7ecc1
ajreynol <andrew.j.reynolds@gmail.com>
2022-10-14 08:34:43 -0500

    More

--- src/proof/lfsc/lfsc_printer.h
+++ src/proof/lfsc/lfsc_printer.h
@@ -121,7 +121,8 @@ class LfscPrinter
                      bool letTop = true);
   /**
    * print let list, prints definitions of lbind on out in order, and closing
-   * parentheses on cparen.
+   * parentheses on cparen. If asDefs is true, then the definition is printed
+   * as a standalone define statement on out.
    */
   void printLetList(std::ostream& out,
                     std::ostream& cparen,
