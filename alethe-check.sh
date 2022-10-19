c3fee4c99aabcd5269ec6b6360244dedfa848a9f
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-10-19 19:00:17 -0300

    generalize to facilitate parallel execution

--- alethe-check.sh
+++ alethe-check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./prod/bin/cvc5 --proof-format-mode=alethe --proof-granularity=theory-rewrite --lang=smt2 --dump-proofs --proof-alethe-res-pivots $@ > tmp.alethe
-tail -n +2 tmp.alethe > pf.alethe
-alethe-proof-checker check --allow-int-real-subtyping pf.alethe $1
-# alethe-proof-checker check pf.alethe $1
+tfile1="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
+tfile2="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
+
+./prod/bin/cvc5 --proof-format-mode=alethe --proof-granularity=theory-rewrite --lang=smt2 --dump-proofs  --proof-alethe-res-pivots $@ > $tfile1
+tail -n +2 $tfile1 > $tfile2
+carcara check --allow-int-real-subtyping $tfile2 $1
