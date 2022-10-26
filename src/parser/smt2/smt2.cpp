16579e413f7e72e08792483727fddb3bbb6f1639
ajreynol <andrew.j.reynolds@gmail.com>
2022-10-26 12:23:08 -0500

    Format

--- src/parser/smt2/smt2.cpp
+++ src/parser/smt2/smt2.cpp
@@ -791,7 +791,8 @@ bool Smt2::sygus() const
 
 bool Smt2::hasGrammars() const
 {
-  return sygus() || d_solver->getOption("produce-abducts") == "true"  || d_solver->getOption("produce-interpolants") == "true";
+  return sygus() || d_solver->getOption("produce-abducts") == "true"
+         || d_solver->getOption("produce-interpolants") == "true";
 }
 
 void Smt2::checkThatLogicIsSet()
