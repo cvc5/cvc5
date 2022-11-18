a5b1ae4fab1f02d881533f41c079bbe31116e654
Yoni Zohar <yoni206@gmail.com>
2022-11-18 20:57:16 +0200

    fix

--- check_error.sh
+++ check_error.sh
@@ -11,5 +11,5 @@ make clean; ccache -C; make -j15 &> ~/gmp_large.log
 cat ~/gmp_large.log |grep conversion|sort > ~/gmp_large_conversion.log
 cat ~/main.log |grep conversion | sort > ~/main_conversion.log
 
-vimdiff ~/gmp_large_conversion.log ~/main.conversion.log
+vimdiff ~/gmp_large_conversion.log ~/main_conversion.log
 
