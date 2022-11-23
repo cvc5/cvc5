38f643d8ef34a1aafcad98f7cc7aa5fadeffa4d6
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-11-23 11:08:05 -0300

    use let-binding equiv in checking

--- alethe-check.sh
+++ alethe-check.sh
@@ -3,6 +3,5 @@
 tfile1="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
 tfile2="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
 
-./prod/bin/cvc5 --proof-format-mode=alethe --proof-granularity=theory-rewrite --lang=smt2 --dump-proofs  --proof-alethe-res-pivots $@ > $tfile1
-tail -n +2 $tfile1 > $tfile2
-carcara check --allow-int-real-subtyping $tfile2 $1
+./prod/bin/cvc5 --proof-format-mode=alethe --proof-granularity=theory-rewrite --lang=smt2 --dump-proofs  --proof-alethe-res-pivots $@ | tail -n +2 > $tfile
+carcara check --allow-int-real-subtyping --expand-let-bindings $tfile $1
