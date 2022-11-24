71f1cdf19f24c2c085b2f2b5c4ec771d6e2df10b
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-11-24 15:04:28 -0300

    fix

--- alethe-check.sh
+++ alethe-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-tfile1="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
-tfile2="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
+# tfile="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
+tfile="/home/hbarbosa/cvc/wt-diff/pf.alethe"
 
 ./prod/bin/cvc5 --proof-format-mode=alethe --proof-granularity=theory-rewrite --lang=smt2 --dump-proofs  --proof-alethe-res-pivots $@ | tail -n +2 > $tfile
 carcara check --allow-int-real-subtyping --expand-let-bindings $tfile $1
