b674f713903e4148a6da7c4f05abde453db3b609
Hanna Lachnitt <lachnitt@stanford.edu>
2022-11-25 14:30:12 -0800

    Fix bug in INT_TIGHT_UB rule translation

--- src/proof/alethe/alethe_post_processor.cpp
+++ src/proof/alethe/alethe_post_processor.cpp
@@ -1467,7 +1467,7 @@ bool AletheProofPostprocessCallback::update(Node res,
     // * the corresponding proof node is (<= i greatestIntLessThan(c))
     case PfRule::INT_TIGHT_UB:
     {
-      Node vp1 = nm->mkNode(kind::SEXPR, d_cl, children[0], res);
+      Node vp1 = nm->mkNode(kind::SEXPR, d_cl, children[0].notNode(), res);
       std::vector<Node> new_children = {vp1, children[0]};
       new_args.push_back(nm->mkConstInt(Rational(1)));
       return addAletheStep(AletheRule::LA_GENERIC, vp1, vp1, {}, new_args, *cdp)
@@ -1492,7 +1492,7 @@ bool AletheProofPostprocessCallback::update(Node res,
     // * the corresponding proof node is (>= i leastIntGreaterThan(c))
     case PfRule::INT_TIGHT_LB:
     {
-      Node vp1 = nm->mkNode(kind::SEXPR, d_cl, children[0], res);
+      Node vp1 = nm->mkNode(kind::SEXPR, d_cl, children[0].notNode(), res);
       std::vector<Node> new_children = {vp1, children[0]};
       new_args.push_back(nm->mkConstInt(Rational(1)));
       return addAletheStep(AletheRule::LA_GENERIC, vp1, vp1, {}, new_args, *cdp)
