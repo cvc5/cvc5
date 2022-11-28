929b75cfe825f963f4fc1bda4e03b2cf32650698
ajreynol <andrew.j.reynolds@gmail.com>
2022-11-28 12:33:53 -0600

    Format

--- src/parser/flex/smt2_cmd_parser.cpp
+++ src/parser/flex/smt2_cmd_parser.cpp
@@ -40,20 +40,21 @@ Command* Smt2CmdParser::parseNextCommand()
       Term t = d_tparser.parseTerm();
     }
     break;
-    case Token::ASSUME_TOK: 
+    case Token::ASSUME_TOK:
     case Token::CONSTRAINT_TOK:
     {
-      bool isAssume = (tok==Token::ASSUME_TOK);
+      bool isAssume = (tok == Token::ASSUME_TOK);
       d_state.checkThatLogicIsSet();
       Term t = d_tparser.parseTerm();
       cmd.reset(new SygusConstraintCommand(t, isAssume));
     }
-      break;
+    break;
     case Token::BLOCK_MODEL_TOK: break;
     case Token::BLOCK_MODEL_VALUES_TOK: break;
-    case Token::CHECK_SAT_TOK: 
+    case Token::CHECK_SAT_TOK:
     {
-      if (d_state.sygus()) {
+      if (d_state.sygus())
+      {
         d_state.parseError("Sygus does not support check-sat command.");
       }
       cmd.reset(new CheckSatCommand());
@@ -99,15 +100,26 @@ Command* Smt2CmdParser::parseNextCommand()
     case Token::DEFINE_FUNS_REC_TOK: break;
     case Token::DEFINE_SORT_TOK: break;
     case Token::ECHO_TOK: break;
-    case Token::EXIT_TOK: { cmd.reset(new QuitCommand()); } break;
+    case Token::EXIT_TOK:
+    {
+      cmd.reset(new QuitCommand());
+    }
+    break;
     case Token::GET_ABDUCT_TOK: break;
     case Token::GET_ABDUCT_NEXT_TOK: break;
-    case Token::GET_ASSERTIONS_TOK: 
-    { d_state.checkThatLogicIsSet(); 
-    cmd.reset(new GetAssertionsCommand()); }break;
+    case Token::GET_ASSERTIONS_TOK:
+    {
+      d_state.checkThatLogicIsSet();
+      cmd.reset(new GetAssertionsCommand());
+    }
+    break;
     case Token::GET_ASSIGNMENT_TOK: break;
-    case Token::GET_DIFFICULTY_TOK: { d_state.checkThatLogicIsSet(); 
-     cmd.reset(new GetDifficultyCommand); }break;
+    case Token::GET_DIFFICULTY_TOK:
+    {
+      d_state.checkThatLogicIsSet();
+      cmd.reset(new GetDifficultyCommand);
+    }
+    break;
     case Token::GET_INFO_TOK:
     {
       const std::string& key = d_tparser.parseKeyword();
@@ -127,10 +139,18 @@ Command* Smt2CmdParser::parseNextCommand()
     case Token::GET_PROOF_TOK: break;
     case Token::GET_QE_TOK: break;
     case Token::GET_QE_DISJUNCT_TOK: break;
-    case Token::GET_UNSAT_ASSUMPTIONS_TOK: { d_state.checkThatLogicIsSet(); 
-    cmd.reset(new GetUnsatAssumptionsCommand); }break;
-    case Token::GET_UNSAT_CORE_TOK: { d_state.checkThatLogicIsSet(); 
-     cmd.reset(new GetUnsatCoreCommand); }break;
+    case Token::GET_UNSAT_ASSUMPTIONS_TOK:
+    {
+      d_state.checkThatLogicIsSet();
+      cmd.reset(new GetUnsatAssumptionsCommand);
+    }
+    break;
+    case Token::GET_UNSAT_CORE_TOK:
+    {
+      d_state.checkThatLogicIsSet();
+      cmd.reset(new GetUnsatCoreCommand);
+    }
+    break;
     case Token::GET_VALUE_TOK: break;
     case Token::INV_CONSTRAINT_TOK: break;
     case Token::POP_TOK: break;
