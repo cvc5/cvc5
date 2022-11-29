5fb62cdfea0a885ce7fcefd8f255a12feea66411
ajreynol <andrew.j.reynolds@gmail.com>
2022-11-29 09:12:29 -0600

    Add proof rule BETA_REDUCE

--- src/proof/proof_rule.h
+++ src/proof/proof_rule.h
@@ -1162,6 +1162,17 @@ enum class PfRule : uint32_t
    * ``APPLY_UF``. \endverbatim
    */
   HO_CONG,
+  /**
+   * \verbatim embed:rst:leading-asterisk
+   * **Equality -- Beta reduction**
+   *
+   * .. math::
+   *
+   *   \inferrule{\mid \lambda x_1\dots x_n.\> t, t_1,\dots,t_n}
+   *   {((\lambda x_1\dots x_n.\> t) t_1 \ldots t_n)=t\{x_1\mapsto t_1,\dots,x_n\mapsto t_n\}}
+   * \endverbatim
+   */
+  BETA_REDUCE,
 
   /**
    * \verbatim embed:rst:leading-asterisk
