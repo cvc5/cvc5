2996eeac5866fa3f62fe28b49de4c75b7af282a4
ajreynol <andrew.j.reynolds@gmail.com>
2022-12-15 18:51:18 -0600

    Fix

--- src/theory/strings/theory_strings.cpp
+++ src/theory/strings/theory_strings.cpp
@@ -1304,7 +1304,7 @@ void TheoryStrings::runInferStep(InferStep s, Theory::Effort e, int effort)
     case CHECK_SEQUENCES_ARRAY_EAGER: d_asolver.checkArrayEager(); break;
     case CHECK_REGISTER_TERMS_NF: checkRegisterTermsNormalForms(); break;
     case CHECK_EXTF_REDUCTION: d_esolver.checkExtfReductions(effort); break;
-    case CHECK_MEMBERSHIP: d_rsolver.checkMemberships(e); break;
+    case CHECK_MEMBERSHIP: d_rsolver.checkMemberships(effort); break;
     case CHECK_CARDINALITY: d_bsolver.checkCardinality(); break;
     default: Unreachable(); break;
   }
