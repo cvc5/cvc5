8fa43c0b17da0c399b1f5ba087bc599bfc84acc7
ajreynol <andrew.j.reynolds@gmail.com>
2022-12-15 18:57:49 -0600

    More

--- src/theory/strings/model_cons.h
+++ src/theory/strings/model_cons.h
@@ -25,6 +25,9 @@ namespace theory {
 namespace strings {
 
 /**
+ * The base class for strings model construction. This is used by TheoryStrings
+ * during collectModeValues for specifying how to construct model values
+ * for string-like terms.
  */
 class ModelCons : protected EnvObj
 {
@@ -32,18 +35,30 @@ class ModelCons : protected EnvObj
   ModelCons(Env& env) : EnvObj(env) {}
   virtual ~ModelCons() {}
 
-  /** Get string representatives from */
+  /**
+   * Get string representatives from
+   * 
+   * Based on the current set of relevant terms in termSet, this gets all
+   * representatives of string-like type to be used for model construction,
+   * and groups them by type.
+   * 
+   * @param termSet The relevant term set
+   * @param repTypes The set of types of terms in the domain of repSet
+   * @param repSet Map from types to the representatives of that type
+   * @param auxEq A list of equalities to assert directly to the model, e.g.
+   * if some term was equated to another.
+   */
   virtual void getStringRepresentativesFrom(
       const std::set<Node>& termSet,
       std::unordered_set<TypeNode>& repTypes,
       std::map<TypeNode, std::unordered_set<Node>>& repSet,
       std::vector<Node>& auxEq) = 0;
-  /** Separate by length
+  /**
+   * Separate by length
    *
    * Separate the string representatives in argument n into a partition cols
    * whose collections have equal length. The i^th vector in cols has length
-   * lts[i] for all elements in col. These vectors are furthmore separated
-   * by string-like type.
+   * lts[i] for all elements in col.
    *
    * Must assign lts to *concrete* lengths.
    */
@@ -51,7 +66,14 @@ class ModelCons : protected EnvObj
                                 std::vector<std::vector<Node>>& cols,
                                 std::vector<Node>& lts) = 0;
   /**
-   * Get the normal form
+   * Get the normal form for n. If the returned vector has length > 1, then the
+   * model value for (all variables in) the equivalence class of n will be
+   * assigned to the model value based on this list. If the returned vector
+   * has length 0, the equivalence class will be assigned the empty string.
+   * If the returned vector has size 1 and contains a constant, the equivalence
+   * class will be assigned that constant. Otherwise, the equivalence class
+   * will be assigned an arbitrary value whose length was determined by
+   * a call to separateByLength.
    */
   virtual std::vector<Node> getNormalForm(Node n) = 0;
 };
