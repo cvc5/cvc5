65a0d0309ac5c5bebccdc25bccbb218efb2ca941
ajreynol <andrew.j.reynolds@gmail.com>
2022-12-17 08:51:33 -0600

    Fix

--- src/parser/api/cpp/command.cpp
+++ src/parser/api/cpp/command.cpp
@@ -170,9 +170,9 @@ void Command::invoke(cvc5::Solver* solver, SymbolManager* sm, std::ostream& out)
   else if (!isMuted())
   {
     printResult(solver, out);
-    // always flush the output
-    out << std::flush;
   }
+  // always flush the output
+  out << std::flush;
 }
 
 std::string Command::toString() const
