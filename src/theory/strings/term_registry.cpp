35818575365b0e0acd7e91759332183904a60475
ajreynol <andrew.j.reynolds@gmail.com>
2023-01-06 15:15:38 -0600

    Fix

--- src/theory/strings/term_registry.cpp
+++ src/theory/strings/term_registry.cpp
@@ -286,7 +286,7 @@ void TermRegistry::preRegisterTerm(TNode n)
   // their arguments have string type and do not introduce any shared
   // terms.
   if (n.hasOperator() && ee->isFunctionKind(k)
-      && kindToTheoryId(k) == THEORY_STRINGS && k != STRING_CONCAT)
+      && kindToTheoryId(k) == THEORY_STRINGS && k != STRING_CONCAT && k != STRING_IN_REGEXP)
   {
     d_functionsTerms.push_back(n);
   }
