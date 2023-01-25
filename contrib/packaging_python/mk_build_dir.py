3722300fef991df63a8b9250c5043fd3bb75dd3b
Vinícius Camillo <viniciusrcamillo@gmail.com>
2023-01-25 10:43:11 -0300

    Changing mk_build_dir.py.

--- contrib/packaging_python/mk_build_dir.py
+++ contrib/packaging_python/mk_build_dir.py
@@ -22,10 +22,10 @@ from skbuild.cmaker import CMaker
 
 python_version = CMaker.get_python_version()
 args = [
-	'-DPYTHON_VERSION_STRING:STRING=' + sys.version.split(' ')[0],
-	'-DPYTHON_INCLUDE_DIR:PATH=' +
+	'-DBUILD_BINDINGS_PYTHON_VERSION:STRING=' + sys.version.split(' ')[0],
+	'-DPython_INCLUDE_DIR:PATH=' +
 			CMaker.get_python_include_dir(python_version),
-	'-DPYTHON_LIBRARY:FILEPATH=' +
+	'-DPython_LIBRARY:FILEPATH=' +
 			CMaker.get_python_library(python_version),
 ]
 
