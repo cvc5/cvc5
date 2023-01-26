702d23255f0331c6ff7cf23fba1249d78c5791d4
Vinícius Camillo <viniciusrcamillo@gmail.com>
2023-01-26 07:23:02 -0300

    Added pyparsing.

--- contrib/packaging_python/mk_clean_wheel.sh
+++ contrib/packaging_python/mk_clean_wheel.sh
@@ -33,12 +33,9 @@ ENVDIR=env$PYVERSION
 $PYTHONBIN -m venv ./$ENVDIR
 source ./$ENVDIR/bin/activate
 
-echo "whereis flex"
-whereis flex
-
 # install packages
 pip install -q --upgrade pip setuptools auditwheel
-pip install -q Cython pytest toml scikit-build flex
+pip install -q Cython pytest toml scikit-build flex pyparsing 
 if [ "$(uname)" == "Darwin" ]; then
     # Mac version of auditwheel
     pip install -q delocate
