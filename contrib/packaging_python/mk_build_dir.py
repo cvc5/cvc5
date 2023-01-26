3cda8b6501402a98e6624a1114c93af3ada2742a
Vinícius Camillo <viniciusrcamillo@gmail.com>
2023-01-26 06:51:13 -0300

    Defining Python_ libraries on mk_build_dir.py.

--- contrib/packaging_python/mk_build_dir.py
+++ contrib/packaging_python/mk_build_dir.py
@@ -22,7 +22,11 @@ from skbuild.cmaker import CMaker
 
 python_version = CMaker.get_python_version()
 args = [
-	'-DBUILD_BINDINGS_PYTHON_VERSION:STRING=' + python_version
+	'-DBUILD_BINDINGS_PYTHON_VERSION:STRING=' + python_version,
+	'-DPython_INCLUDE_DIR:PATH=' +
+			CMaker.get_python_include_dir(python_version),
+	'-DPython_LIBRARY:FILEPATH=' +
+			CMaker.get_python_library(python_version),
 ]
 
 subprocess.check_call(['./configure.sh', *sys.argv[1:], *args])
