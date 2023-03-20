eb2e18e493ce94344c722ade8a6b42c47f32bc5d
ajreynol <andrew.j.reynolds@gmail.com>
2023-03-20 12:58:07 -0500

    New type checking algorithm

--- src/expr/type_checker_template.cpp
+++ src/expr/type_checker_template.cpp
@@ -68,13 +68,6 @@ TypeNode TypeChecker::computeType(NodeManager* nodeManager,
   // Infer the type
   switch (n.getKind())
   {
-    case kind::VARIABLE:
-    case kind::SKOLEM:
-      typeNode = nodeManager->getAttribute(n, TypeAttr());
-      break;
-    case kind::BUILTIN:
-      typeNode = nodeManager->builtinOperatorType();
-      break;
 
       // clang-format off
 ${typerules}
@@ -84,10 +77,6 @@ ${typerules}
       Trace("getType") << "FAILURE" << std::endl;
       Unhandled() << " " << n.getKind();
   }
-
-  nodeManager->setAttribute(n, TypeAttr(), typeNode);
-  nodeManager->setAttribute(n, TypeCheckedAttr(),
-                            check || nodeManager->getAttribute(n, TypeCheckedAttr()));
 
   return typeNode;
 
