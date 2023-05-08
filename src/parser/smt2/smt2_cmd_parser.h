26c6441f649780ed651c2c7f8b3cda4cd1890c0a
ajreynol <andrew.j.reynolds@gmail.com>
2023-05-08 08:30:48 -0500

    Move lexing of command tokens to command parser

--- src/parser/smt2/smt2_cmd_parser.h
+++ src/parser/smt2/smt2_cmd_parser.h
@@ -42,12 +42,20 @@ class Smt2CmdParser
   std::unique_ptr<Command> parseNextCommand();
 
  protected:
+  /** Next command token */
+  Token nextCommandToken();
   /** The lexer */
   Smt2Lexer& d_lex;
   /** The state */
   Smt2State& d_state;
   /** The term parser */
   Smt2TermParser& d_tparser;
+  /** Map strings to tokens */
+  std::map<std::string, Token> d_table;
+  /** is strict */
+  bool d_isStrict;
+  /** is sygus */
+  bool d_isSygus;
 };
 
 }  // namespace parser
