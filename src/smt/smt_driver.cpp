6860098a3e4a61b62fa829b33c1f79d4c4eb403e
ajreynol <andrew.j.reynolds@gmail.com>
2023-05-09 12:49:34 -0500

    More

--- src/smt/smt_driver.cpp
+++ src/smt/smt_driver.cpp
@@ -54,7 +54,9 @@ Result SmtDriver::checkSat(const std::vector<Node>& assumptions)
     Trace("smt") << "SmtSolver::check()" << std::endl;
 
     ResourceManager* rm = d_env.getResourceManager();
+    // begin call for resource manager, which resets per-query information
     rm->beginCall();
+    // if we are already out of (cumulative) resources
     if (rm->out())
     {
       UnknownExplanation why = rm->outOfResources()
