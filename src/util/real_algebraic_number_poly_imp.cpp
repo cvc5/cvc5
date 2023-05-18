eb2afcbe08cf3fa621ef7fc7926036fa869d6a8f
ajreynol <andrew.j.reynolds@gmail.com>
2023-05-18 15:53:38 -0500

    More

--- src/util/real_algebraic_number_poly_imp.cpp
+++ src/util/real_algebraic_number_poly_imp.cpp
@@ -63,8 +63,8 @@ RealAlgebraicNumber::RealAlgebraicNumber(const std::vector<long>& coefficients,
 #ifdef CVC5_ASSERTIONS
   for (long c : coefficients)
   {
-    Assert(std::numeric_limits<std::int32_t>::min() <= c
-           && c <= std::numeric_limits<std::int32_t>::max())
+    Assert(std::numeric_limits<int32_t>::min() <= c
+           && c <= std::numeric_limits<int32_t>::max())
         << "Coefficients need to fit within 32 bit integers. Please use the "
            "constructor based on Integer instead.";
   }
