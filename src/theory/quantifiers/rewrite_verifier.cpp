2157150d0744096963f4c7741e35f87ffab80a6f
ajreynol <andrew.j.reynolds@gmail.com>
2023-06-12 09:58:20 -0500

    More

--- src/theory/quantifiers/rewrite_verifier.cpp
+++ src/theory/quantifiers/rewrite_verifier.cpp
@@ -63,7 +63,7 @@ bool RewriteVerifier::checkEquivalent(Node bv, Node bvr, std::ostream* out)
   }
   size_t npoints = hasVar ? d_sampler->getNumSamplePoints() : 1;
   Trace("sygus-rr-verify") << "Testing rewrite rule " << bv << " ---> " << bvr
-                           << " over " << npoints;
+                           << " over " << npoints
   << " points for " << d_vars << std::endl;
 
   // see if they evaluate to same thing on all sample points
