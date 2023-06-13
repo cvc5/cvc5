b0e1c136a03325c0f659f9c8be25660f231a75d1
ajreynol <andrew.j.reynolds@gmail.com>
2023-06-13 08:59:35 -0500

    Fix

--- src/theory/quantifiers/sygus/rcons_type_info.cpp
+++ src/theory/quantifiers/sygus/rcons_type_info.cpp
@@ -57,13 +57,13 @@ void RConsTypeInfo::initialize(Env& env,
 Node RConsTypeInfo::nextEnum()
 {
   size_t i = Random::getRandom().pickWithProb(d_cp) ? 1 : 0;
+  d_cp *= d_p;
   if (d_enumerators[i] == nullptr)
   {
     // the enumerator is already finished
     return Node::null();
   }
   Node sz = d_enumerators[i]->getCurrent();
-  d_cp *= d_p;
   if (!d_enumerators[i]->increment())
   {
     Trace("sygus-rcons") << "no increment" << std::endl;
