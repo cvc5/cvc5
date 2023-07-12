a5318a03343e8e9df1fcb6c78f5900502253f8c8
ajreynol <andrew.j.reynolds@gmail.com>
2023-07-12 11:07:37 -0500

    Split instantiator file

--- src/theory/quantifiers/cegqi/ceg_instantiator.cpp
+++ src/theory/quantifiers/cegqi/ceg_instantiator.cpp
@@ -1675,22 +1675,6 @@ void CegInstantiator::registerCounterexampleLemma(Node lem,
   }
 }
 
-Instantiator::Instantiator(Env& env, TypeNode tn) : EnvObj(env), d_type(tn)
-{
-  d_closed_enum_type = tn.isClosedEnumerable();
-}
-
-bool Instantiator::processEqualTerm(CegInstantiator* ci,
-                                    SolvedForm& sf,
-                                    Node pv,
-                                    TermProperties& pv_prop,
-                                    Node n,
-                                    CegInstEffort effort)
-{
-  pv_prop.d_type = CEG_TT_EQUAL;
-  return ci->constructInstantiationInc(pv, n, pv_prop, sf);
-}
-
 }  // namespace quantifiers
 }  // namespace theory
 }  // namespace cvc5::internal
