50e0ae53c564faa406539f41cf323cd797cb04fd
Aina Niemetz <aina.niemetz@gmail.com>
2023-07-25 10:10:16 -0700

    Update copyright headers and year in copyright message. (#9909)

--- src/expr/sygus_grammar.h
+++ src/expr/sygus_grammar.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Top contributors (to current version):
- *   Abdalrhman Mohamed
+ *   Abdalrhman Mohamed, Andrew Reynolds
  *
  * This file is part of the cvc5 project.
  *
