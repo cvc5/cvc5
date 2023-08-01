9a72a412e3ee14ae03d8bac71a31190e3bf3c243
Vinícius Camillo <viniciusrcamillo@gmail.com>
2023-08-01 06:52:56 -0300

    Updated hash.

--- cmake/FindPoly.cmake
+++ cmake/FindPoly.cmake
@@ -158,7 +158,7 @@ if(NOT Poly_FOUND_SYSTEM)
     Poly-EP
     ${COMMON_EP_CONFIG}
     URL https://github.com/SRI-CSL/libpoly/archive/refs/tags/v${Poly_VERSION}.tar.gz
-    URL_HASH SHA1=3e5bd56b082847b103e2bedf67c05671e99c157c
+    URL_HASH SHA1=981ff0081bed0d1e604000a3092e7af8881b7a5e
     PATCH_COMMAND
       sed -i.orig
       "s,add_subdirectory(test/polyxx),add_subdirectory(test/polyxx EXCLUDE_FROM_ALL),g"
