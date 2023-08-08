659a6b6d7698c3e67279173dda50f1e99d1e1941
ajreynol <andrew.j.reynolds@gmail.com>
2023-08-08 11:52:20 -0500

    Fix find synth test in python API

--- test/unit/api/python/test_solver.py
+++ test/unit/api/python/test_solver.py
@@ -2252,7 +2252,7 @@ def test_check_synth_next3(solver):
     with pytest.raises(RuntimeError):
         solver.checkSynthNext()
 
-def find_synth(solver):
+def test_find_synth(solver):
     solver.setOption("sygus", "true")
     boolSort = solver.getBooleanSort()
     start = solver.mkVar(boolSort)
@@ -2268,7 +2268,7 @@ def find_synth(solver):
     assert not t.isNull() and t.getSort().isBoolean()
 
 
-def find_synth2(solver):
+def test_find_synth2(solver):
     solver.setOption("sygus", "true")
     solver.setOption("incremental", "true")
     boolSort = solver.getBooleanSort()
