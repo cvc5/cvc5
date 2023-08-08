b94c43cb23fd5fd6af2eae81af58b5da6b51add3
ajreynol <andrew.j.reynolds@gmail.com>
2023-08-08 15:01:42 -0500

    More

--- src/parser/api/cpp/command.cpp
+++ src/parser/api/cpp/command.cpp
@@ -193,20 +193,6 @@ void CommandSuccess::toStream(std::ostream& out) const
   internal::Printer::getPrinter(out)->toStreamCmdSuccess(out);
 }
 
-void Command::resetSolver(cvc5::Solver* solver)
-{
-  std::unique_ptr<internal::Options> opts =
-      std::make_unique<internal::Options>();
-  opts->copyValues(*solver->d_originalOptions);
-  // This reconstructs a new solver object at the same memory location as the
-  // current one. Note that this command does not own the solver object!
-  // It may be safer to instead make the ResetCommand a special case in the
-  // CommandExecutor such that this reconstruction can be done within the
-  // CommandExecutor, who actually owns the solver.
-  solver->~Solver();
-  new (solver) cvc5::Solver(std::move(opts));
-}
-
 void CommandInterrupted::toStream(std::ostream& out) const
 {
   internal::Printer::getPrinter(out)->toStreamCmdInterrupted(out);
@@ -226,6 +212,20 @@ void CommandRecoverableFailure::toStream(std::ostream& out) const
 {
   internal::Printer::getPrinter(out)->toStreamCmdRecoverableFailure(out,
                                                                     d_message);
+}
+
+void Command::resetSolver(cvc5::Solver* solver)
+{
+  std::unique_ptr<internal::Options> opts =
+      std::make_unique<internal::Options>();
+  opts->copyValues(*solver->d_originalOptions);
+  // This reconstructs a new solver object at the same memory location as the
+  // current one. Note that this command does not own the solver object!
+  // It may be safer to instead make the ResetCommand a special case in the
+  // CommandExecutor such that this reconstruction can be done within the
+  // CommandExecutor, who actually owns the solver.
+  solver->~Solver();
+  new (solver) cvc5::Solver(std::move(opts));
 }
 
 internal::Node Command::termToNode(const cvc5::Term& term)
