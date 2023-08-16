d61cc1ede719c0b18e4ea80958ded2b3ef286fa3
ajreynol <andrew.j.reynolds@gmail.com>
2023-08-16 08:59:34 -0500

    Fix

--- src/expr/skolem_manager.cpp
+++ src/expr/skolem_manager.cpp
@@ -162,7 +162,7 @@ Node SkolemManager::mkSkolemFunction(SkolemFunId id, TypeNode tn, Node cacheVal)
     // of the original name.
     if (id == SkolemFunId::INPUT_VARIABLE)
     {
-      k = mkSkolemNode(VARIABLE, cacheVal[0].getConst<String>().toString(), tn);
+      k = mkSkolemNode(VARIABLE, cacheVal[0].getConst<String>().toString(), tn, SKOLEM_EXACT_NAME);
     }
     else
     {
