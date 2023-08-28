57cd8493b8f7fb9b3ea786162e695c468e8c0ca8
mudathirmahgoub <mudathirmahgoub@gmail.com>
2023-08-28 14:59:18 -0500

    address

--- test/api/cpp/proj-issue656.cpp
+++ test/api/cpp/proj-issue656.cpp
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds, Mudathir Mohamed
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Test for project issue #656
+ *
+ */
 #include <cvc5/cvc5.h>
 
 using namespace cvc5;
