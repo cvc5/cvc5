ef8f370afce64c599be84989496f3995e0c40fdd
Haniel Barbosa <hanielbbarbosa@gmail.com>
2023-09-22 15:37:22 -0300

    update documentation

--- include/cvc5/cvc5.h
+++ include/cvc5/cvc5.h
@@ -4393,12 +4393,9 @@ class CVC5_EXPORT Solver
    *
    *     (get-unsat-core-lemmas)
    *
-   * Requires to enable option
-   * :ref:`produce-unsat-core-lemmas <lbl-option-produce-unsat-core-lemmas>`.
+   * Requires the SAT proof unsat core mode, so to enable option
+   * :ref:`unsat-core-mode=sat-proof <lbl-option-unsat-core-mode>`.
    *
-   * .. note::
-   *   This method requires computing unsat cores via the SAT proof, so other
-   *   modes of unsat core are overriden by it.
    * \endverbatim
    *
    * @return A set of terms representing the lemmas used to derive
