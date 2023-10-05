d3557fbe5c59b854940ec5a06bad0170d2c8d20d
mudathirmahgoub <mudathirmahgoub@gmail.com>
2023-10-04 21:00:06 -0500

    more

--- test/unit/api/java/InputParserTest.java
+++ test/unit/api/java/InputParserTest.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestInputParserBlack
+class InputParserTest
 {
   private Solver d_solver;
   private InputLanguage d_lang;  
