74425225078363f72b3ab11ab944cf0bf29aa3fb
mudathirmahgoub <mudathirmahgoub@gmail.com>
2023-10-05 13:20:09 -0500

    more

--- src/api/java/io/github/cvc5/SymbolManager.java
+++ src/api/java/io/github/cvc5/SymbolManager.java
@@ -1,14 +1,75 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Mudathir Mohamed, Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * The cvc5 java API.
+ */
+
 package io.github.cvc5;
+
 public class SymbolManager extends AbstractPointer
 {
-  public SymbolManager(long pointer)
+  SymbolManager(long pointer)
   {
     super(pointer);
   }
+
+  public SymbolManager(Solver solver)
+  {
+    super(newSymbolManager(solver.getPointer()));
+  }
+
+  private static native long newSymbolManager(long solverPointer);
+
   protected String toString(long pointer)
   {
     throw new UnsupportedOperationException(
-        "InputParser.toString() is not supported in the cpp api");
+        "SymbolManager.toString() is not supported in the cpp api");
   }
   protected native void deletePointer(long pointer);
+
+  @Override
+  public boolean equals(Object s)
+  {
+    if (this == s)
+      return true;
+    if (s == null || getClass() != s.getClass())
+      return false;
+    SymbolManager symbolManager = (SymbolManager) s;
+    if (this.pointer == symbolManager.pointer)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * @return true if the logic of this symbol manager has been set.
+   */
+  public boolean isLogicSet()
+  {
+    return isLogicSet(pointer);
+  }
+
+  private native boolean isLogicSet(long pointer);
+
+  /**
+   * @api.note Asserts isLogicSet().
+   *
+   * @return the logic used by this symbol manager
+   */
+  public String getLogic()
+  {
+    return getLogic(pointer);
+  }
+
+  private native String getLogic(long pointer);
 }
--- test/unit/api/java/InputParserTest.java
+++ test/unit/api/java/InputParserTest.java
@@ -26,13 +26,14 @@ import org.junit.jupiter.api.Test;
 class InputParserTest
 {
   private Solver d_solver;
-  private InputLanguage d_lang;    
+  private SymbolManager d_symman;
 
   @BeforeEach
   void setUp()
   {
     d_solver = new Solver();
     d_solver.setOption("parse-only", "true");
+    d_symman = new SymbolManager(d_solver);
   }
 
   @AfterEach
@@ -62,26 +63,27 @@ class InputParserTest
     // a symbol manager is allocated
     assertNotEquals(p.getSymbolManager(), null);
 
-    // InputParser p2 = new InputParser(d_solver, d_symman);
-    // assertEquals(p2.getSymbolManager(), d_symman);
+    InputParser p2 = new InputParser(d_solver, d_symman);
+    assertEquals(p2.getSymbolManager(), d_symman);
   }
 
-  // @Test
-  // void setFileInput()
-  // {
-  //   InputParser p = new InputParser(d_solver);
-  //   assertThrows(CVC5ApiException.class,
-  //       () -> p.setFileInput(InputLanguage.SMT_LIB_2_6, "nonexistent.smt2"));
-  // }
+  @Test
+  void setFileInput()
+  {
+    InputParser p = new InputParser(d_solver);
+    assertThrows(CVC5ApiException.class, () ->
+        p.setFileInput(InputLanguage.SMT_LIB_2_6, "nonexistent.smt2"));
+  }
 
   // @Test
   // void setStreamInput()
   // {
   //   InputParser p = new InputParser(d_solver);
-  //   String input = "(set-logic QF_LIA)\n"
-  //       + "(declare-fun a () Bool)\n"
-  //       + "(declare-fun b () Int)\n";
-  //   p.setStreamInput(InputLanguage.SMT_LIB_2_6, input, "input_parser_black");
+  //   std::stringstream ss;
+  //   ss << "(set-logic QF_LIA)" << std::endl;
+  //   ss << "(declare-fun a () Bool)" << std::endl;
+  //   ss << "(declare-fun b () Int)" << std::endl;
+  //   p.setStreamInput(InputLanguage.SMT_LIB_2_6, ss, "input_parser_black");
   //   assertEquals(p.done(), false);
   //   Command cmd;
   //   std::stringstream out;
@@ -92,7 +94,7 @@ class InputParserTest
   //     {
   //       break;
   //     }
-  //     assertDoesNotThrow(() -> cmd.invoke(d_solver, d_symman.get(), out));
+  //     assertDoesNotThrow(() ->cmd.invoke(d_solver, d_symman.get(), out));
   //   }
   //   assertEquals(p.done(), true);
   // }
@@ -107,22 +109,23 @@ class InputParserTest
   //   p.appendIncrementalStringInput("(set-logic ALL)");
   //   cmd = p.nextCommand();
   //   assertNotEquals(cmd.isNull(), true);
-  //   assertDoesNotThrow(() -> cmd.invoke(d_solver, d_symman.get(), out));
+  //   assertDoesNotThrow(() ->cmd.invoke(d_solver, d_symman.get(), out));
   //   p.appendIncrementalStringInput("(declare-fun a () Bool)");
   //   cmd = p.nextCommand();
   //   assertNotEquals(cmd.isNull(), true);
-  //   assertDoesNotThrow(() -> cmd.invoke(d_solver, d_symman.get(), out));
+  //   assertDoesNotThrow(() ->cmd.invoke(d_solver, d_symman.get(), out));
   //   p.appendIncrementalStringInput("(declare-fun b () Int)");
   //   cmd = p.nextCommand();
   //   assertNotEquals(cmd.isNull(), true);
-  //   assertDoesNotThrow(() -> cmd.invoke(d_solver, d_symman.get(), out));
+  //   assertDoesNotThrow(() ->cmd.invoke(d_solver, d_symman.get(), out));
   // }
 
   // @Test
   // void nextCommand()
   // {
   //   InputParser p = new InputParser(d_solver);
-  //   assertThrows(CVC5ApiException.class, () -> p.nextCommand());
+  //   assertThrows(CVC5ApiException.class, () ->p.nextCommand());
+  //   std::stringstream ss;
   //   p.setStreamInput(InputLanguage.SMT_LIB_2_6, ss, "input_parser_black");
   //   Command cmd = p.nextCommand();
   //   assertEquals(cmd.isNull(), true);
@@ -132,7 +135,8 @@ class InputParserTest
   // void nextTerm()
   // {
   //   InputParser p = new InputParser(d_solver);
-  //   assertThrows(CVC5ApiException.class, () -> p.nextTerm());
+  //   assertThrows(CVC5ApiException.class, () ->p.nextTerm());
+  //   std::stringstream ss;
   //   p.setStreamInput(InputLanguage.SMT_LIB_2_6, ss, "input_parser_black");
   //   assertEquals(p.nextTerm().isNull(), true);
   // }
@@ -147,18 +151,18 @@ class InputParserTest
   //   p.appendIncrementalStringInput("(declare-fun a () Int)");
   //   Command cmd = p.nextCommand();
   //   assertNotEquals(cmd.isNull(), true);
-  //   assertDoesNotThrow(() -> cmd.invoke(d_solver, d_symman.get(), out));
+  //   assertDoesNotThrow(() ->cmd.invoke(d_solver, d_symman.get(), out));
   //   // now parse some terms
   //   Term t;
   //   p.appendIncrementalStringInput("45");
-  //   assertDoesNotThrow(() -> t = p.nextTerm());
+  //   assertDoesNotThrow(() ->t = p.nextTerm());
   //   assertEquals(t.isNull(), false);
   //   p.appendIncrementalStringInput("(+ a 1)");
-  //   assertDoesNotThrow(() -> t = p.nextTerm());
+  //   assertDoesNotThrow(() ->t = p.nextTerm());
   //   assertEquals(t.isNull(), false);
   //   assertEquals(t.getKind(), Kind::ADD);
   //   p.appendIncrementalStringInput("(+ b 1)");
-  //   assertThrows(CVC5ApiException.class, () -> t = p.nextTerm(), ParserException);
+  //   assertThrows(CVC5ApiException.class, () ->t = p.nextTerm(), ParserException);
   // }
 
   // @Test
@@ -168,39 +172,40 @@ class InputParserTest
   //   InputParser p(d_solver, d_symman.get());
   //   // set a logic for the parser
   //   Command cmd = parseLogicCommand(p, "QF_LIA");
-  //   assertDoesNotThrow(() -> cmd.invoke(d_solver, d_symman.get(), out));
+  //   assertDoesNotThrow(() ->cmd.invoke(d_solver, d_symman.get(), out));
   //   assertEquals(d_solver.isLogicSet(), true);
   //   assertEquals(d_solver.getLogic(), "QF_LIA");
   //   assertEquals(d_symman -> isLogicSet(), true);
   //   assertEquals(d_symman -> getLogic(), "QF_LIA");
   //   // cannot set logic on solver now
-  //   assertThrows(CVC5ApiException.class, () -> d_solver.setLogic("QF_LRA"), CVC5ApiException);
+  //   assertThrows(CVC5ApiException.class, () ->d_solver.setLogic("QF_LRA"));
 
   //   // possible to construct another parser with the same solver and symbol
   //   // manager
-  //   InputParser p2 = new InputParser(d_solver, p.getSymbolManager());
+  //   InputParser p2(d_solver, p.getSymbolManager());
 
   //   // possible to construct another parser with a fresh solver
   //   Solver s2;
-  //   InputParser p3 = new InputParser(s2, d_symman.get());
+  //   InputParser p3(s2, d_symman.get());
   //   p3.setIncrementalStringInput(InputLanguage.SMT_LIB_2_6, "input_parser_black");
   //   // logic is automatically set on the solver
   //   assertEquals(s2.isLogicSet(), true);
   //   assertEquals(s2.getLogic(), "QF_LIA");
   //   // we cannot set the logic since it has already been set
-  //   assertThrows(CVC5ApiException.class, () -> parseLogicCommand(p3, "QF_LRA"), ParserException);
+  //   assertThrows(CVC5ApiException.class, () ->parseLogicCommand(p3, "QF_LRA"), ParserException);
 
   //   // using a solver with the same logic is allowed
   //   Solver s3;
   //   s3.setLogic("QF_LIA");
-  //   InputParser p4 = new InputParser(s3, d_symman.get());
+  //   InputParser p4(s3, d_symman.get());
   //   p4.setIncrementalStringInput(InputLanguage.SMT_LIB_2_6, "input_parser_black");
 
   //   // using a solver with a different logic is not allowed
   //   Solver s4;
   //   s4.setLogic("QF_LRA");
-  //   InputParser p5 = new InputParser(s4, d_symman.get());
-  //   assertThrows(CVC5ApiException.class,
-  //       () -> p5.setIncrementalStringInput(InputLanguage.SMT_LIB_2_6, "input_parser_black"));
+  //   InputParser p5(s4, d_symman.get());
+  //   assertThrows(CVC5ApiException.class, () ->
+  //       p5.setIncrementalStringInput(InputLanguage.SMT_LIB_2_6, "input_parser_black"),
+  //       CVC5ApiException);
   // }
 }
