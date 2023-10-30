113f1a988a03d53cf23645827d291f6f1d88e04e
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2023-10-30 09:32:24 -0500

    Fix getEqualityStatus for non-linear multiplication (#10125)
    Fixes #10108.
    
    In detail, getEqualityStatus should not traverse non-linear multiplication, as theory combination should consider these as atomic terms.
    
    This also fixes a minor performance issue in TheoryUF::computeCareGraph, which did not implement an optimization we use in Theory::computeCareGraph.
    
    The benchmark on #10108 now times out.

--- src/theory/uf/theory_uf.cpp
+++ src/theory/uf/theory_uf.cpp
@@ -552,6 +552,12 @@ EqualityStatus TheoryUF::getEqualityStatus(TNode a, TNode b) {
 
 bool TheoryUF::areCareDisequal(TNode x, TNode y)
 {
+  // check for disequality first, as an optimization
+  if (d_equalityEngine->hasTerm(x) && d_equalityEngine->hasTerm(y)
+      && d_equalityEngine->areDisequal(x, y, false))
+  {
+    return true;
+  }
   if (d_equalityEngine->isTriggerTerm(x, THEORY_UF)
       && d_equalityEngine->isTriggerTerm(y, THEORY_UF))
   {
