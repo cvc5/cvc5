cvc5/cvc5

commit 113f1a988a03d53cf23645827d291f6f1d88e04e

Fix getEqualityStatus for non-linear multiplication (#10125)
Fixes #10108.

In detail, getEqualityStatus should not traverse non-linear multiplication, as theory combination should consider these as atomic terms.

This also fixes a minor performance issue in TheoryUF::computeCareGraph, which did not implement an optimization we use in Theory::computeCareGraph.

The benchmark on #10108 now times out.

src/theory/arith/arith_subs.cpp
@@ -29,7 +29,7 @@ void ArithSubs::addArith(const Node& v, const Node& s)
   d_subs.push_back(s);
 }
 
-Node ArithSubs::applyArith(const Node& n) const
+Node ArithSubs::applyArith(const Node& n, bool traverseNlMult) const
 {
   NodeManager* nm = NodeManager::currentNM();
   std::unordered_map<TNode, Node> visited;
@@ -59,7 +59,8 @@ Node ArithSubs::applyArith(const Node& n) const
         TheoryId ctid = theory::kindToTheoryId(ck);
         if ((ctid != THEORY_ARITH && ctid != THEORY_BOOL
              && ctid != THEORY_BUILTIN)
-            || isTranscendentalKind(ck))
+            || isTranscendentalKind(ck)
+            || (!traverseNlMult && ck == Kind::NONLINEAR_MULT))
         {
           // Do not traverse beneath applications that belong to another theory
           // besides (core) arithmetic. Notice that transcendental function

src/theory/arith/theory_arith.cpp
@@ -385,7 +385,7 @@ bool TheoryArith::collectModelValues(TheoryModel* m,
       continue;
     }
     Assert(false) << "A model equality could not be asserted: " << p.first
-                        << " == " << p.second << std::endl;
+                  << " == " << p.second << std::endl;
     // If we failed to assert an equality, it is likely due to theory
     // combination, namely the repaired model for non-linear changed
     // an equality status that was agreed upon by both (linear) arithmetic
@@ -429,8 +429,11 @@ EqualityStatus TheoryArith::getEqualityStatus(TNode a, TNode b) {
   Trace("arith-eq-status") << "Evaluate under " << d_arithModelCacheSubs.d_vars << " / "
                  << d_arithModelCacheSubs.d_subs << std::endl;
   Node diff = NodeManager::currentNM()->mkNode(Kind::SUB, a, b);
+  // do not traverse non-linear multiplication here, since the value of
+  // multiplication in this method should consider the value of the
+  // non-linear multiplication term, and not its evaluation.
   std::optional<bool> isZero =
-      isExpressionZero(d_env, diff, d_arithModelCacheSubs);
+      isExpressionZero(d_env, diff, d_arithModelCacheSubs, false);
   if (isZero)
   {
     EqualityStatus es =
@@ -487,13 +490,9 @@ void TheoryArith::finalizeModelCache()
   for (const auto& [node, repl] : d_arithModelCache)
   {
     Assert(repl.getType().isRealOrInt());
-    // we only keep the domain of the substitution that is for leafs of
-    // arithmetic; otherwise we are using the value of the abstraction of
-    // non-linear term from the linear solver, which can be incorrect.
-    if (Theory::isLeafOf(node, TheoryId::THEORY_ARITH))
-    {
-      d_arithModelCacheSubs.add(node, repl);
-    }
+    // note that node may be an application of non-linear arithmetic, which will
+    // get applied as a substitution in getEqualityStatus.
+    d_arithModelCacheSubs.add(node, repl);
   }
 }
 

src/theory/arith/arith_evaluator.cpp
@@ -23,7 +23,10 @@ namespace cvc5::internal {
 namespace theory {
 namespace arith {
 
-std::optional<bool> isExpressionZero(Env& env, Node expr, const ArithSubs& subs)
+std::optional<bool> isExpressionZero(Env& env,
+                                     Node expr,
+                                     const ArithSubs& subs,
+                                     bool traverseNlMult)
 {
   // Substitute constants and rewrite
   expr = env.getRewriter()->rewrite(expr);
@@ -33,7 +36,7 @@ std::optional<bool> isExpressionZero(Env& env, Node expr, const ArithSubs& subs)
   }
   // we use an arithmetic substitution, which does not traverse into
   // terms that do not belong to the core theory of arithmetic.
-  expr = subs.applyArith(expr);
+  expr = subs.applyArith(expr, traverseNlMult);
   expr = env.getRewriter()->rewrite(expr);
   if (expr.isConst())
   {

src/theory/arith/arith_evaluator.h
@@ -39,7 +39,8 @@ namespace arith {
  */
 std::optional<bool> isExpressionZero(Env& env,
                                      Node expr,
-                                     const ArithSubs& subs);
+                                     const ArithSubs& subs,
+                                     bool traverseNlMult);
 }
 }  // namespace theory
 }  // namespace cvc5::internal

src/theory/arith/arith_subs.h
@@ -41,8 +41,12 @@ class ArithSubs : public Subs
  public:
   /** Add v -> s to the substitution */
   void addArith(const Node& v, const Node& s);
-  /** Return the result of this substitution on n */
-  Node applyArith(const Node& n) const;
+  /**
+   * Return the result of this substitution on n.
+   * @param n The node to apply the substitution
+   * @param traverseNlMult Whether to traverse applications of NONLINEAR_MULT.
+   */
+  Node applyArith(const Node& n, bool traverseNlMult = true) const;
 };
 
 }  // namespace arith

src/theory/care_pair_argument_callback.cpp
@@ -22,6 +22,7 @@ CarePairArgumentCallback::CarePairArgumentCallback(Theory& t) : d_theory(t) {}
 
 bool CarePairArgumentCallback::considerPath(TNode a, TNode b)
 {
+  Trace("ajr-temp") << "considerPath " << a << " " << b << std::endl;
   // builtin cases for when a is clearly equal/disequal to b
   if (a == b)
   {

src/theory/uf/theory_uf.cpp
@@ -552,6 +552,12 @@ EqualityStatus TheoryUF::getEqualityStatus(TNode a, TNode b) {
 
 bool TheoryUF::areCareDisequal(TNode x, TNode y)
 {
+  // check for disequality first, as an optimization
+  if (d_equalityEngine->hasTerm(x) && d_equalityEngine->hasTerm(y)
+      && d_equalityEngine->areDisequal(x, y, false))
+  {
+    return true;
+  }
   if (d_equalityEngine->isTriggerTerm(x, THEORY_UF)
       && d_equalityEngine->isTriggerTerm(y, THEORY_UF))
   {

commit c545219d4354fb649653778779df641d701da6a1

Another fix for arithmetic equality status (#10142)
Issue was introduced in 113f1a9. We need to be careful both for constructing the substitution and how to apply it.

Fixes #10139.

src/theory/arith/arith_subs.cpp
@@ -60,7 +60,8 @@ Node ArithSubs::applyArith(const Node& n, bool traverseNlMult) const
         if ((ctid != THEORY_ARITH && ctid != THEORY_BOOL
              && ctid != THEORY_BUILTIN)
             || isTranscendentalKind(ck)
-            || (!traverseNlMult && ck == Kind::NONLINEAR_MULT))
+            || (!traverseNlMult
+                && (ck == Kind::NONLINEAR_MULT || ck == Kind::IAND)))
         {
           // Do not traverse beneath applications that belong to another theory
           // besides (core) arithmetic. Notice that transcendental function

src/theory/arith/theory_arith.cpp
@@ -490,9 +490,13 @@ void TheoryArith::finalizeModelCache()
   for (const auto& [node, repl] : d_arithModelCache)
   {
     Assert(repl.getType().isRealOrInt());
-    // note that node may be an application of non-linear arithmetic, which will
-    // get applied as a substitution in getEqualityStatus.
-    d_arithModelCacheSubs.add(node, repl);
+    // we only keep the domain of the substitution that is for leafs of
+    // arithmetic; otherwise we are using the value of the abstraction of
+    // non-linear term from the linear solver, which can be incorrect.
+    if (Theory::isLeafOf(node, TheoryId::THEORY_ARITH))
+    {
+      d_arithModelCacheSubs.add(node, repl);
+    }
   }
 }
 

test/regress/cli/CMakeLists.txt
@@ -2259,6 +2259,7 @@ set(regress_1_tests
   regress1/nl/iand-native-2.smt2
   regress1/nl/iand-native-granularities.smt2
   regress1/nl/issue10003-nl-ext-rr.smt2
+  regress1/nl/issue10139-iand.smt2
   regress1/nl/issue3300-approx-sqrt-witness.smt2
   regress1/nl/issue3441.smt2
   regress1/nl/issue3617.smt2
@@ -3312,7 +3313,6 @@ set(regress_2_tests
   regress2/javafe.ast.StandardPrettyPrint.319_no_forall.smt2
   regress2/javafe.ast.WhileStmt.447_no_forall.smt2
   regress2/nl/issue8693-flatten-to-real.smt2
-  regress2/nl/ufnia-factor-open-proof.smt2
   regress2/ooo.rf6.smt2
   regress2/ooo.tag10.smt2
   regress2/piVC_5581bd.smt2
@@ -3575,6 +3575,8 @@ set(regression_disabled_tests
   regress2/nl/dumortier-050317.smt2
   # timeout on some builds after changes to justification heuristic
   regress2/nl/nt-lemmas-bad.smt2
+  # timeout after change to arith theory combination
+  regress2/nl/ufnia-factor-open-proof.smt2
   # timeout after refactoring justification heuristic
   regress2/ho/SYO362^5.smt2
   # timeout after refactoring selectors

test/regress/cli/regress1/nl/issue10139-iand.smt2
@@ -1,0 +1,9 @@
+(set-logic ALL)
+(set-info :status sat)
+(declare-fun a () Int)
+(declare-fun b (Int) Bool)
+(declare-fun c (Int) Int)
+(assert (= (c 0) (c 1)))
+(assert (not (b 1)))
+(assert (b ((_ iand 3) a 1)))
+(check-sat)