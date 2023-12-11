13941ad8cf4b5c981201cd6b5b6084c076ff8e74
Leni Aniva <v@leni.sh>
2023-12-10 22:07:04 -0800

    Added aarch64 platforms

--- flake.nix
+++ flake.nix
@@ -47,6 +47,8 @@
     systems = [
       "x86_64-linux"
       "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
     ];
     perSystem = { system, pkgs, ... }: let
       cadical' = pkgs.stdenv.mkDerivation {
