967ca6a9903915cb1a7d947b0a0c75fac0ed1420
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2023-12-13 12:07:34 -0600

    Add `--safe-options` (#10159)
    This option indicates that further options cannot be expert.
    
    Note that --safe-options --expert-option throws an exception while --expert-option --safe-options does not.

--- include/cvc5/cvc5.h
+++ include/cvc5/cvc5.h
@@ -3111,6 +3111,8 @@ struct CVC5_EXPORT OptionInfo
   std::vector<std::string> aliases;
   /** Whether the option was explicitly set by the user */
   bool setByUser;
+  /** Whether this is an expert option */
+  bool isExpert;
   /** Possible types for ``valueInfo``. */
   using OptionInfoVariant = std::variant<VoidInfo,
                                          ValueInfo<bool>,
