c98a922c779c9b45a31d1d32211a1cdd26bb49bd
ajreynol <andrew.j.reynolds@gmail.com>
2024-01-23 14:01:48 -0600

    Format

--- src/proof/alf/alf_node_converter.cpp
+++ src/proof/alf/alf_node_converter.cpp
@@ -303,7 +303,7 @@ Node AlfNodeConverter::postConvert(Node n)
     ss << "@fp." << printer::smt2::Smt2Printer::smtKindString(k);
     return mkInternalApp(ss.str(), {tnn}, tn);
   }
-  else if (k==Kind::SEXPR)
+  else if (k == Kind::SEXPR)
   {
     // use generic list
     std::vector<Node> args;
