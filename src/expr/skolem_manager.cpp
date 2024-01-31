cvc5/cvc5

commit fffe72027c9a401cfc3619cf59c9e19a02a00ddd

Add special rule for injective functions for bag.map terms (#10306)

src/theory/bags/inference_generator.cpp
@@ -504,6 +504,38 @@ std::tuple<InferInfo, Node, Node> InferenceGenerator::mapDown(Node n, Node e)
   return std::tuple(inferInfo, uf, preImageSize);
 }
 
+InferInfo InferenceGenerator::mapDownInjective(Node n, Node y)
+{
+  Assert(n.getKind() == Kind::BAG_MAP && n[1].getType().isBag());
+  Assert(n[0].getType().isFunction()
+         && n[0].getType().getArgTypes().size() == 1);
+  Assert(y.getType() == n[0].getType().getRangeType());
+
+  InferInfo inferInfo(d_im, InferenceId::BAGS_MAP_DOWN_INJECTIVE);
+
+  Node f = n[0];
+  Node A = n[1];
+  // declare a fresh skolem of type T
+  TypeNode domainType = f.getType().getArgTypes()[0];
+  Node x = d_sm->mkSkolemFunctionTyped(
+      SkolemFunId::BAGS_MAP_PREIMAGE, domainType, {n, y});
+
+  Node mapSkolem = registerAndAssertSkolemLemma(n);
+  Node countY = getMultiplicityTerm(y, mapSkolem);
+  Node countX = getMultiplicityTerm(x, A);
+
+  Node f_x = d_nm->mkNode(Kind::APPLY_UF, f, x);
+  Node y_equals_f_x = y.eqNode(f_x);
+
+  Node count_x_equals_count_y = countX.eqNode(countY);
+  Node conclusion = y_equals_f_x.andNode(count_x_equals_count_y);
+  inferInfo.d_conclusion = conclusion;
+
+  Trace("bags::InferenceGenerator::mapDown")
+      << "conclusion: " << inferInfo.d_conclusion << std::endl;
+  return inferInfo;
+}
+
 InferInfo InferenceGenerator::mapUp1(Node n, Node x)
 {
   Assert(n.getKind() == Kind::BAG_MAP && n[1].getType().isBag());

src/theory/bags/bag_solver.cpp
@@ -294,33 +294,45 @@ void BagSolver::checkMap(Node n)
     InferInfo upInference = d_ig.mapUp1(n, x);
     d_im.lemmaTheoryInference(&upInference);
   }
-  for (const Node& z : downwards)
+
+  if (d_state.isInjective(n[0]))
   {
-    Node y = d_state.getRepresentative(z);
-    if (!d_mapCache.count(n))
+    for (const Node& z : downwards)
     {
-      std::shared_ptr<context::CDHashMap<Node, std::pair<Node, Node>>> nMap =
-          std::make_shared<context::CDHashMap<Node, std::pair<Node, Node>>>(
-              userContext());
-      d_mapCache[n] = nMap;
-    }
-    if (!d_mapCache[n].get()->count(y))
-    {
-      auto [downInference, uf, preImageSize] = d_ig.mapDown(n, y);
-      d_im.lemmaTheoryInference(&downInference);
-      std::pair<Node, Node> yPair = std::make_pair(uf, preImageSize);
-      d_mapCache[n].get()->insert(y, yPair);
-    }
-
-    context::CDHashMap<Node, std::pair<Node, Node>>::iterator it =
-        d_mapCache[n].get()->find(y);
-
-    auto [uf, preImageSize] = it->second;
-
-    for (const Node& x : upwards)
-    {
-      InferInfo upInference = d_ig.mapUp2(n, uf, preImageSize, y, x);
+      InferInfo upInference = d_ig.mapDownInjective(n, z);
       d_im.lemmaTheoryInference(&upInference);
+    }
+  }
+  else
+  {
+    for (const Node& z : downwards)
+    {
+      Node y = d_state.getRepresentative(z);
+      if (!d_mapCache.count(n))
+      {
+        std::shared_ptr<context::CDHashMap<Node, std::pair<Node, Node>>> nMap =
+            std::make_shared<context::CDHashMap<Node, std::pair<Node, Node>>>(
+                userContext());
+        d_mapCache[n] = nMap;
+      }
+      if (!d_mapCache[n].get()->count(y))
+      {
+        auto [downInference, uf, preImageSize] = d_ig.mapDown(n, y);
+        d_im.lemmaTheoryInference(&downInference);
+        std::pair<Node, Node> yPair = std::make_pair(uf, preImageSize);
+        d_mapCache[n].get()->insert(y, yPair);
+      }
+
+      context::CDHashMap<Node, std::pair<Node, Node>>::iterator it =
+          d_mapCache[n].get()->find(y);
+
+      auto [uf, preImageSize] = it->second;
+
+      for (const Node& x : upwards)
+      {
+        InferInfo upInference = d_ig.mapUp2(n, uf, preImageSize, y, x);
+        d_im.lemmaTheoryInference(&upInference);
+      }
     }
   }
 }

src/theory/bags/inference_generator.h
@@ -255,6 +255,22 @@ class InferenceGenerator
    * and skolem is a fresh variable equals (bag.map f A))
    */
   std::tuple<InferInfo, Node, Node> mapDown(Node n, Node e);
+
+  /**
+   * @pre option bagsInjectiveMaps is true
+   * @param n is (bag.map f A) where f is a function (-> E T), A a bag of type
+   * (Bag E)
+   * @param y is a node of Type T
+   * @return an inference that represents the following conjunction
+   * (and
+   *   (= (bag.count x A) (bag.count y skolem))
+   *   (= (f x) y)
+   * )
+   * where skolem is a fresh variable equals (bag.map f A))
+   * and x is a fresh variable unique per n, y.
+   */
+  InferInfo mapDownInjective(Node n, Node y);
+
   /**
    * @param n is (bag.map f A) where f is a function (-> E T), A a bag of type
    * (Bag E)

src/theory/bags/solver_state.cpp
@@ -18,6 +18,7 @@
 #include "expr/attribute.h"
 #include "expr/bound_var_manager.h"
 #include "expr/skolem_manager.h"
+#include "theory/smt_engine_subsolver.h"
 #include "theory/uf/equality_engine.h"
 
 using namespace std;
@@ -155,6 +156,56 @@ void SolverState::reset()
   d_bags.clear();
   d_deq.clear();
   d_cardTerms.clear();
+}
+
+void SolverState::checkInjectivity(Node n)
+{
+  SkolemManager* sm = d_nm->getSkolemManager();
+  Node f = sm->getOriginalForm(n);
+  if (d_functions.find(f) != d_functions.end())
+  {
+    // we already know f
+    return;
+  }
+
+  if (f.isVar())
+  {
+    // no need to solve. f can be assigned any non injective function
+    d_functions[f] = false;
+    return;
+  }
+
+  TypeNode domainType = f.getType().getArgTypes()[0];
+  Node x = sm->mkDummySkolem("x", domainType);
+  Node y = sm->mkDummySkolem("y", domainType);
+  Node f_x = d_nm->mkNode(Kind::APPLY_UF, f, x);
+  Node f_y = d_nm->mkNode(Kind::APPLY_UF, f, y);
+  Node f_x_equals_f_y = f_x.eqNode(f_y);
+  Node not_x_equals_y = x.eqNode(y).notNode();
+  Node query = f_x_equals_f_y.andNode(not_x_equals_y);
+
+  Options subOptions;
+  subOptions.copyValues(d_env.getOptions());
+  SubsolverSetupInfo ssi(d_env, subOptions);
+  Result result = checkWithSubsolver(query, ssi);
+  if (result.getStatus() == Result::Status::UNSAT)
+  {
+    d_functions[f] = true;
+  }
+  else
+  {
+    d_functions[f] = false;
+  }
+}
+
+bool SolverState::isInjective(Node n) const
+{
+  Node f = d_nm->getSkolemManager()->getOriginalForm(n);
+  if (d_functions.find(f) != d_functions.end())
+  {
+    return d_functions.at(f);
+  }
+  return false;
 }
 
 }  // namespace bags

src/theory/bags/solver_state.h
@@ -110,6 +110,23 @@ class SolverState : public TheoryState
    */
   void collectDisequalBagTerms();
 
+  /**
+   * @pre n is a unary function.
+   * Check whether n is injective and store the result.
+   * It uses a sub-solver to check whether the following formula is unsat
+   * (and
+   *   (= (f x) (f y))
+   *   (distinct x y)
+   * )
+   */
+  void checkInjectivity(Node n);
+
+  /**
+   * @pre n is a unary function.
+   * Check whether we found that n is an injective function
+   */
+  bool isInjective(Node n) const;
+
  private:
   /** constants */
   Node d_true;
@@ -142,6 +159,11 @@ class SolverState : public TheoryState
    */
   context::CDHashMap<Node, std::shared_ptr<context::CDHashSet<Node>>>
       d_partElementSkolems;
+
+  /**
+   * A cache for injective functions
+   */
+  std::map<Node, bool> d_functions;
 }; /* class SolverState */
 
 }  // namespace bags

src/theory/bags/theory_bags.cpp
@@ -23,6 +23,7 @@
 #include "theory/quantifiers/fmf/bounded_integers.h"
 #include "theory/rewriter.h"
 #include "theory/theory_model.h"
+#include "theory_bags.h"
 #include "util/rational.h"
 
 using namespace cvc5::internal::kind;
@@ -321,7 +322,9 @@ bool TheoryBags::runInferStep(InferStep s, int effort)
       break;
     }
     case CHECK_BASIC_OPERATIONS: d_solver.checkBasicOperations(); break;
-    case CHECK_QUANTIFIED_OPERATIONS: d_solver.checkQuantifiedOperations(); break;
+    case CHECK_QUANTIFIED_OPERATIONS:
+      d_solver.checkQuantifiedOperations();
+      break;
     case CHECK_CARDINALITY_CONSTRAINTS:
       d_cardSolver.checkCardinalityGraph();
       break;
@@ -352,6 +355,13 @@ bool TheoryBags::collectModelValues(TheoryModel* m,
   // a map from bag representatives to their constructed values
   std::map<Node, Node> processedBags;
 
+  Trace("bags-model") << "d_state equality engine:" << std::endl;
+  Trace("bags-model") << d_state.getEqualityEngine()->debugPrintEqc()
+                      << std::endl;
+
+  Trace("bags-model") << "model equality engine:" << std::endl;
+  Trace("bags-model") << m->getEqualityEngine()->debugPrintEqc() << std::endl;
+
   // get the relevant bag equivalence classes
   for (const Node& n : termSet)
   {
@@ -361,8 +371,13 @@ bool TheoryBags::collectModelValues(TheoryModel* m,
       // we are only concerned here about bag terms
       continue;
     }
-    Node r = d_state.getRepresentative(n);
 
+    if (!Theory::isLeafOf(n, TheoryId::THEORY_BAGS))
+    {
+      continue;
+    }
+
+    Node r = d_state.getRepresentative(n);
     if (processedBags.find(r) != processedBags.end())
     {
       // skip bags whose representatives are already processed
@@ -478,6 +493,12 @@ void TheoryBags::preRegisterTerm(TNode n)
       d_state.addEqualityEngineTriggerPredicate(n);
     }
     break;
+    case Kind::BAG_MAP:
+    {
+      d_state.checkInjectivity(n[0]);
+      d_equalityEngine->addTerm(n);
+      break;
+    }
     case Kind::BAG_FROM_SET:
     case Kind::BAG_TO_SET:
     case Kind::BAG_IS_SINGLETON:

src/theory/bags/theory_bags.h
@@ -82,6 +82,7 @@ class TheoryBags : public Theory
   Node getCandidateModelValue(TNode) override;
   std::string identify() const override { return "THEORY_BAGS"; }
   void preRegisterTerm(TNode n) override;
+
   void presolve() override;
   void computeCareGraph() override;
   void processCarePairArgs(TNode a, TNode b) override;

src/theory/inference_id.cpp
@@ -138,6 +138,7 @@ const char* toString(InferenceId i)
     case InferenceId::BAGS_DIFFERENCE_REMOVE: return "BAGS_DIFFERENCE_REMOVE";
     case InferenceId::BAGS_DUPLICATE_REMOVAL: return "BAGS_DUPLICATE_REMOVAL";
     case InferenceId::BAGS_MAP_DOWN: return "BAGS_MAP_DOWN";
+    case InferenceId::BAGS_MAP_DOWN_INJECTIVE: return "BAGS_MAP_DOWN_INJECTIVE";
     case InferenceId::BAGS_MAP_UP1: return "BAGS_MAP_UP1";
     case InferenceId::BAGS_MAP_UP2: return "BAGS_MAP_UP2";
     case InferenceId::BAGS_FILTER_DOWN: return "BAGS_FILTER_DOWN";

commit 3d5a9d0e786b0f91b56da943739e9491a3364fff

Formalize typing rules for several bag.map skolems (#10326)
Fixes ALF proof checking fails introduced in fffe720.

proofs/alf/theories/Bags.smt3
@@ -31,6 +31,10 @@
 (declare-const @k.BAGS_DEQ_DIFF (-> (! Type :var T :implicit) (Bag T) (Bag T) T))
 (declare-const @k.TABLES_GROUP_PART (-> (! Type :var T :implicit) (Bag (Bag T)) T (Bag T)))
 (declare-const @k.TABLES_GROUP_PART_ELEMENT (-> (! Type :var T :implicit) (Bag (Bag T)) (Bag T) T))
+(declare-const @k.BAGS_MAP_PREIMAGE (-> (! Type :var T :implicit) (! Type :var U :implicit) (-> T U) (Bag T) U Int T))
+(declare-const @k.BAGS_MAP_SUM (-> (! Type :var T :implicit) (! Type :var U :implicit) (-> T U) (Bag T) U Int Int))
+(declare-const @k.BAGS_MAP_PREIMAGE_SIZE (-> (! Type :var T :implicit) (! Type :var U :implicit) (-> T U) (Bag T) U Int))
+(declare-const @k.BAGS_MAP_PREIMAGE_INJECTIVE (-> (! Type :var T :implicit) (! Type :var U :implicit) (-> T U) (Bag T) U T))
 
 ;(declare-const bag.from_set (# x term (apply f_bag.from_set x)))
 ;(declare-const bag.to_set (# x term (apply f_bag.to_set x)))

src/expr/skolem_manager.cpp
@@ -83,6 +83,8 @@ const char* toString(SkolemFunId id)
     case SkolemFunId::BAGS_FOLD_ELEMENTS: return "BAGS_FOLD_ELEMENTS";
     case SkolemFunId::BAGS_FOLD_UNION_DISJOINT: return "BAGS_FOLD_UNION_DISJOINT";
     case SkolemFunId::BAGS_MAP_PREIMAGE: return "BAGS_MAP_PREIMAGE";
+    case SkolemFunId::BAGS_MAP_PREIMAGE_INJECTIVE:
+      return "BAGS_MAP_PREIMAGE_INJECTIVE";
     case SkolemFunId::BAGS_MAP_PREIMAGE_SIZE: return "BAGS_MAP_PREIMAGE_SIZE";
     case SkolemFunId::BAGS_MAP_PREIMAGE_INDEX: return "BAGS_MAP_PREIMAGE_INDEX";
     case SkolemFunId::BAGS_MAP_SUM: return "BAGS_MAP_SUM";
@@ -449,6 +451,7 @@ TypeNode SkolemManager::getTypeFor(SkolemFunId id,
     case SkolemFunId::STRINGS_OCCUR_LEN_RE:
     case SkolemFunId::STRINGS_STOI_RESULT:
     case SkolemFunId::STRINGS_ITOS_RESULT:
+    case SkolemFunId::BAGS_MAP_SUM:
     {
       TypeNode itype = nm->integerType();
       return nm->mkFunctionType(itype, itype);
@@ -466,7 +469,9 @@ TypeNode SkolemManager::getTypeFor(SkolemFunId id,
     case SkolemFunId::STRINGS_DEQ_DIFF:
     case SkolemFunId::STRINGS_STOI_NON_DIGIT:
     case SkolemFunId::BAGS_FOLD_CARD:
-    case SkolemFunId::SETS_FOLD_CARD: return nm->integerType();
+    case SkolemFunId::SETS_FOLD_CARD:
+    case SkolemFunId::BAGS_MAP_PREIMAGE_SIZE:
+    case SkolemFunId::BAGS_MAP_PREIMAGE_INDEX: return nm->integerType();
     // string skolems
     case SkolemFunId::RE_FIRST_MATCH_PRE:
     case SkolemFunId::RE_FIRST_MATCH:
@@ -562,6 +567,18 @@ TypeNode SkolemManager::getTypeFor(SkolemFunId id,
       Assert(cacheVals.size() == 3);
       TypeNode itype = nm->integerType();
       return nm->mkFunctionType(itype, cacheVals[1].getType());
+    }
+    case SkolemFunId::BAGS_MAP_PREIMAGE:
+    {
+      TypeNode itype = nm->integerType();
+      Assert (cacheVals[0].getType().isFunction());
+      TypeNode retType = cacheVals[0].getType().getArgTypes()[0];
+      return nm->mkFunctionType(itype, retType);
+    }
+    case SkolemFunId::BAGS_MAP_PREIMAGE_INJECTIVE:
+    {
+      Assert (cacheVals[0].getType().isFunction());
+      return cacheVals[0].getType().getArgTypes()[0];
     }
     default: break;
   }

src/expr/skolem_manager.h
@@ -165,6 +165,11 @@ enum class SkolemFunId
    */
   BAGS_MAP_PREIMAGE,
   /**
+   * Same as above, but used when f is injective. In this case this is not
+   * indexed, and the returned skolem has type E.
+   */
+  BAGS_MAP_PREIMAGE_INJECTIVE,
+  /**
    * A skolem variable for the size of the preimage of {y} that is unique per
    * terms (bag.map f A), y which might be an element in (bag.map f A). (see the
    * documentation for BAGS_MAP_PREIMAGE)

src/theory/bags/inference_generator.cpp
@@ -415,24 +415,18 @@ std::tuple<InferInfo, Node, Node> InferenceGenerator::mapDown(Node n, Node e)
   Node f = n[0];
   Node A = n[1];
   // declare an uninterpreted function uf: Int -> T
-  TypeNode domainType = f.getType().getArgTypes()[0];
-  TypeNode ufType = d_nm->mkFunctionType(d_nm->integerType(), domainType);
-  Node uf = d_sm->mkSkolemFunctionTyped(
-      SkolemFunId::BAGS_MAP_PREIMAGE, ufType, {n, e});
+  Node uf = d_sm->mkSkolemFunction(SkolemFunId::BAGS_MAP_PREIMAGE, {f, A, e});
 
   // declare uninterpreted function sum: Int -> Int
-  TypeNode sumType =
-      d_nm->mkFunctionType(d_nm->integerType(), d_nm->integerType());
-  Node sum =
-      d_sm->mkSkolemFunctionTyped(SkolemFunId::BAGS_MAP_SUM, sumType, {n, e});
+  Node sum = d_sm->mkSkolemFunction(SkolemFunId::BAGS_MAP_SUM, {f, A, e});
 
   // (= (sum 0) 0)
   Node sum_zero = d_nm->mkNode(Kind::APPLY_UF, sum, d_zero);
   Node baseCase = d_nm->mkNode(Kind::EQUAL, sum_zero, d_zero);
 
   // guess the size of the preimage of e
-  Node preImageSize = d_sm->mkSkolemFunctionTyped(
-      SkolemFunId::BAGS_MAP_PREIMAGE_SIZE, d_nm->integerType(), {n, e});
+  Node preImageSize =
+      d_sm->mkSkolemFunction(SkolemFunId::BAGS_MAP_PREIMAGE_SIZE, {f, A, e});
 
   // (= (sum preImageSize) (bag.count e skolem))
   Node mapSkolem = registerAndAssertSkolemLemma(n);
@@ -516,9 +510,8 @@ InferInfo InferenceGenerator::mapDownInjective(Node n, Node y)
   Node f = n[0];
   Node A = n[1];
   // declare a fresh skolem of type T
-  TypeNode domainType = f.getType().getArgTypes()[0];
-  Node x = d_sm->mkSkolemFunctionTyped(
-      SkolemFunId::BAGS_MAP_PREIMAGE, domainType, {n, y});
+  Node x = d_sm->mkSkolemFunction(SkolemFunId::BAGS_MAP_PREIMAGE_INJECTIVE,
+                                  {f, A, y});
 
   Node mapSkolem = registerAndAssertSkolemLemma(n);
   Node countY = getMultiplicityTerm(y, mapSkolem);
@@ -573,9 +566,8 @@ InferInfo InferenceGenerator::mapUp2(
   Node notEqual =
       d_nm->mkNode(Kind::EQUAL, d_nm->mkNode(Kind::APPLY_UF, f, x), y).negate();
 
-  Node k = d_sm->mkSkolemFunctionTyped(SkolemFunId::BAGS_MAP_PREIMAGE_INDEX,
-                                       d_nm->integerType(),
-                                       {n, uf, preImageSize, y, x});
+  Node k = d_sm->mkSkolemFunction(SkolemFunId::BAGS_MAP_PREIMAGE_INDEX,
+                                  {n, uf, preImageSize, y, x});
   Node inRange = d_nm->mkNode(Kind::AND,
                               d_nm->mkNode(Kind::GEQ, k, d_one),
                               d_nm->mkNode(Kind::LEQ, k, preImageSize));