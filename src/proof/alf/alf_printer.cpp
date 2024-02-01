43e1c5bcc83c446defb95999fb539949130f6c66
ajreynol <andrew.j.reynolds@gmail.com>
2024-02-01 16:21:29 -0600

    Support alpha equivalence in ALF

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -128,6 +128,7 @@ bool AlfPrinter::isHandled(const ProofNode* pfn) const
     case ProofRule::REMOVE_TERM_FORMULA_AXIOM:
     case ProofRule::INSTANTIATE:
     case ProofRule::SKOLEMIZE:
+    case ProofRule::ALPHA_EQUIV:
     case ProofRule::ENCODE_PRED_TRANSFORM:
     case ProofRule::DSL_REWRITE:
     // alf rule is handled
