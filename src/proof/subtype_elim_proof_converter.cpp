ddb65c7bf39896bc3e4f86fcc89065e898e2dae4
ajreynol <andrew.j.reynolds@gmail.com>
2024-02-13 09:54:39 -0600

    CONG

--- src/proof/subtype_elim_proof_converter.cpp
+++ src/proof/subtype_elim_proof_converter.cpp
@@ -68,6 +68,7 @@ Node SubtypeElimConverterCallback::convert(Node res,
       success = true;
       Node lhs = newResc[0];
       Node rhs = newResc[1];
+      NodeManager * nm=  NodeManager::currentNM();
       for (size_t i=0, nchild = lhs.getNumChildren(); i<nchild; i++)
       {
         Node eqOld = newRes[0][i].eqNode(newRes[1][i]);
@@ -76,12 +77,57 @@ Node SubtypeElimConverterCallback::convert(Node res,
         {
           continue;
         }
+        // e.g. t=t becomes (to_real t)=(to_real t) or 0=0 becomes 0.0=0.0?
         if (lhs[i]==rhs[i])
         {
           cdp->addStep(eqNew, ProofRule::REFL, {}, {lhs[i]});
           continue;
         }
-        success = false;
+        // maybe t=s becomes (to_real t)=(to_real s), or t=0 becomes 
+        // (to_real t)=0.0?
+        Node newR[2];
+        Node newREq[2];
+        bool needsTrans = false;
+        for (size_t j=0; j<2; j++)
+        {
+          newR[j] = nm->mkNode(Kind::TO_REAL, newRes[j][i]);
+          if (newR[j]!=newResc[j][i])
+          {
+            // if e.g. (to_real 0) = 0.0, prove by evaluate
+            Node eq = j==1 ? newR[j].eqNode(newResc[j][i]) : newResc[j][i].eqNode(newR[j]);
+            Node peq = d_pc->checkDebug(ProofRule::EVALUATE, {}, {newR[j]});
+            if (!CDProof::isSame(eq, peq))
+            {
+              success = false;
+              break;
+            }
+            newREq[j] = eq;
+            needsTrans = true;
+          }
+        }
+        if (success)
+        {
+          Node nk = ProofRuleChecker::mkKindNode(Kind::TO_REAL);
+          Node ceq = newR[0].eqNode(newR[1]);
+          cdp->addStep(ceq, ProofRule::CONG, {children[i]}, {nk});
+          Trace("ajr-temp") << "...via " << ceq << std::endl;
+          if (needsTrans)
+          {
+            std::vector<Node> tchildren;
+            if (!newREq[0].isNull())
+            {
+              tchildren.push_back(newREq[0]);
+            }
+            tchildren.push_back(ceq);
+            if (!newREq[1].isNull())
+            {
+              tchildren.push_back(newREq[1]);
+            }
+            cdp->addStep(eqNew, ProofRule::TRANS, tchildren, {});
+            Trace("ajr-temp") << "...via trans " << tchildren << std::endl;
+          }
+          continue;
+        }
         break;
       }
     }
