ee316986e61f7f47977880147180b2b1f1631759
ajreynol <andrew.j.reynolds@gmail.com>
2024-02-14 14:32:15 -0600

    Minor

--- src/proof/proof_node_converter.cpp
+++ src/proof/proof_node_converter.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Top contributors (to current version):
- *   Andrew Reynolds, Haniel Barbosa, Gereon Kremer
+ *   Andrew Reynolds
  *
  * This file is part of the cvc5 project.
  *
