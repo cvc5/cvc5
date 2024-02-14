54f2b125a103114fc39c9d2595603716bee5e41c
Aina Niemetz <aina.niemetz@gmail.com>
2024-02-14 13:54:45 -0800

    c++ api: Implement to_string for enums in cvc5_types.h. (#10380)

--- src/api/c/cvc5.cpp
+++ src/api/c/cvc5.cpp
@@ -60,3 +60,116 @@ const char* cvc5_sort_kind_to_string(Cvc5SortKind kind)
   CVC5_CAPI_TRY_CATCH_END;
   return str.c_str();
 }
+
+/* -------------------------------------------------------------------------- */
+/* Cvc5RoundingMode                                                           */
+/* -------------------------------------------------------------------------- */
+
+const char* cvc5_rm_to_string(Cvc5RoundingMode rm)
+{
+  static thread_local std::string str;
+  CVC5_CAPI_TRY_CATCH_BEGIN;
+  CVC5_CAPI_CHECK_RM(rm);
+  str = std::to_string(static_cast<cvc5::RoundingMode>(rm));
+  CVC5_CAPI_TRY_CATCH_END;
+  return str.c_str();
+}
+
+/* -------------------------------------------------------------------------- */
+/* Cvc5UnknownExplanation                                                     */
+/* -------------------------------------------------------------------------- */
+
+const char* cvc5_unknown_explanation_to_string(Cvc5UnknownExplanation exp)
+{
+  static thread_local std::string str;
+  CVC5_CAPI_TRY_CATCH_BEGIN;
+  CVC5_CAPI_CHECK_UNKNOWN_EXPLANATION(exp);
+  str = std::to_string(static_cast<cvc5::UnknownExplanation>(exp));
+  CVC5_CAPI_TRY_CATCH_END;
+  return str.c_str();
+}
+
+/* -------------------------------------------------------------------------- */
+/* Cvc5LearnedLitType                                                         */
+/* -------------------------------------------------------------------------- */
+
+const char* cvc5_modes_learned_lit_type_to_string(Cvc5LearnedLitType type)
+{
+  static thread_local std::string str;
+  CVC5_CAPI_TRY_CATCH_BEGIN;
+  CVC5_CAPI_CHECK_LEARNED_LIT_TYPE(type);
+  str = std::to_string(static_cast<cvc5::modes::LearnedLitType>(type));
+  CVC5_CAPI_TRY_CATCH_END;
+  return str.c_str();
+}
+
+/* -------------------------------------------------------------------------- */
+/* Cvc5BlockedModelsMode                                                      */
+/* -------------------------------------------------------------------------- */
+
+const char* cvc5_modes_block_models_mode_to_string(Cvc5BlockModelsMode mode)
+{
+  static thread_local std::string str;
+  CVC5_CAPI_TRY_CATCH_BEGIN;
+  CVC5_CAPI_CHECK_BLOCK_MODELS_MODE(mode);
+  str = std::to_string(static_cast<cvc5::modes::BlockModelsMode>(mode));
+  CVC5_CAPI_TRY_CATCH_END;
+  return str.c_str();
+}
+
+/* -------------------------------------------------------------------------- */
+/* Cvc5ProofComponent                                                         */
+/* -------------------------------------------------------------------------- */
+
+const char* cvc5_modes_proof_component_to_string(Cvc5ProofComponent pc)
+{
+  static thread_local std::string str;
+  CVC5_CAPI_TRY_CATCH_BEGIN;
+  CVC5_CAPI_CHECK_PROOF_COMPONENT(pc);
+  str = std::to_string(static_cast<cvc5::modes::ProofComponent>(pc));
+  CVC5_CAPI_TRY_CATCH_END;
+  return str.c_str();
+}
+
+/* -------------------------------------------------------------------------- */
+/* Cvc5ProofFormat                                                            */
+/* -------------------------------------------------------------------------- */
+
+const char* cvc5_modes_proof_format_to_string(Cvc5ProofFormat format)
+{
+  static thread_local std::string str;
+  CVC5_CAPI_TRY_CATCH_BEGIN;
+  CVC5_CAPI_CHECK_PROOF_FORMAT(format);
+  str = std::to_string(static_cast<cvc5::modes::ProofFormat>(format));
+  CVC5_CAPI_TRY_CATCH_END;
+  return str.c_str();
+}
+
+/* -------------------------------------------------------------------------- */
+/* Cvc5FindSynthTarget                                                        */
+/* -------------------------------------------------------------------------- */
+
+const char* cvc5_modes_find_synthesis_target_to_string(
+    Cvc5FindSynthTarget target)
+{
+  static thread_local std::string str;
+  CVC5_CAPI_TRY_CATCH_BEGIN;
+  CVC5_CAPI_CHECK_FIND_SYNTH_TARGET(target);
+  str = std::to_string(static_cast<cvc5::modes::FindSynthTarget>(target));
+  CVC5_CAPI_TRY_CATCH_END;
+  return str.c_str();
+}
+
+/* -------------------------------------------------------------------------- */
+/* Cvc5InputLanguage                                                          */
+/* -------------------------------------------------------------------------- */
+
+const char* cvc5_modes_input_language_to_string(Cvc5InputLanguage lang)
+{
+  static thread_local std::string str;
+  CVC5_CAPI_TRY_CATCH_BEGIN;
+  CVC5_CAPI_CHECK_INPUT_LANGUAGE(lang);
+  str = std::to_string(static_cast<cvc5::modes::InputLanguage>(lang));
+  CVC5_CAPI_TRY_CATCH_END;
+  return str.c_str();
+}
