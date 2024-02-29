cvc5/cvc5

commit f77908cdb196f75fbc63f7c3d05ea35a1e4c7f44

Check that system CaDiCaL is at least >= 1.6 (#10330)
The minimum version of CaDiCaL that cvc5 supports is 1.6 (due to the use
of `CaDiCaL::ExternalPropagator`). However, this version is neither
checked or documented.

This PR makes two changes:

1) It updates `cmake/FindCaDiCaL.cmake` to do a configure-time `try_run`
check to obtain the version of CaDiCaL; if the version of CaDiCaL is too
low, it will fall back to the `--auto-download` behaviour (if that flag
is passed)
2) It documents in `INSTALL.rst` that this version is required

Signed-off-by: Andrew V. Teylu <andrew@tey.lu>

cmake/FindCaDiCaL.cmake
@@ -23,18 +23,37 @@ find_library(CaDiCaL_LIBRARIES NAMES cadical)
 
 set(CaDiCaL_FOUND_SYSTEM FALSE)
 if(CaDiCaL_INCLUDE_DIR AND CaDiCaL_LIBRARIES)
-  set(CaDiCaL_FOUND_SYSTEM TRUE)
+  # Try to compile our version file
+  try_run(RUN_RESULT_VAR
+    COMPILE_RESULT_VAR
+    SOURCE_FROM_CONTENT CaDiCaL_version.cpp
+    "
+    #include <cadical.hpp>
+    #include <iostream>
 
-  # Unfortunately it is not part of the headers
-  find_program(CaDiCaL_BINARY NAMES cadical)
-  if(CaDiCaL_BINARY)
-    execute_process(
-      COMMAND ${CaDiCaL_BINARY} --version OUTPUT_VARIABLE CaDiCaL_VERSION
-    )
-  else()
+    int main(void)
+    {
+      std::cout << CaDiCaL::Solver::version() << std::endl;
+      return 0;
+    }
+    "
+    LINK_LIBRARIES ${CaDiCaL_LIBRARIES}
+    RUN_OUTPUT_VARIABLE CaDiCaL_VERSION
+  )
+
+  # If this failed to compile or run, we have bigger issues
+  if (NOT ${RUN_RESULT_VAR} EQUAL 0 OR NOT ${COMPILE_RESULT_VAR})
     set(CaDiCaL_VERSION "")
   endif()
 
+  # Minimum supported version
+  set(CaDiCaL_FIND_VERSION "1.6.0")
+
+  # Set FOUND_SYSTEM to true; check_system_version will unset this if the
+  # version is less than the minimum required
+  set(CaDiCaL_FOUND_SYSTEM TRUE)
+
+  # Check the version against the required version
   check_system_version("CaDiCaL")
 endif()
 

commit bc25369184e6c4fed07d45b6ef588dba46bfaf1c

Correct FindCaDiCaL.cmake to support CMake 3.12 (#10432)
This PR fixes an issue introduced in #10330, where we use features from
CMake 3.25, despite the fact we are targeting CMake 3.12 as the
oldest-supported version.

Signed-off-by: Andrew V. Teylu <andrew@tey.lu>

cmake/FindCaDiCaL.cmake
@@ -23,10 +23,10 @@ find_library(CaDiCaL_LIBRARIES NAMES cadical)
 
 set(CaDiCaL_FOUND_SYSTEM FALSE)
 if(CaDiCaL_INCLUDE_DIR AND CaDiCaL_LIBRARIES)
-  # Try to compile our version file
-  try_run(RUN_RESULT_VAR
-    COMPILE_RESULT_VAR
-    SOURCE_FROM_CONTENT CaDiCaL_version.cpp
+
+  # Generate our version check file in CMAKE_BINARY_DIR
+  set(CaDiCaL_version_src "${CMAKE_BINARY_DIR}/CaDiCaL_version.cpp")
+  file(WRITE ${CaDiCaL_version_src}
     "
     #include <cadical.hpp>
     #include <iostream>
@@ -37,9 +37,28 @@ if(CaDiCaL_INCLUDE_DIR AND CaDiCaL_LIBRARIES)
       return 0;
     }
     "
+  )
+
+  # `try_run` doesn't have a way to specify a specific include dirs, so we need
+  # to set (and then reset) `CMAKE_CXX_FLAGS`; our version file is a .cpp file,
+  # so CXX_FLAGS are the right flags
+  set(OLD_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+
+  # cvc5 doesn't support MSVC, so we're fine to hard-code `-I` as the include
+  # flag
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I${CaDiCaL_INCLUDE_DIR}")
+
+  # Try to compile and run our version file
+  try_run(RUN_RESULT_VAR
+    COMPILE_RESULT_VAR
+    ${CMAKE_BINARY_DIR}
+    ${CaDiCaL_version_src}
     LINK_LIBRARIES ${CaDiCaL_LIBRARIES}
     RUN_OUTPUT_VARIABLE CaDiCaL_VERSION
   )
+
+  # Restore our CXX flags after checking with `try_run`
+  set(CMAKE_CXX_FLAGS ${OLD_CXX_FLAGS})
 
   # If this failed to compile or run, we have bigger issues
   if (NOT ${RUN_RESULT_VAR} EQUAL 0 OR NOT ${COMPILE_RESULT_VAR})