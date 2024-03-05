91f1f941c9f043f8beed6f9ebacb424954e908c2
ajreynol <andrew.j.reynolds@gmail.com>
2024-03-05 14:13:57 -0600

    Format

--- src/preprocessing/passes/sygus_inference.cpp
+++ src/preprocessing/passes/sygus_inference.cpp
@@ -15,10 +15,10 @@
 
 #include "preprocessing/passes/sygus_inference.h"
 
-#include "smt/logic_exception.h"
 #include "options/quantifiers_options.h"
 #include "preprocessing/assertion_pipeline.h"
 #include "preprocessing/preprocessing_pass_context.h"
+#include "smt/logic_exception.h"
 #include "smt/solver_engine.h"
 #include "theory/quantifiers/quantifiers_attributes.h"
 #include "theory/quantifiers/quantifiers_preprocess.h"
@@ -71,7 +71,8 @@ PreprocessingPassResult SygusInference::applyInternal(
       }
     }
   }
-  else if (options().quantifiers.sygusInference==options::SygusInferenceMode::ON)
+  else if (options().quantifiers.sygusInference
+           == options::SygusInferenceMode::ON)
   {
     std::stringstream ss;
     ss << "Cannot translate input to sygus for --sygus-inference";
