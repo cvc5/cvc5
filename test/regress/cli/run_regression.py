fdf9ce71386cfcd873d94a1423e64068cf996d0a
Aina Niemetz <aina.niemetz@gmail.com>
2024-03-12 09:35:09 -0700

    Update copyright headers. (#10459)

--- test/regress/cli/run_regression.py
+++ test/regress/cli/run_regression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 ###############################################################################
 # Top contributors (to current version):
-#   Andres Noetzli, Abdalrhman Mohamed, Aina Niemetz
+#   Andres Noetzli, Abdalrhman Mohamed, Andrew Reynolds
 #
 # This file is part of the cvc5 project.
 #
-# Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
+# Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
 # in the top-level source directory and their institutional affiliations.
 # All rights reserved.  See the file COPYING in the top-level source
 # directory for licensing information.
