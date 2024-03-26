283086602e0fb9372c7821f56a315c2e35efbb36
ajreynol <andrew.j.reynolds@gmail.com>
2024-03-26 10:47:05 -0500

    More

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -305,6 +305,10 @@ void AlfPrinter::print(std::ostream& out, std::shared_ptr<ProofNode> pfn)
           {
             processed.insert(key);
             std::string origName = v.getName();
+            if (origName.substr(0,4)!="alf.")
+            {
+              continue;
+            }
             origName = origName.substr(origName.find(".")+1);
             origName = origName.substr(origName.find(".")+1);
             outVars << "(define " << v << " () (alf.var \"" << origName << "\" " << v.getType() << "))"
