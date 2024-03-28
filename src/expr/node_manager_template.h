257051a4a169a31e97c2ff625fabaf3f580eb3ce
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2024-03-28 08:17:33 -0500

    Apply suggestions from code review
    Co-authored-by: Abdalrhman Mohamed <abdoo8080@outlook.com>

--- src/expr/node_manager_template.h
+++ src/expr/node_manager_template.h
@@ -1037,7 +1037,7 @@ class NodeManager
   /** A mapping for sorts allocated by mkSortConstructor where fresh is false */
   std::map<std::pair<std::string, size_t>, TypeNode> d_nfreshSorts;
 
-  /** A mapping for variables constructed where fresh is false */
+  /** A mapping for variables constructed when fresh is false */
   std::map<std::pair<std::string, TypeNode>, Node> d_nfreshVars;
 
   TupleTypeCache d_tt_cache;
