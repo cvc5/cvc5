a39a233c76a1c3c2c08d1e6ebe943a4c1b218106
ajreynol <andrew.j.reynolds@gmail.com>
2024-04-10 17:29:31 -0500

    Fix

--- src/expr/nary_term_util.cpp
+++ src/expr/nary_term_util.cpp
@@ -16,6 +16,7 @@
 #include "expr/nary_term_util.h"
 
 #include "expr/attribute.h"
+#include "expr/skolem_manager.h"
 #include "theory/bv/theory_bv_utils.h"
 #include "theory/strings/word.h"
 #include "util/bitvector.h"
