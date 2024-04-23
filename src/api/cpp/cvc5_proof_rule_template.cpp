914c6949f650c8aaded25f9caee4df1312b5283d
ajreynol <andrew.j.reynolds@gmail.com>
2024-04-23 13:46:49 -0500

    Format

--- src/api/cpp/cvc5_proof_rule_template.cpp
+++ src/api/cpp/cvc5_proof_rule_template.cpp
@@ -222,8 +222,7 @@ const char* toString(cvc5::ProofRewriteRule rule)
   {
     case ProofRewriteRule::NONE: return "NONE";
     //================================================= ad-hoc rules
-    case ProofRewriteRule::EXISTS_ELIM:
-      return "exists-elim";
+    case ProofRewriteRule::EXISTS_ELIM: return "exists-elim";
     case ProofRewriteRule::RE_LOOP_ELIM:
       return "re-loop-elim";
       //================================================= RARE rules
