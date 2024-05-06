ea3612386941a80812b312c2d0975b3162ea36f0
ajreynol <andrew.j.reynolds@gmail.com>
2024-05-06 12:24:35 -0500

    More

--- test/unit/api/cpp/solver_black.cpp
+++ test/unit/api/cpp/solver_black.cpp
@@ -1821,6 +1821,19 @@ TEST_F(TestApiBlackSolver, simplify)
   ASSERT_NO_THROW(slv.simplify(x));
 }
 
+TEST_F(TestApiBlackSolver, simplifyApplySubs)
+{
+  d_solver->setOption("incremental", "true");
+  Term x = d_tm.mkConst(intSort, "x");
+  Term zero = d_tm.mkInteger(0);
+  Term eq = d_tm.mkTerm(Kind::EQUAL, x, zero);
+  d_solver->assertFormula(eq);
+  ASSERT_NO_THROW(d_solver->checkSat());
+  
+  ASSERT_EQ(d_solver->simplify(x, false), x);
+  ASSERT_EQ(d_solver->simplify(x, true), zero);
+}
+
 TEST_F(TestApiBlackSolver, assertFormula)
 {
   ASSERT_NO_THROW(d_solver->assertFormula(d_tm.mkTrue()));
