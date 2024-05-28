4496833195dcc67e3e836a20d303123a80d2a020
ajreynol <andrew.j.reynolds@gmail.com>
2024-05-28 08:39:07 -0500

    Testing div by zero

--- test/unit/api/java/TermTest.java
+++ test/unit/api/java/TermTest.java
@@ -814,6 +814,9 @@ class TermTest
     assertEquals("1/18446744073709551617", Utils.getRational(real8.getRealValue()));
     assertEquals("18446744073709551617/1", Utils.getRational(real9.getRealValue()));
     assertEquals("23432343/10000", Utils.getRational(real10.getRealValue()));
+
+    assertThrows(CVC5ApiException.class, () -> d_tm.mkReal("1/0");
+    assertThrows(CVC5ApiException.class, () -> d_tm.mkReal("2/0000");
   }
 
   @Test
