6f736b503486833fb8df06e62ea609894698c7ef
ajreynol <andrew.j.reynolds@gmail.com>
2024-06-09 15:41:36 -0500

    Implement

--- src/rewriter/singleton_elim_process.cpp
+++ src/rewriter/singleton_elim_process.cpp
@@ -24,8 +24,41 @@ SingletonElimConverter::SingletonElimConverter(Env& env) : d_tpg(env, nullptr)
 
 std::shared_ptr<ProofNode> SingletonElimConverter::convert(const Node& n, const Node& nse)
 {
-  // TODO
-
+  std::unordered_set<std::pair<TNode, TNode>, TNodePairHashFunction> visited;
+  std::unordered_set<std::pair<TNode, TNode>, TNodePairHashFunction>::iterator
+      it;
+  std::unordered_map<Node, Node>::iterator subsIt;
+  std::vector<std::pair<TNode, TNode>> stack;
+  stack.emplace_back(n, nse);
+  std::pair<TNode, TNode> curr;
+  while (!stack.empty())
+  {
+    curr = stack.back();
+    stack.pop_back();
+    if (curr.first==curr.second)
+    {
+      continue;
+    }
+    if (visited.find(curr)!=visited.end())
+    {
+      continue;
+    }
+    visited.insert(curr);
+    if (curr.first.getKind()==Kind::APPLY_SINGLETON)
+    {
+      Node eq = curr.first.eqNode(curr.second);
+      d_tpg.addRewriteStep(curr.first, curr.second, ProofRule::ACI_NORM, {}, {eq});
+      stack.pop_back();
+      continue;
+    }
+    // else recurse
+    size_t nchild = curr.first.getNumChildren();
+    Assert (curr.second.getNumChildren()==nchild);
+    for (size_t i=0; i<nchild; i++)
+    {
+      stack.emplace_back(n[i], nse[i]);
+    }
+  }
   Node eq = n.eqNode(nse);
   return d_tpg.getProofFor(eq);
 }
