5cb77a925819646e3349f67fa4b79d9d7f932234
ajreynol <andrew.j.reynolds@gmail.com>
2024-06-10 10:44:16 -0500

    Simplify

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -42,7 +42,6 @@ AlfPrinter::AlfPrinter(Env& env,
     : EnvObj(env),
       d_tproc(atp),
       d_termLetPrefix("@t"),
-      d_ltproc(nodeManager(), atp),
       d_rdb(rdb)
 {
   d_pfType = nodeManager()->mkSort("proofType");
@@ -443,9 +442,7 @@ void AlfPrinter::printDslRule(std::ostream& out, ProofRewriteRule r)
       {
         out << " ";
       }
-      // note we apply list conversion to premises as well.
       Node cc = d_tproc.convert(su.apply(c));
-      cc = d_ltproc.convert(cc);
       out << cc;
     }
     out << ")" << std::endl;
@@ -461,7 +458,6 @@ void AlfPrinter::printDslRule(std::ostream& out, ProofRewriteRule r)
   }
   out << ")" << std::endl;
   Node sconc = d_tproc.convert(su.apply(conc));
-  sconc = d_ltproc.convert(sconc);
   Assert(sconc.getKind() == Kind::EQUAL);
   out << "  :conclusion (= " << sconc[0] << " " << sconc[1] << ")" << std::endl;
   out << ")" << std::endl;
