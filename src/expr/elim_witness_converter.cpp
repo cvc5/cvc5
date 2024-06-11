fabb187e03415d74bfd192670761daa6d1cafdeb
ajreynol <andrew.j.reynolds@gmail.com>
2024-06-11 09:55:15 -0500

    More

--- src/expr/elim_witness_converter.cpp
+++ src/expr/elim_witness_converter.cpp
@@ -32,10 +32,11 @@ Node ElimWitnessNodeConverter::postConvert(Node n)
     NodeManager* nm = nodeManager();
     SkolemManager* skm = nm->getSkolemManager();
     std::vector<Node> nchildren(n.begin(), n.end());
-    Node exists = nm->mkNode(Kind::EXISTS, nchildren);
+    nchildren[1] = nchildren[1].notNode();
+    Node q = nm->mkNode(Kind::FORALL, nchildren);
     Node k = skm->mkSkolemFunction(SkolemId::QUANTIFIERS_SKOLEMIZE,
-                                   {exists, n[0][0]});
-    d_exists.push_back(exists);
+                                   {q, n[0][0]});
+    d_exists.push_back(q.notNode());
     return k;
   }
   return n;
