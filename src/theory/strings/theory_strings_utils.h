d9defc5774ca170b90aa80a8902622fe7f148a99
ajreynol <andrew.j.reynolds@gmail.com>
2024-06-11 07:00:30 -0500

    Minor

--- src/theory/strings/theory_strings_utils.h
+++ src/theory/strings/theory_strings_utils.h
@@ -72,11 +72,11 @@ Node mkPrefix(Node t, Node n);
 Node mkSuffix(Node t, Node n);
 
 /**
- * Returns (lpre t n), which is (str.substr t 0 (- (str.len t) n)).
+ * Returns (str.substr t 0 (- (str.len t) n)).
  */
 Node mkPrefixExceptLen(Node t, Node n);
 /**
- * Returns (lsuf t n), which is (str.substr t (- (str.len t) n) n).
+ * Returns (str.substr t (- (str.len t) n) n).
  */
 Node mkSuffixOfLen(Node t, Node n);
 
