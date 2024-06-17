4f2d96afb4df7bfd8bc687d784f3c78b7a473812
Leni Aniva <v@leni.sh>
2024-06-17 14:20:36 -0700

    build: Invoke cxx-build

--- src/api/rust/src/lib.rs
+++ src/api/rust/src/lib.rs
@@ -1,9 +1,18 @@
-#[cxx::bridge]
-mod primitive {
-    unsafe extern "C++" {
-		 //include!(concat!(env!("CVC5_INCLUDE_DIR"), "/cvc5/cvc5.h"));
-		 include!("${CVC5_INCLUDE_DIR}/cvc5/cvc5.h");
-	 }
+#[cxx::bridge(namespace = "cvc5")]
+pub mod primitive {
+	unsafe extern "C++" {
+		include!("cvc5/cvc5.h");
+		type CVC5ApiException;
+		type CVC5ApiUnsupportedException;
+		type Result;
+	}
+
+}
+#[cxx::bridge(namespace = "cvc5::parser")]
+pub mod parser {
+	unsafe extern "C++" {
+		include!("cvc5/cvc5_parser.h");
+	}
 }
 
 pub const PLACEHOLDER: &str = "placeholder";
