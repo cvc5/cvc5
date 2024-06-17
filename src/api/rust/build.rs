b0a1837af17489fe590621e5d48aafa8b6f680df
Leni Aniva <v@leni.sh>
2024-06-17 13:48:33 -0700

    test: Rust API testing stub

--- src/api/rust/build.rs
+++
@@ -1,8 +0,0 @@
-use std::{env, path::PathBuf};
-
-fn main() -> Result<(), Box<dyn std::error::Error>>
-{
-	println!("cargo:warning=Build stub");
-
-	Ok(())
-}
--- src/api/rust/src/lib.rs
+++ src/api/rust/src/lib.rs
@@ -1,2 +1,9 @@
+#[cxx::bridge]
+mod primitive {
+    unsafe extern "C++" {
+		 //include!(concat!(env!("CVC5_INCLUDE_DIR"), "/cvc5/cvc5.h"));
+		 include!("${CVC5_INCLUDE_DIR}/cvc5/cvc5.h");
+	 }
+}
 
-pub const DUMMY: &str = "test";
+pub const PLACEHOLDER: &str = "placeholder";
