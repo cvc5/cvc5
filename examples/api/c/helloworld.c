1ac2af2f3f0be15149cec1526a0d295ab67017d3
Aina Niemetz <aina.niemetz@gmail.com>
2024-06-21 16:50:01 -0700

    c api: Add helloworld example.

---
+++ examples/api/c/helloworld.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Aina Niemetz
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * A very simple cvc5 example.
+ */
+
+#include <cvc5/c/cvc5.h>
+#include <stdio.h>
+
+int main()
+{
+  Cvc5TermManager* tm = cvc5_term_manager_new();
+  Cvc5* slv = cvc5_new(tm);
+  Cvc5Term hello = cvc5_mk_const(tm, cvc5_get_boolean_sort(tm), "Hello World!");
+  Cvc5Term assumptions[1] = {hello};
+  printf("%s is %s\n",
+         cvc5_term_to_string(hello),
+         cvc5_result_to_string(cvc5_check_sat_assuming(slv, 1, assumptions)));
+  cvc5_delete(slv);
+  cvc5_term_manager_delete(tm);
+  return 0;
+}
