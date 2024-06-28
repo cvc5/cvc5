c82beb140927f10c0f809167cfeaf7b5861e07d7
Elizaveta Pertseva <46387494+limpa105@users.noreply.github.com>
2024-06-28 14:51:07 -0700

    fixed cocoa issue with MacOs (#10983)
    This likely fixes issue
    [#10973](https://github.com/cvc5/cvc5/issues/10973) but definitely fixes
    the issue on M1

--- cmake/FindCoCoA.cmake
+++ cmake/FindCoCoA.cmake
@@ -58,6 +58,12 @@ if(NOT CoCoA_FOUND_SYSTEM)
     message(FATAL_ERROR "Can not build CoCoA, missing binary for patch")
   endif()
 
+  # On macOS, we have to set `-isysroot` to make sure that include headers are
+  # found because they are not necessarily installed at /usr/include anymore.
+  if(CMAKE_OSX_SYSROOT)
+    set(CXXFLAGS "${CMAKE_CXX_SYSROOT_FLAG} ${CMAKE_OSX_SYSROOT}")
+  endif()
+
   ExternalProject_Add(
     CoCoA-EP
     ${COMMON_EP_CONFIG}
@@ -68,7 +74,7 @@ if(NOT CoCoA_FOUND_SYSTEM)
     PATCH_COMMAND patch -p1 -d <SOURCE_DIR>
         -i ${CMAKE_CURRENT_LIST_DIR}/deps-utils/CoCoALib-0.99800-trace.patch
     BUILD_IN_SOURCE YES
-    CONFIGURE_COMMAND ${SHELL} ./configure --prefix=<INSTALL_DIR> --with-libgmp=${GMP_LIBRARY} --with-cxx=${CMAKE_CXX_COMPILER}
+    CONFIGURE_COMMAND ${SHELL} ./configure --prefix=<INSTALL_DIR> --with-libgmp=${GMP_LIBRARY} --with-cxx=${CMAKE_CXX_COMPILER} --with-cxxflags=${CXXFLAGS} 
     BUILD_COMMAND ${make_cmd} library
     BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libcocoa.a
   )
