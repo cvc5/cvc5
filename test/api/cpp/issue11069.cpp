e550b3d8ac59f5d73634c6daac2d1398842d0541
ajreynol <andrew.j.reynolds@gmail.com>
2024-07-18 09:29:32 -0500

    Fix

--- test/api/cpp/issue11069.cpp
+++ test/api/cpp/issue11069.cpp
@@ -1,4 +1,20 @@
-#include <iostream>
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Test for issue #11069.
+ */
+#include <cvc5/cvc5.h>
+
+#include <cassert>
 
 using namespace cvc5;
 using namespace cvc5::parser;
