9a30b636b4534ce7eda3ded207fa4dbd28cf06e7
ajreynol <andrew.j.reynolds@gmail.com>
2024-07-19 06:07:47 -0500

    Revert to fresh binder semantics in parser

--- src/parser/smt2/smt2_cmd_parser.cpp
+++ src/parser/smt2/smt2_cmd_parser.cpp
@@ -402,10 +402,12 @@ std::unique_ptr<Cmd> Smt2CmdParser::parseNextCommand()
       {
         d_state.pushScope();
       }
+      bool freshBinders = d_state.usingFreshBinders();
+      // If d_freshBinders is false,
       // Must use fresh=false here to ensure that variables introduced by
       // define-fun are accurate with respect to proofs, i.e. variables of
       // the same name and type are indeed the same variable.
-      std::vector<Term> terms = d_state.bindBoundVars(sortedVarNames, false);
+      std::vector<Term> terms = d_state.bindBoundVars(sortedVarNames, freshBinders);
       Term expr = d_tparser.parseTerm();
       if (!flattenVars.empty())
       {
