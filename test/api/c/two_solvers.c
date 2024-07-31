63b043746c1de2b587afdf806825edd80443d97e
Aina Niemetz <aina.niemetz@gmail.com>
2024-07-31 01:17:54 -0700

    c api: Add API test two_solvers. (#11104)
    Co-authored-by: Daniel Larraz <daniel-larraz@users.noreply.github.com>

--- test/api/c/two_solvers.c
+++ test/api/c/two_solvers.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Top contributors (to current version):
- *   Aina Niemetz, Morgan Deters, Mathias Preiner
+ *   Aina Niemetz
  *
  * This file is part of the cvc5 project.
  *
@@ -13,19 +13,19 @@
  * A simple test of multiple SmtEngines.
  */
 
-#include <cvc5/cvc5.h>
-
-#include <sstream>
-
-using namespace cvc5;
-using namespace std;
+#include <cvc5/c/cvc5.h>
 
 int main()
 {
-  TermManager tm;
-  Solver s1(tm);
-  Solver s2(tm);
-  Result res1 = s1.checkSatAssuming(tm.mkBoolean(false));
-  Result res2 = s2.checkSatAssuming(tm.mkBoolean(false));
-  return res1.isUnsat() && res2.isUnsat() ? 0 : 1;
+  Cvc5TermManager* tm = cvc5_term_manager_new();
+  Cvc5* solver1 = cvc5_new(tm);
+  Cvc5* solver2 = cvc5_new(tm);
+  Cvc5Term assumptions[1] = {cvc5_mk_false(tm)};
+  Cvc5Result res1 = cvc5_check_sat_assuming(solver1, 1, assumptions);
+  Cvc5Result res2 = cvc5_check_sat_assuming(solver2, 1, assumptions);
+  int res = cvc5_result_is_unsat(res1) && cvc5_result_is_unsat(res2) ? 0 : 1;
+  cvc5_delete(solver1);
+  cvc5_delete(solver2);
+  cvc5_term_manager_delete(tm);
+  return res;
 }
