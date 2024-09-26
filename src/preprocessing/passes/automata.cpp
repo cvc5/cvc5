b8a22158e118ea2fe532c146bed682e9a1fb8e0b
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-09-25 22:36:40 -0300

    starting to add functions to automata.cpp

---
+++ src/preprocessing/passes/automata.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Automata preprocessing pass.
+ *
+ */
+
+#include "preprocessing/passes/automata.h"
+
+#include <cmath>
+
+#include "base/check.h"
+#include "expr/node_algorithm.h"
+#include "expr/skolem_manager.h"
+#include "options/base_options.h"
+#include "options/options.h"
+#include "preprocessing/assertion_pipeline.h"
+#include "preprocessing/preprocessing_pass_context.h"
+#include "smt/logic_exception.h"
+
+using namespace cvc5::internal;
+using namespace cvc5::internal::theory;
+
+namespace cvc5::internal {
+namespace preprocessing {
+namespace passes {
+
+/* -------------------------------------------------------------------------- */
+
+namespace {
+
+}
+/* -------------------------------------------------------------------------- */
+
+}  // namespace passes
+}  // namespace preprocessing
+}  // namespace cvc5::internal
