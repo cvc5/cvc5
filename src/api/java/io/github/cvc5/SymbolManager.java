ab279267746337f2a543336a3afe08165907fce4
ajreynol <andrew.j.reynolds@gmail.com>
2024-09-26 09:46:49 -0500

    Java

--- src/api/java/io/github/cvc5/SymbolManager.java
+++ src/api/java/io/github/cvc5/SymbolManager.java
@@ -129,4 +129,27 @@ public class SymbolManager extends AbstractPointer
   }
 
   private native long[] getDeclaredTerms(long pointer);
+
+
+
+  /**
+   * Get a mapping from terms to names that have been given to them via the
+   * :named attribute.
+   *
+   * @return A map of the named terms to their name.
+   */
+  public Map<Term, String> getNamedTerms()
+  {
+    Map<Long, String> map = getNamedTerms(pointer);
+    Map<Term, String> ret = new HashMap<>();
+    for (Map.Entry<Long, String> entry : map.entrySet())
+    {
+      Term key = new Term(entry.getKey());
+      String value = entry.getValue();
+      ret.put(key, value);
+    }
+    return ret;
+  }
+
+  private native Map<Long, String> getNamedTerms(long pointer);
 }
