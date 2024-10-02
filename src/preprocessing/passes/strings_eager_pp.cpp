10bc90c65cdafea83c5e33180cbbaf61b533f5e1
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-10-02 08:21:12 -0500

    skolem caches: Refactor to not use NodeManager::currentNM() (#11215)
    Co-authored-by: Andrew Reynolds <andrew.j.reynolds@gmail.com>

--- src/preprocessing/passes/strings_eager_pp.cpp
+++ src/preprocessing/passes/strings_eager_pp.cpp
@@ -32,7 +32,7 @@ PreprocessingPassResult StringsEagerPp::applyInternal(
     AssertionPipeline* assertionsToPreprocess)
 {
   NodeManager* nm = nodeManager();
-  theory::strings::SkolemCache skc(nullptr);
+  theory::strings::SkolemCache skc(nm, nullptr);
   theory::strings::StringsPreprocess pp(d_env, &skc);
   for (size_t i = 0, nasserts = assertionsToPreprocess->size(); i < nasserts;
        ++i)
