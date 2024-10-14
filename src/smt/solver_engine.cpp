1692575ed45e4019935a2f2e2f3e57fa67ab7a25
ajreynol <andrew.j.reynolds@gmail.com>
2024-10-14 16:03:58 -0500

    Revert

--- src/smt/solver_engine.cpp
+++ src/smt/solver_engine.cpp
@@ -1798,7 +1798,7 @@ void SolverEngine::proofToString(std::ostream& out,
 {
   options::ProofFormatMode format_mode =
       getOptions().proof.proofFormatMode;
-  d_pfManager->printProof(out, fp, format_mode, ProofScopeMode::UNIFIED);
+  d_pfManager->printProof(out, fp, format_mode, ProofScopeMode::DEFINITIONS_AND_ASSERTIONS);
 }
 
 void SolverEngine::printInstantiations(std::ostream& out)
