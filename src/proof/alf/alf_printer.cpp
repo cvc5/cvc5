eabce22d6c8d8fa172394d91ffbfb055f50014dc
ajreynol <andrew.j.reynolds@gmail.com>
2024-10-22 10:10:40 -0500

    Add Eunoia definitions for missing quantifiers rules

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -63,12 +63,13 @@ AlfPrinter::AlfPrinter(Env& env,
   d_false = nodeManager()->mkConst(false);
 }
 
-bool AlfPrinter::isHandled(const ProofNode* pfn) const
+bool AlfPrinter::isHandled(const Options& opts, const ProofNode* pfn)
 {
   const std::vector<Node> pargs = pfn->getArguments();
   switch (pfn->getRule())
   {
     // List of handled rules
+    case ProofRule::ASSUME:
     case ProofRule::SCOPE:
     case ProofRule::REFL:
     case ProofRule::SYMM:
@@ -221,7 +222,7 @@ bool AlfPrinter::isHandled(const ProofNode* pfn) const
       if (k == Kind::STRING_TO_CODE || k == Kind::STRING_FROM_CODE)
       {
         // must use standard alphabet size
-        return options().strings.stringsAlphaCard == String::num_codes();
+        return opts.strings.stringsAlphaCard == String::num_codes();
       }
       return k == Kind::STRING_CONTAINS || k == Kind::STRING_INDEXOF
              || k == Kind::STRING_IN_REGEXP;
@@ -243,8 +244,7 @@ bool AlfPrinter::isHandled(const ProofNode* pfn) const
   return false;
 }
 
-bool AlfPrinter::isHandledTheoryRewrite(ProofRewriteRule id,
-                                        const Node& n) const
+bool AlfPrinter::isHandledTheoryRewrite(ProofRewriteRule id, const Node& n)
 {
   switch (id)
   {
@@ -252,6 +252,9 @@ bool AlfPrinter::isHandledTheoryRewrite(ProofRewriteRule id,
     case ProofRewriteRule::BETA_REDUCE:
     case ProofRewriteRule::ARITH_STRING_PRED_ENTAIL:
     case ProofRewriteRule::ARITH_STRING_PRED_SAFE_APPROX:
+    case ProofRewriteRule::EXISTS_ELIM:
+    case ProofRewriteRule::QUANT_UNUSED_VARS:
+    //case ProofRewriteRule::QUANT_MINISCOPE:
     case ProofRewriteRule::QUANT_MINISCOPE_FV:
     case ProofRewriteRule::RE_LOOP_ELIM:
     case ProofRewriteRule::SETS_IS_EMPTY_EVAL:
@@ -269,7 +272,7 @@ bool AlfPrinter::isHandledTheoryRewrite(ProofRewriteRule id,
   return false;
 }
 
-bool AlfPrinter::isHandledBitblastStep(const Node& eq) const
+bool AlfPrinter::isHandledBitblastStep(const Node& eq)
 {
   Assert(eq.getKind() == Kind::EQUAL);
   if (eq[0].isVar())
@@ -289,7 +292,7 @@ bool AlfPrinter::isHandledBitblastStep(const Node& eq) const
   return false;
 }
 
-bool AlfPrinter::canEvaluate(Node n) const
+bool AlfPrinter::canEvaluate(Node n)
 {
   std::unordered_set<TNode> visited;
   std::vector<TNode> visit;
@@ -379,7 +382,7 @@ bool AlfPrinter::canEvaluate(Node n) const
   return true;
 }
 
-bool AlfPrinter::canEvaluateRegExp(Node r) const
+bool AlfPrinter::canEvaluateRegExp(Node r)
 {
   Assert(r.getType().isRegExp());
   Trace("alf-printer-debug") << "canEvaluateRegExp? " << r << std::endl;
@@ -919,7 +922,7 @@ void AlfPrinter::printStepPost(AlfPrintChannel* out, const ProofNode* pn)
   std::vector<std::shared_ptr<ProofNode>> children;
   getChildrenFromProofRule(pn, children);
   std::vector<Node> args;
-  bool handled = isHandled(pn);
+  bool handled = isHandled(options(), pn);
   if (handled)
   {
     getArgsFromProofRule(pn, args);
