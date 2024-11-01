cb28a52da3fe7e7b8a55da619a899f6afdbe1a84
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2024-11-01 15:24:14 -0500

    Add Eunoia definition of ARITH_REDUCTION (#11298)
    The transdental functions are not yet covered for now.
    
    Also makes a minor simplification to the code for operator elim.
    
    A followup PR will modify this rule to avoid introducing mixed
    arithmetic.

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -205,6 +205,16 @@ bool AlfPrinter::isHandled(const ProofNode* pfn) const
       return res[0][0].getType().isRealOrInt();
     }
     break;
+    case ProofRule::ARITH_REDUCTION:
+    {
+      Kind k = pargs[0].getKind();
+      return k == Kind::TO_INTEGER || k == Kind::IS_INTEGER
+             || k == Kind::DIVISION || k == Kind::DIVISION_TOTAL
+             || k == Kind::INTS_DIVISION || k == Kind::INTS_DIVISION_TOTAL
+             || k == Kind::INTS_MODULUS || k == Kind::INTS_MODULUS_TOTAL
+             || k == Kind::ABS;
+    }
+    break;
     case ProofRule::STRING_REDUCTION:
     {
       // depends on the operator
