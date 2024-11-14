858fc8b3f3316ee121e569675eb1e4238af458fe
ajreynol <andrew.j.reynolds@gmail.com>
2024-11-14 08:31:16 -0600

    Disable for now

--- src/theory/arith/linear/theory_arith_private.cpp
+++ src/theory/arith/linear/theory_arith_private.cpp
@@ -1804,7 +1804,7 @@ bool TheoryArithPrivate::outputLemma(TNode lem, InferenceId id) {
 
 void TheoryArithPrivate::outputTrustedConflict(TrustNode conf, InferenceId id)
 {
-  AlwaysAssert(!isProofEnabled() || conf.getGenerator() != nullptr);
+  //AlwaysAssert(!isProofEnabled() || conf.getGenerator() != nullptr);
   Trace("arith::channel") << "Arith trusted conflict: " << conf << std::endl;
   d_containing.outputTrustedConflict(conf, id);
 }
