e8b19e748bab00be67be4dc3354835971c31ed40
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2024-12-05 10:11:18 -0600

    Make macro reconstruction robust to partial failures (#11413)
    In some rare cases, macro reconstruction may partially fill in a proof
    and fail. This can lead to open proofs since we were not *overwriting*
    the proof with a trust step in such cases.
    
    This PR fixes the issue and adds a regression.

--- src/rewriter/basic_rewrite_rcons.cpp
+++ src/rewriter/basic_rewrite_rcons.cpp
@@ -237,7 +237,10 @@ void BasicRewriteRCons::ensureProofForTheoryRewrite(
   args.push_back(
       nodeManager()->mkConstInt(Rational(static_cast<uint32_t>(id))));
   args.push_back(eq);
-  cdp->addStep(eq, ProofRule::THEORY_REWRITE, {}, args);
+  // always overwrite, in case we partially completed a proof in a macro method
+  // above
+  cdp->addStep(
+      eq, ProofRule::THEORY_REWRITE, {}, args, false, CDPOverwrite::ALWAYS);
 }
 
 bool BasicRewriteRCons::ensureProofMacroBoolNnfNorm(CDProof* cdp,
@@ -818,6 +821,7 @@ bool BasicRewriteRCons::ensureProofMacroQuantVarElimEq(CDProof* cdp,
       // where the latter implies the former, but they are not equivalent
       if (rewrite(body1r[i]) != rewrite(body1re[i]))
       {
+        Trace("brc-macro") << "...failed to rewrite" << std::endl;
         return false;
       }
       if (body1r[i] == body1re[i])
