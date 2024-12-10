5ef4d4318dbd23aa759e296a5b2be2a084c9e786
ajreynol <andrew.j.reynolds@gmail.com>
2024-12-10 10:34:43 -0600

    Fix

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -291,7 +291,7 @@ bool AlfPrinter::isHandledTheoryRewrite(ProofRewriteRule id, const Node& n)
     case ProofRewriteRule::STR_CTN_MULTISET_SUBSET:
       // only strings are supported, since it is non-trivial to show
       // distinctness of sequence characters.
-      return n[0].getType().isString();
+      return n[0][0].getType().isString();
     case ProofRewriteRule::STR_IN_RE_EVAL:
       Assert(n[0].getKind() == Kind::STRING_IN_REGEXP && n[0][0].isConst());
       return canEvaluateRegExp(n[0][1]);
