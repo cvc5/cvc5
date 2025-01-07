0eb0668694cb0f08663bc055ebe7aab349e23e7e
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2025-01-07 11:57:47 -0600

    Simplify interface for ppAssert (#11401)
    We never use `PP_ASSERT_STATUS_CONFLICT` currently, and moreover don't
    have proof support in the nonclausal simplifier if this ever were to be
    used, thus this PR simplifies the interface to return a bool (solved vs.
    unsolved).

--- src/theory/sets/theory_sets.cpp
+++ src/theory/sets/theory_sets.cpp
@@ -192,12 +192,11 @@ TrustNode TheorySets::ppRewrite(TNode n, std::vector<SkolemLemma>& lems)
   return d_internal->ppRewrite(n, lems);
 }
 
-Theory::PPAssertStatus TheorySets::ppAssert(
-    TrustNode tin, TrustSubstitutionMap& outSubstitutions)
+bool TheorySets::ppAssert(TrustNode tin, TrustSubstitutionMap& outSubstitutions)
 {
   TNode in = tin.getNode();
   Trace("sets-proc") << "ppAssert : " << in << std::endl;
-  Theory::PPAssertStatus status = Theory::PP_ASSERT_STATUS_UNSOLVED;
+  bool status = false;
 
   // this is based off of Theory::ppAssert
   if (in.getKind() == Kind::EQUAL)
@@ -211,7 +210,7 @@ Theory::PPAssertStatus TheorySets::ppAssert(
       if (!in[0].getType().isSet() || !options().sets.setsExp)
       {
         outSubstitutions.addSubstitutionSolved(in[0], in[1], tin);
-        status = Theory::PP_ASSERT_STATUS_SOLVED;
+        status = true;
       }
     }
     else if (in[1].isVar() && d_valuation.isLegalElimination(in[1], in[0]))
@@ -219,7 +218,7 @@ Theory::PPAssertStatus TheorySets::ppAssert(
       if (!in[0].getType().isSet() || !options().sets.setsExp)
       {
         outSubstitutions.addSubstitutionSolved(in[1], in[0], tin);
-        status = Theory::PP_ASSERT_STATUS_SOLVED;
+        status = true;
       }
     }
   }
