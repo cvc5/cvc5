85ff7d2bffc4b28d9c7223303121dcf15181540f
ajreynol <andrew.j.reynolds@gmail.com>
2025-01-13 14:31:08 -0600

    Anoterh

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -383,6 +383,7 @@ bool AlfPrinter::canEvaluate(Node n)
         case Kind::STRING_TO_LOWER:
         case Kind::STRING_TO_UPPER:
         case Kind::STRING_REV:
+        case Kind::STRING_AT:
         case Kind::BITVECTOR_EXTRACT:
         case Kind::BITVECTOR_CONCAT:
         case Kind::BITVECTOR_ADD:
