ee49b03a128dac1ba63af18aead652eb5695cfeb
Aina Niemetz <aina.niemetz@gmail.com>
2025-01-22 15:26:18 -0800

    Update copyright headers.

--- cmake/update_rpath_macos.cmake
+++ cmake/update_rpath_macos.cmake
@@ -1,3 +1,20 @@
+###############################################################################
+# Top contributors (to current version):
+#   Daniel Larraz
+#
+# This file is part of the cvc5 project.
+#
+# Copyright (c) 2009-2025 by the authors listed in the file AUTHORS
+# in the top-level source directory and their institutional affiliations.
+# All rights reserved.  See the file COPYING in the top-level source
+# directory for licensing information.
+# #############################################################################
+#
+# [[ Add one-line brief description here ]]
+#
+# [[ Add lengthier description here ]]
+# \todo document this file
+##
 # If RPATH is provided, add it unless it already exists
 if(RPATH)
    # otool -l output is in the format:
