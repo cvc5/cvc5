4f3d959166182700adf1d813a35a56420963f9fd
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2025-02-07 16:34:15 -0600

    Add Eunoia definitions of ARITH_MULT_SIGN and ARITH_MULT_ABS_COMPARISION (#11497)
    Review https://github.com/cvc5/cvc5/pull/11476 first.

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -138,6 +138,8 @@ bool AlfPrinter::isHandled(const Options& opts, const ProofNode* pfn)
     case ProofRule::ARITH_MULT_POS:
     case ProofRule::ARITH_MULT_NEG:
     case ProofRule::ARITH_MULT_TANGENT:
+    case ProofRule::ARITH_MULT_SIGN:
+    case ProofRule::ARITH_MULT_ABS_COMPARISON:
     case ProofRule::ARITH_TRICHOTOMY:
     case ProofRule::ARITH_TRANS_EXP_NEG:
     case ProofRule::ARITH_TRANS_EXP_POSITIVITY:
