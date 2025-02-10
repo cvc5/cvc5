abe5413c0e35b9796a302b46108030144675f2a4
ajreynol <andrew.j.reynolds@gmail.com>
2025-02-10 13:39:21 -0600

    Consolidate sets evaluation rules, implement in Eunoia

--- include/cvc5/cvc5_proof_rule.h
+++ include/cvc5/cvc5_proof_rule.h
@@ -3309,82 +3309,21 @@ enum ENUM(ProofRewriteRule)
   EVALUE(MACRO_SUBSTR_STRIP_SYM_LENGTH),
   /**
    * \verbatim embed:rst:leading-asterisk
-   * **Sets -- distinct sets**
+   * **Sets -- sets evaluate operator**
    *
    * .. math::
-   *   (A = B) = \bot
+   *   \mathit{f}(t_1, t_2) = t
    *
-   * where :math:`A` and :math:`B` are distinct set values, that is,
-   * the Node::isConst method returns true for both. This rule
-   * verifies that this returns true for both terms and that these
-   * terms are distinct.
-   *
-   * \endverbatim
-   */
-  EVALUE(MACRO_SETS_DISTINCT_SETS),
-  /**
-   * \verbatim embed:rst:leading-asterisk
-   * **Sets -- sets intersection evaluate**
-   *
-   * .. math::
-   *   \mathit{set.inter}(t_1, t_2) = t
-   *
-   * where :math:`t_1` and :math:`t_2` are set values, that is,
-   * the Node::isConst method returns true for both, and
-   * where :math:`t` is an intersection of the component elements of
+   * where :math:`f` is one of :math:`\mathit{set.inter}, \mathit{set.minus}, \mathit{set.union}`,
+   * and :math:`t` is the result of evaluating :math:`f` on
    * :math:`t_1` and :math:`t_2`.
    *
-   * \endverbatim
-   */
-  EVALUE(MACRO_SETS_INTER_EVAL),
-  /**
-   * \verbatim embed:rst:leading-asterisk
-   * **Sets -- sets minus evaluate**
-   *
-   * .. math::
-   *   \mathit{set.minus}(t_1, t_2) = t
-   *
-   * where :math:`t_1` and :math:`t_2` are set values, that is,
-   * the Node::isConst method returns true for both, and
-   * where :math:`t` is the difference of the component elements of
-   * :math:`t_1` and :math:`t_2`.
-   *
-   * \endverbatim
-   */
-  EVALUE(MACRO_SETS_MINUS_EVAL),
-  /**
-   * \verbatim embed:rst:leading-asterisk
-   * **Sets -- sets union normalize**
-   *
-   * .. math::
-   *   \mathit{set.union}(t_1, t_2) = t
-   * 
-   * where :math:`t` is a union of the component elements of
-   * :math:`t_1` and :math:`t_2`.
-   * 
    * Note we use this rule only when :math:`t_1` and :math:`t_2` are set values,
    * that is, the Node::isConst method returns true for both.
    *
    * \endverbatim
    */
-  EVALUE(SETS_UNION_NORM),
-  /**
-   * \verbatim embed:rst:leading-asterisk
-   * **Sets -- empty tester evaluation**
-   *
-   * .. math::
-   *   \mathit{sets.is\_empty}(\epsilon) = \top
-   *
-   * where :math:`\epsilon` is the empty set, or alternatively:
-   *
-   * .. math::
-   *   \mathit{sets.is\_empty}(c) = \bot
-   *
-   * where :math:`c` is a constant set that is not the empty set.
-   *
-   * \endverbatim
-   */
-  EVALUE(SETS_IS_EMPTY_EVAL),
+  EVALUE(SETS_EVAL_OP),
   /**
    * \verbatim embed:rst:leading-asterisk
    * **Sets -- sets insert elimination**
