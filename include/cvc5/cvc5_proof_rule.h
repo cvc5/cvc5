9023e0338eaeb4fc715bdfd044029f015a31942d
ajreynol <andrew.j.reynolds@gmail.com>
2025-02-19 18:27:38 -0600

    Eo

--- include/cvc5/cvc5_proof_rule.h
+++ include/cvc5/cvc5_proof_rule.h
@@ -3787,6 +3787,8 @@ enum ENUM(ProofRewriteRule)
   EVALUE(BV_COMMUTATIVE_XOR),
   /** Auto-generated from RARE rule bv-commutative-mul */
   EVALUE(BV_COMMUTATIVE_MUL),
+  /** Auto-generated from RARE rule bv-commutative-comp */
+  EVALUE(BV_COMMUTATIVE_COMP),
   /** Auto-generated from RARE rule bv-or-zero */
   EVALUE(BV_OR_ZERO),
   /** Auto-generated from RARE rule bv-mul-one */
@@ -3823,6 +3825,12 @@ enum ENUM(ProofRewriteRule)
   EVALUE(BV_NEG_IDEMP),
   /** Auto-generated from RARE rule bv-sub-eliminate */
   EVALUE(BV_SUB_ELIMINATE),
+  /** Auto-generated from RARE rule bv-ite-width-one */
+  EVALUE(BV_ITE_WIDTH_ONE),
+  /** Auto-generated from RARE rule bv-eq-xor-solve */
+  EVALUE(BV_EQ_XOR_SOLVE),
+  /** Auto-generated from RARE rule bv-eq-not-solve */
+  EVALUE(BV_EQ_NOT_SOLVE),
   /** Auto-generated from RARE rule bv-ugt-eliminate */
   EVALUE(BV_UGT_ELIMINATE),
   /** Auto-generated from RARE rule bv-uge-eliminate */
@@ -3883,6 +3891,8 @@ enum ENUM(ProofRewriteRule)
   EVALUE(BV_USUBO_ELIMINATE),
   /** Auto-generated from RARE rule bv-ssubo-eliminate */
   EVALUE(BV_SSUBO_ELIMINATE),
+  /** Auto-generated from RARE rule bv-nego-eliminate */
+  EVALUE(BV_NEGO_ELIMINATE),
   /** Auto-generated from RARE rule bv-ite-equal-children */
   EVALUE(BV_ITE_EQUAL_CHILDREN),
   /** Auto-generated from RARE rule bv-ite-const-children-1 */
