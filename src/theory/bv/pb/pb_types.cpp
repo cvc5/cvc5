5dcdbd4493518251bd7b2c01bde7dfc582a7954c
Alan Prado <alanctprado@gmail.com>
2025-02-23 23:33:34 -0300

    PB Types
    Signed-off-by: Alan Prado <alanctprado@gmail.com>

---
+++ src/theory/bv/pb/pb_types.cpp
@@ -0,0 +1,126 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Alan Prado
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * TODO(alanctprado)
+ */
+
+#include "theory/bv/pb/pb_types.h"
+#include "util/rational.h"
+
+#include <sstream>
+
+namespace cvc5::internal {
+namespace theory {
+namespace bv {
+namespace pb {
+
+PbVariable::PbVariable(const Integer& id) : id(id.toString()) {}
+
+PbVariable::PbVariable(const int& id) : id(std::to_string(id)) {}
+
+std::ostream& operator<<(std::ostream& os, const PbVariable& var)
+{
+  os << "x" << var.id;
+  return os;
+}
+
+PbLiteral::PbLiteral(const PbVariable& var, const bool p)
+    : variable(var), polarity(p) {}
+
+PbLiteral::PbLiteral(const Integer& id, const bool p)
+    : variable(PbVariable(id)), polarity(p) {}
+
+PbLiteral::PbLiteral(const int& id, const bool p)
+    : variable(PbVariable(id)), polarity(p) {}
+
+std::ostream& operator<<(std::ostream& os, const PbLiteral& lit)
+{
+  os << (lit.polarity ? "" : "~") << lit.variable;
+  return os;
+}
+
+Node PbLiteral::toNode(NodeManager* nm) const
+{
+  return nm->mkBoundVar((std::ostringstream() << this).str(),
+                        nm->booleanType());
+}
+
+PbConstraint::PbConstraint(const std::vector<PbLiteral>& literals,
+                           const std::vector<Integer>& coefficients,
+                           Kind relationalOperator,
+                           const Integer& constant,
+                           NodeManager* nm)
+{
+  std::vector<Node> terms = generateProducts(literals, coefficients, nm);
+  terms.push_back(nm->mkConstInt(0));  // Ensure ADD has at least two operands
+  Node lhs = nm->mkNode(Kind::ADD, terms);
+  Node rhs = nm->mkConstInt(constant);
+  constraint = nm->mkNode(relationalOperator, lhs, rhs);
+}
+
+PbConstraint::PbConstraint(const PbLiteral& literal,
+                           const Integer& coefficient,
+                           Kind relationalOperator,
+                           const Integer& constant,
+                           NodeManager* nm)
+{
+  Node term = nm->mkNode(Kind::MULT,
+                         nm->mkConstInt(coefficient),
+                         literal.toNode(nm));
+  Node lhs = nm->mkNode(Kind::ADD, term, nm->mkConstInt(0)); // Ensure ADD has at least two operands
+  Node rhs = nm->mkConstInt(constant);
+  constraint = nm->mkNode(relationalOperator, lhs, rhs);
+}
+
+std::vector<Node> PbConstraint::generateProducts(
+    const std::vector<PbLiteral>& literals,
+    const std::vector<Integer>& coefficients,
+    NodeManager* nm)
+{
+  Assert(literals.size() == coefficients.size());
+
+  auto createTerm = [&nm](const PbLiteral& lit, const Integer& coef) {
+    return nm->mkNode(Kind::MULT,
+                      nm->mkConstInt(coef),
+                      lit.toNode(nm));
+  };
+
+  std::vector<Node> terms(literals.size());
+  std::transform(literals.begin(),
+                 literals.end(),
+                 coefficients.begin(),
+                 terms.begin(),
+                 createTerm);
+  return terms;
+}
+
+Node PbConstraint::toNode() const
+{
+  return constraint;
+}
+
+PbConstraintSet::PbConstraintSet(const std::set<PbConstraint> constraints,
+                                 NodeManager* nm)
+{
+  std::vector<Node> constraint_vector(constraints.size());
+  std::transform(constraints.begin(),
+                 constraints.end(),
+                 constraint_vector.begin(),
+                 [](PbConstraint& x) { return x.toNode(); });
+  constraintSet = nm->mkNode(Kind::SEXPR, constraint_vector);
+}
+
+
+}  // namespace pb
+}  // namespace bv
+}  // namespace theory
+}  // namespace cvc5::internal
---
+++ src/theory/bv/pb/pb_types.h
@@ -0,0 +1,124 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Alan Prado
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * TODO(alanctprado)
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__THEORY__BV__PB__PB_TYPES_H
+#define CVC5__THEORY__BV__PB__PB_TYPES_H
+
+#include "util/integer.h"
+#include "expr/node.h"
+
+#include <string>
+
+namespace cvc5::internal {
+namespace theory {
+namespace bv {
+namespace pb {
+
+/**
+ * Represents the possible values of a Pseudo-Boolean variable.
+ * These values are used to describe the assignment or state of a variable.
+ */
+enum class PbValue { UNKNOWN, TRUE, FALSE };
+
+/**
+ * Represents the possible states of a Pseudo-Boolean solver.
+ * These states indicate the outcome or current status of the solver.
+ */
+enum class PbSolveState { UNKNOWN, SAT, UNSAT };
+
+/**
+ * A pseudo-Boolean variable identified by a unique ID.
+ */
+class PbVariable
+{
+ public:
+  explicit PbVariable(const Integer& id);
+  explicit PbVariable(const int& id);
+
+  friend std::ostream& operator<<(std::ostream& os, const PbVariable& var);
+
+ private:
+  std::string id;
+};
+
+/**
+ * A pseudo-Boolean literal, consisting of a variable and its polarity.
+ */
+class PbLiteral
+{
+ public:
+  explicit PbLiteral(const PbVariable& var, const bool p = true);
+  explicit PbLiteral(const Integer& id, const bool p = true);
+  explicit PbLiteral(const int& id, const bool p = true);
+
+  friend std::ostream& operator<<(std::ostream& os, const PbLiteral& var);
+
+  Node toNode(NodeManager* nm) const;
+
+ private:
+  PbVariable variable;
+  bool polarity;
+};
+
+/**
+ * Represents a pseudo-Boolean constraint, which is a linear inequality or
+ * equality involving pseudo-Boolean literals and coefficients.
+ * The constraint is stored in a Node to leverage hash consing.
+ */
+class PbConstraint
+{
+ public:
+  explicit PbConstraint(const std::vector<PbLiteral>& literals,
+                        const std::vector<Integer>& coefficients,
+                        Kind relationalOperator,
+                        const Integer& constant,
+                        NodeManager* nm);
+  explicit PbConstraint(const PbLiteral& literal,
+                        const Integer& coefficient,
+                        Kind relationalOperator,
+                        const Integer& constant,
+                        NodeManager* nm);
+  Node toNode() const;
+
+ private:
+  Node constraint;
+  std::vector<Node> generateProducts(const std::vector<PbLiteral>& literals,
+                                     const std::vector<Integer>& coefficients,
+                                     NodeManager* nm);
+};
+
+
+/**
+ * Represents a set of pseudo-Boolean constraints.
+ * The set of constraints is stored in a Node to leverage hash consing.
+ */
+class PbConstraintSet
+{
+ public:
+  explicit PbConstraintSet(const std::set<PbConstraint> constraints,
+                           NodeManager* nm);
+
+ private:
+  Node constraintSet;
+};
+
+}  // namespace pb
+}  // namespace bv
+}  // namespace theory
+}  // namespace cvc5::internal
+
+#endif  // CVC5__THEORY__BV__PB__PB_TYPES_H
