610832e359f1e6172de5af3e78bd3e5fbc618784
ajreynol <andrew.j.reynolds@gmail.com>
2025-02-27 08:35:06 -0600

    Add proof elaboration for MACRO_RE_INTER_UNION_CONST_ELIM

--- src/rewriter/basic_rewrite_rcons.h
+++ src/rewriter/basic_rewrite_rcons.h
@@ -258,6 +258,16 @@ class BasicRewriteRCons : protected EnvObj
   bool ensureProofMacroStrInReInclusion(CDProof* cdp, const Node& eq);
   /**
    * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_RE_INTER_UNION_CONST_ELIM.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_RE_INTER_UNION_CONST_ELIM.
+   * @return true if added a closed proof of eq to cdp.
+   */
+  bool ensureProofMacroReInterUnionConstElim(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
    * ProofRewriteRule::MACRO_QUANT_MERGE_PRENEX.
    *
    * @param cdp The proof to add to.
