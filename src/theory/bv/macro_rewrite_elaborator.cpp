94950d9ffb68a56691dbc4b2786916adec251be4
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2025-03-10 14:52:08 -0500

    Add proof elaboration for And/Or/Xor simplify rules (#11720)
    Work towards #11676.

--- src/theory/bv/macro_rewrite_elaborator.cpp
+++ src/theory/bv/macro_rewrite_elaborator.cpp
@@ -36,8 +36,100 @@ bool MacroRewriteElaborator::ensureProofFor(CDProof* cdp,
 {
   Assert(eq.getKind() == Kind::EQUAL);
   Trace("bv-rew-elab") << "ensureProofFor: " << id << " " << eq << std::endl;
+  switch (id)
+  {
+    case ProofRewriteRule::MACRO_BV_OR_SIMPLIFY:
+    case ProofRewriteRule::MACRO_BV_AND_SIMPLIFY:
+    case ProofRewriteRule::MACRO_BV_XOR_SIMPLIFY:
+      return ensureProofForSimplify(cdp, eq);
+    default: break;
+  }
   // TODO PR #11676
   return false;
+}
+
+bool MacroRewriteElaborator::ensureProofForSimplify(CDProof* cdp,
+                                                    const Node& eq)
+{
+  // below, we group all of the constant children into one nested
+  // child, prove the grouping by ACI_NORM, evaluate the constant
+  // child, then prove the equality by another instance of ACI_NORM
+  // if necessary.
+  NodeManager* nm = nodeManager();
+  Kind k = eq[0].getKind();
+  std::vector<Node> consts;
+  std::vector<Node> nconsts;
+  for (const Node& cc : eq[0])
+  {
+    if (cc.isConst())
+    {
+      consts.push_back(cc);
+    }
+    else
+    {
+      nconsts.push_back(cc);
+    }
+  }
+  if (consts.size() <= 1 || nconsts.empty())
+  {
+    Assert(false) << "BV simplify: no constant eval";
+    return false;
+  }
+  std::vector<Node> transEq;
+  Node cc = nm->mkNode(k, consts);
+  nconsts.insert(nconsts.begin(), cc);
+  Node eq0c = nm->mkNode(k, nconsts);
+  Node equiv1 = eq[0].eqNode(eq0c);
+  cdp->addStep(equiv1, ProofRule::ACI_NORM, {}, {equiv1});
+  transEq.push_back(equiv1);
+  Node ccr = evaluate(cc, {}, {});
+  Node ceq = cc.eqNode(ccr);
+  cdp->addTrustedStep(ceq, TrustId::MACRO_THEORY_REWRITE_RCONS_SIMPLE, {}, {});
+  std::vector<Node> premises;
+  premises.push_back(ceq);
+  Node equiv2 = proveCong(cdp, eq0c, premises);
+  Assert(equiv2.getKind() == Kind::EQUAL);
+  transEq.push_back(equiv2);
+  if (equiv2[1] != eq[1])
+  {
+    // could be ACI_NORM or ABSORB, just send generic subgoal.
+    Node equiv3 = equiv2[1].eqNode(eq[1]);
+    cdp->addTrustedStep(
+        equiv3, TrustId::MACRO_THEORY_REWRITE_RCONS_SIMPLE, {}, {});
+    transEq.push_back(equiv3);
+  }
+  cdp->addStep(eq, ProofRule::TRANS, transEq, {});
+  return true;
+}
+
+Node MacroRewriteElaborator::proveCong(CDProof* cdp,
+                                       const Node& n,
+                                       const std::vector<Node>& premises)
+{
+  std::vector<Node> cpremises = premises;
+  std::vector<Node> cargs;
+  ProofRule cr = expr::getCongRule(n, cargs);
+  cpremises.resize(n.getNumChildren());
+  // add REFL if a premise is not provided
+  for (size_t i = 0, npremises = cpremises.size(); i < npremises; i++)
+  {
+    if (cpremises[i].isNull())
+    {
+      Node refl = n[i].eqNode(n[i]);
+      cdp->addStep(refl, ProofRule::REFL, {}, {n[i]});
+      cpremises[i] = refl;
+    }
+  }
+  Trace("brc-macro") << "- cong " << cr << " " << cpremises << " " << cargs
+                     << std::endl;
+  ProofChecker* pc = d_env.getProofNodeManager()->getChecker();
+  Node eq = pc->checkDebug(cr, cpremises, cargs);
+  Trace("brc-macro") << "...returns " << eq << std::endl;
+  if (!eq.isNull())
+  {
+    cdp->addStep(eq, cr, cpremises, cargs);
+  }
+  return eq;
 }
 
 }  // namespace bv
