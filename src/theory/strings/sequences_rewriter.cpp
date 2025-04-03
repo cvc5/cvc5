9d9920033373422e6ae6b4a786987e1d5eb5f6b5
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2025-04-03 08:32:14 -0500

    Miscellaneous changes to string rewriter (#11779)
    This fixes a missing case of rewriting RE inter/union.
    
    It also ensures we do not pass the rewriter to the arithmetic utility,
    to avoid rewrites that cannot be constructed. It refactors the
    arithmetic entailment utility to do slightly more reasoning that is
    independent of the rewriter.
    
    We have no more strings rewrite holes on main after this PR.

--- src/theory/strings/sequences_rewriter.cpp
+++ src/theory/strings/sequences_rewriter.cpp
@@ -1113,6 +1113,7 @@ Node SequencesRewriter::rewriteAndOrRegExp(TNode node)
         return returnRewrite(node, ni, Rewrite::RE_AND_EMPTY);
       }
       // otherwise, can ignore
+      changed = true;
     }
     else if (nik == Kind::REGEXP_STAR
              && ni[0].getKind() == Kind::REGEXP_ALLCHAR)
@@ -1122,6 +1123,7 @@ Node SequencesRewriter::rewriteAndOrRegExp(TNode node)
         return returnRewrite(node, ni, Rewrite::RE_OR_ALL);
       }
       // otherwise, can ignore
+      changed = true;
     }
     else if (std::find(node_vec.begin(), node_vec.end(), ni) == node_vec.end())
     {
