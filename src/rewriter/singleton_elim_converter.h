4ef8c98fc52de450a2bc58b758be071fa61a7aae
ajreynol <andrew.j.reynolds@gmail.com>
2025-05-01 19:24:24 -0500

    Missing

---
+++ src/rewriter/singleton_elim_converter.h
@@ -0,0 +1,55 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Rewrite database singleton elimination converter.
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__REWRITER__SINGLETON_ELIM_CONVERTER__H
+#define CVC5__REWRITER__SINGLETON_ELIM_CONVERTER__H
+
+#include <cvc5/cvc5_proof_rule.h>
+
+#include "expr/node.h"
+#include "expr/node_converter.h"
+#include "proof/conv_proof_generator.h"
+#include "proof/proof.h"
+#include "smt/env_obj.h"
+
+namespace cvc5::internal {
+namespace rewriter {
+
+/**
+ * A class capable of proving equalities of the form (= n nse) where nse
+ * is the result of removing singletons (e.g. nodes of kind APPLY_SINGLETON)
+ * from n. It does so using a term conversion proof generator.
+ */
+class SingletonElimConverter : protected EnvObj
+{
+ public:
+  SingletonElimConverter(Env& env);
+  /**
+   * Return the proof of (= n nse) where nse is the result of eliminating
+   * singletons from n.
+   */
+  std::shared_ptr<ProofNode> convert(const Node& n, const Node& nse);
+
+ private:
+  /** A TConvProofGenerator */
+  TConvProofGenerator d_tpg;
+};
+
+}  // namespace rewriter
+}  // namespace cvc5::internal
+
+#endif /* CVC5__REWRITER__SINGLETON_ELIM_PROCESS__H */
