2bbb6f6de07b64e35e119f536dceb39321403f23
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2025-05-09 14:34:01 -0500

    Apply suggestions from code review
    Co-authored-by: Abdalrhman Mohamed <abdoo8080@outlook.com>

--- src/theory/arith/linear/theory_arith_private.cpp
+++ src/theory/arith/linear/theory_arith_private.cpp
@@ -3457,7 +3457,7 @@ bool TheoryArithPrivate::postCheck(Theory::Effort effortLevel)
             Node lem = possibleLemma.getProven();
             if (lemmas.find(lem) != lemmas.end())
             {
-              Trace("arith-round-robin") << "..already fail lemma" << std::endl;
+              Trace("arith-round-robin") << "..already failed lemma" << std::endl;
               continue;
             }
             lemmas.insert(lem);
