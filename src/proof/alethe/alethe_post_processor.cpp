bf8e4c1a8c74439ff7adcb6cadb9309659cad8fa
Lachnitt <lachnitt@stanford.edu>
2025-05-19 13:39:50 -0700

    Translation of QUANT_UNUSED_VARS to Alethe (#11849)
    Should go after #11399.
    
    Adds translation for QUANT_MERGE_PRENEX and QUANT_UNUSED_VARS
    
    Note: #11848 fixes cvc5's documentation that previously only included
    forall and not exists.
    
    ---------
    
    Co-authored-by: Haniel Barbosa <hanielbbarbosa@gmail.com>

--- src/proof/alethe/alethe_post_processor.cpp
+++ src/proof/alethe/alethe_post_processor.cpp
@@ -140,6 +140,28 @@ bool AletheProofPostprocessCallback::updateTheoryRewriteProofRewriteRule(
                            new_args,
                            *cdp);
     }
+    // ======== QUANT_MERGE_PRENEX
+    // This rule is translated according to the clause pattern.
+    case ProofRewriteRule::QUANT_MERGE_PRENEX:
+    {
+      return addAletheStep(AletheRule::QNT_JOIN,
+                           res,
+                           nm->mkNode(Kind::SEXPR, d_cl, res),
+                           {},
+                           {},
+                           *cdp);
+    }
+    // ======== QUANT_UNUSED_VARS
+    // This rule is translated according to the clause pattern.
+    case ProofRewriteRule::QUANT_UNUSED_VARS:
+    {
+      return addAletheStep(AletheRule::QNT_RM_UNUSED,
+                           res,
+                           nm->mkNode(Kind::SEXPR, d_cl, res),
+                           {},
+                           {},
+                           *cdp);
+    }
     default: break;
   }
   return addAletheStep(AletheRule::HOLE,
