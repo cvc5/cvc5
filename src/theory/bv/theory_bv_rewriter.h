b8a8d89941fca18f73a1639ae1ad022b2e5ed16d
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2025-06-02 15:53:10 -0500

    Ensure constant folding on BV overflow predicates (#11929)
    Fixes https://github.com/cvc5/cvc5/issues/11928.

--- src/theory/bv/theory_bv_rewriter.h
+++ src/theory/bv/theory_bv_rewriter.h
@@ -62,7 +62,7 @@ class TheoryBVRewriter : public TheoryRewriter
    * or disabled, because their elimination
    * produces simple equalities.
    */
-  Node eliminateOverflows(Node node);
+  static Node eliminateOverflows(Node node);
 
  private:
   static RewriteResponse IdentityRewrite(TNode node, bool prerewrite = false);
@@ -117,6 +117,12 @@ class TheoryBVRewriter : public TheoryRewriter
   static RewriteResponse RewriteEagerAtom(TNode node, bool prerewrite = false);
   static RewriteResponse RewriteSize(TNode node, bool prerewrite = false);
   static RewriteResponse RewriteConstBvSym(TNode node, bool prerewrite = false);
+  /**
+   * Rewrite overflow, used for all bitvector kinds that are eliminated at
+   * preprocessing. This applies the elimination if node is applied to all
+   * constants.
+   */
+  static RewriteResponse RewriteOverflow(TNode node, bool prerewrite = false);
 
   void initializeRewrites();
 
