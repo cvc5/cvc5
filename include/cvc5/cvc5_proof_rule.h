bec8d5d99d88eb60a12c2dd7e1a81c352edd34ba
ajreynol <andrew.j.reynolds@gmail.com>
2025-06-16 09:47:18 -0500

    More

--- include/cvc5/cvc5_proof_rule.h
+++ include/cvc5/cvc5_proof_rule.h
@@ -586,7 +586,9 @@ enum ENUM(ProofRule)
   EVALUE(MACRO_RESOLUTION_TRUST),
   /**
    * \verbatim embed:rst:leading-asterisk
-   * **Boolean -- N-ary Resolution + Factoring + Reordering**
+   * **Boolean -- Chain multiset resolution**
+   *
+   * This rule combines Resolution + Factoring + Reordering.
    *
    * .. math::
    *   \inferrule{C_1 \dots C_n \mid C, (pol_1 \dots pol_{n-1}), (L_1 \dots L_{n-1})}{C}
@@ -605,7 +607,7 @@ enum ENUM(ProofRule)
    *   C_i'`
    *
    * The result of the chain resolution is :math:`C`, which is equal, in its set
-   * representation, to :math:`C_n'`
+   * representation, to :math:`C_n'`.
    * \endverbatim
    */
   EVALUE(CHAIN_M_RESOLUTION),
