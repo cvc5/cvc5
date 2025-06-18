f19f42f377ae96a9475517b0fcdb1f4d2fa895fe
Mathias Preiner <mathias.preiner@gmail.com>
2025-06-18 12:41:07 -0700

    Update copyright headers, add missing header documentation. (#11990)
    Adds missing header documentation for new files, remove carriage returns
    from new files.

--- src/theory/arrays/skolem_cache.cpp
+++ src/theory/arrays/skolem_cache.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Top contributors (to current version):
- *   Mathias Preiner, Andrew Reynolds, Daniel Larraz
+ *   Andrew Reynolds, Daniel Larraz, Mathias Preiner
  *
  * This file is part of the cvc5 project.
  *
