3ff81b3f4c814682417fc29e8d60ada5b41951d1
ajreynol <andrew.j.reynolds@gmail.com>
2025-08-11 10:57:24 -0500

    Safer

--- src/rewriter/rewrite_db_proof_cons.cpp
+++ src/rewriter/rewrite_db_proof_cons.cpp
@@ -1417,10 +1417,13 @@ Node RewriteDbProofCons::doEvaluate(const Node& n)
   // Only possible to evaluate if we rewrite to a constant. This is worthwhile
   // to check since the rewrite of n has likely already been computed, whereas
   // the evaluator below is not (globally) cached.
-  Node nr = rewrite(n);
-  if (!nr.isConst())
+  if (!expr::hasAbstractSubterm(n))
   {
-    return Node::null();
+    Node nr = rewrite(n);
+    if (!nr.isConst())
+    {
+      return Node::null();
+    }
   }
   auto [itv, inserted] = d_evalCache.emplace(n, Node());
   if (inserted)
