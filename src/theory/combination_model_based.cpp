f4cb0649c4e71dae34ab60b33915d6962df9be7a
ajreynol <andrew.j.reynolds@gmail.com>
2025-08-26 10:01:52 -0500

    Minor

--- src/theory/combination_model_based.cpp
+++ src/theory/combination_model_based.cpp
@@ -126,6 +126,7 @@ void CombinationModelBased::combineTheories()
       TheoryId tid = Theory::theoryOf(n);
       Theory* t = d_te.theoryOf(tid);
       eq::EqualityEngine* eet = t->getEqualityEngine();
+      Assert (eet!=nullptr);
       for (const std::pair<const Node, std::vector<Node>>& cts : ri.d_congTerms)
       {
         for (const Node& nother : cts.second)
