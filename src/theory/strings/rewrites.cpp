8e01b70a8f902102fc85a2df8e213dc45e7454ee
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2025-10-15 09:56:17 -0500

    Minor improvements to handling corner cases of RE (#12108)
    Makes the following changes:
    - `re.range` applied to constant non-singleton strings rewrites to
    `re.none`. Adds 2 RARE rules for this case and a regression.
    - Guards calling partial derivatives in the RE solver for non-constant
    RE, which can happen if `--re-first-class` is enabled.

--- src/theory/strings/rewrites.cpp
+++ src/theory/strings/rewrites.cpp
@@ -219,6 +219,7 @@ const char* toString(Rewrite r)
     case Rewrite::STR_LT_ELIM: return "STR_LT_ELIM";
     case Rewrite::RE_RANGE_SINGLE: return "RE_RANGE_SINGLE";
     case Rewrite::RE_RANGE_EMPTY: return "RE_RANGE_EMPTY";
+    case Rewrite::RE_RANGE_NON_SINGLETON: return "RE_RANGE_NON_SINGLETON";
     case Rewrite::RE_OPT_ELIM: return "RE_OPT_ELIM";
     case Rewrite::RE_PLUS_ELIM: return "RE_PLUS_ELIM";
     case Rewrite::RE_DIFF_ELIM: return "RE_DIFF_ELIM";
