4b2e07610778ceb207323e091732c6a253db04ab
ajreynol <andrew.j.reynolds@gmail.com>
2025-10-22 12:14:34 -0500

    Comment

--- src/expr/subtype_elim_node_converter.cpp
+++ src/expr/subtype_elim_node_converter.cpp
@@ -48,6 +48,13 @@ Node SubtypeElimNodeConverter::postConvert(Node n)
   else if (k == Kind::DIVISION || k == Kind::DIVISION_TOTAL || k == Kind::GEQ
            || k == Kind::GT || k == Kind::LEQ || k == Kind::LT)
   {
+    // Each of the above terms we allow overloading (both arguments Int, or both
+    // arguments Real) but not mixing arithmetic types.
+    // This means that we allow real division between integer terms here, in
+    // contrast to the SMT-LIB standard which only defines real division for
+    // real terms. We do so to allow rational constants e.g. (/ 1 3) to be
+    // represented as division terms, which avoids the need for logic-specific
+    // parsing of the syntax for rational values in SMT-LIB.
     convertToRealChildren =
         isRealTypeStrict(n[0].getType()) || isRealTypeStrict(n[1].getType());
   }
