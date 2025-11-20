c79c768d94fae83036bf5798fa557dbdf2720009
Thomas Hader <tom@hader.email>
2025-11-20 10:28:57 -0800

    minor fixes in theory_bv

--- src/theory/bv/theory_bv.cpp
+++ src/theory/bv/theory_bv.cpp
@@ -46,7 +46,7 @@ TheoryBV::TheoryBV(Env& env,
   switch (options().bv.bvSolver)
   {
     case options::BVSolver::BITBLAST:
-      d_internal.reset(new BVSolverBitblast(env, d_state, d_im));
+      d_internal.reset(new BVSolverBitblast(d_env, d_state, d_im));
       break;
 
     default:
@@ -56,8 +56,6 @@ TheoryBV::TheoryBV(Env& env,
   d_theoryState = &d_state;
   d_inferManager = &d_im;
 }
-
-TheoryBV::~TheoryBV() {}
 
 TheoryRewriter* TheoryBV::getTheoryRewriter() { return &d_rewriter; }
 
