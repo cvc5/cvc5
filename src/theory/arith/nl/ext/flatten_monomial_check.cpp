4298fc433263914dac1e5a5023a47d28e86e8ce0
ajreynol <andrew.j.reynolds@gmail.com>
2025-12-22 10:25:13 -0600

    Fix assertion for nl-ext-flatten-mon

--- src/theory/arith/nl/ext/flatten_monomial_check.cpp
+++ src/theory/arith/nl/ext/flatten_monomial_check.cpp
@@ -361,7 +361,9 @@ void FlattenMonomialCheck::addToFlattenMonMap(const Node& ns,
     for (const Node& e : exp)
     {
       ArithSubs asTmp;
-      asTmp.add(e[0], e[1]);
+      Node es = as.applyArith(e[1]);
+      asTmp.add(e[0], es);
+      Assert (!expr::hasSubterm(es, e[0]));
       for (size_t j = 0, nums = as.d_subs.size(); j < nums; j++)
       {
         as.d_subs[j] = asTmp.applyArith(as.d_subs[j]);
