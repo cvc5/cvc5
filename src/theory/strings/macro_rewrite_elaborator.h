58fe630c8c96122ca3ce87a12946d1887bd4468a
ajreynol <andrew.j.reynolds@gmail.com>
2026-04-22 12:38:43 -0500

    More

--- src/theory/strings/macro_rewrite_elaborator.h
+++ src/theory/strings/macro_rewrite_elaborator.h
@@ -46,27 +46,172 @@ class MacroRewriteElaborator : protected EnvObj
   bool ensureProofFor(CDProof* cdp, ProofRewriteRule id, const Node& eq);
 
  private:
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_ARITH_STRING_PRED_ENTAIL.
+   *
+   * This takes an equality of the form (r t1 t2) = c, where r is an arithmetic
+   * relation and c is a Boolean constant. This elaboration consists of several
+   * steps, roughly in five steps:
+   * - Normalize the relation r to >= or =.
+   * - Unfold str.len applications in t1 and t2.
+   * - Normalize the relation to one comparing with zero, e.g. (- t1 t2) >= 0.
+   * - Find an approximation for e.g. (- t1 t2) based on Noetzli et al CAV
+   *   2019, using ProofRewriteRule::ARITH_STRING_PRED_SAFE_APPROX.
+   * - Prove the approximation using
+   *   ProofRewriteRule::ARITH_STRING_PRED_ENTAIL.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_ARITH_STRING_PRED_ENTAIL.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForArithStringPredEntail(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_RE_INTER_UNION_INCLUSION.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_RE_INTER_UNION_INCLUSION.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForReInterUnionInclusion(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_RE_INTER_UNION_CONST_ELIM.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_RE_INTER_UNION_CONST_ELIM.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForReInterUnionConstElim(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_SUBSTR_STRIP_SYM_LENGTH.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_SUBSTR_STRIP_SYM_LENGTH.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForSubstrStripSymLength(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_STR_EQ_LEN_UNIFY_PREFIX.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_STR_EQ_LEN_UNIFY_PREFIX.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForStrEqLenUnifyPrefix(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_STR_EQ_LEN_UNIFY.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_STR_EQ_LEN_UNIFY.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForStrEqLenUnify(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_STR_SPLIT_CTN or
+   * ProofRewriteRule::MACRO_STR_STRIP_ENDPOINTS.
+   *
+   * @param id The macro rule we are expanding.
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_STR_SPLIT_CTN or
+   * ProofRewriteRule::MACRO_STR_STRIP_ENDPOINTS.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForOverlap(ProofRewriteRule id,
                              CDProof* cdp,
                              const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_STR_COMPONENT_CTN.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_STR_COMPONENT_CTN.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForStrComponentCtn(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_STR_CONST_NCTN_CONCAT.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_STR_CONST_NCTN_CONCAT.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForStrConstNCtnConcat(CDProof* cdp, const Node& eq);
+  /**
+   * Elaborate a rewrite eq that was proven by
+   * ProofRewriteRule::MACRO_STR_IN_RE_INCLUSION.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The rewrite proven by
+   * ProofRewriteRule::MACRO_STR_IN_RE_INCLUSION.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForStrInReInclusion(CDProof* cdp, const Node& eq);
+  /**
+   * Add to cdp a proof of eq from free asumption eqi, where eqi is the result
+   * of term conversion via RewriteDbNodeConverter.
+   *
+   * @param cdp The proof to add to.
+   * @param eq The original equality.
+   * @param eqi The equality after conversion.
+   */
   void ensureProofForEncodeTransform(CDProof* cdp,
                                      const Node& eq,
                                      const Node& eqi);
+  /**
+   * @param cdp The proof to add to.
+   * @param eq The rewrite that can be proven by ProofRule::ARITH_POLY_NORM_REL.
+   * @return true if added a closed proof of eq to cdp.
+   */
   bool ensureProofForArithPolyNormRel(CDProof* cdp, const Node& eq);
+  /**
+   * Prove that any string term is in a regular expression that characterizes
+   * it. Return the proven regular expression. For example, given
+   * (str.++ x "A" y), this method returns
+   * (str.in_re (str.++ x "A" y) (re.++ Sigma* (str.to_re "A") Sigma*)).
+   */
   Node proveGeneralReMembership(CDProof* cdp, const Node& n);
+  /**
+   * Prove symmetry of equality eq, in particular this proves eq[1] == eq[0]
+   * where eq is an equality and adds it to cdp.
+   */
   Node proveSymm(CDProof* cdp, const Node& eq);
+  /**
+   * Prove congruence for left hand side term n.
+   * If n is a term of the form (f t1 ... tn), this proves
+   *  (= (f t1 ... sn) (f s1 .... sn))
+   * where si is different from ti iff premises[i] is the equality (= ti si).
+   * Note that we permit providing null premises[i] in which case si is ti
+   * and we prove (= ti ti) by REFL. For example, given
+   *   n = (f b a c) and premises = { null, a=b, null }
+   * we prove:
+   *   ----- REFL        ---- REFL
+   *   b = b      a = b  c = c
+   *   ------------------------ CONG
+   *   (f b a c) = (f b b c)
+   */
   Node proveCong(CDProof* cdp,
                  const Node& n,
                  const std::vector<Node>& premises);
+  /**
+   * Assuming cdp has proofs of (=> A B) and (=> B A), this ensures we
+   * have a proof of (= A B).
+   */
   Node proveDualImplication(CDProof* cdp,
                             const Node& impl,
                             const Node& implRev);
