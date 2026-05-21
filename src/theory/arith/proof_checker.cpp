0f0846b3d1869c6c5349ecc5117ffe9ba6226788
ajreynol <andrew.j.reynolds@gmail.com>
2026-05-21 11:42:59 -0500

    Format

--- src/theory/arith/proof_checker.cpp
+++ src/theory/arith/proof_checker.cpp
@@ -85,8 +85,8 @@ Node ArithProofRuleChecker::checkInternal(ProofRule id,
       Assert(args.size() == 2);
       Node mult = args[0];
       Kind rel = args[1].getKind();
-      Assert(rel == Kind::EQUAL || rel == Kind::LT
-             || rel == Kind::LEQ || rel == Kind::GT || rel == Kind::GEQ);
+      Assert(rel == Kind::EQUAL || rel == Kind::LT || rel == Kind::LEQ
+             || rel == Kind::GT || rel == Kind::GEQ);
       Node lhs = args[1][0];
       Node rhs = args[1][1];
       Node zero = nm->mkConstRealOrInt(mult.getType(), Rational(0));
@@ -103,8 +103,8 @@ Node ArithProofRuleChecker::checkInternal(ProofRule id,
       Assert(args.size() == 2);
       Node mult = args[0];
       Kind rel = args[1].getKind();
-      Assert(rel == Kind::EQUAL || rel == Kind::LT
-             || rel == Kind::LEQ || rel == Kind::GT || rel == Kind::GEQ);
+      Assert(rel == Kind::EQUAL || rel == Kind::LT || rel == Kind::LEQ
+             || rel == Kind::GT || rel == Kind::GEQ);
       Kind rel_inv = reverseRelationKind(rel);
       Node lhs = args[1][0];
       Node rhs = args[1][1];
